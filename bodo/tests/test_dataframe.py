eb31a084995809232f159e4fcf4c2b49876d6eb9
Ehsan Totoni <ehsan.tn@gmail.com>
2019-08-06 22:44:39 -0700

    fix data for df pow test

--- bodo/tests/test_dataframe.py
+++ bodo/tests/test_dataframe.py
@@ -629,7 +629,7 @@ def test_dataframe_inplace_binary_op(op):
     exec(func_text, {}, loc_vars)
     test_impl = loc_vars['test_impl']
 
-    df = pd.DataFrame({'A': [4, 6, 7, 1, -3]}, index=[3, 5, 0, 7, 2])
+    df = pd.DataFrame({'A': [4, 6, 7, 1, 3]}, index=[3, 5, 0, 7, 2])
     test_func(test_impl, (df, df), copy_input=True)
     test_func(test_impl, (df, 2), copy_input=True)
 
