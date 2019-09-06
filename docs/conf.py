eed718d4803d071183796b6247939df2c0fbe882
cat-Yu <catherineyu984@gmail.com>
2019-09-06 11:33:29 -0400

    adds a .nojekyll file for doc builds

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ import sys
 import os
 import sphinx_bootstrap_theme
 import bodo
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -39,7 +40,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -92,7 +94,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
+#html_theme = 'bootstrap'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
