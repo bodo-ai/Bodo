87bbdb8560783130720d17681a3e97a17865b68f
cat-Yu <catherineyu984@gmail.com>
2019-09-26 11:59:28 -0400

    Update setup_conda.sh

--- buildscripts/setup_conda.sh
+++ buildscripts/setup_conda.sh
@@ -22,12 +22,9 @@ source deactivate
 
 conda remove --all -q -y -n $CONDA_ENV
 
-conda create -n $CONDA_ENV -q -y numpy scipy pandas boost cmake
+conda create -n $CONDA_ENV -q -y numpy scipy pandas boost cmake h5py pyarrow mpich mpi
 source activate $CONDA_ENV
-$CONDA_INSTALL pyarrow
-#$CONDA_INSTALL daal-devel daal-include daal -c intel --no-deps
-#$CONDA_INSTALL tbb
-$CONDA_INSTALL pyspark
+
 
 # install compilers
 if [[ "$unamestr" == 'Linux' ]]; then
@@ -38,9 +35,6 @@ else
     echo "Error in compiler install"
 fi
 
-$CONDA_INSTALL mpich mpi -c conda-forge --no-deps
 $CONDA_INSTALL -c numba/label/dev numba
-$CONDA_INSTALL libgfortran
-$CONDA_INSTALL h5py
 
 if [ "$RUN_COVERAGE" == "yes" ]; then $CONDA_INSTALL coveralls; fi
