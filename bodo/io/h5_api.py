687f996b11eac2716e5cc343b7354c7d4091c88d
Ehsan Totoni <ehsan.tn@gmail.com>
2019-10-22 14:07:33 -0400

    remove unused functions

--- bodo/io/h5_api.py
+++ bodo/io/h5_api.py
@@ -246,16 +246,6 @@ def h5read():
     return
 
 
-def h5create_dset():
-    """dummy function for C h5_create_dset"""
-    return
-
-
-def h5create_group():
-    """dummy function for C h5create_group"""
-    return
-
-
 def h5write():
     """dummy function for C h5_write"""
     return
@@ -286,22 +276,6 @@ class H5Read(AbstractTemplate):
         assert not kws
         assert len(args) == 6
         return signature(types.int32, *unliteral_all(args))
-
-
-@infer_global(h5create_dset)
-class H5CreateDSet(AbstractTemplate):
-    def generic(self, args, kws):
-        assert not kws
-        assert len(args) == 4
-        return signature(h5file_type, *unliteral_all(args))
-
-
-@infer_global(h5create_group)
-class H5CreateGroup(AbstractTemplate):
-    def generic(self, args, kws):
-        assert not kws
-        assert len(args) == 2
-        return signature(h5file_type, *unliteral_all(args))
 
 
 @infer_global(h5write)
