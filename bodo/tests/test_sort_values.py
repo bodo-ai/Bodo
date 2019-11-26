d5dfbfb9ba6722dc45db3e6e7a61d84e1e82704b
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2019-11-26 16:30:01 +0100

    Introduction of more tests + removal of the sorting which is kind of the goal here.
      Mathieu

--- bodo/tests/test_sort_values.py
+++ bodo/tests/test_sort_values.py
@@ -23,4 +23,92 @@ def test_sort_values_1col():
             eVal = i*i % 34
             eListA.append(eVal)
         return pd.DataFrame({"A": eListA})
-    check_func(test_impl, (get_quasi_random(100),), sort_output=True)
+    check_func(test_impl, (get_quasi_random(100),), sort_output=False)
+
+
+def test_sort_values_1col_np_array():
+    """
+    Test drop_duplicates(): with just one column
+    """
+
+    def test_impl(df1):
+        df2 = df1.sort_values("A")
+        return df2
+
+    def get_quasi_random_int8(n):
+        eListA = np.array([0]*n, dtype=np.int8)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    def get_quasi_random_uint8(n):
+        eListA = np.array([0]*n, dtype=np.uint8)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    
+    def get_quasi_random_int16(n):
+        eListA = np.array([0]*n, dtype=np.int16)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    def get_quasi_random_uint16(n):
+        eListA = np.array([0]*n, dtype=np.uint16)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    
+    def get_quasi_random_int32(n):
+        eListA = np.array([0]*n, dtype=np.int32)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    def get_quasi_random_uint32(n):
+        eListA = np.array([0]*n, dtype=np.uint32)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    
+    def get_quasi_random_int64(n):
+        eListA = np.array([0]*n, dtype=np.int64)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    def get_quasi_random_uint64(n):
+        eListA = np.array([0]*n, dtype=np.uint64)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    
+    def get_quasi_random_float32(n):
+        eListA = np.array([0]*n, dtype=np.float32)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+    def get_quasi_random_float64(n):
+        eListA = np.array([0]*n, dtype=np.float64)
+        for i in range(n):
+            eVal = i*i % 34
+            eListA[i] = eVal
+        return pd.DataFrame({"A": eListA})
+
+    check_func(test_impl, (get_quasi_random_int8(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_uint8(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_int16(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_uint16(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_int32(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_uint32(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_int64(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_uint64(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_float32(100),), sort_output=False)
+    check_func(test_impl, (get_quasi_random_float64(100),), sort_output=False)
+
+
