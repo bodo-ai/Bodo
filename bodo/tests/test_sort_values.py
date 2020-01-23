9e241afe188a91ba117cae3698b0844993eeddad
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-01-23 09:29:37 +0100

    Support Index name in df.sort_values() (#409)
    * Correct the debugging code of sort_values.
    
    * Some correction for finding the keys.
    
    * Success at the Q12 problem.
    
    * Removal of print statement.
    
    * One bug addressed.
    
    * Address issues.
    
    * Some correction.
    
    * Update bodo/hiframes/pd_dataframe_ext.py
    
    Implement suggested correction
    
    Co-Authored-By: Ehsan Totoni <ehsan.tn@gmail.com>
    
    * Update bodo/hiframes/pd_dataframe_ext.py
    
    Implement suggestion.
    
    Co-Authored-By: Ehsan Totoni <ehsan.tn@gmail.com>
    
    * Applying black to the test code.
    
    * Removal of not necessary test for none + test cleeaning.
    
    Co-authored-by: Ehsan Totoni <ehsan.tn@gmail.com>

--- bodo/tests/test_sort_values.py
+++ bodo/tests/test_sort_values.py
@@ -183,29 +183,28 @@ def test_sort_values_strings_variable_length(n, len_str):
     )
 
 
-
 def test_sort_values_string_list():
     """Test the list of string. Missing values in a string array"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by='A', kind="mergesort", na_position='last', axis=0)
+        df2 = df1.sort_values(by="A", kind="mergesort", na_position="last", axis=0)
         return df2
 
     def get_random_string_dataframe(n, len_str):
         random.seed(5)
         str_vals = []
         for _ in range(n):
-            val1 = random.randint(1,len_str)
-            if val1==1:
-                val=np.nan
+            val1 = random.randint(1, len_str)
+            if val1 == 1:
+                val = np.nan
             else:
                 val = "".join(random.choices(string.ascii_uppercase, k=val1))
             str_vals.append(val)
         df = pd.DataFrame({"A": str_vals})
         return df
 
-    df1 = get_random_string_dataframe(100,10)
+    df1 = get_random_string_dataframe(100, 10)
     check_func(test_impl1, (df1,), sort_output=False)
-
 
 
 @pytest.mark.parametrize("n, len_str", [(100, 30), (1000, 10), (10, 30), (100, 30)])
@@ -224,8 +223,8 @@ def test_sort_values_strings(n, len_str):
         str_vals = []
         for _ in range(n):
             prob = random.randint(1, 10)
-            if prob==1:
-                val=np.nan
+            if prob == 1:
+                val = np.nan
             else:
                 k = random.randint(1, len_str)
                 val = "".join(random.choices(string.ascii_uppercase, k=k))
@@ -235,10 +234,11 @@ def test_sort_values_strings(n, len_str):
         return df
 
     bodo_func = bodo.jit(test_impl)
-    df1 = get_random_strings_array(n,len_str)
+    df1 = get_random_strings_array(n, len_str)
     pd.testing.assert_frame_equal(
         bodo_func(df1), test_impl(df1),
     )
+
 
 #    TODO: Solve the bug in the check that makes the following fail
 #    (problem of conversion of dataFrames)
@@ -247,37 +247,40 @@ def test_sort_values_strings(n, len_str):
 #    )
 
 
-
-
-
 @pytest.mark.parametrize("n, len_siz", [(100, 30), (1000, 10), (10, 30), (100, 30)])
 def test_sort_values_two_columns_nan(n, len_siz):
     """Test with two columns with some NaN entries"""
 
     def test_impl1(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl2(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'first', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="first", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl3(df1):
-        df2 = df1.sort_values(by='A', ascending = False, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=False, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl4(df1):
-        df2 = df1.sort_values(by='A', ascending = False, na_position = 'first', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=False, na_position="first", kind="mergesort", axis=0
+        )
         return df2
 
-
-
-    def get_random_column(n,n_row):
+    def get_random_column(n, n_row):
         str_vals = []
         for _ in range(n):
             prob = random.randint(1, 10)
-            if prob==1:
-                val=np.nan
+            if prob == 1:
+                val = np.nan
             else:
                 val = random.randint(1, len_siz)
             str_vals.append(val)
@@ -285,7 +288,9 @@ def test_sort_values_two_columns_nan(n, len_siz):
 
     def get_random_dataframe_two_columns(n, len_siz):
         random.seed(5)
-        df = pd.DataFrame({"A": get_random_column(n, len_siz), "B": get_random_column(n,len_siz)})
+        df = pd.DataFrame(
+            {"A": get_random_column(n, len_siz), "B": get_random_column(n, len_siz)}
+        )
         return df
 
     df1 = get_random_dataframe_two_columns(n, len_siz)
@@ -295,69 +300,113 @@ def test_sort_values_two_columns_nan(n, len_siz):
     check_func(test_impl4, (df1,), sort_output=False)
 
 
+def test_sort_values_simplest():
+    """Simplest case of sort_values"""
+
+    def test_impl1(df1):
+        df2 = df1.sort_values("A")
+        return df2
+
+    df1 = pd.DataFrame({"A": [1, 2, 2]})
+    check_func(test_impl1, (df1,), sort_output=False)
 
 
+def test_sort_values_by_index():
+    """Sorting with a non-trivial index"""
 
+    def test_impl1(df1):
+        df2 = df1.sort_values("index_name")
+        return df2
 
+    df1 = pd.DataFrame({"A": [1, 2, 2]}, index=[2, 1, 0])
+    df1.index.name = "index_name"
+    check_func(test_impl1, (df1,), sort_output=False)
 
 
 def test_sort_values_nan_case():
     """Test of NaN values for the sorting with all possible values of ascending and na_position"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl2(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'first', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="first", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl3(df1):
-        df2 = df1.sort_values(by='A', ascending = False, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=False, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl4(df1):
-        df2 = df1.sort_values(by='A', ascending = False, na_position = 'first', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=False, na_position="first", kind="mergesort", axis=0
+        )
         return df2
 
-    df1 = pd.DataFrame({"A": [2, np.nan, 7, np.nan, -1, -4, np.nan, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]})
+    df1 = pd.DataFrame(
+        {
+            "A": [2, np.nan, 7, np.nan, -1, -4, np.nan, 1, 2],
+            "B": [3, 6, 0, 1, 2, -4, 7, 7, 2],
+        }
+    )
     check_func(test_impl1, (df1,), sort_output=False)
     check_func(test_impl2, (df1,), sort_output=False)
     check_func(test_impl3, (df1,), sort_output=False)
     check_func(test_impl4, (df1,), sort_output=False)
 
 
-
 def test_sort_values_bool_list():
     """Test of NaN values for the sorting with vector of ascending"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by=['A','B'], kind="mergesort", axis=0)
+        df2 = df1.sort_values(by=["A", "B"], kind="mergesort", axis=0)
         return df2
 
     def test_impl2(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = True, kind="mergesort", axis=0)
+        df2 = df1.sort_values(by=["A", "B"], ascending=True, kind="mergesort", axis=0)
         return df2
 
     def test_impl3(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = [True,True], kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by=["A", "B"], ascending=[True, True], kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl4(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = False, kind="mergesort", axis=0)
+        df2 = df1.sort_values(by=["A", "B"], ascending=False, kind="mergesort", axis=0)
         return df2
 
     def test_impl5(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = [False,False], kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by=["A", "B"], ascending=[False, False], kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl6(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = [True,False], kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by=["A", "B"], ascending=[True, False], kind="mergesort", axis=0
+        )
         return df2
 
     def test_impl7(df1):
-        df2 = df1.sort_values(by=['A','B'], ascending = [False,True], kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by=["A", "B"], ascending=[False, True], kind="mergesort", axis=0
+        )
         return df2
 
-    df1 = pd.DataFrame({"A": [2, np.nan, 7, np.nan, -1, -4, np.nan, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]})
+    df1 = pd.DataFrame(
+        {
+            "A": [2, np.nan, 7, np.nan, -1, -4, np.nan, 1, 2],
+            "B": [3, 6, 0, 1, 2, -4, 7, 7, 2],
+        }
+    )
     check_func(test_impl1, (df1,), sort_output=False)
     check_func(test_impl2, (df1,), sort_output=False)
     check_func(test_impl3, (df1,), sort_output=False)
@@ -367,26 +416,29 @@ def test_sort_values_bool_list():
     check_func(test_impl7, (df1,), sort_output=False)
 
 
-
 def test_sort_values_nan_case_simple():
     """Test of NaN values for the sorting in a numpy array"""
+
     def test_impl(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
     df1 = pd.DataFrame({"A": [2, np.nan, 7, np.nan]})
     check_func(test_impl, (df1,), sort_output=False)
 
 
-
-
 def test_sort_values_nullable_int_array():
     """Test of NaN values for the sorting for a nullable int bool array"""
+
     def test_impl(df1):
-        df2 = df1.sort_values(by='A', ascending = True, na_position = 'last', kind="mergesort", axis=0)
+        df2 = df1.sort_values(
+            by="A", ascending=True, na_position="last", kind="mergesort", axis=0
+        )
         return df2
 
-    nullarr = pd.array([13, None, 17], dtype='UInt16')
+    nullarr = pd.array([13, None, 17], dtype="UInt16")
     df1 = pd.DataFrame({"A": nullarr})
     bodo_func = bodo.jit(test_impl)
     pd.testing.assert_frame_equal(
@@ -394,26 +446,26 @@ def test_sort_values_nullable_int_array():
     )
 
 
-
 def test_sort_values_numpy_nan():
     """Test of the code without NaN values"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by='A', kind="mergesort", axis=0)
+        df2 = df1.sort_values(by="A", kind="mergesort", axis=0)
         return df2
 
     df1 = pd.DataFrame({"A": [13, np.nan, 17]})
     check_func(test_impl1, (df1,), sort_output=False)
 
 
-
 def test_sort_with_nan_entries():
     """Test of the dataframe with nan entries"""
+
     def impl1(df):
         return df.sort_values(by="A", kind="mergesort")
 
     df1 = pd.DataFrame({"A": ["AA", np.nan, "", "D", "GG"]})
     df2 = pd.DataFrame({"A": [1, 8, 4, np.nan, 3]})
-    df3 = pd.DataFrame({"A": pd.array([1,2,None,3], dtype='UInt16')})
+    df3 = pd.DataFrame({"A": pd.array([1, 2, None, 3], dtype="UInt16")})
     df4 = pd.DataFrame({"A": pd.Series([1, 8, 4, np.nan, 3], dtype="Int32")})
     df5 = pd.DataFrame({"A": pd.Series(["AA", np.nan, "", "D", "GG"])})
     check_func(impl1, (df1,), sort_output=False)
@@ -425,25 +477,31 @@ def test_sort_with_nan_entries():
 
 def test_sort_values_two_columns():
     """Test of the code without NaN values"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by=['A', 'B'], kind="mergesort", axis=0)
+        df2 = df1.sort_values(by=["A", "B"], kind="mergesort", axis=0)
         return df2
 
-    df1 = pd.DataFrame({"A": [2, -3, 7, 10, -1, -4, 0, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]})
+    df1 = pd.DataFrame(
+        {"A": [2, -3, 7, 10, -1, -4, 0, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]}
+    )
     check_func(test_impl1, (df1,), sort_output=False)
 
 
 def test_sort_values_regular_case():
     """Test of the code without NaN values"""
+
     def test_impl1(df1):
-        df2 = df1.sort_values(by='A', ascending = True, kind="mergesort", axis=0)
+        df2 = df1.sort_values(by="A", ascending=True, kind="mergesort", axis=0)
         return df2
 
     def test_impl2(df1):
-        df2 = df1.sort_values(by='A', ascending = False, kind="mergesort", axis=0)
+        df2 = df1.sort_values(by="A", ascending=False, kind="mergesort", axis=0)
         return df2
 
-    df1 = pd.DataFrame({"A": [2, -3, 7, 10, -1, -4, 0, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]})
+    df1 = pd.DataFrame(
+        {"A": [2, -3, 7, 10, -1, -4, 0, 1, 2], "B": [3, 6, 0, 1, 2, -4, 7, 7, 2]}
+    )
     check_func(test_impl1, (df1,), sort_output=False)
     check_func(test_impl2, (df1,), sort_output=False)
 
@@ -540,22 +598,37 @@ def test_sort_values_ascending_bool():
         return df.sort_values(by=["A", "B"], ascending=[True])
 
     def impl5(df):
-        return df.sort_values(by=["A", "B"], ascending=[True,False,True])
+        return df.sort_values(by=["A", "B"], ascending=[True, False, True])
 
     def impl6(df):
         return df.sort_values(by=["A", "B"], ascending=(True, False))
 
-    with pytest.raises(BodoError, match="'ascending' parameter must be of type bool or list of bool"):
+    with pytest.raises(
+        BodoError, match="'ascending' parameter must be of type bool or list of bool"
+    ):
         bodo.jit(impl1)(df)
-    with pytest.raises(BodoError, match="'ascending' parameter must be of type bool or list of bool"):
+    with pytest.raises(
+        BodoError, match="'ascending' parameter must be of type bool or list of bool"
+    ):
         bodo.jit(impl2)(df)
-    with pytest.raises(ValueError, match="ascending should be bool or a list of bool of the number of keys"):
+    with pytest.raises(
+        ValueError,
+        match="ascending should be bool or a list of bool of the number of keys",
+    ):
         bodo.jit(impl3)(df, True)
-    with pytest.raises(ValueError, match="ascending should be bool or a list of bool of the number of keys"):
+    with pytest.raises(
+        ValueError,
+        match="ascending should be bool or a list of bool of the number of keys",
+    ):
         bodo.jit(impl4)(df)
-    with pytest.raises(ValueError, match="ascending should be bool or a list of bool of the number of keys"):
+    with pytest.raises(
+        ValueError,
+        match="ascending should be bool or a list of bool of the number of keys",
+    ):
         bodo.jit(impl5)(df)
-    with pytest.raises(BodoError, match="parameter must be of type bool or list of bool"):
+    with pytest.raises(
+        BodoError, match="parameter must be of type bool or list of bool"
+    ):
         bodo.jit(impl6)(df)
 
 
@@ -590,7 +663,9 @@ def test_sort_values_kind_no_spec():
     def impl3(df):
         return df.sort_values(by=["A"], kind=2)
 
-    with pytest.warns(BodoWarning, match="specifying sorting algorithm is not supported"):
+    with pytest.warns(
+        BodoWarning, match="specifying sorting algorithm is not supported"
+    ):
         bodo.jit(impl1)(df)
     with pytest.warns(
         BodoWarning, match="specifying sorting algorithm is not supported"
@@ -616,10 +691,13 @@ def test_sort_values_na_position_no_spec():
     def impl3(df):
         return df.sort_values(by=["A"], na_position=0)
 
-    with pytest.raises(BodoError, match="na_position parameter must be a literal constant"):
+    with pytest.raises(
+        BodoError, match="na_position parameter must be a literal constant"
+    ):
         bodo.jit(impl1)(df)
     with pytest.raises(BodoError, match="na_position should either be"):
         bodo.jit(impl2)(df)
-    with pytest.raises(BodoError, match="na_position parameter must be a literal constant"):
+    with pytest.raises(
+        BodoError, match="na_position parameter must be a literal constant"
+    ):
         bodo.jit(impl3)(df)
-
