943b8ac97c886eddd54954ed4b03114f00210681
Ehsan Totoni <ehsan.tn@gmail.com>
2020-06-04 17:27:44 -0400

    Update Arrow version to 0.17.1

--- buildscripts/setup_conda.sh
+++ buildscripts/setup_conda.sh
@@ -34,7 +34,7 @@ else
     echo "Error in compiler install"
 fi
 
-$CONDA_INSTALL -c conda-forge pyarrow=0.17.0
+$CONDA_INSTALL -c conda-forge pyarrow=0.17.1
 $CONDA_INSTALL pandas>=1.0.0 -c conda-forge
 $CONDA_INSTALL numba=0.49.1 -c conda-forge
 $CONDA_INSTALL mpi4py -c conda-forge
