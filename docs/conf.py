d42ff3048154070f3341acded572889fa7e32825
Ehsan Totoni <ehsan.tn@gmail.com>
2020-06-11 16:51:35 -0400

    avoid copying and showing source files in docs

--- docs/conf.py
+++ docs/conf.py
@@ -123,6 +123,8 @@ html_theme_options = {
 }
 
 html_show_sphinx = False
+html_copy_source = False
+html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
