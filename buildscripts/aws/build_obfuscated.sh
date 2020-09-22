6edfb80770de1e9e38267fe25f5e088a9418226b
Farah Hariri <elhariri.farah@gmail.com>
2020-09-22 07:42:09 -0500

    fix cds

--- buildscripts/aws/build_obfuscated.sh
+++ buildscripts/aws/build_obfuscated.sh
@@ -13,17 +13,17 @@ flake8 bodo
 
 echo "********** Obfuscating **********"
 conda install -y astor -c conda-forge
-cd obfuscation
+cd $CODEBUILD_SRC_DIR/obfuscation
 ./do_obfuscation.py
 
 
 echo "********** Building Bodo **********"
 source activate bodo_build
 if [[ "$UseNumbaDev" == "true" ]]; then
-    cd buildscripts/bodo-numba-dev-conda-recipe/
+    cd $CODEBUILD_SRC_DIR/buildscripts/bodo-numba-dev-conda-recipe/
     echo "Using Numba-Dev Conda Recipe"
 else
-    cd buildscripts/bodo-conda-recipe/
+    cd $CODEBUILD_SRC_DIR/buildscripts/bodo-conda-recipe/
 fi
 export CHECK_LICENSE_EXPIRED="0"
 export CHECK_LICENSE_CORE_COUNT="0"
