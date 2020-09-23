9f1f50cef0dd062b3a8232a28792d6f2480a5ad2
Farah Hariri <elhariri.farah@gmail.com>
2020-09-23 09:23:54 -0500

    conda activate

--- buildscripts/aws/run_nightly_tests.sh
+++ buildscripts/aws/run_nightly_tests.sh
@@ -3,6 +3,7 @@ set -exo pipefail
 
 USERNAME=`credstash -r us-east-2 get artifactory.ci.username`
 TOKEN=`credstash -r us-east-2 get artifactory.ci.token`
+export PATH=$HOME/miniconda3/bin:$PATH
 
 # ------ Conda Config -----------
 export PATH=$HOME/miniconda3/bin:$PATH
@@ -14,10 +15,10 @@ cd $CODEBUILD_SRC_DIR
 ./buildscripts/aws/test_installs.sh
 
 # ------ Install Bodo -----------
+source activate $CONDA_ENV
 conda install -c https://${USERNAME}:${TOKEN}@bodo.jfrog.io/artifactory/api/conda/bodo-binary/bodo-2020.09-py38hc547734_19.tar.bz2 -c conda-forge bodo
 
 # ------ Run Tests -----------
-pip install pytest
 git clone https://github.com/Bodo-inc/engine-e2e-tests.git $ENGINE_E2E_TESTS
 cd $ENGINE_E2E_TESTS
 pytest
