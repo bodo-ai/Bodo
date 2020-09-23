c795b4eb88070803e915411027a52804847192e5
Farah Hariri <elhariri.farah@gmail.com>
2020-09-23 09:13:33 -0500

    pytest...

--- buildscripts/aws/run_nightly_tests.sh
+++ buildscripts/aws/run_nightly_tests.sh
@@ -17,6 +17,7 @@ cd $CODEBUILD_SRC_DIR
 conda install -c https://${USERNAME}:${TOKEN}@bodo.jfrog.io/artifactory/api/conda/bodo-binary/bodo-2020.09-py38hc547734_19.tar.bz2 -c conda-forge bodo
 
 # ------ Run Tests -----------
+pip install pytest
 git clone https://github.com/Bodo-inc/engine-e2e-tests.git $ENGINE_E2E_TESTS
 cd $ENGINE_E2E_TESTS
 pytest
--- buildscripts/aws/test_installs.sh
+++ buildscripts/aws/test_installs.sh
@@ -8,7 +8,5 @@ source activate $CONDA_ENV
 conda install -y -c conda-forge boto3 botocore "s3fs>=0.4.2"
 conda install -y -c conda-forge pymysql sqlalchemy
 conda install -y flake8
-pip install pytest
-# install coverage
-pip install pytest-cov
+pip install pytest pytest-cov
 
