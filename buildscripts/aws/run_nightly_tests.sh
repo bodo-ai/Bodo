20988769bcbce39d521bce6f44c8fc42b624d327
Farah Hariri <elhariri.farah@gmail.com>
2020-09-23 09:46:43 -0500

    refactors

--- buildscripts/aws/run_nightly_tests.sh
+++ buildscripts/aws/run_nightly_tests.sh
@@ -4,18 +4,9 @@ set -exo pipefail
 USERNAME=`credstash -r us-east-2 get artifactory.ci.username`
 TOKEN=`credstash -r us-east-2 get artifactory.ci.token`
 export PATH=$HOME/miniconda3/bin:$PATH
-
-# ------ Conda Config -----------
-export PATH=$HOME/miniconda3/bin:$PATH
-conda remove --all -q -y -n $CONDA_ENV
-conda create -n $CONDA_ENV
-
-# ------ Test Installs -----------
-cd $CODEBUILD_SRC_DIR
-./buildscripts/aws/test_installs.sh
+source activate $CONDA_ENV
 
 # ------ Install Bodo -----------
-source activate $CONDA_ENV
 conda install -c https://${USERNAME}:${TOKEN}@bodo.jfrog.io/artifactory/api/conda/bodo-binary/bodo-2020.09-py38hc547734_19.tar.bz2 -c conda-forge bodo
 
 # ------ Run Tests -----------
--- buildscripts/aws/test_installs.sh
+++ buildscripts/aws/test_installs.sh
@@ -5,6 +5,7 @@
 set -eo pipefail
 export PATH=$HOME/miniconda3/bin:$PATH
 source activate $CONDA_ENV
+
 conda install -y -c conda-forge boto3 botocore "s3fs>=0.4.2"
 conda install -y -c conda-forge pymysql sqlalchemy
 conda install -y flake8
