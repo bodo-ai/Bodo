dd2d3019ff6f94fb664182a83134cb026a525b27
Farah Hariri <elhariri.farah@gmail.com>
2020-09-23 13:27:32 -0500

    remove comments out...done debugging

--- buildscripts/aws/publish_binary.sh
+++ buildscripts/aws/publish_binary.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -exo pipefail
 
-# echo "********* Zipping Binary **********"
-# cd $CODEBUILD_SRC_DIR
-# ls bodo-inc/linux-64
-# zip -r bodo-linux.zip bodo-inc
+echo "********* Zipping Binary **********"
+cd $CODEBUILD_SRC_DIR
+ls bodo-inc/linux-64
+zip -r bodo-linux.zip bodo-inc
 
-# echo "********** Publishing to Artifactory **********"
+echo "********** Publishing to Artifactory **********"
 pip install credstash
-# USERNAME=`credstash -r us-east-2 get artifactory.ci.username`
-# TOKEN=`credstash -r us-east-2 get artifactory.ci.token`
+USERNAME=`credstash -r us-east-2 get artifactory.ci.username`
+TOKEN=`credstash -r us-east-2 get artifactory.ci.token`
 
-# for package in `ls $HOME/miniconda3/envs/bodo_build/conda-bld/linux-64/bodo*.tar.bz2`; do
-#     package_name=`basename $package`
-#     curl -u${USERNAME}:${TOKEN} -T $package "https://bodo.jfrog.io/artifactory/bodo-binary/linux-64/$package_name"
-# done
-    
+for package in `ls $HOME/miniconda3/envs/bodo_build/conda-bld/linux-64/bodo*.tar.bz2`; do
+    package_name=`basename $package`
+    curl -u${USERNAME}:${TOKEN} -T $package "https://bodo.jfrog.io/artifactory/bodo-binary/linux-64/$package_name"
+done
