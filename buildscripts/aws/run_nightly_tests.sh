c636553397503036bded517afd1fd8ad12eb4810
Farah Hariri <elhariri.farah@gmail.com>
2020-09-25 08:01:16 -0500

    install the same version that we posted to artifactory

--- buildscripts/aws/run_nightly_tests.sh
+++ buildscripts/aws/run_nightly_tests.sh
@@ -7,7 +7,8 @@ export PATH=$HOME/miniconda3/bin:$PATH
 source activate $CONDA_ENV
 
 # ------ Install Bodo -----------
-conda install -c https://${USERNAME}:${TOKEN}@bodo.jfrog.io/artifactory/api/conda/bodo-binary -c conda-forge bodo
+sub_channel=`cat $CODEBUILD_SRC_DIR/bodo_subchannel`
+conda install -c https://${USERNAME}:${TOKEN}@bodo.jfrog.io/artifactory/api/conda/bodo-binary/$sub_channel -c conda-forge bodo
 
 # ------ Run Tests -----------
 git clone https://github.com/Bodo-inc/engine-e2e-tests.git $ENGINE_E2E_TESTS
