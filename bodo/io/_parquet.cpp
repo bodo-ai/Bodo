f7bea1b09fdd20088bd8196545565e5c7530ecc1
Juan Galvez <juan@bodo.ai>
2021-01-18 12:32:21 -0600

    C++ get_dataset_reader: store partition values in DatasetReader

--- bodo/io/_parquet.cpp
+++ bodo/io/_parquet.cpp
@@ -30,6 +30,8 @@
 struct DatasetReader {
     // Filepaths, only for the files that this process has to read
     std::vector<std::string> filepaths;
+    // TODO comment
+    std::vector<std::vector<int64_t>> part_vals;
     // If S3, then store the bucket region here
     std::string bucket_region = "";
     /// Starting row for first file (files[0])
@@ -249,6 +251,28 @@ DatasetReader *get_dataset_reader(char *file_name, bool parallel,
                 PyObject *p = PyObject_GetAttrString(piece, "path");
                 const char *c_path = PyUnicode_AsUTF8(p);
                 ds_reader->filepaths.push_back(c_path);
+
+                // for this parquet file: store partition value of each
+                // partition column in ds_reader
+                PyObject *partition_keys_py = PyObject_GetAttrString(piece, "partition_keys");
+                if (PyList_Size(partition_keys_py) > 0) {
+
+                    ds_reader->part_vals.emplace_back();
+                    std::vector<int64_t> &vals = ds_reader->part_vals.back();
+
+                    PyObject *part_keys_iter = PyObject_GetIter(partition_keys_py);
+                    Py_DECREF(partition_keys_py);
+                    PyObject *key_val_tuple;
+                    while ((key_val_tuple = PyIter_Next(part_keys_iter))) {
+                        PyObject* part_val_py = PyTuple_GetItem(key_val_tuple, 1);
+                        int64_t part_val = PyLong_AsLongLong(part_val_py);
+                        vals.emplace_back(part_val);
+                        Py_DECREF(part_val_py);
+                        Py_DECREF(key_val_tuple);
+                    }
+                    Py_DECREF(part_keys_iter);
+                }
+
                 Py_DECREF(p);
             }
 
