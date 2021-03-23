eb77b318a3ad61e3406ee4adfe1cc91041f51a68
Ehsan Totoni <ehsan.tn@gmail.com>
2021-03-23 16:33:49 -0400

    [BE-412] Avoid recompilation of impl in inlined overloads

--- bodo/numba_compat.py
+++ bodo/numba_compat.py
@@ -45,6 +45,9 @@ from numba.core.types.functions import (
 )
 from numba.core.typing.templates import (
     AbstractTemplate,
+    Signature,
+    _EmptyImplementationEntry,
+    _inline_info,
     _OverloadAttributeTemplate,
     _OverloadMethodTemplate,
     infer_global,
@@ -679,6 +682,126 @@ if (
 numba.core.typing.templates.make_overload_method_template = (
     make_overload_method_template
 )
+
+
+# replace overload inline handling to avoid recompilation
+def generic(self, args, kws):
+    """
+    Type the overloaded function by compiling the appropriate
+    implementation for the given args.
+    """
+    from numba.core.typed_passes import PreLowerStripPhis
+
+    disp, new_args = self._get_impl(args, kws)
+    if disp is None:
+        return
+    # Compile and type it for the given types
+    disp_type = types.Dispatcher(disp)
+    # Store the compiled overload for use in the lowering phase if there's
+    # no inlining required (else functions are being compiled which will
+    # never be used as they are inlined)
+    if not self._inline.is_never_inline:
+        # need to run the compiler front end up to type inference to compute
+        # a signature
+        from numba.core import compiler, typed_passes
+        from numba.core.inline_closurecall import InlineWorker
+
+        fcomp = disp._compiler
+        flags = compiler.Flags()
+
+        # Updating these causes problems?!
+        # fcomp.targetdescr.options.parse_as_flags(flags,
+        #                                         fcomp.targetoptions)
+        # flags = fcomp._customize_flags(flags)
+
+        # spoof a compiler pipline like the one that will be in use
+        tyctx = fcomp.targetdescr.typing_context
+        tgctx = fcomp.targetdescr.target_context
+        compiler_inst = fcomp.pipeline_class(
+            tyctx,
+            tgctx,
+            None,
+            None,
+            None,
+            flags,
+            None,
+        )
+        inline_worker = InlineWorker(
+            tyctx,
+            tgctx,
+            fcomp.locals,
+            compiler_inst,
+            flags,
+            None,
+        )
+
+        # If the inlinee contains something to trigger literal arg dispatch
+        # then the pipeline call will unconditionally fail due to a raised
+        # ForceLiteralArg exception. Therefore `resolve` is run first, as
+        # type resolution must occur at some point, this will hit any
+        # `literally` calls and because it's going via the dispatcher will
+        # handle them correctly i.e. ForceLiteralArg propagates. This having
+        # the desired effect of ensuring the pipeline call is only made in
+        # situations that will succeed. For context see #5887.
+        resolve = disp_type.dispatcher.get_call_template
+        template, pysig, folded_args, kws = resolve(new_args, kws)
+
+        # Bodo change:
+        # avoid recompiling the implementation if info already available
+        if folded_args in self._inline_overloads:
+            return self._inline_overloads[folded_args]["iinfo"].signature
+
+        ir = inline_worker.run_untyped_passes(
+            disp_type.dispatcher.py_func, enable_ssa=True
+        )
+
+        (typemap, return_type, calltypes, _) = typed_passes.type_inference_stage(
+            self.context, ir, folded_args, None
+        )
+        ir = PreLowerStripPhis()._strip_phi_nodes(ir)
+        ir._definitions = numba.core.ir_utils.build_definitions(ir.blocks)
+
+        sig = Signature(return_type, folded_args, None)
+        # this stores a load of info for the cost model function if supplied
+        # it by default is None
+        self._inline_overloads[sig.args] = {"folded_args": folded_args}
+        # this stores the compiled overloads, if there's no compiled
+        # overload available i.e. function is always inlined, the key still
+        # needs to exist for type resolution
+
+        # NOTE: If lowering is failing on a `_EmptyImplementationEntry`,
+        #       the inliner has failed to inline this entry corretly.
+        impl_init = _EmptyImplementationEntry("always inlined")
+        self._compiled_overloads[sig.args] = impl_init
+        if not self._inline.is_always_inline:
+            # this branch is here because a user has supplied a function to
+            # determine whether to inline or not. As a result both compiled
+            # function and inliner info needed, delaying the computation of
+            # this leads to an internal state mess at present. TODO: Fix!
+            sig = disp_type.get_call_type(self.context, new_args, kws)
+            self._compiled_overloads[sig.args] = disp_type.get_overload(sig)
+            # store the inliner information, it's used later in the cost
+            # model function call
+        iinfo = _inline_info(ir, typemap, calltypes, sig)
+        self._inline_overloads[sig.args] = {"folded_args": folded_args, "iinfo": iinfo}
+    else:
+        sig = disp_type.get_call_type(self.context, new_args, kws)
+        self._compiled_overloads[sig.args] = disp_type.get_overload(sig)
+    return sig
+
+
+# make sure generic() hasn't changed before replacing it
+lines = inspect.getsource(numba.core.typing.templates._OverloadFunctionTemplate.generic)
+if (
+    hashlib.sha256(lines.encode()).hexdigest()
+    != "1205804c405eecc4e15e0c9e3695f6d4ce0957a3c60907cdcb629c694cd094ce"
+):  # pragma: no cover
+    warnings.warn(
+        "numba.core.typing.templates._OverloadFunctionTemplate.generic has changed"
+    )
+
+
+numba.core.typing.templates._OverloadFunctionTemplate.generic = generic
 
 
 def bound_function(template_key, no_unliteral=False):
