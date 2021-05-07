a6b1bcf8d6388e240aec875a5b8cd44f660221c8
Nick Riasanovsky <nick@bodo.ai>
2021-05-06 18:50:57 -0700

    [BE-659] Common Matplotlib Configuration Functions (#2861)
    * supported more utility functions

--- bodo/tests/test_matplotlib.py
+++ bodo/tests/test_matplotlib.py
@@ -51,7 +51,7 @@ def test_usage_example(fig_test, fig_ref):
     """
 
     def impl(input_fig):
-        x = np.linspace(0, 2, 100)
+        x = np.arange(10)
         ax = input_fig.subplots()  # Create a figure and an axes.
         ax.plot(x, x, label="linear")  # Plot some data on the axes.
         ax.plot(x, x ** 2, label="quadratic")  # Plot more data on the axes...
@@ -72,7 +72,7 @@ def test_usage_axes_example(fig_test, fig_ref):
     """
 
     def impl(input_fig):
-        x = np.linspace(0, 2, 100)
+        x = np.arange(10)
         axes = input_fig.subplots(nrows=4, ncols=2)  # Create a figure and an axes.
         axes[0][1].plot(x, x, label="linear")  # Plot some data on the axes.
         axes[1][0].plot(x, x ** 2, label="quadratic")  # Plot more data on the axes...
@@ -102,9 +102,74 @@ def test_usage_replicated_example(fig_test, fig_ref):
         ax.set_title("Simple Plot")  # Add a title to the axes.
         ax.legend()  # Add a legend.
 
-    x = np.linspace(0, 2, 100)
+    x = np.arange(10)
     impl(x, fig_ref)
     bodo.jit(impl)(x, fig_test)
+
+
+@bodo_check_figures_equal(extensions=["png"], tol=0.1)
+def test_axes_set_off(fig_test, fig_ref):
+    """
+    Tests a series of axes functions followed by turning
+    the axis off.
+    """
+
+    def impl(input_fig):
+        x = np.arange(10)
+        ax = input_fig.subplots()
+        ax.plot(x, x, label="linear")
+        ax.plot(x, x ** 2, label="quadratic")
+        ax.plot(x, x ** 3, label="cubic")
+        ax.set_xscale("symlog")
+        ax.set_yscale("log")
+        ax.set_xlim(left=1.0, right=2.2)
+        ax.set_ylim(1.0, top=10.0)
+        ax.set_xticks((1.0, 1.2, 1.4), minor=False)
+        ax.set_xticklabels(("Ready", "set", "go"))
+        ax.set_yticks((1.1, 2.2, 7.4), minor=True)
+        ax.set_yticklabels(("Fighting", "Irish"))
+        ax.grid(True, which="major", axis="x")
+        ax.annotate("BigPoint", (1, 1))
+        ax.set_axis_off()
+        input_fig.suptitle("Go Team!")
+        input_fig.tight_layout(pad=2.0)
+
+    impl(fig_ref)
+    bodo.jit(impl)(fig_test)
+
+
+@bodo_check_figures_equal(extensions=["png"], tol=0.1)
+def test_axes_set_on(fig_test, fig_ref):
+    """
+    Tests a series of axes functions followed by turning
+    the axis off and then on. This should maintain all of
+    the original changes.
+    """
+
+    def impl(input_fig):
+        x = np.arange(10)
+        ax = input_fig.subplots()
+        ax.plot(x, x, label="linear")
+        ax.plot(x, x ** 2, label="quadratic")
+        ax.plot(x, x ** 3, label="cubic")
+        ax.set_xscale("symlog")
+        ax.set_yscale("log")
+        ax.set_xlim(left=1.0, right=2.2)
+        ax.set_ylim(1.0, top=10.0)
+        ax.set_xticks((1.0, 1.2, 1.4), minor=False)
+        ax.set_xticklabels(("Ready", "set", "go"))
+        ax.set_yticks((1.1, 2.2, 7.4), minor=True)
+        ax.set_yticklabels(("Fighting", "Irish"))
+        ax.grid(True, which="major", axis="x")
+        ax.annotate("BigPoint", (1, 1))
+        ax.set_axis_off()
+        ax.set_axis_on()
+        input_fig.suptitle("Go Team!")
+        input_fig.tight_layout(pad=2.0)
+
+    impl(fig_ref)
+
+    bodo.jit(impl)(fig_test)
 
 
 def test_mpl_subplots_const_error(memory_leak_check):
