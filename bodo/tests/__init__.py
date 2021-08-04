026949f6f8ef1fa1c6f6e9a43ed66efcbc5e4257
Nick Riasanovsky <nick@bodo.ai>
2021-08-04 16:18:04 -0400

    [BE-1122] Split series tests into multiple files (#3039)
    * split series tests into multiple files
    
    * made Juan's changes
    
    * fixed fixture imports

--- bodo/tests/__init__.py
+++ bodo/tests/__init__.py
@@ -1,17 +1,2 @@
 # Copyright (C) 2019 Bodo Inc. All rights reserved.
-from bodo.tests.test_basic import *
-from bodo.tests.test_series import *
-from bodo.tests.test_dataframe import *
-from bodo.tests.test_hiframes import *
-
 # from bodo.tests.test_d4p import *
-from bodo.tests.test_date import *
-from bodo.tests.test_strings import *
-
-from bodo.tests.test_groupby import *
-from bodo.tests.test_join import *
-from bodo.tests.test_rolling import *
-
-from bodo.tests.test_ml import *
-
-from bodo.tests.test_io import *
