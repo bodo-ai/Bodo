85d131e76d6b616cd623b17b6795fb11c3657fae
keatoooon <38367344+keatoooon@users.noreply.github.com>
2021-11-03 12:18:14 -0700

    added check to do_obfuscation (#3299)
    * added check to do_obfuscation
    
    * Update obfuscation/do_obfuscation.py
    
    Co-authored-by: juanjgalvez <juan@bodo.ai>
    
    * updated do_obfuscation to run from differing working directories
    
    * small change to comment
    
    Co-authored-by: juanjgalvez <juan@bodo.ai>

--- obfuscation/do_obfuscation.py
+++ obfuscation/do_obfuscation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Copyright (C) 2020 Bodo Inc. All rights reserved.
+import os
 import subprocess
 import sys
-import os
 
 """obfuscation of the ir, hiframes, etc. directories using the obfuscate script
 
@@ -14,14 +14,25 @@ The output shows the result of the operations."""
 
 list_dir = ["", "dl", "ir", "io", "utils", "hiframes", "libs"]
 
+file_dir = os.path.dirname(os.path.realpath(__file__))
+
+if "pre-commit" in os.listdir(os.path.join(file_dir, "../.git/hooks")):
+    raise Exception(
+        "Error! Pre-commit hooks are installed. This can cause issues when building locally, due to obfuscation removing the directives that disable pre-commits for specific sections of code.\nTo fix, please run `pre-commit uninstall` from within the Bodo repository."
+    )
+
 list_files = []
 for e_dir in list_dir:
-    complete_dir = os.path.join("../bodo", e_dir)
+    complete_dir = os.path.join(file_dir, os.path.join("../bodo", e_dir))
     content_dir = os.listdir(complete_dir)
     for e_file in content_dir:
         if e_file.endswith(".py"):
             list_files.append(os.path.join(complete_dir, e_file))
 
-list_command_args = [sys.executable, "./obfuscate.py", "file"] + list_files
+list_command_args = [
+    sys.executable,
+    os.path.join(file_dir, "obfuscate.py"),
+    "file",
+] + list_files
 
 exit(subprocess.run(list_command_args).returncode)
