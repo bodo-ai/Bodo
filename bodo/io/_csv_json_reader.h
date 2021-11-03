0b7830c61dabf5cc8c5657acb614a53c4566a48b
Hadia Ahmed <hadia@bodo.ai>
2021-11-03 05:25:52 -0700

    [BE-1344] [BE-1445] Support skiprows list in pd.read_csv (#3297)

--- bodo/io/_csv_json_reader.h
+++ bodo/io/_csv_json_reader.h
@@ -25,13 +25,20 @@ extern "C" void PyInit_json(PyObject *);
  * @param[in] bucket_region: s3 bucket server location (needed if data is in s3
  * path)
  * @param[in] chunksize: number of rows per chunk
+ * @param[in] is_skiprows_list: whether skiprows is a single number (i.e. skip
+ *nrows from beginning) or list of specific row numbers. See file_chunk_reader
+ *doxygen in _csv_json_readear.cpp
+ * @param[in] skiprows_list_len: number of rows to skip
+ * @param[in] pd_low_memory: flag whether user explicitly requested low_memory
+ * mode.
  * @return     HPATIO file-like object to read the owned chunk through
  *pandas.read_csv
  **/
-extern "C" PyObject *csv_file_chunk_reader(const char *fname, bool is_parallel,
-                                           int64_t skiprows, int64_t nrows,
-                                           bool header, const char *compression,
-                                           const char *bucket_region, int64_t chunksize=0);
+extern "C" PyObject *csv_file_chunk_reader(
+    const char *fname, bool is_parallel, int64_t *skiprows, int64_t nrows,
+    bool header, const char *compression, const char *bucket_region,
+    int64_t chunksize = 0, bool is_skiprows_list = false,
+    int64_t skiprows_list_len = 0, bool pd_low_memory = false);
 
 /**
  * Split file into chunks and return a file-like object per rank. The returned
@@ -53,7 +60,6 @@ extern "C" PyObject *json_file_chunk_reader(const char *fname, bool lines,
                                             const char *compression,
                                             const char *bucket_region);
 
-
 /**
  * Update the reader being used in an iterator to
  * prepare for the next pd.read_csv call.
@@ -64,16 +70,14 @@ extern "C" PyObject *json_file_chunk_reader(const char *fname, bool lines,
  * @return   If there is more data data to read. false triggers
  * a stop iteration.
  */
-extern "C" bool update_csv_reader(PyObject * reader);
-
+extern "C" bool update_csv_reader(PyObject *reader);
 
 /**
  * Mark a CSVReader as unread
  *
  * @param[in]  reader  the CSVReader containing the file info.
  */
-extern "C" void initialize_csv_reader(PyObject * reader);
-
+extern "C" void initialize_csv_reader(PyObject *reader);
 
 /**
  * Split string into chunks and return a file-like object per rank. The returned
