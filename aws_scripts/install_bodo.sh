4530720c1338ed906cce8749824987a77d43aa09
Nick Riasanovsky <nick@bodo.ai>
2022-04-20 11:25:47 -0400

    Remove h5py version lock (#3798)
    * remove h5py version lock
    
    * lock hdf5 version because of build issues
    
    * remove hdf5 on regular CI
    
    * updated conda install for pytest due to python3.7 issue
    
    * switched the pip installation
    
    * fixed the python version

--- aws_scripts/install_bodo.sh
+++ aws_scripts/install_bodo.sh
@@ -12,5 +12,4 @@ conda create -n Bodo python
 cd /home/ubuntu
 eval "$(conda shell.bash hook)"
 conda activate Bodo
-HDF5_VERSION=`python -c "import sys; print('1.12.*=*mpich*') if sys.version_info.minor == 10 else print('1.10.*=*mpich*')"`
-conda install -y bodo h5py scipy hdf5=$HDF5_VERSION -c file://home/ubuntu/bodo-inc/ -c bodo.ai -c conda-forge
+conda install -y bodo h5py scipy hdf5 -c file://home/ubuntu/bodo-inc/ -c bodo.ai -c conda-forge
