87a6fb7d2b387d2cf1119416f29150cab7a89e8a
Srinivas Lade <srinulade1@gmail.com>
2022-06-02 16:23:31 -0400

    Iceberg CI Corrections for Release (#3912)

---
+++ buildscripts/iceberg/get_channel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+
+IS_RELEASE=`git tag --points-at HEAD`
+# if it's not a release, publish to the dev channel
+if [[ -z $IS_RELEASE ]]; then
+    artifactory_channel="bodo.ai-dev"
+else
+    artifactory_channel="bodo.ai"
+fi
+echo $artifactory_channel
