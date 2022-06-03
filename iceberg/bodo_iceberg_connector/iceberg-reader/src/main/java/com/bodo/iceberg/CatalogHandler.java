2f2c8d5c0394398d589c4c88898259f5d1ef13a4
Ehsan Totoni <ehsan.tn@gmail.com>
2022-06-03 08:04:24 -0400

    [BE-2911] Basic Iceberg read support for Glue catalog (#3917)
    * [BE-2911] Basic Iceberg read support for Glue catalog
    
    * Update error message
    
    * separate glue message

--- iceberg/bodo_iceberg_connector/iceberg-reader/src/main/java/com/bodo/iceberg/CatalogHandler.java
+++ iceberg/bodo_iceberg_connector/iceberg-reader/src/main/java/com/bodo/iceberg/CatalogHandler.java
@@ -9,6 +9,8 @@ import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.net.URIBuilder;
 import org.apache.iceberg.CatalogProperties;
 import org.apache.iceberg.Table;
+import org.apache.iceberg.aws.glue.GlueCatalog;
+import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.hadoop.HadoopTables;
@@ -37,14 +39,29 @@ public class CatalogHandler {
     Map<String, String> params =
         uri_builder.getQueryParams().stream()
             .collect(Collectors.toMap(NameValuePair::getName, NameValuePair::getValue));
-    String catalogType = params.getOrDefault("type", "hive");
+
+    // find catalog type (detect good defaults if not specified)
+    String catalogType = params.get("type");
+    if (catalogType == null) {
+      // glue does not always need a URI so connection string can be just "glue"
+      if (this.connStr.startsWith("glue")) catalogType = "glue";
+      // assume regular files if there is no URI scheme or it's file://
+      else if (uri_builder.getScheme() == null || uri_builder.getScheme().equals("file"))
+        catalogType = "file";
+      // assume HMS by default if thrift protocol used
+      else if (uri_builder.getScheme().equals("thrift")) catalogType = "hive";
+      else
+        throw new UnsupportedOperationException(
+            String.format(
+                "Cannot detect Iceberg catalog type from connection string '%s'", this.connStr));
+    }
     params.remove("type");
 
     // catalog URI (without parameters)
     String uri_str = uri_builder.removeQuery().build().toString();
 
     // local file system case (no scheme or file://)
-    if (uri_builder.getScheme() == null || uri_builder.getScheme().equals("file")) {
+    if (catalogType.equals("file")) {
       HadoopTables hadoopTables = new HadoopTables();
       // Set CWD for opening the metadata files later.
       System.setProperty("user.dir", uri_str);
@@ -58,28 +75,39 @@ public class CatalogHandler {
     Namespace db_namespace = Namespace.of(this.dbName);
     TableIdentifier name = TableIdentifier.of(db_namespace, this.tableName);
     params.forEach((k, v) -> properties.put(k, v));
+    Catalog catalog;
 
-    // Hive metastore case
-    if (catalogType.equals("hive")) {
-      HiveCatalog catalog = new HiveCatalog();
-      catalog.setConf(conf);
-      // TODO[BE-2833]: explore using CachingCatalog
-      catalog.initialize("hive_catalog", properties);
-      return catalog.loadTable(name);
+    // initialize the proper catalog
+    // TODO[BE-2833]: explore using CachingCatalog
+    switch (catalogType) {
+        // Hive metastore
+      case "hive":
+        catalog = new HiveCatalog();
+        ((HiveCatalog) catalog).setConf(conf);
+        break;
+        // Nessie metastore
+      case "nessie":
+      case "arctic":
+        catalog = new NessieCatalog();
+        ((NessieCatalog) catalog).setConf(conf);
+        // Nessie connector requires warehouse to be set, even though it may not be necessary
+        // https://github.com/apache/iceberg/blob/82db4fa9ef6d958f5b0f9742c16e30d28338a584/nessie/src/main/java/org/apache/iceberg/nessie/NessieCatalog.java#L134
+        properties.put(CatalogProperties.WAREHOUSE_LOCATION, params.getOrDefault("warehouse", ""));
+        break;
+      case "glue":
+        catalog = new GlueCatalog();
+        ((GlueCatalog) catalog).setConf(conf);
+        // Glue catalog requires warehouse to be set to non-empty even though it may not be
+        // necessary
+        properties.put(
+            CatalogProperties.WAREHOUSE_LOCATION, params.getOrDefault("warehouse", "s3"));
+        break;
+      default:
+        throw new UnsupportedOperationException(
+            String.format("Iceberg catalog type '%s' not supported yet.", catalogType));
     }
 
-    // Nessie metastore case
-    if (catalogType.equals("nessie")) {
-      NessieCatalog catalog = new NessieCatalog();
-      catalog.setConf(conf);
-      // Nessie connector requires warehouse to be set, even though it may not be necessary
-      // https://github.com/apache/iceberg/blob/82db4fa9ef6d958f5b0f9742c16e30d28338a584/nessie/src/main/java/org/apache/iceberg/nessie/NessieCatalog.java#L134
-      properties.put(CatalogProperties.WAREHOUSE_LOCATION, params.getOrDefault("warehouse", ""));
-      catalog.initialize("nessie_catalog", properties);
-      return catalog.loadTable(name);
-    }
-
-    throw new UnsupportedOperationException(
-        String.format("Iceberg catalog type '%s' not supported yet.", catalogType));
+    catalog.initialize(String.format("%s_catalog", catalogType), properties);
+    return catalog.loadTable(name);
   }
 }
