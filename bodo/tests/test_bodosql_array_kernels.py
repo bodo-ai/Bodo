af118986d2f07471d74e71f258be27ac96abca3b
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2022-07-01 09:59:56 -0400

    [BE-3068] Create a series and array kernel for coalesce (#4016)
    * Initial commit
    
    * Edge cases handled
    
    * Revisions
    
    * Revisions
    
    * Revisions
    
    * Revisions
    
    * Removed leftover slows
    
    * Typo revision
    
    Co-authored-by: Kian Nassre <knassre@Kians-MacBook-Pro.local>
    Co-authored-by: Kian Nassre <knassre@kians-mbp.wifi.local.cmu.edu>

--- bodo/tests/test_bodosql_array_kernels.py
+++ bodo/tests/test_bodosql_array_kernels.py
@@ -4,6 +4,7 @@
 
 import re
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -281,3 +282,202 @@ def test_error_lpad_rpad():
 
     with pytest.raises(BodoError, match=err_msg6):
         bodo.jit(impl4)(A2, 10, "_")
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        pytest.param(
+            (
+                pd.Series(
+                    pd.array(
+                        [1, None, 3, None, 5, None, 7, None], dtype=pd.Int32Dtype()
+                    )
+                ),
+                pd.Series(
+                    pd.array(
+                        [2, 3, 5, 7, None, None, None, None], dtype=pd.Int32Dtype()
+                    )
+                ),
+            ),
+            id="int_series_2",
+        ),
+        pytest.param(
+            (
+                pd.Series(
+                    pd.array(
+                        [1, 2, None, None, 3, 4, None, None], dtype=pd.Int32Dtype()
+                    )
+                ),
+                None,
+                pd.Series(
+                    pd.array(
+                        [None, None, None, None, None, None, None, None],
+                        dtype=pd.Int32Dtype(),
+                    )
+                ),
+                pd.Series(
+                    pd.array(
+                        [None, 5, None, 6, None, None, None, 7], dtype=pd.Int32Dtype()
+                    )
+                ),
+                42,
+                pd.Series(
+                    pd.array(
+                        [8, 9, 10, None, None, None, None, 11], dtype=pd.Int32Dtype()
+                    )
+                ),
+            ),
+            id="int_series_scalar_6",
+        ),
+        pytest.param((None, None, 3, 4, 5, None), id="int_scalar_6"),
+        pytest.param(
+            (None, None, None, None, None, None),
+            id="all_null_6",
+            marks=pytest.mark.slow,
+        ),
+        pytest.param(
+            (
+                pd.Series(
+                    pd.array(
+                        [None, "AB", None, "CD", None, "EF", None, "GH"],
+                        dtype=pd.StringDtype(),
+                    )
+                ),
+                pd.Series(
+                    pd.array(
+                        ["IJ", "KL", None, None, "MN", "OP", None, None],
+                        dtype=pd.StringDtype(),
+                    )
+                ),
+            ),
+            id="string_series_2",
+            marks=pytest.mark.slow,
+        ),
+        pytest.param(
+            (
+                pd.Series(
+                    pd.array(
+                        [1, None, 3, None, 5, None, 7, None], dtype=pd.Int16Dtype()
+                    )
+                ),
+                pd.Series(
+                    pd.array(
+                        [2, 3, 5, 2**38, None, None, None, None],
+                        dtype=pd.Int64Dtype(),
+                    )
+                ),
+            ),
+            id="mixed_int_series_2",
+            marks=pytest.mark.slow,
+        ),
+        pytest.param(
+            (
+                pd.Series(
+                    pd.array(
+                        [4, None, 64, None, 256, None, 1024, None],
+                        dtype=pd.UInt16Dtype(),
+                    )
+                ),
+                pd.Series(
+                    pd.array(
+                        [1.1, 1.2, 1.3, 1.4, None, None, None, None],
+                        dtype=np.float64,
+                    )
+                ),
+            ),
+            id="int_float_series_2",
+        ),
+        pytest.param((42,), id="int_1", marks=pytest.mark.slow),
+        pytest.param((42,), id="none_1", marks=pytest.mark.slow),
+        pytest.param(
+            (pd.array([1, 2, 3, 4, 5]),), id="int_array_1", marks=pytest.mark.slow
+        ),
+    ],
+)
+def test_coalesce(args):
+    def impl1(A, B):
+        return bodo.libs.bodosql_array_kernels.coalesce((A, B))
+
+    def impl2(A, B, C, D, E, F):
+        return bodo.libs.bodosql_array_kernels.coalesce((A, B, C, D, E, F))
+
+    def impl3(A):
+        return bodo.libs.bodosql_array_kernels.coalesce((A,))
+
+    def coalesce_scalar_fn(*args):
+        for arg in args:
+            if not pd.isna(arg):
+                return arg
+
+    coalesce_answer = vectorized_sol(args, coalesce_scalar_fn, None)
+
+    if len(args) == 2:
+        check_func(
+            impl1, args, py_output=coalesce_answer, check_dtype=False, reset_index=True
+        )
+    elif len(args) == 6:
+        check_func(
+            impl2, args, py_output=coalesce_answer, check_dtype=False, reset_index=True
+        )
+    elif len(args) == 1:
+        check_func(
+            impl3, args, py_output=coalesce_answer, check_dtype=False, reset_index=True
+        )
+
+
+@pytest.mark.slow
+def test_option_coalesce():
+    def impl1(arr, scale1, scale2, flag1, flag2):
+        A = scale1 if flag1 else None
+        B = scale2 if flag2 else None
+        return bodosql_array_kernels.coalesce((A, arr, B))
+
+    arr, scale1, scale2 = pd.array(["A", None, "C", None, "E"]), "", " "
+    for flag1 in [True, False]:
+        for flag2 in [True, False]:
+            if flag1:
+                answer = pd.Series(["", "", "", "", ""])
+            elif flag2:
+                answer = pd.Series(["A", " ", "C", " ", "E"])
+            else:
+                answer = pd.Series(["A", None, "C", None, "E"])
+            check_func(
+                impl1,
+                (arr, scale1, scale2, flag1, flag2),
+                py_output=answer,
+                check_dtype=False,
+            )
+
+
+@pytest.mark.slow
+def test_error_coalesce():
+    def impl1(A, B, C):
+        return bodosql_array_kernels.coalesce((A, B, C))
+
+    def impl2(A, B, C):
+        return bodosql_array_kernels.coalesce([A, B, C])
+
+    def impl3():
+        return bodosql_array_kernels.coalesce(())
+
+    # Note: not testing non-constant tuples because the kernel is only used
+    # by BodoSQL in cases where we do the code generation and can guarantee
+    # that the tuple is constant
+
+    err_msg1 = re.escape("Cannot coalesce columns with different dtypes")
+    err_msg2 = re.escape("Coalesce argument must be a tuple")
+    err_msg3 = re.escape("Cannot coalesce 0 columns")
+
+    A = pd.Series(["A", "B", "C", "D", "E"])
+    B = pd.Series(["D", "E", "F", "G", "H"])
+    C = pd.Series([123, 456, 789, 123, 456])
+
+    with pytest.raises(BodoError, match=err_msg1):
+        bodo.jit(impl1)(A, B, C)
+
+    with pytest.raises(BodoError, match=err_msg2):
+        bodo.jit(impl2)(A, B, C)
+
+    with pytest.raises(BodoError, match=err_msg3):
+        bodo.jit(impl3)()
--- bodo/transforms/distributed_analysis.py
+++ bodo/transforms/distributed_analysis.py
@@ -1506,6 +1506,22 @@ class DistributedAnalysis:
                 self._meet_several_array_dists(arrays, array_dists)
             return
 
+        if fdef == ("coalesce", "bodo.libs.bodosql_array_kernels"):
+            # Note: this will fail if the tuple argument is not constant,
+            # but this should never happen because we control code generation
+            elems = guard(find_build_tuple, self.func_ir, rhs.args[0])
+            assert (
+                elems is not None
+            ), "Internal error, unable to find build tuple for arg0 of coalesce"
+
+            arrays = [lhs]
+            for arg in elems:
+                if is_array_typ(self.typemap[arg.name]):
+                    arrays.append(arg.name)
+            if len(arrays) > 1:
+                self._meet_several_array_dists(arrays, array_dists)
+            return
+
         if fdef == ("first_last_valid_index", "bodo.libs.array_kernels"):
             # doesn't affect distribution of either input or output
             return
