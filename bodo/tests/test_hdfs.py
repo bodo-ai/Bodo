bodo-ai/Bodo

commit 6547ddec84a2497391bebd49cdbbef560120ddd1

[BE-1571] Avoid writing empty files in `to_parquet` (#3980)

bodo/io/_csv_json_writer.cpp
@@ -79,11 +79,16 @@ void write_buff(char *_path_name, char *buff, int64_t start, int64_t count,
             }
             return;
         }
+
+        // We need to create a directory when writing a distributed
+        // table to a posix filesystem.
+        if (fs_option == Bodo_Fs::posix && is_parallel) {
+            create_dir_posix(myrank, dirname, path_name);
+        }
         // handling s3 and hdfs with arrow
         // & handling posix json directory outputs with std::filesystem
         open_outstream(fs_option, is_parallel, myrank, suffix.substr(1),
-                       dirname, fname, orig_path, path_name, &out_stream,
-                       bucket_region);
+                       dirname, fname, orig_path, &out_stream, bucket_region);
         status = out_stream->Write(buff, count);
         CHECK_ARROW(status, "arrow::io::OutputStream::Write");
         // writing an extra '\n' to the end of json files inside of directory

bodo/io/_fs_io.cpp
@@ -202,10 +202,31 @@ void open_file_appendstream(
     *out_stream = hdfs_fs->OpenAppendStream(fname).ValueOrDie();
 }
 
+void create_dir_posix(int myrank, std::string &dirname,
+                      std::string &path_name) {
+    // create output directory
+    int error = 0;
+    if (std::filesystem::exists(dirname)) {
+        if (!std::filesystem::is_directory(dirname)) error = 1;
+    } else {
+        // for the parallel case, 'dirname' is the directory where the
+        // different parts of the distributed table are stored (each as
+        // a file)
+        std::filesystem::create_directory(dirname);
+    }
+    MPI_Allreduce(MPI_IN_PLACE, &error, 1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
+    if (error) {
+        if (myrank == 0)
+            std::cerr << "Bodo parquet write ERROR: a process reports "
+                         "that path "
+                      << path_name << " exists and is not a directory"
+                      << std::endl;
+    }
+}
+
 void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
                     const std::string &file_type, std::string &dirname,
                     std::string &fname, std::string &orig_path,
-                    std::string &path_name,
                     std::shared_ptr<::arrow::io::OutputStream> *out_stream,
                     const std::string &bucket_region) {
     PyObject *f_mod = nullptr;
@@ -217,26 +238,7 @@ void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
             return;
         }
         if (is_parallel) {
-            // create output directory
-            int error = 0;
-            if (std::filesystem::exists(dirname)) {
-                if (!std::filesystem::is_directory(dirname)) error = 1;
-            } else {
-                // for the parallel case, 'dirname' is the directory where the
-                // different parts of the distributed table are stored (each as
-                // a file)
-                std::filesystem::create_directory(dirname);
-            }
-            MPI_Allreduce(MPI_IN_PLACE, &error, 1, MPI_INT, MPI_LOR,
-                          MPI_COMM_WORLD);
-            if (error) {
-                if (myrank == 0)
-                    std::cerr << "Bodo parquet write ERROR: a process reports "
-                                 "that path "
-                              << path_name << " exists and is not a directory"
-                              << std::endl;
-                return;
-            }
+            // assumes that the directory has already been created
             std::filesystem::path out_path(dirname);
             out_path /= fname;  // append file name to output path
             posix_open_file_outstream(file_type, out_path.string(), out_stream);

bodo/io/_fs_io.h
@@ -115,6 +115,17 @@ void open_file_appendstream(
     std::shared_ptr<::arrow::fs::HadoopFileSystem> hdfs_fs,
     std::shared_ptr<::arrow::io::OutputStream> *out_stream);
 
+/**
+ * @brief Create a directory in a posix filesystem
+ *
+ * This function is assumed to be happening in parallel, i.e.
+ * it should be called by all ranks
+ * @param myrank current MPI rank
+ * @param dirname directory name to create
+ * @param path_name path to dir to create (exclude prefix, include path)
+ */
+void create_dir_posix(int myrank, std::string &dirname, std::string &path_name);
+
 /*
  * Open arrow::io::OutputStream for csv/json/parquet write
  * writing csv/json to posix:
@@ -128,13 +139,12 @@ void open_file_appendstream(
  * @param dirname: directory name to store the files
  * @param fname: name of file to open (exclude prefix, excludes path)
  * @param orig_path: name of file to open (include prefix & path)
- * @param path_name: name of file to open (exclude prefix, include path)
  * @param out_stream: the OutputStream to open
+ * @param bucket_region: Region of the S3 bucket in case writing to S3
  */
 void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
                     const std::string &file_type, std::string &dirname,
                     std::string &fname, std::string &orig_path,
-                    std::string &path_name,
                     std::shared_ptr<::arrow::io::OutputStream> *out_stream,
                     const std::string &bucket_region);
 

bodo/io/parquet_write.cpp
@@ -529,8 +529,17 @@ void pq_write(const char *_path_name, const table_info *table,
     extract_fs_dir_path(_path_name, is_parallel, ".parquet", myrank, num_ranks,
                         &fs_option, &dirname, &fname, &orig_path, &path_name);
 
+    // We need to create a directory when writing a distributed
+    // table to a posix filesystem.
+    if (fs_option == Bodo_Fs::posix && is_parallel) {
+        create_dir_posix(myrank, dirname, path_name);
+    }
+    // Do not write a file if there are no rows to write.
+    if (table->nrows() == 0) {
+        return;
+    }
     open_outstream(fs_option, is_parallel, myrank, "parquet", dirname, fname,
-                   orig_path, path_name, &out_stream, bucket_region);
+                   orig_path, &out_stream, bucket_region);
 
     // copy column names to a std::vector<string>
     std::vector<std::string> col_names;

bodo/tests/test_io.py
@@ -43,7 +43,7 @@ from bodo.tests.utils import (
     get_start_end,
     reduce_sum,
 )
-from bodo.utils.testing import ensure_clean
+from bodo.utils.testing import ensure_clean, ensure_clean_dir
 from bodo.utils.typing import BodoError, BodoWarning
 from bodo.utils.utils import is_call_assign
 
@@ -2504,6 +2504,28 @@ def test_write_parquet_row_group_size(memory_leak_check):
         finally:
             os.remove(output_filename)
     bodo.barrier()
+
+
+def test_write_parquet_no_empty_files(memory_leak_check):
+    """Test that when a rank has no data, it doesn't write a file"""
+    # The test is most useful when run with multiple ranks
+    # but should pass on a single rank too.
+
+    @bodo.jit(distributed=["df"])
+    def impl(df, out_name):
+        df.to_parquet(out_name)
+
+    if bodo.get_rank() == 0:
+        df = pd.DataFrame({"A": [1], "B": [1]})
+    else:
+        df = pd.DataFrame({"A": [], "B": []})
+
+    output_filename = "1row.pq"
+    with ensure_clean_dir(output_filename):
+        impl(df, output_filename)
+        bodo.barrier()
+        # Only rank 0 should've written a file
+        assert len(os.listdir(output_filename)) == 1
 
 
 def test_csv_bool1(datapath, memory_leak_check):

commit 10b4d3b10c422a9f7d67c2ce31cd94f3534892af

Fix bug in `to_parquet` for HDFS that causes hang when there's no rows to write on some ranks (#4067)
Fixes a bug that was introduced by #3980.

bodo/io/_csv_json_writer.cpp
@@ -81,14 +81,15 @@ void write_buff(char *_path_name, char *buff, int64_t start, int64_t count,
         }
 
         // We need to create a directory when writing a distributed
-        // table to a posix filesystem.
-        if (fs_option == Bodo_Fs::posix && is_parallel) {
-            create_dir_posix(myrank, dirname, path_name);
+        // table to a posix or hadoop filesystem.
+        if (is_parallel) {
+            create_dir_parallel(fs_option, myrank, dirname, path_name,
+                                orig_path, suffix.substr(1));
         }
         // handling s3 and hdfs with arrow
         // & handling posix json directory outputs with std::filesystem
-        open_outstream(fs_option, is_parallel, myrank, suffix.substr(1),
-                       dirname, fname, orig_path, &out_stream, bucket_region);
+        open_outstream(fs_option, is_parallel, suffix.substr(1), dirname, fname,
+                       orig_path, &out_stream, bucket_region);
         status = out_stream->Write(buff, count);
         CHECK_ARROW(status, "arrow::io::OutputStream::Write");
         // writing an extra '\n' to the end of json files inside of directory

bodo/io/_fs_io.cpp
@@ -224,7 +224,38 @@ void create_dir_posix(int myrank, std::string &dirname,
     }
 }
 
-void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
+void create_dir_hdfs(int myrank, std::string &dirname, std::string &orig_path,
+                     const std::string &file_type) {
+    PyObject *f_mod = nullptr;
+    // get hdfs_get_fs function
+    PyObject *hdfs_func_obj = nullptr;
+    import_fs_module(Bodo_Fs::hdfs, file_type, f_mod);
+    get_get_fs_pyobject(Bodo_Fs::hdfs, file_type, f_mod, hdfs_func_obj);
+    hdfs_get_fs_t hdfs_get_fs =
+        (hdfs_get_fs_t)PyNumber_AsSsize_t(hdfs_func_obj, NULL);
+    arrow::Status status;
+    std::shared_ptr<::arrow::fs::HadoopFileSystem> hdfs_fs;
+    hdfs_get_fs(orig_path, &hdfs_fs);
+    if (myrank == 0) {
+        status = hdfs_fs->CreateDir(dirname);
+        CHECK_ARROW(status, "Hdfs::MakeDirectory", file_type);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    Py_DECREF(f_mod);
+    Py_DECREF(hdfs_func_obj);
+}
+
+void create_dir_parallel(Bodo_Fs::FsEnum fs_option, int myrank,
+                         std::string &dirname, std::string &path_name,
+                         std::string &orig_path, const std::string &file_type) {
+    if (fs_option == Bodo_Fs::posix) {
+        create_dir_posix(myrank, dirname, path_name);
+    } else if (fs_option == Bodo_Fs::hdfs) {
+        create_dir_hdfs(myrank, dirname, orig_path, file_type);
+    }
+}
+
+void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel,
                     const std::string &file_type, std::string &dirname,
                     std::string &fname, std::string &orig_path,
                     std::shared_ptr<::arrow::io::OutputStream> *out_stream,
@@ -279,11 +310,7 @@ void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
         std::shared_ptr<::arrow::fs::HadoopFileSystem> hdfs_fs;
         hdfs_get_fs(orig_path, &hdfs_fs);
         if (is_parallel) {
-            if (myrank == 0) {
-                status = hdfs_fs->CreateDir(dirname);
-                CHECK_ARROW(status, "Hdfs::MakeDirectory", file_type);
-            }
-            MPI_Barrier(MPI_COMM_WORLD);
+            // assumes that the directory has already been created
             open_file_outstream(fs_option, file_type, dirname + "/" + fname,
                                 NULL, hdfs_fs, out_stream);
         } else {

bodo/io/_fs_io.h
@@ -116,15 +116,20 @@ void open_file_appendstream(
     std::shared_ptr<::arrow::io::OutputStream> *out_stream);
 
 /**
- * @brief Create a directory in a posix filesystem
+ * @brief Create a directory in a posix / hadoop filesystem.
  *
  * This function is assumed to be happening in parallel, i.e.
  * it should be called by all ranks
+ * @param fs_option: file system to write to
  * @param myrank current MPI rank
  * @param dirname directory name to create
  * @param path_name path to dir to create (exclude prefix, include path)
+ * @param orig_path: name of file to open (include prefix & path)
+ * @param file_type: type of file, 'csv', 'json', or 'parquet'
  */
-void create_dir_posix(int myrank, std::string &dirname, std::string &path_name);
+void create_dir_parallel(Bodo_Fs::FsEnum fs_option, int myrank,
+                         std::string &dirname, std::string &path_name,
+                         std::string &orig_path, const std::string &file_type);
 
 /*
  * Open arrow::io::OutputStream for csv/json/parquet write
@@ -134,15 +139,14 @@ void create_dir_posix(int myrank, std::string &dirname, std::string &path_name);
  *  use std::filesystem to create directory if necessary
  * @param fs_option: file system to write to
  * @param is_parallel: true if the table is part of a distributed table
- * @param myrank: current MPI rank
- * @param file_type: type of file, 'csv', "json', or 'parquet'
+ * @param file_type: type of file, 'csv', 'json', or 'parquet'
  * @param dirname: directory name to store the files
  * @param fname: name of file to open (exclude prefix, excludes path)
  * @param orig_path: name of file to open (include prefix & path)
  * @param out_stream: the OutputStream to open
  * @param bucket_region: Region of the S3 bucket in case writing to S3
  */
-void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel, int myrank,
+void open_outstream(Bodo_Fs::FsEnum fs_option, bool is_parallel,
                     const std::string &file_type, std::string &dirname,
                     std::string &fname, std::string &orig_path,
                     std::shared_ptr<::arrow::io::OutputStream> *out_stream,

bodo/io/parquet_write.cpp
@@ -530,16 +530,18 @@ void pq_write(const char *_path_name, const table_info *table,
                         &fs_option, &dirname, &fname, &orig_path, &path_name);
 
     // We need to create a directory when writing a distributed
-    // table to a posix filesystem.
-    if (fs_option == Bodo_Fs::posix && is_parallel) {
-        create_dir_posix(myrank, dirname, path_name);
+    // table to a posix or hadoop filesystem.
+    if (is_parallel) {
+        create_dir_parallel(fs_option, myrank, dirname, path_name, orig_path,
+                            "parquet");
     }
+
     // Do not write a file if there are no rows to write.
     if (table->nrows() == 0) {
         return;
     }
-    open_outstream(fs_option, is_parallel, myrank, "parquet", dirname, fname,
-                   orig_path, &out_stream, bucket_region);
+    open_outstream(fs_option, is_parallel, "parquet", dirname, fname, orig_path,
+                   &out_stream, bucket_region);
 
     // copy column names to a std::vector<string>
     std::vector<std::string> col_names;

docker/bodo_dev_hdfs/Dockerfile
@@ -19,15 +19,16 @@ ENV PATH /root/miniconda3/bin/:${PATH}
 # Creation of the BODODEV environment
 #
 
-RUN conda create -n BODODEV numpy scipy pandas=${BODO_PD_VERSION:-'1.4.*'} boost-cpp=1.74.0 cmake h5py mpich='3.4.*=h*' mpi 'gcc_linux-64>=9' 'gxx_linux-64>=9' python=3.9 -c conda-forge
-RUN conda install -n BODODEV python numba=0.55.1 -c conda-forge
-RUN conda install -n BODODEV hdf5='1.10.*=*mpich*' -c conda-forge
-RUN conda install -n BODODEV python pyarrow=7.0.0 fsspec>=2021.09 -c conda-forge
+RUN conda create -n BODODEV numpy scipy pandas=${BODO_PD_VERSION:-'1.4.*'} boost-cpp=1.74.0 cmake h5py mpich mpi 'gcc_linux-64>=9' 'gxx_linux-64>=9' python=3.9 -c conda-forge
+RUN conda install -n BODODEV python numba=0.55.2 -c conda-forge
+RUN conda install -n BODODEV 'hdf5=1.12.*=*mpich*' -c conda-forge
+RUN conda install -n BODODEV python pyarrow=7.0.0 fsspec -c conda-forge
 RUN conda install -n BODODEV pytest ipython
 RUN conda install -n BODODEV sqlalchemy pymysql xlrd openpyxl mpi4py cython -c conda-forge
-RUN conda install -n BODODEV scikit-learn='1.0.*' -c conda-forge
+# RUN conda install -n BODODEV scikit-learn='1.0.*' -c conda-forge
 RUN conda install -n BODODEV openjdk=8
 RUN conda install -n BODODEV hdfs3 -c conda-forge
+RUN conda instal -n BODODEV ccache -c conda-forge
 
 # installing hadoop and libhdfs (JNI)
 ARG hdfs=3.2.1

bodo/tests/test_hdfs.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2019 Bodo Inc.
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -23,6 +24,40 @@ def test_partition_cols(hdfs_datapath):
         bd_out = pd.read_parquet(bd_fname)
         pd_out = pd.read_parquet(pd_fname)
     pd.testing.assert_frame_equal(bd_out, pd_out)
+
+
+def test_hdfs_write_parquet_no_empty_files(hdfs_datapath, memory_leak_check):
+    """Test that when a rank has no data, it doesn't write a file"""
+    # The test is most useful when run with multiple ranks
+    # but should pass on a single rank too.
+    from urllib.parse import urlparse
+
+    from pyarrow.fs import FileSelector, FileType
+    from pyarrow.fs import HadoopFileSystem as HdFS
+
+    output_filename = hdfs_datapath("1row.pq")
+    options = urlparse(output_filename)
+
+    @bodo.jit(distributed=["df"])
+    def impl(df, out_name):
+        df.to_parquet(out_name)
+
+    if bodo.get_rank() == 0:
+        df = pd.DataFrame({"A": [1], "B": [1]})
+    else:
+        df = pd.DataFrame({"A": [], "B": []})
+
+    with ensure_clean2(output_filename):
+        impl(df, output_filename)
+        bodo.barrier()
+        # Only rank 0 should've written a file
+        fs = HdFS(host=options.hostname, port=options.port, user=options.username)
+        fi = fs.get_file_info(options.path)
+        is_dir = (not fi.size) and fi.type == FileType.Directory
+        assert is_dir, "Not a directory"
+        file_selector = FileSelector(options.path, recursive=True)
+        file_stats = fs.get_file_info(file_selector)
+        assert len(file_stats) == 1
 
 
 def test_hdfs_pq_groupby3(datapath, hdfs_datapath):