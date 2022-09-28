aad0994dd02cba23d4b7feae63aff1245de04c07
Ehsan Totoni <ehsan.tn@gmail.com>
2022-09-28 08:29:15 -0400

    [BE-3679] Avoid schema matching in parquet dict-encoding estimation (#4362)
    * [BE-3679] Avoid schema matching in parquet dict-encoding estimation
    
    * add test

--- bodo/transforms/typing_pass.py
+++ bodo/transforms/typing_pass.py
@@ -2573,7 +2573,10 @@ class TypingTransforms:
         # Sort the columns to ensure same order on all ranks
         str_columns = sorted(str_columns)
         dict_str_cols = bodo.io.parquet_pio.determine_str_as_dict_columns(
-            iceberg_pq_dset.pq_dataset, pyarrow_table_schema, str_columns
+            iceberg_pq_dset.pq_dataset,
+            pyarrow_table_schema,
+            str_columns,
+            is_iceberg=True,
         )
         all_dict_str_cols = set(dict_str_cols) | set(_bodo_read_as_dict)
 
