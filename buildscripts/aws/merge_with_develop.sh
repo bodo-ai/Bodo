83bf1b6b60902071b1fa0efc6ad8f8d19bc026bb
Hadia Ahmed <hadia@bodo.ai>
2022-10-28 17:01:26 -0700

    Replace use of master branch with develop (#4462)

--- buildscripts/aws/merge_with_develop.sh
+++ buildscripts/aws/merge_with_develop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -exo pipefail
 
-# File for merging with master. Doesn't work if the merge has to be done manually.
+# File for merging with develop. Doesn't work if the merge has to be done manually.
 
 # Set info to avoid errors
 git config --global user.email "nick@bodo.ai"
 git config --global user.name "Nick Riasanovsky"
-git pull --no-edit origin master
+git pull --no-edit origin develop
