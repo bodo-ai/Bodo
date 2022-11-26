f45bd29feb7471e366412731de91f1ee674732a9
Nick Riasanovsky <nick@bodo.ai>
2022-11-25 20:06:02 -0500

    [BE-3896] Support dictionary encoding for coalesce (#4505)
    * implemented get_str_arr_item_copy for coalesce
    
    * Added the basics for a test
    
    * updated the docstring to do dict encoding in a separate PR [run CI]
    
    * updated the docstring
    
    * added the criteria for dictionary encoding
    
    * Added the dictionary implementation [run CI]
    
    * process for getting IR is mostly figured out, need a break
    
    * updated the dictionary implementation
    
    * updated the test
    
    * updated the process to figure out how to search. However need to determine the IR variables
    
    * updated the actual test [run CI]
    
    * Added a dict test [run CI]
    
    * fixed except for the memory leak
    
    * fixed optional type memory leak [run CI]
    
    * fixed expected dictionary encoding test [run CI]
    
    * optimize the implementation and respond to ehsan's feedback [run CI]
    
    * Added Kian's changes [run CI]
    
    * added the test
    
    * add support for sequential handling [run CI]
    
    * added the is_parallel argument
    
    * added an is_parallel flag for concat [run CI]
    
    * updated init_dict_arr
    
    * fixed unboxing and constant lowering
    
    * updated the C++ API
    
    * fixed more init occurrences
    
    * updated the convert_local_dictionary_to_global handling/implementation
    
    * updated the equality implementations
    
    * added an api to drop duplicates and reverted concat
    
    * fixed a refactor typo
    
    * updated the implementation to always drop duplicates
    
    * updated the join implementation
    
    * fixed the bug
    
    * fix the existing tests [run CI]
    
    * fixed bugs in concat handling [run CI]
    
    * propagate is_parallel [run CI]
    
    * fixed a bug [run CI]
    
    * added pragma no covers [run CI]
    
    * updated hashing to require unique values
    
    * updated the comments
    
    * updated the comments based on my sync with Hadia [run CI]
    
    * revert concat [run CI]
    
    * fixed broken test [run CI]
    
    * fixed the global typo [run CI]
    
    * Made ehsan's suggested change [run CI]
    
    * Fixed typo [run CI]
    
    Fixed typo in code.
    
    Co-authored-by: Ehsan Totoni <ehsan.tn@gmail.com>
    
    * Fixed a typo [run CI]
    
    Co-authored-by: Ehsan Totoni <ehsan.tn@gmail.com>

--- bodo/libs/str_ext.py
+++ bodo/libs/str_ext.py
@@ -164,6 +164,24 @@ def overload_unicode_to_utf8(s):
     return lambda s: unicode_to_utf8_and_len(s)[0]  # pragma: no cover
 
 
+def unicode_to_utf8_len(s):  # pragma: no cover
+    return s
+
+
+@overload(unicode_to_utf8_len)
+def overload_unicode_to_utf8_len(s):
+    """Obtain the length of a unicode string when encoded in
+    utf8. Currently this requires converting to utf8.
+
+    Args:
+        s (types.unicode_type): String in unicode
+
+    Returns:
+        types.int64: length of the string as a utf8 string.
+    """
+    return lambda s: unicode_to_utf8_and_len(s)[1]  # pragma: no cover
+
+
 @overload(max)
 def overload_builtin_max(lhs, rhs):
     if lhs == types.unicode_type and rhs == types.unicode_type:
