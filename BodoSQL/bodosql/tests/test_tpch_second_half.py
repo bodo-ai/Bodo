d417a949c09955d83ff8b662a9bfaa9aac0bc883
Srinivas Lade <srinivas@bodo.ai>
2022-12-06 15:31:43 -0500

    [BE-4042] Increase BodoSQL Smoke and TPCH Test Timeouts (#4596)

--- BodoSQL/bodosql/tests/test_tpch_second_half.py
+++ BodoSQL/bodosql/tests/test_tpch_second_half.py
@@ -168,7 +168,7 @@ def test_tpch_q15_blazingsql(tpch_data, spark_info, memory_leak_check):
 
 # test_tpch_q16 has timed out at 350 seconds with
 # multiple processes
-@pytest.mark.timeout(500)
+@pytest.mark.timeout(600)
 @pytest.mark.slow
 def test_tpch_q16(tpch_data, spark_info, memory_leak_check):
     BRAND = "BRAND#45"
@@ -309,6 +309,7 @@ def test_tpch_q18(tpch_data, spark_info, memory_leak_check):
     )
 
 
+@pytest.mark.timeout(600)
 @pytest.mark.slow
 def test_tpch_q19(tpch_data, spark_info, memory_leak_check):
     QUANTITY1 = 1
@@ -469,6 +470,7 @@ def test_tpch_q21(tpch_data, spark_info, memory_leak_check):
     )
 
 
+@pytest.mark.timeout(600)
 @pytest.mark.slow
 def test_tpch_q22(tpch_data, spark_info, memory_leak_check):
     I1 = 13
