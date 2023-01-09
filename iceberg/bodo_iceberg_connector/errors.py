0cb75be3ca8891adffc005cb978c501cce7e8697
Nick Riasanovsky <nick@bodo.ai>
2023-01-09 16:52:03 -0500

    [BE-4167] Fix py4j exception handling (#4741)
    * fix py4j exception handling [run CI]
    
    * Removed arrow_number_to_decimal [run CI]

--- iceberg/bodo_iceberg_connector/errors.py
+++ iceberg/bodo_iceberg_connector/errors.py
@@ -1,4 +1,4 @@
-from py4j.protocol import Py4JJavaError
+from py4j.protocol import Py4JError, Py4JJavaError, Py4JNetworkError
 
 
 class IcebergError(Exception):
@@ -24,17 +24,25 @@ class IcebergJavaError(IcebergError):
         self.java_error = java_error
 
     @classmethod
-    def from_java_error(cls, e: Py4JJavaError):
+    def from_java_error(cls, e: Py4JError):
         # TODO: figure out how to get this to work with subclasses
-        if (
-            str(e.java_exception.getClass())
-            == "class org.apache.iceberg.exceptions.NoSuchTableException"
-        ):
-            return cls("No such Iceberg table found", str(e.java_exception))
-        elif (
-            str(e.java_exception.getClass())
-            == "class org.apache.iceberg.exceptions.RuntimeIOException"
-        ):
-            return cls("Unable to find Iceberg table", str(e.java_exception))
+        if isinstance(e, Py4JJavaError):
+            if (
+                str(e.java_exception.getClass())
+                == "class org.apache.iceberg.exceptions.NoSuchTableException"
+            ):
+                return cls("No such Iceberg table found", str(e.java_exception))
+            elif (
+                str(e.java_exception.getClass())
+                == "class org.apache.iceberg.exceptions.RuntimeIOException"
+            ):
+                return cls("Unable to find Iceberg table", str(e.java_exception))
+            else:
+                return cls("Unknown Iceberg Error", str(e.java_exception))
+        elif isinstance(e, Py4JNetworkError):
+            return cls("Unexpected Py4J Network Error: " + str(e))
         else:
-            return cls("Unknown Iceberg Error", str(e.java_exception))
+            assert isinstance(
+                e, Py4JError
+            ), "from_java_error(): Only Py4JErrors should be handled."
+            return cls("Unexpected Py4J Error: " + str(e))
--- iceberg/bodo_iceberg_connector/table_info.py
+++ iceberg/bodo_iceberg_connector/table_info.py
@@ -6,7 +6,7 @@ Python APIs used to gather metadata information about the underlying Iceberg tab
 from bodo_iceberg_connector.catalog_conn import parse_conn_str
 from bodo_iceberg_connector.errors import IcebergJavaError
 from bodo_iceberg_connector.py4j_support import get_java_table_handler
-from py4j.protocol import Py4JJavaError
+from py4j.protocol import Py4JError
 
 
 def bodo_connector_get_current_snapshot_id(
@@ -22,7 +22,7 @@ def bodo_connector_get_current_snapshot_id(
             table,
         )
         snapshot_id = bodo_iceberg_table_reader.getSnapshotId()
-    except Py4JJavaError as e:
+    except Py4JError as e:
         raise IcebergJavaError.from_java_error(e)
 
     return int(snapshot_id)
