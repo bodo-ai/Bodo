be4e9d2062a51adf8097fad11f359a59dd56d07c
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-02-13 16:35:14 -0500

    [BE-4363] Fixing TO_BINARY & DATE_FROM_PART due to Azure Nightly failures (#4871)
    * Initial commit: refactoring TO_DATE overloads, testing WIP
    
    * Started JAVA modifications, need to add to tables
    
    * Filled out operator table
    
    * Added TO_TIMESTAMP(tz_aware) support
    
    * Added mappings to switch statements
    
    * Added / updated tests, modified tz logic
    
    * Testing adjustments
    
    * Added names to distributed set for broadcasting
    
    * Propagated argument changes
    
    * Adjusted str->date validation rules
    
    * Fixed numeric string rules
    
    * Changed testing rules for local timezone
    
    * Fixed TRY_TO_TIMESTAMP_TZ Java type rules for preserving existing timezone
    
    * Added numeric tests, adjusted scale handling at compile time
    
    * Added scale support on Java side
    
    * Scale handling on JAVA side (wip)
    
    * Fixed float type handling and scale calculation
    
    * Fixed TO_TIMESTAMP numeric tests
    
    * Fixed error message on Java side
    
    * Fixed LTZ timezone preservation rule [RUN CI]
    
    * Added documentation
    
    * Added accidentally deleted line + minor Bodo testing adjustment
    
    * CI fixes, removing some invalid CAST tests
    
    * Dummy commit [RUN CI]
    
    * Divorced TO_DATE and TO_TIMESTAMP
    
    * Revisions, removing TO_DATE integer tests
    
    * Dummy commit [RUN CI]
    
    * Minor typo [RUN CI]
    
    * Fixing optional tests from unused file [RUN CI]
    
    * Initial commit: created new tests that correctly test Snowflake rounding behavior and drafted new rounding kernel
    
    * Adjustments to tests and kernel [RUN CI]
    
    * Reverting accidental changes
    
    * Dummy commit [RUN CI]
    
    * Typo revisions
    
    * Revisions
    
    * Revert "[BE-4363] Revert TO_BINARY + DATE_FROM_PART PR's due to Azure Nightly failures (#4869)"
    
    This reverts commit 3431776936acd46667cf3ce43a6a8fd2b20f7d18.
    
    * Added the 2 nightly fixes [RUN CI]
    
    * Revisions [RUN CI]
    
    * resolving the final conflict [RUN CI]
    
    * Fixed rogue addition from PR reversion [RUN CI]
    
    * Dummy commit [RUN CI]
    
    * Dummy commit [RUN CI]
    
    * Fixed fixture [RUN CI]
    
    * Fixed fixture [RUN CI]
    
    * Re-fixing double variable declaration [RUN CI]
    
    * Including TIMEFROMPARTS [RUN CI]
    
    * Fixing error message test [RUN CI]
    
    * Re-running CI after upstream changes [RUN CI]
    
    * Final revisions [RUN CI]
    
    * Final fixes and merge conflict resolution [RUN CI]
    
    * Final fixes and merge conflict resolution [RUN CI]
    
    * Fixing the same merge conflict again
    
    * Dummy commit [RUN CI]
    
    ---------
    
    Co-authored-by: Kian Nassre <knassre@Kians-MacBook-Pro-3.local>
    Co-authored-by: Kian Nassre <knassre@kians-mbp.myfiosgateway.com>
    Co-authored-by: Kian Nassre <knassre@kians-mbp-3.wifi.local.cmu.edu>

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/BodoSQLOperatorTables/OperatorTableUtils.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/BodoSQLOperatorTables/OperatorTableUtils.java
@@ -2,6 +2,9 @@ package com.bodosql.calcite.application.BodoSQLOperatorTables;
 
 import java.util.*;
 import org.apache.calcite.rel.type.*;
+import org.apache.calcite.sql.type.OperandTypes;
+import org.apache.calcite.sql.type.SqlOperandTypeChecker;
+import org.apache.calcite.sql.type.SqlTypeFamily;
 
 public class OperatorTableUtils {
   /**
@@ -18,5 +21,28 @@ public class OperatorTableUtils {
       }
     }
     return false;
+  }
+
+  /**
+   * Creates an OperandTypeChecker for a function that has several arguments with consistent types,
+   * but some of them are optional. Calling the function with arguments (3, A, B, C, D, E) is the
+   * same as creating an OR of the families (A, B, C), (A, B, C, D), (A, B, C, D, E)
+   *
+   * @param min the minimum number of arguments required for the function call
+   * @param families the types for each argument when they are provided
+   * @return an OperandTypeChecker with the specs mentioned above
+   */
+  public static SqlOperandTypeChecker argumentRange(int min, SqlTypeFamily... families) {
+    assert min <= families.length;
+    List<SqlTypeFamily> familyList = new ArrayList<SqlTypeFamily>();
+    for (int i = 0; i < min; i++) {
+      familyList.add(families[i]);
+    }
+    SqlOperandTypeChecker rule = OperandTypes.family(familyList);
+    for (int i = min; i < families.length; i++) {
+      familyList.add(families[i]);
+      rule = OperandTypes.or(rule, OperandTypes.family(familyList));
+    }
+    return rule;
   }
 }
