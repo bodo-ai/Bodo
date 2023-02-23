65d49b6995473736ac0b92b4c37a656bdde2108b
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-02-23 10:15:10 -0600

    [BE-4279] Switch RelationalAlgebraGenerator to use withExpand = false (#4943)
    This switches the RelationalAlgebraGenerator to use withExpand = false
    and to then use the `SubQueryRemoveRule` to remove subqueries and the
    `RelDecorrelator` to remove the correlation nodes.
    
    It also adds a new planner rule that will remove RexOver nodes from join
    conditions. The `SubQueryRemoveRule` has a tendency to put these into
    join conditions when the previous code would not. Since we can't handle
    these nodes inside of join conditions, this planner rule will move it to
    a projection before the join and then remove the field after the join is
    complete.
    
    This updates the existing set of tests to work. The ultimate goal of
    this is to enable `ANY/ALL` behavior in a subsequent PR. The other
    reason is because the Calcite developers have made clean that withExpand
    = false is intended to be the default behavior and will be the option
    that's generally supported in the future.
    
    In addition, this commit removes unused test dependencies that
    interfered with the execution of the test framework through maven. The
    tests could be run through IntelliJ, but Maven didn't seem to pick them
    up because of the other test frameworks that existed but also had no
    tests defined for them.
    
    This also extends the test framework for planner rules and code
    generation to include a way to update the tests. This can be done with:
    
        UPDATE_EXPECT=1 mvn test -fae
    
    This sets an environment variable in the test framework that will write
    the actual output to the expected output files. `-fae` tells the test
    framework to continue even when a failure is encountered. The tests
    still get marked as failed because maven seems to cache successes so
    succeeding causes the tests not to run again when the environment
    variable is gone.

---
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/bodo_sql_rules/JoinExtractOverRule.java
@@ -0,0 +1,30 @@
+package com.bodosql.calcite.application.bodo_sql_rules;
+
+import org.apache.calcite.rel.logical.LogicalJoin;
+import org.apache.calcite.rex.RexOver;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.NotNull;
+
+@BodoSQLStyleImmutable
+@Value.Enclosing
+public class JoinExtractOverRule extends AbstractJoinExtractOverRule {
+  protected JoinExtractOverRule(@NotNull AbstractJoinExtractOverRule.Config config) {
+    super(config);
+  }
+
+  @Value.Immutable
+  public interface Config extends AbstractJoinExtractOverRule.Config {
+    Config DEFAULT =
+        ImmutableJoinExtractOverRule.Config.of()
+            .withOperandSupplier(
+                b ->
+                    b.operand(LogicalJoin.class)
+                        .predicate(join -> RexOver.containsOver(join.getCondition()))
+                        .anyInputs());
+
+    @Override
+    default JoinExtractOverRule toRule() {
+      return new JoinExtractOverRule(this);
+    }
+  }
+}
