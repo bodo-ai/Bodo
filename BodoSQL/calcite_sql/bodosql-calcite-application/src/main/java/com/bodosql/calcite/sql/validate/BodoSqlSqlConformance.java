35c125cd575ef9786110d43e83c83bf6f1443de6
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-03-01 15:32:03 -0600

    [BE-4454] Merge the calcite-core package with calcite-application (#4988)
    In order to simplify the build and testing, I'm merging these two
    projects. They aren't deployed separate from each other and the current
    setup just increases the chance of confusion about where to look for a
    certain file.
    
    If there's a compelling reason for us to split these in the future for
    deployment reasons, we can do that then.

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/sql/validate/BodoSqlSqlConformance.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/sql/validate/BodoSqlSqlConformance.java
@@ -7,9 +7,8 @@ package com.bodosql.calcite.sql.validate;
 import org.apache.calcite.sql.validate.SqlAbstractConformance;
 
 public class BodoSqlSqlConformance extends SqlAbstractConformance {
-	@Override
-	public boolean
-	isPercentRemainderAllowed() {
-		return true;
-	}
+  @Override
+  public boolean isPercentRemainderAllowed() {
+    return true;
+  }
 }
