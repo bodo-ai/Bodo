9699503fb8ca5015d324ea504178b95b7414d2b4
Srinivas Lade <srinivas@bodo.ai>
2023-03-02 09:52:41 -0500

    [BE-2477] Add Hive Kerberos Support (#4987)

---
+++ iceberg/bodo_iceberg_connector/java_helpers.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING, Dict
+
+from bodo_iceberg_connector.py4j_support import (
+    get_hadoop_conf_class,
+    get_system_class,
+)
+
+if TYPE_CHECKING:
+    from py4j.java_gateway import JavaObject
+
+
+def set_system_property(key: str, value: str):
+    """Run Java's `System.setProperty` Function on the Internal Java Process"""
+    System = get_system_class()
+    System.setProperty(key, value)  # type: ignore
+
+
+def build_hadoop_conf(d: Dict[str, str]):
+    """Build a Hadoop Configuration Object on the Internal Java Process"""
+    Configuration = get_hadoop_conf_class()
+    conf: "JavaObject" = Configuration()  # type: ignore
+    for k, v in d.items():
+        conf.set(k, v)  # type: ignore
+
+    return conf
