ed8bdab4969ee2f1d68317460f9ae76e6b066f40
Jeff Tan <43154019+jefftan969@users.noreply.github.com>
2023-03-19 02:54:40 -0400

    [BE-4434] Support intersect all (#5022)
    * [run ci] fix intersect nullable column renaming typo
    
    * [run ci] revert intersect all change
    
    * [run ci] updated tests, correct implementation depending on cumcount()
    
    * [run ci] update comment
    
    * fix parametrize [run ci]
    
    * working impl of intersect all with cumsum [run ci]
    
    * [run ci] fix dropna bug
    
    * [run ci] clarify nan behavior
    
    * [run ci] always check na
    
    * [run ci]
    
    * [run ci] update intersect all codegen to avoid updating df's in place
    
    * [run ci] fix impl bugs
    
    * [run ci] merge ehsan fix from develop
    
    * [run ci] update comment

--- BodoSQL/bodosql/tests/test_set_ops.py
+++ BodoSQL/bodosql/tests/test_set_ops.py
@@ -24,9 +24,7 @@ def union_cmds(request):
     params=[
         "INTERSECT",
         pytest.param("INTERSECT DISTINCT", marks=pytest.mark.slow),
-        pytest.param(
-            "INTERSECT ALL", marks=pytest.mark.skip("[BE-4134] Support INTERSECT ALL")
-        ),
+        "INTERSECT ALL",
     ]
 )
 def intersect_cmds(request):
@@ -53,6 +51,25 @@ def null_set_df():
     return {"table1": pd.DataFrame(data=int_data, dtype="Int64")}
 
 
+@pytest.fixture(
+    params=[
+        {
+            # What Spark uses to demonstrate set operators:
+            # https://spark.apache.org/docs/latest/sql-ref-syntax-qry-select-setops.html
+            "table1": pd.DataFrame({"A": [3, 1, 2, 2, 3, 4]}),
+            "table2": pd.DataFrame({"B": [5, 1, 2, 2]}),
+        },
+        {
+            # Testing how duplicates are handled
+            "table1": pd.DataFrame({"A": [1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5]}),
+            "table2": pd.DataFrame({"B": [1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 6]}),
+        },
+    ]
+)
+def set_ops_dfs(request):
+    return request.param
+
+
 def make_tz_aware_df(tz):
     """
     Make a dataframe of tz-aware timestamps with the given timezone.
@@ -67,12 +84,12 @@ def make_tz_aware_df(tz):
     return pd.DataFrame(data=tz_aware_data)
 
 
-def test_union_numeric_cols(basic_df, union_cmds, spark_info, memory_leak_check):
+def test_union_numeric_cols(set_ops_dfs, union_cmds, spark_info, memory_leak_check):
     """
     Test that UNION [ALL/DISTINCT] works for a single numeric column
     """
-    query = f"SELECT A FROM table1 {union_cmds} SELECT B FROM table1 {union_cmds} SELECT C FROM table1"
-    check_query(query, basic_df, spark_info, check_dtype=False)
+    query = f"SELECT A FROM table1 {union_cmds} SELECT B FROM table2"
+    check_query(query, set_ops_dfs, spark_info, check_dtype=False)
 
 
 def test_union_nullable_numeric_many_cols(
@@ -141,8 +158,15 @@ def test_union_tz_aware_cols(union_cmds, representative_tz, memory_leak_check):
     """
     df = make_tz_aware_df(representative_tz)
     py_output = pd.DataFrame({"A": pd.concat((df["A"], df["B"]))})
-    if "ALL" not in union_cmds:  # Drop duplicates for UNION, UNION DISTINCT
+    if "ALL" in union_cmds:
+        # For UNION ALL, number of NA's should be A_na + B_na
+        num_na = df["A"].isna().sum() + df["B"].isna().sum()
+        py_output = py_output.dropna()
+        py_output = pd.DataFrame({"A": list(py_output["A"]) + [None] * num_na})
+    else:
+        # Drop duplicates for UNION [DISTINCT]
         py_output = py_output.drop_duplicates()
+
     ctx = {"table1": df}
     query = f"SELECT A FROM table1 {union_cmds} SELECT B FROM table1"
     check_query(query, ctx, None, expected_output=py_output)
@@ -150,19 +174,19 @@ def test_union_tz_aware_cols(union_cmds, representative_tz, memory_leak_check):
 
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "query,unknown1,unknown2",
+    "query,col1,col2",
     [
         # [BS-381] These queries are not valid for spark
         ("SELECT 1,2 UNION SELECT 1,2", [1], [2]),
-        ("SELECT 1,2 UNION SELECT 1,3", [1, 1], [2, 3]),
+        ("SELECT 1,2 UNION DISTINCT SELECT 1,3", [1, 1], [2, 3]),
         ("SELECT 1,2 UNION ALL SELECT 1,2", [1, 1], [2, 2]),
     ],
 )
-def test_union_scalars(query, unknown1, unknown2, memory_leak_check):
+def test_union_scalars(query, col1, col2, memory_leak_check):
     """
     Tests that UNION [ALL/DISTINCT] works for scalars
     """
-    py_output = pd.DataFrame({"unknown1": unknown1, "unknown2": unknown2})
+    py_output = pd.DataFrame({"col1": col1, "col2": col2})
     check_query(
         query,
         dict(),
@@ -174,13 +198,13 @@ def test_union_scalars(query, unknown1, unknown2, memory_leak_check):
 
 
 def test_intersect_numeric_cols(
-    basic_df, intersect_cmds, spark_info, memory_leak_check
+    set_ops_dfs, intersect_cmds, spark_info, memory_leak_check
 ):
     """
     Test that INTERSECT [ALL/DISTINCT] works for a single numeric column
     """
-    query = f"SELECT A FROM table1 {intersect_cmds} SELECT B FROM table1 {intersect_cmds} SELECT C FROM table1"
-    check_query(query, basic_df, spark_info, check_dtype=False)
+    query = f"SELECT A FROM table1 {intersect_cmds} SELECT B FROM table2"
+    check_query(query, set_ops_dfs, spark_info, check_dtype=False)
 
 
 def test_intersect_nullable_numeric_many_cols(
@@ -225,15 +249,14 @@ def test_intersect_tz_aware_cols(intersect_cmds, representative_tz, memory_leak_
     """
     df = make_tz_aware_df(representative_tz)
     py_output = df[["A"]].merge(df[["B"]].rename(columns={"B": "A"}), on="A")
-
-    # If there is 1 NA in the output it should exist.
-    # TODO: Double check intersect null behavior with Snowflake.
-    na_entries = py_output["A"].isna()
-    # If there is 1 NA in the output it should exist.
-    append_na = py_output["A"].isna().any()
-    py_output = py_output[~na_entries]
-    if append_na:
-        py_output = pd.DataFrame({"A": list(py_output["A"]) + [None]})
+    if "ALL" in intersect_cmds:
+        # For INTERSECT ALL, number of NA's should be min(A_na, B_na)
+        num_na = min(df["A"].isna().sum(), df["B"].isna().sum())
+        py_output = py_output.dropna()
+        py_output = pd.DataFrame({"A": list(py_output["A"]) + [None] * num_na})
+    else:
+        # Drop duplicates for INTERSECT [DISTINCT]
+        py_output = py_output.drop_duplicates()
 
     ctx = {"table1": df}
     query = f"SELECT A FROM table1 {intersect_cmds} SELECT B FROM table1"
@@ -242,24 +265,28 @@ def test_intersect_tz_aware_cols(intersect_cmds, representative_tz, memory_leak_
 
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "query,unknown1,unknown2",
+    "query,col1,col2",
     [
         # [BS-381] These queries are not valid for spark
-        ("SELECT 1,2 INTERSECT SELECT 2,3", [], []),
         ("SELECT 1,2 INTERSECT SELECT 1,2", [1], [2]),
+        ("SELECT 1,2 INTERSECT DISTINCT SELECT 2,3", [], []),
         pytest.param(
             "SELECT 1,2 INTERSECT ALL SELECT 1,2",
+            [1],
+            [2],
+        ),
+        (
+            "(SELECT 1,2 UNION ALL SELECT 1,2) INTERSECT ALL (SELECT 1,2 UNION ALL SELECT 1,2)",
             [1, 1],
             [2, 2],
-            marks=pytest.mark.skip("[BE-4134] Support intersect all"),
         ),
     ],
 )
-def test_intersect_scalars(query, unknown1, unknown2, memory_leak_check):
+def test_intersect_scalars(query, col1, col2, memory_leak_check):
     """
     Test that INTERSECT [ALL/DISTINCT] works for scalars
     """
-    py_output = pd.DataFrame({"unknown1": unknown1, "unknown2": unknown2})
+    py_output = pd.DataFrame({"col1": col1, "col2": col2})
     check_query(
         query,
         dict(),
@@ -270,12 +297,12 @@ def test_intersect_scalars(query, unknown1, unknown2, memory_leak_check):
     )
 
 
-def test_except_cols(basic_df, except_cmds, spark_info, memory_leak_check):
+def test_except_cols(set_ops_dfs, except_cmds, spark_info, memory_leak_check):
     """
     Test that EXCEPT/MINUS works for columns
     """
-    query = f"SELECT A, B FROM table1 {except_cmds} SELECT A, C FROM table1"
-    check_query(query, basic_df, spark_info, only_python=True)
+    query = f"SELECT A FROM table1 {except_cmds} SELECT B FROM table2"
+    check_query(query, set_ops_dfs, spark_info, only_python=True)
 
 
 def test_except_string_cols(
@@ -315,18 +342,18 @@ def test_except_tz_aware_cols(except_cmds, representative_tz, memory_leak_check)
 @pytest.mark.slow
 @pytest.mark.skip("[BS-379] Except not supported")
 @pytest.mark.parametrize(
-    "query,unknown1,unknown2",
+    "query,col1,col2",
     [
         # [BS-381] These queries are not valid for spark
         ("SELECT 1,2 EXCEPT SELECT 2,3", [1], [2]),
         ("SELECT 1,2 EXCEPT SELECT 1,2", [], []),
     ],
 )
-def test_except_scalars(query, unknown1, unknown2, memory_leak_check):
+def test_except_scalars(query, col1, col2, memory_leak_check):
     """
     Tests that EXCEPT works for scalars
     """
-    py_output = pd.DataFrame({"unknown1": unknown1, "unknown2": unknown2})
+    py_output = pd.DataFrame({"col1": col1, "col2": col2})
     check_query(
         query,
         dict(),
