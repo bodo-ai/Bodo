ddfd41c11f84834e8eb5ab961c2150949508096f
Alice Lai <alice@bodo.ai>
2023-04-05 12:28:26 -0400

    [BE-4441] Support string transforms (except for CONCAT_WS) for filter pushdown (#5070)

--- bodo/ir/filter.py
+++ bodo/ir/filter.py
@@ -1,116 +1,102 @@
 """
 This file contains dictionaries mapping BodoSQL kernel name to 
-corresponding Arrow compute functions.
+corresponding SQL functions. This file also contains
+supported_arrow_funcs_map, which is a dictionary that maps
+BodoSQL kernel name to an equivalent PyArrow compute function.
 
 Dictionaries are separated by category 
 (string functions, datetime functions, etc.) and 
 number of arguments.
 
 The keys are the name of the BodoSQL kernel.
-The values are tuple pairs of the
-    - Snowflake SQL Function Name
-    - PyArrow Compute function 
 """
-from typing import Dict, Tuple, Union
+from typing import Tuple, Union
 
 from numba.core import ir
 
-
-def use_same_name_sql_func_arrow_func(x: Dict[str, str]):
-    return {k: (v, v) for k, v in x.items()}
-
-
-def use_same_name_kernel_sql_func(x: Dict[str, str]):
-    return {k: (k, v) for k, v in x.items()}
-
-
-date_funcs_no_arg_map: Dict[str, Tuple[str, str]] = {
-    "get_hour": ("HOUR", "hour"),
-    "get_minute": ("MINUTE", "minute"),
-    "get_second": ("SECOND", "second"),
-    "get_year": ("YEAR", "year"),
-    # TODO: YEAROFWEEK seems to map to get_year, but I think thats wrong (no account for weeks that start in previous year)
-    "yearofweekiso": ("YEAROFWEEKISO", "iso_year"),
-    "dayofmonth": ("DAY", "day"),
-    "dayofweek": ("DAYOFWEEK", "day_of_week"),
-    "dayofweekiso": ("DAYOFWEEKISO", "day_of_week"),
-    "dayofyear": ("DAYOFYEAR", "day_of_year"),
-    # TODO: Why are there 2 different ones?
-    "get_week": ("WEEK", "week"),
-    "get_weekofyear": ("WEEKOFYEAR", "week"),
-    # TODO: WEEKISO seems to map to get_weekofyear, but I think thats wrong (non ISO version)
-    "get_month": ("MONTH", "month"),
-    "get_quarter": ("QUARTER", "quarter"),
+string_funcs_no_arg_map = {
+    "lower": "LOWER",
+    "upper": "UPPER",
+    "length": "LENGTH",
+    "reverse": "REVERSE",
 }
 
-numeric_funcs_no_arg_map = use_same_name_sql_func_arrow_func(
-    {
-        "abs": "abs",
-        "sign": "sign",
-        "ceil": "ceil",
-        "floor": "floor",
-    }
-)
+numeric_funcs_no_arg_map = {
+    "abs": "ABS",
+    "sign": "SIGN",
+    "ceil": "CEIL",
+    "floor": "FLOOR",
+}
 
-numeric_funcs_one_arg_map = use_same_name_sql_func_arrow_func(
-    {
-        "mod": "mod",
-        "round": "round",
-        "truncate": "trunc",
-        "trunc": "trunc",
-    }
-)
+date_funcs_no_arg_map = {
+    "get_hour": "HOUR",
+    "get_minute": "MINUTE",
+    "get_second": "SECOND",
+    # TODO (srilman): YEAROFWEEK seems to map to get_year, but I think thats wrong (no account for weeks that start in previous year)
+    "get_year": "YEAR",
+    "yearofweekiso": "YEAROFWEEKISO",
+    "dayofmonth": "DAY",
+    "dayofweek": "DAYOFWEEK",
+    "dayofweekiso": "DAYOFWEEKISO",
+    "dayofyear": "DAYOFYEAR",
+    # TODO (srilman): Why are there 2 different ones?
+    "get_week": "WEEK",
+    "get_weekofyear": "WEEKOFYEAR",
+    # TODO (srilman): WEEKISO seems to map to get_weekofyear, but I think thats wrong (non ISO version)
+    "get_month": "MONTH",
+    "get_quarter": "QUARTER",
+}
 
-# SQL functions that only operate over a single input column
-string_funcs_no_arg_map = use_same_name_kernel_sql_func(
-    {
-        "lower": "utf8_lower",
-        "upper": "utf8_upper",
-        "length": "utf8_length",
-        "reverse": "utf8_reverse",
-        "ltrim": "utf8_ltrim_whitespace",
-        "rtrim": "utf8_rtrim_whitespace",
-        "trim": "utf8_trim_whitespace",
-        "initcap": "utf8_capitalize",
-        "reverse": "utf8_reverse",
-    }
-)
+string_funcs_map = {
+    "ltrim": "LTRIM",
+    "rtrim": "RTRIM",
+    "lpad": "LPAD",
+    "rpad": "RPAD",
+    "trim": "TRIM",
+    "split": "SPLIT_PART",
+    "contains": "CONTAINS",
+    "coalesce": "COALESCE",
+    "repeat": "REPEAT",
+    "translate": "TRANSLATE",
+    "strtok": "STRTOK",
+    "split": "SPLIT_PART",
+    "initcap": "INITCAP",
+    "concat_ws": "CONCAT",
+}
 
-# SQL functions that take in 1 additional argument
-string_funcs_one_arg_map = use_same_name_kernel_sql_func(
-    {
-        "lpad": "utf8_lpad",
-        "rpad": "utf8_rpad",
-        "ltrim": "utf8_rtrim",
-        "rtrim": "utf8_rtrim",
-        "trim": "utf8_trim",
-        "split": "split_pattern",
-        "startswith": "starts_with",
-        "endswith": "ends_with",
-        "contains": "match_substring",
-        "coalesce": "coalesce",
-        "case_insensitive_startswith": "starts_with",
-        "case_insensitive_endswith": "ends_with",
-        "case_insensitive_contains": "match_substring",
-    }
-)
+numeric_funcs_map = {
+    "mod": "MOD",
+    "round": "ROUND",
+    "trunc": "TRUNC",
+    "truncate": "TRUNCATE",
+}
 
-
-# TODO: Replace with | operation once upgrading to Python 3.9
+# TODO (srilman): Replace with | operation once upgrading to Python 3.9
 supported_funcs_no_arg_map = {
     **string_funcs_no_arg_map,
     **numeric_funcs_no_arg_map,
     **date_funcs_no_arg_map,
 }
 
-supported_compute_funcs = list(
-    string_funcs_one_arg_map.keys()
-    | string_funcs_no_arg_map.keys()
-    | numeric_funcs_no_arg_map.keys()
-    | numeric_funcs_one_arg_map.keys()
-    | date_funcs_no_arg_map.keys()
-)
+supported_funcs_map = {
+    **supported_funcs_no_arg_map,
+    **numeric_funcs_map,
+    **string_funcs_map,
+}
 
-
+supported_arrow_funcs_map = {
+    "lower": "utf8_lower",
+    "upper": "utf8_upper",
+    "length": "utf8_length",
+    "reverse": "utf8_reverse",
+    "startswith": "starts_with",
+    "endswith": "ends_with",
+    "contains": "match_substring",
+    "coalesce": "coalesce",
+    "case_insensitive_startswith": "starts_with",
+    "case_insensitive_endswith": "ends_with",
+    "case_insensitive_contains": "match_substring",
+    "initcap": "utf8_capitalize",
+}
 # Typing Aliases
 Filter = Tuple[Union[str, "Filter"], str, Union[ir.Var, str]]
