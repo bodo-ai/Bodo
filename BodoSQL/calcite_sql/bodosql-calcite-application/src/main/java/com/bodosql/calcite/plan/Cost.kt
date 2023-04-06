86699e94abc463e152937391a75158533c0754c0
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-04-06 18:27:17 -0500

    [BE-4534] Add VolcanoPlanner as an option and include output in tests (#5174)
    This change introduces a new program for using the volcano planner as
    the sole optimizer. This is only included for unit tests and outputs its
    own file for testing purposes.
    
    In addition to the volcano planner, a custom cost structure has been
    included. The custom cost structure includes values for cpu, io, and
    memory and compares values based on a calculation utilizing these
    values. The output for both optimized versions of the plan now uses the
    new cost structure for comparison purposes.
    
    As an initial test of using the metadata providers, a custom provider
    has been added for pandas nodes that inserts the number of ranks as part
    of the parallelism attribute. This allows nodes with `PandasRel` to know
    how many ranks are available for the purposes of cost generation. In
    addition, the ability to override this value is included as part of the
    `PandasRel` interface. The actual number of ranks isn't passed through
    as it just defaults to 2 at the current moment.
    
    The output for the optimized plans has been changed to both include
    costs and include ids when there are duplicate nodes with the same id.
    The ids have been normalized to reduce churn from changing plans. Only
    ids for nodes with duplicates are included because we don't want a
    change in the plan in one place to cause all of the ids to change and
    make the diff unreadable. Similarly, costs are also per operation rather
    than Calcite's default of a cumulative operation. The cumulative
    operation means that every operation above one operation would change if
    we modified the cost structure even if the overall plan didn't change.
    This change prevents that from changing the diff.
    
    No other relational operators have been modified to give a custom cost.
    That will be a future issue and project.

---
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/plan/Cost.kt
@@ -0,0 +1,152 @@
+package com.bodosql.calcite.plan
+
+import org.apache.calcite.plan.RelOptCost
+import org.apache.calcite.plan.RelOptPlanner
+import org.apache.calcite.plan.RelOptUtil
+import java.text.DecimalFormat
+import kotlin.math.abs
+
+/**
+ * Represents the cost of a relational operation.
+ *
+ * The cost is composed of the cpu, io, and mem used to complete an operation.
+ * If a resource is divided between multiple processing units, the cost would be
+ * the usage of each processing unit. As an example, a parallel operation
+ * might be divided by the number of ranks it can effectively utilize so an operation
+ * that uses two ranks effectively costs half for the same operation that uses
+ * one rank.
+ *
+ * The row count for an operation is included as part of the cost, but is
+ * not used for cost comparisons.
+ */
+class Cost private constructor(private val rows: Double, private val cpu: Double, private val io: Double, val mem: Double, cost: Double?) : RelOptCost {
+
+    /**
+     * Initializes a cost with the given resources.
+     *
+     * @param rows the number of rows produced by this operation.
+     * @param cpu amount of cpu resource utilized by this operation.
+     * @param io amount of io resource utilized by this operation.
+     * @param mem amount of mem resource utilized by this operation.
+     */
+    constructor(rows: Double = 0.0, cpu: Double = 0.0, io: Double = 0.0, mem: Double = 0.0) : this(rows, cpu, io, mem, null)
+
+    /**
+     * The cost represents the single numeric value that determines
+     * how expensive this operation is.
+     *
+     * While costs include information about rows, cpu, io, etc
+     * for debugging, the cost is the only thing used for comparisons.
+     *
+     * We can use whichever method we find most appropriate for this
+     * cost calculation. For now, I'm just equally dividing it between
+     * the cpu, io, and memory components. Row count is not considered.
+     * This is because row count is mostly represented in the other
+     * metrics.
+     */
+    val value = cost ?: (cpu + io + mem)
+
+    override fun equals(other: RelOptCost): Boolean {
+        return if (other is Cost) {
+            return rows == other.rows &&
+                    cpu == other.cpu &&
+                    io == other.io &&
+                    mem == other.mem
+        } else false
+    }
+
+    override fun getRows(): Double = rows
+
+    override fun getCpu(): Double = cpu
+
+    override fun getIo(): Double = io
+
+    override fun isInfinite(): Boolean = value.isInfinite()
+
+    override fun isEqWithEpsilon(other: RelOptCost): Boolean = isEqWithEpsilon(convert(other))
+
+    private fun isEqWithEpsilon(other: Cost): Boolean = abs(other.value - this.value) < RelOptUtil.EPSILON
+
+    override fun isLe(other: RelOptCost): Boolean = isLe(convert(other))
+
+    private fun isLe(other: Cost): Boolean = this.value <= other.value
+
+    override fun isLt(other: RelOptCost): Boolean = isLt(convert(other))
+
+    private fun isLt(other: Cost): Boolean = this.value < other.value
+
+    override fun plus(other: RelOptCost): RelOptCost {
+        return convert(other).let { c ->
+            Cost(
+                rows = rows + c.rows,
+                cpu = cpu + c.cpu,
+                io = io + c.io,
+                mem = mem + c.mem,
+                cost = value + c.value,
+            )
+        }
+    }
+
+    override fun minus(other: RelOptCost): RelOptCost {
+        return convert(other).let { c ->
+            Cost(
+                rows = rows + c.rows,
+                cpu = cpu + c.cpu,
+                io = io + c.io,
+                mem = mem + c.mem,
+                cost = value + c.value,
+            )
+        }
+    }
+
+    override fun multiplyBy(factor: Double): RelOptCost {
+        return Cost(
+            rows = rows * factor,
+            cpu = cpu * factor,
+            io = io * factor,
+            mem = mem * factor,
+            cost = value * factor,
+        )
+    }
+
+    override fun divideBy(other: RelOptCost): Double {
+        return convert(other).let { c -> value / c.value }
+    }
+
+    override fun toString(): String = "{${df(rows)} rows, ${df(cpu)} cpu, ${df(io)} io, ${df(mem)} mem}".format(rows, cpu, io, mem)
+
+    companion object {
+        private val DECIMAL_FORMAT = DecimalFormat("#.###")
+
+        private fun df(decimal: Double): String = DECIMAL_FORMAT.format(decimal)
+
+        @JvmField
+        val INFINITY: Cost = Cost(0.0, 0.0, 0.0, 0.0, Double.POSITIVE_INFINITY)
+
+        private fun convert(cost: RelOptCost): Cost = if (cost is Cost)
+            cost
+        else
+            Cost(cost.rows, cost.cpu, cost.io, 0.0)
+    }
+}
+
+/**
+ * Convenience method for producing a cost.
+ *
+ * This function will determine if the cost factory supports the extra parameters such as memory
+ * and will use those if available. Otherwise, it will pass the row count, cpu, and io as they are
+ * and ignore the other parameters.
+ *
+ * All parameters except row count are optional.
+ *
+ * @param rows the number of rows produced by this operation.
+ * @param cpu amount of cpu resource utilized by this operation.
+ * @param io amount of io resource utilized by this operation.
+ * @param mem amount of mem resource utilized by this operation.
+ */
+fun RelOptPlanner.makeCost(rows: Double, cpu: Double = 0.0, io: Double = 0.0, mem: Double = 0.0): RelOptCost {
+    return when (val factory = costFactory) {
+        is CostFactory -> factory.makeCost(rows, cpu, io, mem)
+        else -> factory.makeCost(rows, cpu, io)
+    }
+}
