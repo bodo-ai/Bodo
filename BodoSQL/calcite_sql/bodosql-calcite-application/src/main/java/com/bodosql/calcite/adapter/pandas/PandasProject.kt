c761657559e68bfbc010e035b0a311c430cd6e89
Nick Riasanovsky <nick@bodo.ai>
2023-04-12 10:21:05 -0400

    [BE-4679] Fix collation requirements for PandasProject (#5218)
    * trying to convert the output convention
    
    * created an example bad test
    
    * revert other changes
    
    * fixed the insert into example, need to fix other places in the code [run CI]

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasProject.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasProject.kt
@@ -5,8 +5,10 @@ import com.bodosql.calcite.ir.Module
 import com.google.common.collect.ImmutableList
 import org.apache.calcite.plan.RelOptCluster
 import org.apache.calcite.plan.RelTraitSet
+import org.apache.calcite.rel.RelCollationTraitDef
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.core.Project
+import org.apache.calcite.rel.metadata.RelMdCollation
 import org.apache.calcite.rel.type.RelDataType
 import org.apache.calcite.rex.RexNode
 
@@ -33,7 +35,11 @@ class PandasProject(
     companion object {
         fun create(input: RelNode, projects: List<RexNode>, rowType: RelDataType): PandasProject {
             val cluster = input.cluster
+            val mq = cluster.metadataQuery
             val traitSet = cluster.traitSet().replace(PandasRel.CONVENTION)
+                .replaceIfs(
+                    RelCollationTraitDef.INSTANCE,
+                    {RelMdCollation.project(mq, input, projects)})
             return PandasProject(cluster, traitSet, input, projects, rowType)
         }
     }
