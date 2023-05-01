51cde955e5518f34f59eaa6ba719be951c6db8a8
Nick Riasanovsky <nick@bodo.ai>
2023-05-01 19:52:47 -0400

    [BSE-166] Add computeSelfCost for PandasFilter (#5289)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasFilter.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasFilter.kt
@@ -2,12 +2,17 @@ package com.bodosql.calcite.adapter.pandas
 
 import com.bodosql.calcite.ir.Dataframe
 import com.bodosql.calcite.ir.Module
+import com.bodosql.calcite.plan.Cost
+import com.bodosql.calcite.plan.makeCost
 import org.apache.calcite.plan.RelOptCluster
+import org.apache.calcite.plan.RelOptCost
+import org.apache.calcite.plan.RelOptPlanner
 import org.apache.calcite.plan.RelTraitSet
 import org.apache.calcite.rel.RelCollationTraitDef
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.core.Filter
 import org.apache.calcite.rel.metadata.RelMdCollation
+import org.apache.calcite.rel.metadata.RelMetadataQuery
 import org.apache.calcite.rex.RexNode
 
 class PandasFilter(
@@ -27,6 +32,20 @@ class PandasFilter(
 
     override fun emit(builder: Module.Builder, inputs: () -> List<Dataframe>): Dataframe {
         TODO("Not yet implemented")
+    }
+
+    override fun computeSelfCost(planner: RelOptPlanner, mq: RelMetadataQuery): RelOptCost {
+        val inputRows = mq.getRowCount(input)
+        // Filter cost consists of two parts. The cost to compute the actual filter
+        // and the cost to filter each column.
+        val conditionCost = condition.accept(RexCostEstimator).multiplyBy(inputRows)
+        // Compute the expected number of output rows
+        val outputRows = mq.getRowCount(this)
+        // We will assume the CPU cost to apply the boolean filter to each row is 0
+        // and just consider the memory allocation of the new output.
+        val filterCost = Cost(mem = mq.getAverageRowSize(this) ?: 0.0) .multiplyBy(outputRows)
+        val totalCost = (conditionCost + filterCost) as Cost
+        return planner.makeCost(rows = outputRows, from = totalCost)
     }
 
     companion object {
