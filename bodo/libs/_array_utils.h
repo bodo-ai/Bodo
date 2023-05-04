bodo-ai/Bodo

commit 2bf8231fdbe6a59383164a90d88a5e3c43cb63fc

[BE-4664] Replace manual decrefs and raw pointers in C++ with smart pointers (#5219)
* avoid incref in array_to_info

* replace meminfos with buffers

* move methods to cpp file

* update alloc functions

* replace array_info* with shared_ptr in arrow_reader

* replace array_info* with shared_ptr in parquet_reader

* replace array_info* with shared_ptr in _array_operations.cpp

* replace array_info* with shared_ptr in _array_operations.h

* replace array_info* with shared_ptr in _array_utils.h

* replace array_info* with shared_ptr in _array_utils.cpp

* replace array_info* with shared_ptr in iceberg_transforms.cpp

* replace array_info* with shared_ptr in _shuffle.cpp

* replace array_info* with shared_ptr in bodo_to_arrow

* remove unnecessary std::move() calls

* update array to info functions

* update info to array wrappers

* update struct array child indexing

* refactor get_child_info

* update string_array_from_sequence

* update str functions

* replace array_info* with shared_ptr in _shuffle.h

* replace array_info* with shared_ptr in _array_hash.h/cpp

* make to_arrow() a function

* avoid deleting C++ input tables in Python

* replace delete_table_decref_arrays with delete_table in Python

* convert remaining array_info* to shared_ptr

* use shared_ptr for comm_info

* fix shuffle_info leak

* use shared_ptr for table_info

* replace new table_info

* remove manual incref/decref in iceberg write

* remove C++ decref calls

* remove decref/incref/delete from _join.cpp

* remove decref/incref in _shuffle.cpp

* remove decref/delete from iceberg_transforms.cpp

* delete incref_array uses

* remove delete_info_decref_array

* delete delete_table()

* remove delete_table_decref_arrays

* remove more deletes

* update delete_info_decref_array in python

* fix delete_table

* delete unused functions

* fix compilation errors

* fix more compilation errors

bodo/libs/_array_utils.h
@@ -125,7 +125,8 @@ concept NullSentinelDtype = ((DType == Bodo_CTypes::FLOAT32) ||
 
 // ------------------------------------------------
 
-inline void CheckEqualityArrayType(array_info* arr1, array_info* arr2) {
+inline void CheckEqualityArrayType(std::shared_ptr<array_info> arr1,
+                                   std::shared_ptr<array_info> arr2) {
     if (arr1->arr_type != arr2->arr_type) {
         throw std::runtime_error(
             "array_info passed to Cpp code have different arr_type");
@@ -213,22 +214,23 @@ inline double GetDoubleEntry(Bodo_CTypes::CTypeEnum dtype, char* ptr) {
  * @param long_write_idxs is the vector of indices in the long table
  * @return one column of the table output.
  */
-array_info* RetrieveArray_TwoColumns(
-    array_info* const& arr1, array_info* const& arr2,
+std::shared_ptr<array_info> RetrieveArray_TwoColumns(
+    std::shared_ptr<array_info> const& arr1,
+    std::shared_ptr<array_info> const& arr2,
     std::vector<int64_t> const& short_write_idxs,
     std::vector<int64_t> const& long_write_idxs);
 
 /** This function returns the column with the rows with the rows given in
  * "ListIdx"
  *
- * @param array_info* : the input pointer
+ * @param std::shared_ptr<array_info> : the input pointer
  * @param ListIdx is the vector of list of rows selected
  * @param use_nullable_arr use nullable int/float output for Numpy array input
  * @return one array
  */
-array_info* RetrieveArray_SingleColumn(array_info* in_arr,
-                                       std::vector<int64_t> const& ListIdx,
-                                       bool use_nullable_arr = false);
+std::shared_ptr<array_info> RetrieveArray_SingleColumn(
+    std::shared_ptr<array_info> in_arr, std::vector<int64_t> const& ListIdx,
+    bool use_nullable_arr = false);
 
 /** This function uses the combinatorial information computed in the
  * "ListIdx" array and return the column with the selected rows.
@@ -238,9 +240,9 @@ array_info* RetrieveArray_SingleColumn(array_info* in_arr,
  * @param use_nullable_arr use nullable int/float output for Numpy array input
  * @return one array
  */
-array_info* RetrieveArray_SingleColumn_arr(array_info* in_arr,
-                                           array_info* idx_arr,
-                                           bool use_nullable_arr = false);
+std::shared_ptr<array_info> RetrieveArray_SingleColumn_arr(
+    std::shared_ptr<array_info> in_arr, std::shared_ptr<array_info> idx_arr,
+    bool use_nullable_arr = false);
 
 /** This function takes a table, a list of rows and returns the rows obtained
  * by selecting the rows.
@@ -251,9 +253,9 @@ array_info* RetrieveArray_SingleColumn_arr(array_info* in_arr,
  * selected
  * @return the table output.
  */
-table_info* RetrieveTable(table_info* const& in_table,
-                          std::vector<int64_t> const& ListIdx,
-                          int const& n_cols_arg = -1);
+std::shared_ptr<table_info> RetrieveTable(
+    std::shared_ptr<table_info> const& in_table,
+    std::vector<int64_t> const& ListIdx, int const& n_cols_arg = -1);
 
 /**
  * @brief select rows and columns in input table specified by list of indices
@@ -262,11 +264,11 @@ table_info* RetrieveTable(table_info* const& in_table,
  * @param in_table input table to select from
  * @param rowInds list of row indices to select
  * @param colInds list of column indices to select
- * @return table_info* output table with selected rows/columns
+ * @return std::shared_ptr<table_info> output table with selected rows/columns
  */
-table_info* RetrieveTable(table_info* const& in_table,
-                          std::vector<int64_t> const& rowInds,
-                          std::vector<size_t> const& colInds);
+std::shared_ptr<table_info> RetrieveTable(
+    std::shared_ptr<table_info> const& in_table,
+    std::vector<int64_t> const& rowInds, std::vector<size_t> const& colInds);
 
 /** This code test if two keys are equal (Before that the hash should have been
  * used) It is used that way because we assume that the left key have the same
@@ -280,8 +282,9 @@ table_info* RetrieveTable(table_info* const& in_table,
  * @param is_na_equal should na values be considered equal
  * @return True if they are equal and false otherwise.
  */
-bool TestEqualColumn(const array_info* arr1, int64_t pos1,
-                     const array_info* arr2, int64_t pos2, bool is_na_equal);
+bool TestEqualColumn(const std::shared_ptr<array_info> arr1, int64_t pos1,
+                     const std::shared_ptr<array_info> arr2, int64_t pos2,
+                     bool is_na_equal);
 
 /* This function test if two rows of two arrow columns (which may or may not be
  * the same) are equal, greater or lower than the other.
@@ -315,7 +318,7 @@ int ComparisonArrowColumn(std::shared_ptr<arrow::Array> const& arr1,
  * @param iRow2 the row of the second key
  * @return True if they are equal and false otherwise.
  */
-inline bool TestEqual(std::vector<array_info*> const& columns,
+inline bool TestEqual(std::vector<std::shared_ptr<array_info>> const& columns,
                       size_t const& n_key, size_t const& shift_key1,
                       size_t const& iRow1, size_t const& shift_key2,
                       size_t const& iRow2) {
@@ -345,7 +348,8 @@ inline bool TestEqual(std::vector<array_info*> const& columns,
  * @param is_na_equal Are NA values considered equal
  * @return True if they are equal and false otherwise.
  */
-inline bool TestEqualJoin(const table_info* table1, const table_info* table2,
+inline bool TestEqualJoin(const std::shared_ptr<table_info> table1,
+                          const std::shared_ptr<table_info> table2,
                           size_t const& iRow1, size_t const& iRow2,
                           size_t const& n_key, bool is_na_equal) {
     // iteration over the list of key for the comparison.
@@ -662,15 +666,17 @@ inline int NumericComparison(Bodo_CTypes::CTypeEnum const& dtype, char* ptr1,
  * largest, if false smallest.
  * @return true if (shift_key1,iRow1) < (shift_key2,iRow2) , false otherwise
  */
-bool KeyComparisonAsPython(size_t const& n_key, int64_t* vect_ascending,
-                           std::vector<array_info*> const& columns1,
-                           size_t const& shift_key1, size_t const& iRow1,
-                           std::vector<array_info*> const& columns2,
-                           size_t const& shift_key2, size_t const& iRow2,
-                           int64_t* na_position);
+bool KeyComparisonAsPython(
+    size_t const& n_key, int64_t* vect_ascending,
+    std::vector<std::shared_ptr<array_info>> const& columns1,
+    size_t const& shift_key1, size_t const& iRow1,
+    std::vector<std::shared_ptr<array_info>> const& columns2,
+    size_t const& shift_key2, size_t const& iRow2, int64_t* na_position);
 
-int KeyComparisonAsPython_Column(bool const& na_position_bis, array_info* arr1,
-                                 size_t const& iRow1, array_info* arr2,
+int KeyComparisonAsPython_Column(bool const& na_position_bis,
+                                 std::shared_ptr<array_info> arr1,
+                                 size_t const& iRow1,
+                                 std::shared_ptr<array_info> arr2,
                                  size_t const& iRow2);
 
 /**
@@ -687,8 +693,9 @@ int KeyComparisonAsPython_Column(bool const& na_position_bis, array_info* arr1,
  * @param is_parallel Are the arrays distributed (for tracing purposes).
  * @return uint8_t* Output bitmask.
  */
-uint8_t* bitwise_and_null_bitmasks(const std::vector<array_info*>& arrays,
-                                   const bool is_parallel);
+uint8_t* bitwise_and_null_bitmasks(
+    const std::vector<std::shared_ptr<array_info>>& arrays,
+    const bool is_parallel);
 
 // ----------------------- Debug functions -----------------------
 
@@ -698,7 +705,7 @@ uint8_t* bitwise_and_null_bitmasks(const std::vector<array_info*>& arrays,
  * @param os is the output stream
  * @param arr is the pointer.
  */
-void DEBUG_PrintColumn(std::ostream& os, const array_info* arr);
+void DEBUG_PrintColumn(std::ostream& os, const std::shared_ptr<array_info> arr);
 
 /** The DEBUG_PrintSetOfColumn is printing the contents of the table to
  * the output stream.
@@ -711,14 +718,14 @@ void DEBUG_PrintColumn(std::ostream& os, const array_info* arr);
  * @param ListArr the list of columns in input
  * @return Nothing. Everything is put in the stream
  */
-void DEBUG_PrintSetOfColumn(std::ostream& os,
-                            std::vector<array_info*> const& ListArr);
+void DEBUG_PrintSetOfColumn(
+    std::ostream& os, std::vector<std::shared_ptr<array_info>> const& ListArr);
 
 /**
  * @brief Print the contents of a table to the output stream.
  * See DEBUG_PrintSetOfColumn for more details
  */
-void DEBUG_PrintTable(std::ostream& os, table_info* table);
+void DEBUG_PrintTable(std::ostream& os, std::shared_ptr<table_info> table);
 
 /**
  * @brief Prints contents of a std::unordered_map to the output stream.
@@ -740,9 +747,10 @@ void DEBUG_PrintUnorderedMap(std::ostream& os,
  * @return nothing. Everything is printed to the stream
  */
 void DEBUG_PrintRefct(std::ostream& os,
-                      std::vector<array_info*> const& ListArr);
+                      std::vector<std::shared_ptr<array_info>> const& ListArr);
 
-inline bool does_keys_have_nulls(std::vector<array_info*> const& key_cols) {
+inline bool does_keys_have_nulls(
+    std::vector<std::shared_ptr<array_info>> const& key_cols) {
     for (auto key_col : key_cols) {
         if ((key_col->arr_type == bodo_array_type::NUMPY &&
              (key_col->dtype == Bodo_CTypes::FLOAT32 ||
@@ -760,8 +768,9 @@ inline bool does_keys_have_nulls(std::vector<array_info*> const& key_cols) {
     return false;
 }
 
-inline bool does_row_has_nulls(std::vector<array_info*> const& key_cols,
-                               int64_t const& i) {
+inline bool does_row_has_nulls(
+    std::vector<std::shared_ptr<array_info>> const& key_cols,
+    int64_t const& i) {
     for (auto key_col : key_cols) {
         if (key_col->arr_type == bodo_array_type::CATEGORICAL) {
             std::vector<char> vectNaN = RetrieveNaNentry(key_col->dtype);
@@ -827,9 +836,10 @@ std::string GetArrType_as_string(bodo_array_type::arr_type_enum arr_type);
  * be fully deleted (decref arrays and delete pointers).
  *
  * @param table_chunks input tables which are assumed to have the same schema
- * @return table_info* concatenated table
+ * @return std::shared_ptr<table_info> concatenated table
  */
-table_info* concat_tables(const std::vector<table_info*>& table_chunks);
+std::shared_ptr<table_info> concat_tables(
+    const std::vector<std::shared_ptr<table_info>>& table_chunks);
 
 /**
  * @brief Concatenate the arrays into a single array.
@@ -847,9 +857,10 @@ table_info* concat_tables(const std::vector<table_info*>& table_chunks);
  * concat_tables)
  *
  * @param arrays Vector of arrays to concatenate together.
- * @return array_info* Concatenated array.
+ * @return std::shared_ptr<array_info> Concatenated array.
  */
-array_info* concat_arrays(std::vector<array_info*>& arrays);
+std::shared_ptr<array_info> concat_arrays(
+    std::vector<std::shared_ptr<array_info>>& arrays);
 
 /**
  * @brief Check if the interval (arr1[idx], arr2[idx]) is within
@@ -867,8 +878,10 @@ array_info* concat_arrays(std::vector<array_info*>& arrays);
  * @param arr2 Array for the end of the interval
  * @param idx Index to check
  */
-inline bool within_bounds_of_rank(array_info* bounds_arr, uint32_t rank,
-                                  int n_pes, array_info* arr1, array_info* arr2,
+inline bool within_bounds_of_rank(std::shared_ptr<array_info> bounds_arr,
+                                  uint32_t rank, int n_pes,
+                                  std::shared_ptr<array_info> arr1,
+                                  std::shared_ptr<array_info> arr2,
                                   uint64_t idx) {
     // na_position_bis is true in our case since asc = true and na_last = true
     // which means that na_bis = (!na_last) ^ asc = true
@@ -891,7 +904,8 @@ inline bool within_bounds_of_rank(array_info* bounds_arr, uint32_t rank,
  * @param strict Whether to consider [A, A] to be a bad interval
  * @return If the interval [arr1[idx], arr2[idx]] is bad
  */
-inline bool is_bad_interval(array_info* arr1, array_info* arr2, uint64_t idx,
+inline bool is_bad_interval(std::shared_ptr<array_info> arr1,
+                            std::shared_ptr<array_info> arr2, uint64_t idx,
                             bool strict = true) {
     auto comp = KeyComparisonAsPython_Column(true, arr1, idx, arr2, idx);
     // strict == true: comp == -1 (A > B) or comp == 0 (A == B)
@@ -939,8 +953,8 @@ inline std::vector<T> flatten(std::vector<std::vector<T>> const& vec,
  * index that can be used in restore_col_order to restore the original ordering.
  */
 inline const std::unordered_map<uint64_t, uint64_t> move_cols_to_front(
-    table_info* table, std::vector<uint64_t> col_ids) {
-    std::vector<array_info*> new_columns(table->columns.size());
+    std::shared_ptr<table_info> table, std::vector<uint64_t> col_ids) {
+    std::vector<std::shared_ptr<array_info>> new_columns(table->columns.size());
     std::set<uint64_t> col_ids_set(col_ids.begin(), col_ids.end());
     std::unordered_map<uint64_t, uint64_t> col_mapping;
     uint64_t itr = 0;
@@ -973,8 +987,9 @@ inline const std::unordered_map<uint64_t, uint64_t> move_cols_to_front(
  * of move_cols_to_front.
  */
 inline void restore_col_order(
-    table_info* table, std::unordered_map<uint64_t, uint64_t>& col_mapping) {
-    std::vector<array_info*> new_columns(table->columns.size());
+    std::shared_ptr<table_info> table,
+    std::unordered_map<uint64_t, uint64_t>& col_mapping) {
+    std::vector<std::shared_ptr<array_info>> new_columns(table->columns.size());
     for (auto it = col_mapping.begin(); it != col_mapping.end(); it++) {
         new_columns[it->first] = table->columns[it->second];
     }

commit 17f3640c27c857ef718d7b905f0db2f0ccdaa460

[BSE-257] Pass `shared_ptr`s by reference in tight loops to avoid copy overheads (#5325)
(As title) and some other minor changes.
This solves ~50% of the regression from #5219.

bodo/libs/_array_utils.h
@@ -769,7 +769,7 @@ inline bool does_keys_have_nulls(
 inline bool does_row_has_nulls(
     std::vector<std::shared_ptr<array_info>> const& key_cols,
     int64_t const& i) {
-    for (auto key_col : key_cols) {
+    for (auto& key_col : key_cols) {
         if (key_col->arr_type == bodo_array_type::CATEGORICAL) {
             std::vector<char> vectNaN = RetrieveNaNentry(key_col->dtype);
             size_t siztype = numpy_item_size[key_col->dtype];

bodo/libs/_bodo_common.h
@@ -637,7 +637,7 @@ std::shared_ptr<array_info> create_dict_string_array(
  */
 
 template <typename T>
-inline T& getv(std::shared_ptr<array_info> arr, size_t idx) {
+inline T& getv(const std::shared_ptr<array_info>& arr, size_t idx) {
     return ((T*)arr->data1())[idx];
 }
 

bodo/libs/_groupby.cpp
@@ -412,8 +412,6 @@ class GroupbyPipeline {
         // Add key-sort column and index to col_sets
         // to apply head_computation on them as well.
         if (head_op && return_index) {
-            std::vector<std::shared_ptr<array_info>> input_cols;
-            input_cols.push_back(index_col);
             // index-column
             col_sets.push_back(
                 makeColSet({index_col}, index_col, Bodo_FTypes::head,
@@ -1128,7 +1126,7 @@ class GroupbyPipeline {
                     new_key_col->set_null_bit(j, bit);
                 }
             }
-            out_table->columns.push_back(new_key_col);
+            out_table->columns.push_back(std::move(new_key_col));
         }
     }
 

bodo/libs/_groupby_col_set.cpp
@@ -669,7 +669,7 @@ GeneralUdfColSet::GeneralUdfColSet(std::shared_ptr<array_info> in_col,
 GeneralUdfColSet::~GeneralUdfColSet() {}
 
 void GeneralUdfColSet::fill_in_columns(
-    std::shared_ptr<table_info> general_in_table,
+    const std::shared_ptr<table_info>& general_in_table,
     const grouping_info& grp_info) const {
     std::shared_ptr<array_info> in_col = this->in_col;
     std::vector<std::vector<int64_t>> group_rows(grp_info.num_groups);
@@ -852,7 +852,7 @@ void HeadColSet::alloc_update_columns(
 void HeadColSet::update(const std::vector<grouping_info>& grp_infos) {
     head_computation(this->in_col, this->update_cols[0], head_row_list);
 }
-void HeadColSet::set_head_row_list(std::vector<int64_t> row_list) {
+void HeadColSet::set_head_row_list(std::vector<int64_t>& row_list) {
     head_row_list = row_list;
 }
 

bodo/libs/_groupby_col_set.h
@@ -333,7 +333,7 @@ class GeneralUdfColSet : public UdfColSet {
      * Fill in the input table for general UDF cfunc. See udf_general_fn
      * and aggregate.py::gen_general_udf_cb for more information.
      */
-    void fill_in_columns(std::shared_ptr<table_info> general_in_table,
+    void fill_in_columns(const std::shared_ptr<table_info>& general_in_table,
                          const grouping_info& grp_info) const;
 };
 
@@ -459,7 +459,7 @@ class HeadColSet : public BasicColSet {
 
     virtual void update(const std::vector<grouping_info>& grp_infos);
 
-    void set_head_row_list(std::vector<int64_t> row_list);
+    void set_head_row_list(std::vector<int64_t>& row_list);
 
    private:
     std::vector<int64_t> head_row_list;

bodo/libs/_groupby_common.cpp
@@ -23,8 +23,8 @@
  * @param use_sql_rules: If true, use SQL rules for null handling. If false, use
  * Pandas rules.
  */
-void aggfunc_output_initialize_kernel(std::shared_ptr<array_info> out_col,
-                                      int ftype, bool use_sql_rules) {
+void aggfunc_output_initialize_kernel(
+    const std::shared_ptr<array_info>& out_col, int ftype, bool use_sql_rules) {
     // Generate an error message for unsupported paths that includes the name
     // of the function and the dtype.
     std::string error_msg = std::string("unsupported aggregate function: ") +
@@ -410,8 +410,8 @@ void aggfunc_output_initialize_kernel(std::shared_ptr<array_info> out_col,
     }
 }
 
-void aggfunc_output_initialize(std::shared_ptr<array_info> out_col, int ftype,
-                               bool use_sql_rules) {
+void aggfunc_output_initialize(const std::shared_ptr<array_info>& out_col,
+                               int ftype, bool use_sql_rules) {
     aggfunc_output_initialize_kernel(out_col, ftype, use_sql_rules);
 }
 

bodo/libs/_groupby_common.h
@@ -18,8 +18,8 @@
  * @param[in] ftype The function type
  * @param[in] use_sql_rules Are we using SQL rules?
  */
-void aggfunc_output_initialize(std::shared_ptr<array_info> out_col, int ftype,
-                               bool use_sql_rules);
+void aggfunc_output_initialize(const std::shared_ptr<array_info>& out_col,
+                               int ftype, bool use_sql_rules);
 
 /**
  * @brief Returns the array type and dtype required for output columns based on

bodo/libs/_groupby_do_apply_to_column.cpp
@@ -1370,8 +1370,8 @@ void apply_to_column_nullable(
  * mapping.
  */
 template <typename T, int ftype, int dtype>
-void apply_to_column(std::shared_ptr<array_info> in_col,
-                     std::shared_ptr<array_info> out_col,
+void apply_to_column(const std::shared_ptr<array_info>& in_col,
+                     const std::shared_ptr<array_info>& out_col,
                      std::vector<std::shared_ptr<array_info>>& aux_cols,
                      const grouping_info& grp_info) {
     switch (in_col->arr_type) {
@@ -1401,8 +1401,8 @@ void apply_to_column(std::shared_ptr<array_info> in_col,
     }
 }
 
-void do_apply_to_column(std::shared_ptr<array_info> in_col,
-                        std::shared_ptr<array_info> out_col,
+void do_apply_to_column(const std::shared_ptr<array_info>& in_col,
+                        const std::shared_ptr<array_info>& out_col,
                         std::vector<std::shared_ptr<array_info>>& aux_cols,
                         const grouping_info& grp_info, int ftype) {
     // macro to reduce code duplication

bodo/libs/_groupby_do_apply_to_column.h
@@ -22,8 +22,8 @@
  * @param grp_info Grouping information relating rows to the proper group.
  * @param ftype The aggregate function to apply
  */
-void do_apply_to_column(std::shared_ptr<array_info> in_col,
-                        std::shared_ptr<array_info> out_col,
+void do_apply_to_column(const std::shared_ptr<array_info>& in_col,
+                        const std::shared_ptr<array_info>& out_col,
                         std::vector<std::shared_ptr<array_info>>& aux_cols,
                         const grouping_info& grp_info, int ftype);
 

bodo/libs/_shuffle.cpp
@@ -777,7 +777,7 @@ void drop_duplicates_local_dictionary(std::shared_ptr<array_info> dict_array,
     if (dict_array->has_deduped_local_dictionary) {
         return;
     }
-    update_local_dictionary_remove_duplicates(dict_array, false,
+    update_local_dictionary_remove_duplicates(std::move(dict_array), false,
                                               sort_dictionary_if_modified);
 }
 
@@ -805,7 +805,7 @@ void convert_local_dictionary_to_global(std::shared_ptr<array_info> dict_array,
         // is_parallel=True implementation.
         return;
     }
-    update_local_dictionary_remove_duplicates(dict_array, true,
+    update_local_dictionary_remove_duplicates(std::move(dict_array), true,
                                               sort_dictionary_if_modified);
 }
 
@@ -814,7 +814,8 @@ void make_dictionary_global_and_unique(std::shared_ptr<array_info> dict_array,
                                        bool sort_dictionary_if_modified) {
     convert_local_dictionary_to_global(dict_array, is_parallel,
                                        sort_dictionary_if_modified);
-    drop_duplicates_local_dictionary(dict_array, sort_dictionary_if_modified);
+    drop_duplicates_local_dictionary(std::move(dict_array),
+                                     sort_dictionary_if_modified);
 }
 
 // shuffle_array