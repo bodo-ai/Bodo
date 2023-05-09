7aa0b0a5640dd385f8f6ad4d7dcf46d61709fdbf
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-05-09 15:56:17 -0500

    [BSE-39] Support LIKE ANY/ALL with ESCAPE clause (#5309)
    This adds support for LIKE ANY/ALL with the ESCAPE clause. This is done
    by creating a customer operator for `LIKE ANY/ALL` that combines code
    from both the like operator and the quantify operator. This custom code
    ensures that code has the correct type and casts are performed if type
    coercion is allowed. The parser utilizes these custom operators instead
    of any of the operators in Calcite core.
    
    After that, a convertlet table is used to transform the `SqlNode` to the
    equivalent `RexNode`. This is used instead of modifying the
    `SqlToRelConverter` in order to allow the `LIKE ANY/ALL` operator to be
    rewritten as an expanded list of conditions joined by `AND/OR`.
    
    Tests are added for the various stages that this affects. No changes to
    code generation were made.
    
    Not entirely thrilled with the error messages generated by invalid types
    at the moment, but that will have to be a future improvement.
    
    This also removes NOT LIKE ANY from the parser as it's not supported
    snowflake syntax. This also removes it from testing since it's not
    something that needs to be supported.

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/prepare/BodoConvertletTable.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/prepare/BodoConvertletTable.kt
@@ -1,11 +1,18 @@
 package com.bodosql.calcite.prepare
 
+import com.bodosql.calcite.application.BodoSQLCodegenException
+import com.bodosql.calcite.sql.`fun`.SqlLikeQuantifyOperator
 import org.apache.calcite.rex.RexNode
+import org.apache.calcite.rex.RexUtil
 import org.apache.calcite.sql.SqlCall
 import org.apache.calcite.sql.SqlKind
+import org.apache.calcite.sql.SqlNodeList
+import org.apache.calcite.sql.`fun`.SqlLibraryOperators
+import org.apache.calcite.sql.`fun`.SqlStdOperatorTable
 import org.apache.calcite.sql2rel.SqlRexContext
 import org.apache.calcite.sql2rel.SqlRexConvertlet
 import org.apache.calcite.sql2rel.SqlRexConvertletTable
+import java.lang.IllegalStateException
 
 /**
  * Custom convertlet table for Bodo code generation. Handles custom functions
@@ -19,6 +26,9 @@ class BodoConvertletTable(private val inner: SqlRexConvertletTable) : SqlRexConv
             // in the standard convertlet table. We natively support these
             // operations so avoid converting them to another pattern.
             SqlKind.LEAST, SqlKind.GREATEST -> AliasConverter
+            SqlKind.LIKE -> if (call.operator is SqlLikeQuantifyOperator)
+                LikeQuantifyConverter else inner.get(call)
+
             else -> inner.get(call)
         }
     }
@@ -27,6 +37,48 @@ class BodoConvertletTable(private val inner: SqlRexConvertletTable) : SqlRexConv
         override fun convertCall(cx: SqlRexContext, call: SqlCall): RexNode {
             val operands = call.operandList.map { op -> cx.convertExpression(op) }
             return cx.rexBuilder.makeCall(call.operator, operands)
+        }
+    }
+
+    /**
+     * Handles the conversion of LIKE ANY/ALL operators to the RexNode equivalent.
+     *
+     * At the moment, this convertlet only handles sets such as LIKE ANY ('%a%', '%b%')
+     * and converts that to an equivalent AND/OR expression with a comparison
+     * against each part of the set.
+     *
+     * It does not handle subqueries which are valid in this context. Subqueries
+     * would require further support from the planner and decorrelator to support
+     * and can't be done with a simple expansion transformation.
+     */
+    private object LikeQuantifyConverter : SqlRexConvertlet {
+        override fun convertCall(cx: SqlRexContext, call: SqlCall): RexNode {
+            assert(call.operandCount() >= 2)
+
+            val op = call.operator as SqlLikeQuantifyOperator
+            val likeOp = if (op.caseSensitive)
+                // Case insensitive is not standard SQL so it's in the library operators.
+                SqlStdOperatorTable.LIKE else SqlLibraryOperators.ILIKE
+
+            val arg0 = cx.convertExpression(call.operandList[0])
+            val arg2 = call.operandList.getOrNull(2)?.let { sqlNode -> cx.convertExpression(sqlNode) }
+            val arg1 = when (val arg1 = call.operandList[1]) {
+                is SqlNodeList -> arg1.list.map { n ->
+                    val expr = cx.convertExpression(n)
+                    if (arg2 != null) {
+                        cx.rexBuilder.makeCall(likeOp, arg0, expr, arg2)
+                    } else {
+                        cx.rexBuilder.makeCall(likeOp, arg0, expr)
+                    }
+                }
+                else -> throw BodoSQLCodegenException("Unsupported argument to $op: $arg1")
+            }
+
+            return when (op.comparisonKind) {
+                SqlKind.SOME -> RexUtil.composeDisjunction(cx.rexBuilder, arg1)
+                SqlKind.ALL -> RexUtil.composeConjunction(cx.rexBuilder, arg1)
+                else -> throw IllegalStateException()
+            }
         }
     }
 }
