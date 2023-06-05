fa2d5bf786bdbee9ecb6d964ddf5f546752022f9
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-06-05 14:28:32 -0500

    [BSE-505] Fix various regressions in the volcano planner (#5527)
    Fix various regressions in the volcano planner
    
    This contains numerous fixes to enable the volcano planner to be used as
    an equivalent of the existing heuristic planner.
    
    RelMdSize has been updated to provide some suitable defaults for
    `VARCHAR` and `VARBINARY`. It has also been given a suitable value for
    `UNKNOWN` just to ensure it doesn't completely mess up the estimation of
    sizes for types. This will help prevent strange numbers from appearing
    when attempting to retrieve the size of rows or columns.
    
    In order to simplify the construction of the `RelMetadataProvider`, a
    single class `PandasRelMetadataProvider` now exists to encapsulate that
    logic. The actual contents haven't changed but they are now not
    duplicated in two locations.
    
    The test harness has been fixed to always use this custom
    `RelMetadataProvider` so the plan cost outputs are consistent with what
    the planner was seeing. This was a visual error and didn't affect the
    results. Still, I spent quite some time trying to understand why the
    numbers weren't consistent with the output. The `RelMetadataProvider`
    was being reverted back to the one used by the Heuristic Planner at some
    point and the custom logic for `RelMdSize` was not being used in the
    explain output.
    
    The `RexCostEstimator` has been fixed to consider the evaluation of the
    partition keys or order keys. Previously, these were accidentally
    skipped so complex expressions in these positions were ignored and the
    planner couldn't recognize that it would be more efficient to only
    compute them in one location.

---
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/metadata/PandasRelMetadataProvider.kt
@@ -0,0 +1,37 @@
+package com.bodosql.calcite.rel.metadata
+
+import org.apache.calcite.rel.metadata.*
+
+class PandasRelMetadataProvider(ranks: Int) : RelMetadataProvider by
+ChainedRelMetadataProvider.of(
+    listOf(
+        // Inject information about the number of ranks
+        // for Pandas queries as the parallelism attribute.
+        ReflectiveRelMetadataProvider.reflectiveSource(
+            // Used to include the number of ranks for planner costs.
+            // Not really used yet but planned to be used for computation
+            // costs of nodes with highly parallel operations.
+            PandasRelMdParallelism(ranks),
+            BuiltInMetadata.Parallelism.Handler::class.java
+        ),
+        ReflectiveRelMetadataProvider.reflectiveSource(
+            PandasRelMdSize(),
+            BuiltInMetadata.Size.Handler::class.java
+        ),
+        DefaultRelMetadataProvider.INSTANCE,
+    )
+) {
+    /**
+     * Default constructor for this metadata provider.
+     *
+     * This constructor is temporary until we have a way to
+     * inject the number of ranks into the PandasRelMetadataProvider
+     * from the Python code.
+     *
+     * The number of ranks isn't meaningfully used by the planner yet,
+     * but placing it at 2 in case it starts to be used that way
+     * and to signify that Pandas operations are intended to be
+     * parallelized.
+     */
+    constructor() : this(ranks = 2)
+}
