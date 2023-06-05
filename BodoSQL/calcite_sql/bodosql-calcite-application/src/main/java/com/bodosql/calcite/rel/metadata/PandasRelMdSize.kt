b7f429b28300856bc1ecc835a7914ab45e4f3818
Jonathan A. Sternberg <jonathan@bodo.ai>
2023-06-05 14:28:32 -0500

    [BSE-505] Fix various regressions in the volcano planner (#5527)
    Fix various regressions in the volcano planner
    
    This contains numerous fixes to enable the volcano planner to be used as
    an equivalent of the existing heuristic planner.
    
    RelMdSize has been updated to provide some suitable defaults for
    `VARCHAR` and `VARBINARY`. It has also been given a suitable value for
    `UNKNOWN` just to ensure it doesn't completely mess up the estimation of
    sizes for types. This will help prevent strange numbers from appearing
    when attempting to retrieve the size of rows or columns.
    
    In order to simplify the construction of the `RelMetadataProvider`, a
    single class `PandasRelMetadataProvider` now exists to encapsulate that
    logic. The actual contents haven't changed but they are now not
    duplicated in two locations.
    
    The test harness has been fixed to always use this custom
    `RelMetadataProvider` so the plan cost outputs are consistent with what
    the planner was seeing. This was a visual error and didn't affect the
    results. Still, I spent quite some time trying to understand why the
    numbers weren't consistent with the output. The `RelMetadataProvider`
    was being reverted back to the one used by the Heuristic Planner at some
    point and the custom logic for `RelMdSize` was not being used in the
    explain output.
    
    The `RexCostEstimator` has been fixed to consider the evaluation of the
    partition keys or order keys. Previously, these were accidentally
    skipped so complex expressions in these positions were ignored and the
    planner couldn't recognize that it would be more efficient to only
    compute them in one location.

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/metadata/PandasRelMdSize.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/metadata/PandasRelMdSize.kt
@@ -3,6 +3,9 @@ package com.bodosql.calcite.rel.metadata
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.metadata.RelMdSize
 import org.apache.calcite.rel.metadata.RelMetadataQuery
+import org.apache.calcite.rel.type.RelDataType
+import org.apache.calcite.sql.type.SqlTypeName
+import kotlin.math.min
 
 class PandasRelMdSize : RelMdSize() {
     /**
@@ -15,4 +18,50 @@ class PandasRelMdSize : RelMdSize() {
         rel.rowType.fieldList.map { rowType ->
             averageTypeValueSize(rowType.type)
         }
+
+    override fun averageTypeValueSize(type: RelDataType): Double? =
+        // Customize the behavior of VARCHAR and VARBINARY.
+        // These two types will give negative values if their precisions
+        // are negative so we want to override them to something that makes
+        // sense in the case that they are negative.
+        when (type.sqlTypeName) {
+            SqlTypeName.VARCHAR -> {
+                val precision = if (type.precision >= 0) type.precision else DEFAULT_VARCHAR_PRECISION
+                // 100.0 is an arbitrary number taken from calcite's core code.
+                min(precision.toDouble() * BYTES_PER_CHARACTER, 100.0)
+            }
+
+            SqlTypeName.VARBINARY -> {
+                val precision = if (type.precision >= 0) type.precision else DEFAULT_VARBINARY_PRECISION
+                // 100.0 is an arbitrary number taken from calcite's core code.
+                min(precision.toDouble(), 100.0)
+            }
+            // Keep things sane when an unknown value is present.
+            // This really shouldn't occur, but don't want to invalidate
+            // an entire plan cost calculation based on an unknown field.
+            SqlTypeName.UNKNOWN -> 8.0
+            else -> super.averageTypeValueSize(type)
+    }
+
+    companion object {
+        /**
+         * Default precision for a VARCHAR that has no precision set
+         * for the purposes of estimating sizes.
+         *
+         * Number is arbitrary. This seems like a suitable number for
+         * something that will likely not be too long but also is still
+         * significant for memory use.
+         */
+        private const val DEFAULT_VARCHAR_PRECISION: Int = 20
+
+        /**
+         * Default precision for a VARBINARY that has no precision set
+         * for the purposes of estimating sizes.
+         *
+         * Number is arbitrary. This seems like a suitable number for
+         * something that will likely not be too long but also is still
+         * significant for memory use.
+         */
+        private const val DEFAULT_VARBINARY_PRECISION: Int = 20
+    }
 }
