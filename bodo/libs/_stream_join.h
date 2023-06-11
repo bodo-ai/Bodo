d92c87e887154960b3f62ec9bb1163f830e5173a
Sahil Gupta <sahil@bodo.ai>
2023-06-11 13:35:17 -0400

    Reset dictionary in `ArrayBuildBuffer::Clear` (#5580)
    (As title)
    This didn't show up in current tests because they only shuffle once. If we set the SHUFFLE_THRESHOLD to 1 and batch sizes to a small number, this will throw an error.

--- bodo/libs/_stream_join.h
+++ bodo/libs/_stream_join.h
@@ -330,6 +330,18 @@ struct ArrayBuildBuffer {
             } break;
             case bodo_array_type::DICT: {
                 this->dict_indices->Clear();
+                // Reset the dictionary to point to the one
+                // in the dict builder:
+                this->data_array->child_arrays[0] =
+                    this->dict_builder->dict_buff->data_array;
+                // Reset the flags:
+                this->data_array->has_global_dictionary =
+                    false;  // by default, dictionary builders are not global.
+                this->data_array->has_deduped_local_dictionary =
+                    true;  // the dictionary builder guarantees deduplication by
+                           // design.
+                this->data_array->has_sorted_dictionary =
+                    false;  // by default, dictionary builders are not sorted.
             } break;
             default: {
                 throw std::runtime_error(
