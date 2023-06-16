8ad1490233685019ae5239c30cc923b3ca71c28e
tathougies-bodo <127988135+tathougies-bodo@users.noreply.github.com>
2023-06-15 17:33:35 -0700

    Fix minio install script (#5601)
    Fix minio build script

--- buildscripts/setup_minio.sh
+++ buildscripts/setup_minio.sh
@@ -8,7 +8,7 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
   sudo wget -P /usr/local/bin https://dl.min.io/server/minio/release/linux-amd64/minio
 elif [[ "$unamestr" == 'Darwin' ]]; then
-  sudo wget -P /usr/local/bin https://dl.min.io/server/minio/release/darwin-amd64/minio
+  sudo curl -o /usr/local/bin/minio https://dl.min.io/server/minio/release/darwin-amd64/minio
 else
   echo Error
 fi
