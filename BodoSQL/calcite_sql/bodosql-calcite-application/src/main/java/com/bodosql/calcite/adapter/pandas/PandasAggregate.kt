0b99acea19430a963fade0f191a83fd535240042
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-07-19 07:33:01 -0400

    [BSE-713] Add restrictions to getStreamingTrait (#5804)
    * Adding new tests and updating aggregate rule
    
    * Moving smoke test changes to BSE-713
    
    * Updating comments and codegen tests [RUN CI]
    
    * Adding codegen for new tests [RUN CI]
    
    * Updating companion object
    
    * Switch to a simpler argument passing scheme
    
    * Fixing inference rules
    
    * Restoring to more complex argument passing out of necessity
    
    * [RUN CI]
    
    * [RUN CI]

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasAggregate.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasAggregate.kt
@@ -1,5 +1,7 @@
 package com.bodosql.calcite.adapter.pandas
 
+import com.bodosql.calcite.application.RelationalAlgebraGenerator
+import com.bodosql.calcite.application.Utils.AggHelpers.aggContainsFilter
 import com.bodosql.calcite.ir.Dataframe
 import com.bodosql.calcite.ir.StateVariable
 import com.bodosql.calcite.traits.BatchingProperty
@@ -22,8 +24,33 @@ class PandasAggregate(
 
     init {
         assert(convention == PandasRel.CONVENTION)
-        // Require streaming if we have enabled streaming.
-        assert(traitSet.containsIfApplicable(BatchingProperty.STREAMING))
+        // Require streaming if we have enabled streaming and it makes sense for this node.
+        if (getStreamingTrait(this.groupSets, this.aggCalls) == BatchingProperty.STREAMING)
+            assert(traitSet.containsIfApplicable(BatchingProperty.STREAMING))
+    }
+
+    companion object {
+        /**
+         * Determine the streaming trait that can be used for an aggregation.
+         *
+         * @param groupSets the grouping sets used by the aggregation node.
+         * If there is more than one grouping in the sets, or the singleton
+         * grouping is a no-groupby aggregation, then the aggregation
+         * node is not supported with streaming.
+         * @param aggCallList the aggregations being applied to the data. If
+         * any of the aggregations require a filter (e.g. for a PIVOT query)
+         * then the aggregation node is not supported with streaming.
+         */
+        fun getStreamingTrait(groupSets: List<ImmutableBitSet>, aggCallList: List<AggregateCall>): BatchingProperty {
+            // If streaming is disabled or grouping sets are being used, do not use streaming
+            if (!RelationalAlgebraGenerator.enableGroupbyStreaming || groupSets.size != 1) return BatchingProperty.SINGLE_BATCH
+            // If the aggregation is a no-groupby agg, do not use streaming
+            val groupSet = groupSets[0]
+            if (groupSet.cardinality() == 0) return BatchingProperty.SINGLE_BATCH
+            // If any of the aggregations has a filter (e.g. for a PIVOT query), do not use streaming
+            if (aggContainsFilter(aggCallList)) return BatchingProperty.SINGLE_BATCH
+            return BatchingProperty.STREAMING
+        }
     }
 
     override fun copy(traitSet: RelTraitSet, input: RelNode, groupSet: ImmutableBitSet, groupSets: List<ImmutableBitSet>?, aggCalls: List<AggregateCall>): PandasAggregate {
