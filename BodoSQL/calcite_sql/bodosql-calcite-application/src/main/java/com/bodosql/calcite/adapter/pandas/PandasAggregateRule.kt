2d33eb5f5fe724ab1961aa6aa1e306d0344cde75
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-07-19 07:33:01 -0400

    [BSE-713] Add restrictions to getStreamingTrait (#5804)
    * Adding new tests and updating aggregate rule
    
    * Moving smoke test changes to BSE-713
    
    * Updating comments and codegen tests [RUN CI]
    
    * Adding codegen for new tests [RUN CI]
    
    * Updating companion object
    
    * Switch to a simpler argument passing scheme
    
    * Fixing inference rules
    
    * Restoring to more complex argument passing out of necessity
    
    * [RUN CI]
    
    * [RUN CI]

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasAggregateRule.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/pandas/PandasAggregateRule.kt
@@ -1,6 +1,7 @@
 package com.bodosql.calcite.adapter.pandas
 
 import com.bodosql.calcite.application.RelationalAlgebraGenerator
+import com.bodosql.calcite.application.Utils.AggHelpers
 import com.bodosql.calcite.traits.BatchingProperty
 import org.apache.calcite.plan.Convention
 import org.apache.calcite.rel.RelNode
@@ -8,12 +9,7 @@ import org.apache.calcite.rel.convert.ConverterRule
 import org.apache.calcite.rel.core.Aggregate
 import org.apache.calcite.rel.core.AggregateCall
 import org.apache.calcite.rel.logical.LogicalAggregate
-import org.apache.calcite.rex.RexCall
-import org.apache.calcite.rex.RexInputRef
-import org.apache.calcite.rex.RexLiteral
-import org.apache.calcite.rex.RexNode
 import org.apache.calcite.sql.SqlKind
-import org.apache.calcite.sql.type.SqlTypeName
 
 class PandasAggregateRule private constructor(config: Config) : ConverterRule(config) {
     companion object {
@@ -44,21 +40,9 @@ class PandasAggregateRule private constructor(config: Config) : ConverterRule(co
             return null;
         }
 
-        val streamingTrait = getStreamingTrait()
+        val streamingTrait = PandasAggregate.getStreamingTrait(rel.groupSets, rel.aggCallList)
         val traitSet = rel.cluster.traitSet().replace(PandasRel.CONVENTION).replace(streamingTrait)
         return PandasAggregate(rel.cluster, traitSet, convert(agg.input, traitSet.replace(streamingTrait)),
             agg.groupSet, agg.groupSets, agg.aggCallList)
-    }
-
-    /**
-     * Determine the streaming Trait for a newly created Groupby.
-     *
-     * TODO: restrict this to the operations that are actually supported.
-     * e.g.: require a groupby (not just select MAX(A) from table)
-     * don't support filters (e.g. the paths that would require groupby.apply)
-     */
-    fun getStreamingTrait(): BatchingProperty {
-        return if (RelationalAlgebraGenerator.enableGroupbyStreaming) BatchingProperty.STREAMING
-        else BatchingProperty.SINGLE_BATCH
     }
 }
