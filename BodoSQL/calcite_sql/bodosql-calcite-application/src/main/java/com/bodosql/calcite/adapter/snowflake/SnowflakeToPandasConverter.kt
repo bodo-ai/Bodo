f67dfc05840a768516e617d85aacfe1cf4b76c91
Nick Riasanovsky <nick@bodo.ai>
2023-08-11 14:27:54 -0400

    [BSE-935] Normalize all RexNodes (#5965)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/snowflake/SnowflakeToPandasConverter.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/snowflake/SnowflakeToPandasConverter.kt
@@ -185,7 +185,7 @@ class SnowflakeToPandasConverter(cluster: RelOptCluster, traits: RelTraitSet, in
         val isLastVar = currentPipeline.getExitCond()
         val readArrowNextCall = Expr.Call("bodo.io.arrow_reader.read_arrow_next", listOf(stateVar))
         builder.add(Op.TupleAssign(listOf(tableChunkVar, isLastVar), readArrowNextCall))
-        return BodoEngineTable(tableChunkVar.name, this)
+        return BodoEngineTable(tableChunkVar.emit(), this)
     }
 
     /**
--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/Utils/RexNormalizer.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/Utils/RexNormalizer.kt
@@ -3,25 +3,44 @@ package com.bodosql.calcite.application.Utils
 import org.apache.calcite.rex.RexBuilder
 import org.apache.calcite.rex.RexCall
 import org.apache.calcite.rex.RexNode
+import org.apache.calcite.rex.RexOver
 import org.apache.calcite.rex.RexShuttle
+import org.apache.calcite.rex.RexUtil.toCnf
 import org.apache.calcite.sql.SqlKind
+import org.apache.calcite.sql.`fun`.SqlCastFunction
+import java.lang.Exception
+import java.lang.RuntimeException
 
 /**
  * Force each call to be normalized in a RexNode.
  */
 class RexNormalizer(private val rexBuilder: RexBuilder) : RexShuttle() {
+
+    override fun visitOver(over: RexOver): RexNode {
+        return over
+    }
     override fun visitCall(call: RexCall): RexNode {
-        val operands = call.operands.map { op -> op.accept(this) }
-        return when (call.kind) {
-            SqlKind.AND, SqlKind.OR -> {
-                // The order of AND/OR doesn't really matter but can
-                // differ in plans due to different planner rule orders.
-                // Use the digest of each operand to sort. The digest
-                // should be deterministic.
-                val sorted = operands.sortedBy { it.toString() }
-                rexBuilder.makeCall(call.op, sorted)
+        // Use an array to pass a boolean by reference and update
+        // inside visitList.
+        val update = booleanArrayOf(false)
+        var newOperands= visitList(call.operands, update)
+        // The order of AND/OR can create issues for deterministic planning.
+        if (call.kind == SqlKind.AND || call.kind == SqlKind.OR) {
+            val sortedOperands = newOperands.sortedBy { it.toString() }
+            if (sortedOperands != newOperands) {
+                newOperands = sortedOperands
+                update[0] = true
             }
-            else -> call
+        }
+        return if (update[0]) {
+            // Cast functions have a separate API and cannot use the generic makeCall.
+            if (call.op is SqlCastFunction) {
+                rexBuilder.makeCast(call.getType(), newOperands[0])
+            } else {
+                rexBuilder.makeCall(call.op, newOperands)
+            }
+        } else {
+            call
         }
     }
 }
