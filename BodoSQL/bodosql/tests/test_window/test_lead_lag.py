4d045c926c5c435ee197ac176a0af240500909ea
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-08-22 10:05:51 -0400

    [BSE-516] Use new codegen for lead/lag (#5980)
    Updating codegen for LEAD and LAG to use the new C++ kernels.

--- BodoSQL/bodosql/tests/test_window/test_lead_lag.py
+++ BodoSQL/bodosql/tests/test_window/test_lead_lag.py
@@ -1,27 +1,36 @@
-import pandas as pd
 import numpy as np
-import pytest
+import pandas as pd
 from bodosql.tests.test_window.window_common import count_window_applies
 from bodosql.tests.utils import check_query
 
 
 def test_lead_lag(spark_info, memory_leak_check):
     """Tests the window functions LEAD/LAG with a minimal set of combinations
-       that cover the essential of behavior."""
+    that cover the essential of behavior."""
     selects = [
         "LEAD(I) RESPECT NULLS OVER (PARTITION BY P ORDER BY O)",
         "LAG(S, 10) RESPECT NULLS OVER (PARTITION BY P ORDER BY O)",
         "LEAD(I, 5, 0) RESPECT NULLS OVER (PARTITION BY P ORDER BY O)",
         "LEAD(S, 3) IGNORE NULLS OVER (PARTITION BY P ORDER BY O)",
-        "LAG(I, 7, -1) IGNORE NULLS OVER (PARTITION BY P ORDER BY O)"
+        "LAG(I, 7, -1) IGNORE NULLS OVER (PARTITION BY P ORDER BY O)",
+        "LEAD(S, 1, 'hello') IGNORE NULLS OVER (PARTITION BY P ORDER BY O)",
     ]
     query = f"SELECT I, P, O, {', '.join(selects)} FROM table1"
-    ctx = {"table1": pd.DataFrame({
-        "I": pd.Series([None if (i**2)%6<2 else i for i in range(100)], dtype=pd.Int32Dtype()),
-        "S": pd.Series([None if (i**3)%17>10 else str(i) for i in range(100)]),
-        "P": pd.Series(["A", "B"] * 50),
-        "O": pd.Series([np.tan(i) for i in range(100)])
-    })}
+    ctx = {
+        "table1": pd.DataFrame(
+            {
+                "I": pd.Series(
+                    [None if (i**2) % 6 < 2 else i for i in range(500)],
+                    dtype=pd.Int32Dtype(),
+                ),
+                "S": pd.Series(
+                    [None if (i**3) % 17 > 10 else str(i) for i in range(500)]
+                ),
+                "P": pd.Series(["A", "B", "C", "D", "E"] * 100),
+                "O": pd.Series([np.tan(i) for i in range(500)]),
+            }
+        )
+    }
     pandas_code = check_query(
         query,
         ctx,
