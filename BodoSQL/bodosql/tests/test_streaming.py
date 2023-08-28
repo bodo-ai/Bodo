1a4b0865c87ed38630d650bc02a6d99b6a3c3263
Keaton Drebes <38367344+keatoooon@users.noreply.github.com>
2023-08-28 16:59:24 -0400

    [BSE-1054] Add streaming groupby verbose logging information (#6124)

--- BodoSQL/bodosql/tests/test_streaming.py
+++ BodoSQL/bodosql/tests/test_streaming.py
@@ -2,6 +2,8 @@
 """
 Test correctness of BodoSQL operations that handle streaming data.
 """
+import io
+
 import bodosql
 import pytest
 from bodosql.tests.test_types.snowflake_catalog_common import (  # noqa
@@ -9,11 +11,18 @@ from bodosql.tests.test_types.snowflake_catalog_common import (  # noqa
 )
 
 import bodo
-from bodo.tests.utils import check_func
+from bodo.tests.user_logging_utils import (
+    check_logger_msg,
+    create_string_io_logger,
+    set_logging_stream,
+)
+from bodo.tests.utils import check_func, pytest_snowflake
+
+pytestmark = pytest_snowflake
 
 
 @pytest.mark.skip(
-    "Appears to be running on AWS CI, which is causing a number of PR CI failures, so temporarily skipping"
+    "Appears to be running on AWS CI, which is causing a number of PR CI failures, so temporarily skipping: https://bodo.atlassian.net/browse/BSE-1059"
 )
 def test_streaming_cache(snowflake_sample_data_snowflake_catalog, memory_leak_check):
     if not bodo.bodosql_use_streaming_plan:
@@ -47,3 +56,44 @@ def test_streaming_cache(snowflake_sample_data_snowflake_catalog, memory_leak_ch
         sort_output=True,
         reset_index=True,
     )
+
+
+def test_streaming_groupby_aggregate_timer(
+    snowflake_sample_data_snowflake_catalog, memory_leak_check
+):
+    """Tests that we properly handle timer information for streaming groupby aggregate"""
+
+    if not bodo.bodosql_use_streaming_plan:
+        pytest.skip("Test only runs with streaming plan enabled")
+
+    # Row number query is needed to prevent Aggregate push down into Snowflake
+    query = """SELECT C_ADDRESS, MAX(ROW_NUM) FROM
+        (SELECT *, ROW_NUMBER() OVER (PARTITION BY C_ADDRESS ORDER BY C_CUSTKEY) as ROW_NUM FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER)
+        GROUP BY C_ADDRESS"""
+
+    bc = bodosql.BodoSQLContext(catalog=snowflake_sample_data_snowflake_catalog)
+
+    def impl(bc):
+        return bc.sql(query)
+
+    stream = io.StringIO()
+    logger = create_string_io_logger(stream)
+
+    with set_logging_stream(logger, 2):
+        plan = bc.generate_plan(query)
+        bodo.jit(impl)(bc)
+
+        assert (
+            "PandasAggregate" in plan
+        ), "Streaming plan did not actually generate a PandasAggregate node"
+        for relNodeStr in plan.split("\n"):
+            relNodeStr = relNodeStr.strip()
+            # Confirm that all the nodes in the plan that should
+            # show up in the generated log, actually do.
+            if not (
+                relNodeStr.startswith("PandasTableScan")
+                or relNodeStr.startswith("CombineStreamsExchange")
+                or relNodeStr.startswith("SeparateStreamExchange")
+                or relNodeStr.startswith("Snowflake")
+            ):
+                check_logger_msg(stream, relNodeStr, check_case=False)
