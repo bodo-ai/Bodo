f67cfd9ce436748266f36bce46276713be58ce32
Nick Riasanovsky <nick@bodo.ai>
2023-08-28 12:50:23 -0400

    [BSE-1055] update join cost to be fully based on the input (#6119)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/core/JoinBase.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/core/JoinBase.kt
@@ -34,29 +34,30 @@ abstract class JoinBase(
         val conditionCost = condition.accept(RexCostEstimator)
             .multiplyBy(conditionRows)
 
-        // For Bodo the build side is always the left input. This table
-        // influences the total cost because the build table needs to be collected
-        // before the probe side and is inserted into any hashmaps.
+        // Compute the memory cost from each of the inputs. Join must use every column
         val buildRows = mq.getRowCount(this.left)
-        val averageBuildRowSize = mq.getAverageRowSize(this.left)
-        // Add a multiplier to try ensure the build cost isn't too impactful.
-        // If we have a LEFT JOIN increase this threshold so if its close we do
-        // a right join instead.
+        val averageBuildRowSize = mq.getAverageRowSize(this.left) ?: 8.0
+        val probeRows = mq.getRowCount(this.right)
+        val averageProbeRowSize = mq.getAverageRowSize(this.right) ?: 8.0
+        // Build cost should be higher than probe cost because we must make the hash table.
+        // If we have a LEFT JOIN and similar sizes we may want to do a right join because
+        // there is overhead to tracking the misses.
         // TODO: Decide on the actual size threshold.
-        val baseMultiplier = 0.3
-        val multiplier = if (this.joinType == JoinRelType.LEFT) { baseMultiplier * 1.0 } else { baseMultiplier }
-        val buildCost = Cost(mem = averageBuildRowSize ?: 0.0).multiplyBy(buildRows).multiplyBy(multiplier)
+        val baseBuildMultiplier = 1.3
+        val buildMultiplier = if (this.joinType == JoinRelType.LEFT) { baseBuildMultiplier * 1.0 } else { baseBuildMultiplier }
+        val buildCost = Cost(mem = averageBuildRowSize).multiplyBy(buildRows).multiplyBy(buildMultiplier)
+        val probeCost = Cost(mem = averageProbeRowSize).multiplyBy(probeRows)
 
         // We now want to compute the expected cost of producing this join's output.
         // We do this by taking the output rows and multiplying by the number
         // of rows we are estimated to produce. The join condition itself will influence
         // the estimated row count.
         val rows = mq.getRowCount(this)
-        val averageRowSize = mq.getAverageRowSize(this)
-        val outputCost = Cost(mem = averageRowSize ?: 0.0).multiplyBy(rows)
+        val averageRowSize = mq.getAverageRowSize(this) ?: 8.0
+        val outputCost = Cost(mem = averageRowSize).multiplyBy(rows)
 
         // Final cost is these three combined.
-        val totalCost = conditionCost.plus(buildCost).plus(outputCost)
+        val totalCost = conditionCost.plus(buildCost).plus(probeCost).plus(outputCost)
         return planner.makeCost(rows = rows, from = totalCost)
     }
 }
