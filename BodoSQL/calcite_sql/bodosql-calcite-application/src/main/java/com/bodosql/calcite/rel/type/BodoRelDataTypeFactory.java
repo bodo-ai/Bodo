fa81623faf7072f9282e31c05e27a4403fb5e61e
Nick Riasanovsky <nick@bodo.ai>
2023-08-29 14:13:43 -0400

    [BSE-1052] Add a Variant Type with minimal functionality matching TZAware Timestamp (#6116)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/type/BodoRelDataTypeFactory.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/type/BodoRelDataTypeFactory.java
@@ -5,18 +5,27 @@ import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.sql.type.BodoTZInfo;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.sql.type.TZAwareSqlType;
-
+import org.apache.calcite.sql.type.VariantSqlType;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public interface BodoRelDataTypeFactory extends RelDataTypeFactory {
   /**
    * Creates a {@link TZAwareSqlType} with the given time zone information.
+   *
    * @param tzInfo timezone information. if null, uses the type system default.
    * @return TZAwareSqlType
    */
   RelDataType createTZAwareSqlType(@Nullable BodoTZInfo tzInfo);
 
-  static RelDataType createTZAwareSqlType(RelDataTypeFactory typeFactory, @Nullable BodoTZInfo tzInfo) {
+  /**
+   * Creates a {@link VariantSqlType}.
+   *
+   * @return The VariantSQLType
+   */
+  RelDataType createVariantSqlType();
+
+  static RelDataType createTZAwareSqlType(
+      RelDataTypeFactory typeFactory, @Nullable BodoTZInfo tzInfo) {
     if (typeFactory instanceof BodoRelDataTypeFactory) {
       BodoRelDataTypeFactory bodoTypeFactory = (BodoRelDataTypeFactory) typeFactory;
       return bodoTypeFactory.createTZAwareSqlType(tzInfo);
@@ -24,5 +33,14 @@ public interface BodoRelDataTypeFactory extends RelDataTypeFactory {
 
     // Timezone information is lost if used without an appropriate type factory.
     return typeFactory.createSqlType(SqlTypeName.TIMESTAMP_WITH_LOCAL_TIME_ZONE);
+  }
+
+  static RelDataType createVariantSqlType(RelDataTypeFactory typeFactory) {
+    if (typeFactory instanceof BodoRelDataTypeFactory) {
+      BodoRelDataTypeFactory bodoTypeFactory = (BodoRelDataTypeFactory) typeFactory;
+      return bodoTypeFactory.createVariantSqlType();
+    }
+    throw new UnsupportedOperationException(
+        "createVariantSqlType() is only supported with the BodoRelDataTypeFactory");
   }
 }
