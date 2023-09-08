6441ca09c6fa9f4e2e45183a5432cb6f8343f2e5
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-09-08 08:40:49 -0400

    [BSE-1077] Support GET_PATH inside SnowflakeProject (#6182)
    Allows `GET_PATH` calls to be pushed into Snowflake by modifying the `AbstractSnowflakeProjectRule` to allow mapping arbitrary `RexNode` to an input ref from the split off projection via a hashtable of RexNode to the index int he resulting SnowflakeProject. The hashtable is then generated by first calculating the set of all rex nodes from the input projection that are either input refs or call nodes with certain properties. Currently, the only call nodes allowed are the ones whose name is `GET_PATH`, but this is easily extensible in future.
    
    To minimize the codegen file diffs, input refs maintain their col names but anything else is given a dummy col name in the form `$EXPR4`.
    
    Several new tests are added. Not all of these tests have well formed plans at the moment, but will become well formed once we support casting to/from variant properly.

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/snowflake/AbstractSnowflakeProjectRule.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/adapter/snowflake/AbstractSnowflakeProjectRule.kt
@@ -6,6 +6,7 @@ import org.apache.calcite.plan.RelOptRuleCall
 import org.apache.calcite.plan.RelRule
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.core.Project
+import org.apache.calcite.rex.RexCall
 import org.apache.calcite.rex.RexInputRef
 import org.apache.calcite.rex.RexNode
 import org.apache.calcite.rex.RexShuttle
@@ -22,37 +23,47 @@ import org.immutables.value.Value
 abstract class AbstractSnowflakeProjectRule protected constructor(config: Config) :
     RelRule<AbstractSnowflakeProjectRule.Config>(config) {
 
-    private fun isAllInputRefs(project: Project): Boolean {
-        return project.projects.map { it is RexInputRef }.all { it }
-    }
-
     /**
      * Take a projection that contains some operations from pure column accesses and generates equivalent
      * nodes that consist of two parts:
-     * 1. A LogicalProjection equivalent to the original projection with only input Refs "re-indexed"
-     * 2. A Snowflake Project contains only InputRefs that prune any unused columns
+     * 1. A LogicalProjection equivalent to the original projection with any pushed nodes replaced with
+     *    "re-indexed" references to the next part.
+     * 2. A Snowflake Project contains only RexNodes that are deemed push-able into Snowflake. See
+     *    PushableDetectionVisitor for a description of what types of RexNodes can be pushed.
      *
      * This is done, so the LogicalProjection keeps any function calls / window functions
      * with the column selection is separated and pushed into Snowflake.
      */
-    private fun splitComputeProjection(project: Project, snowflakeRel: SnowflakeRel, relBuilder: RelBuilder): RelNode {
-        // First we will determine what columns are used and use that to build a SnowflakeProjection that just
-        // prunes columns.
-        val usedColumns = determineUsedInputColumns(project, snowflakeRel.getRowType().fieldCount)
+    private fun splitComputeProjection(project: Project, snowflakeRel: SnowflakeRel, relBuilder: RelBuilder, pushableNodes: Set<RexNode>): RelNode {
+        // After we determine what columns are used, we use that to build a SnowflakeProjection that just
+        // prunes columns and any other nodes that can be pushed. The set of pushable nodes is sorted so that
+        // input refs come first, and they are in the same order that they were originally. This sort order needs
+        // to be a deterministic total ordering in order to avoid issues during testing.
+        val usedColumns = pushableNodes.toList().sortedWith(
+            compareBy<RexNode> {
+                if (it is RexInputRef) { 0 } else { 1 }
+            }.thenBy {
+                if (it is RexInputRef) { it.index } else { it.toString() }
+            },
+        )
         relBuilder.push(snowflakeRel)
-        val fieldIndexRemap = mutableMapOf<Int, Int>()
+        val nodeRemap = mutableMapOf<RexNode, Int>()
         var nextIndex = 0
         // Remap the field indices in the current projection to the indices
         // after pruning columns.
-        usedColumns.forEachIndexed {
-                idx, keep ->
-            if (keep) {
-                fieldIndexRemap[idx] = nextIndex
-                nextIndex++
+        usedColumns.forEach {
+                node ->
+            nodeRemap[node] = nextIndex
+            nextIndex++
+        }
+        val fieldSelects = nodeRemap.map { entry -> entry.key }
+        val fieldNames = nodeRemap.map { entry ->
+            if (entry.key is RexInputRef) {
+                snowflakeRel.rowType.fieldNames[(entry.key as RexInputRef).index]
+            } else {
+                "\$EXPR${entry.value}"
             }
         }
-        val fieldSelects = fieldIndexRemap.map { entry -> relBuilder.field(entry.key) }
-        val fieldNames = fieldIndexRemap.map { entry -> snowflakeRel.getRowType().fieldNames[entry.key] }
         // Next we create the snowflake project from the logical project.
         val catalogTable = snowflakeRel.getCatalogTable()
         val snowflakeProject = SnowflakeProject.create(
@@ -64,9 +75,9 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
             catalogTable,
         )
         relBuilder.push(snowflakeProject)
-        // Now we need to update all the inputRef indices in the original project and generate
-        // the final projection.
-        val visitor = InputRefUpdate(fieldIndexRemap, relBuilder)
+        // Now we need to generate the final projection by replacing all the
+        // RexNodes that were pushable with references to the snowflake project.
+        val visitor = SplitUpdate(nodeRemap, relBuilder)
         val newProjects = project.projects.map { x -> x.accept(visitor) }
         val finalProject = BodoLogicalProject.create(
             snowflakeProject,
@@ -79,36 +90,43 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
     }
 
     /**
-     * RexShuttle implementation that only modifies a RexNode by updating
-     * all RexInputRefs with the new column number. If a section of the RexNode
+     * RexShuttle implementation that replaces RexNodes with references to a column
+     * from the newly created SnowflakeProject node. If a section of the RexNode
      * can avoid being changed it returns the original RexNode.
      *
      * All other changes are methods for ensuring the children are properly traversed
      * and all nodes are updated.
+     *
+     * Currently, only supports replacing input refs and calls.
      */
 
-    private class InputRefUpdate constructor(
-        private val indexMap: Map<Int, Int>,
+    private class SplitUpdate constructor(
+        private val rexMap: Map<RexNode, Int>,
         private val builder: RelBuilder,
     ) :
         RexShuttle() {
         override fun visitInputRef(inputRef: RexInputRef): RexNode {
-            val oldIndex = inputRef.index
-            val newIndex = indexMap[oldIndex]!!
-            if (oldIndex == newIndex) {
-                // Avoid creating new nodes when possible
-                return inputRef
-            } else {
-                return builder.field(newIndex)
+            if (rexMap.containsKey(inputRef)) {
+                return builder.field(rexMap[inputRef]!!)
             }
+            // Avoid creating new nodes when possible
+            return inputRef
+        }
+
+        override fun visitCall(call: RexCall): RexNode {
+            if (rexMap.containsKey(call)) {
+                return builder.field(rexMap[call]!!)
+            }
+            return super.visitCall(call)
         }
     }
 
     /**
-     * Creates a projection that consists of only pruning columns. This is basically trivial
-     * to implement as we do not need to modify the projection at all.
+     * Transforms a projection that exists entirely of nodes that can be pushed to Snowflake
+     * into a SnowflakeProject. This is basically trivial to implement as we do not need to
+     * modify the projection at all.
      */
-    private fun createOnlyPruningProjection(project: Project, snowflakeRel: SnowflakeRel): RelNode {
+    private fun createSnowflakeProjection(project: Project, snowflakeRel: SnowflakeRel): RelNode {
         val catalogTable = snowflakeRel.getCatalogTable()
         return SnowflakeProject.create(
             project.cluster,
@@ -125,10 +143,13 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
             return
         }
         val (proj, snowflakeRel) = extractNodes(call)
-        val newNode = if (isAllInputRefs(proj)) {
-            createOnlyPruningProjection(proj, snowflakeRel)
+        val pushableNodes = getPushableNodes(proj)
+        // If every node in the project is pushable, just turn the PandasProject
+        // into a SnowflakeProject, otherwise split it off into two.
+        val newNode = if (pushableNodes.containsAll(proj.projects)) {
+            createSnowflakeProjection(proj, snowflakeRel)
         } else {
-            splitComputeProjection(proj, snowflakeRel, call.builder())
+            splitComputeProjection(proj, snowflakeRel, call.builder(), pushableNodes)
         }
         call.transformTo(newNode)
     }
@@ -152,11 +173,33 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
     companion object {
 
         /**
-         * Class to detect all columns that are used in a RexNode.
+         * Class to detect all nodes that can be pushed into a SnowflakeProject.
+         *
+         * Currently only allows the following to be considered pushable nodes:
+         *
+         * - Input refs
+         * - Calls to GET_PATH (where the first operand is also a pushable node)
          */
-        private class InputRefVisitor(private val mutableList: MutableList<Boolean>) : RexVisitorImpl<Unit>(true) {
+        private class PushableDetectionVisitor(private val pushableNodes: MutableSet<RexNode>) : RexVisitorImpl<Unit>(true) {
             override fun visitInputRef(inputRef: RexInputRef) {
-                mutableList[inputRef.index] = true
+                pushableNodes.add(inputRef)
+            }
+
+            // Returns whether a rex node can be pushed into Snowflake.
+            fun isPushableCall(node: RexNode): Boolean {
+                if (node is RexInputRef) return true
+                if (node is RexCall) {
+                    return node.operator.name == "GET_PATH" && isPushableCall(node.operands[0])
+                }
+                return false
+            }
+
+            override fun visitCall(call: RexCall) {
+                if (isPushableCall(call)) {
+                    pushableNodes.add(call)
+                } else {
+                    call.operands.forEach { it.accept(this) }
+                }
             }
         }
 
@@ -164,13 +207,13 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
          * Determine the input columns that are directly referenced by a projection.
          */
         @JvmStatic
-        private fun determineUsedInputColumns(project: Project, maxNumCols: Int): List<Boolean> {
-            val mutableList = MutableList(maxNumCols) { false }
-            val visitor = InputRefVisitor(mutableList)
+        private fun getPushableNodes(project: Project): Set<RexNode> {
+            val mutableSet = HashSet<RexNode>()
+            val visitor = PushableDetectionVisitor(mutableSet)
             for (column in project.projects) {
                 column.accept(visitor)
             }
-            return mutableList.toList()
+            return mutableSet
         }
 
         /**
@@ -181,11 +224,8 @@ abstract class AbstractSnowflakeProjectRule protected constructor(config: Config
          */
         @JvmStatic
         fun isPushableProject(project: Project): Boolean {
-            // Note: We use getRowType() because it may be initialized to null.
-            val inputType = project.input.getRowType()
-            val inputCols = inputType.fieldCount
-            val usedColumns = determineUsedInputColumns(project, inputCols)
-            return !usedColumns.all { x -> x } and usedColumns.any { x -> x }
+            val pushableNodes = getPushableNodes(project)
+            return pushableNodes.isNotEmpty()
         }
     }
 
