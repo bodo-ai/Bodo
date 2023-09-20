abdec38d712bd6bbb00eb79db17cd28fba64b603
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-09-20 19:02:42 -0400

    [BSE-1261] Fix `bodosql/tests/test_types/test_snowflake_catalog_filter_pushdown.py::test_snowflake_coalesce_constant_date_string_filter_pushdown` Failure Azure Nightly  (#6313)

--- BodoSQL/bodosql/tests/test_types/test_snowflake_catalog_filter_pushdown.py
+++ BodoSQL/bodosql/tests/test_types/test_snowflake_catalog_filter_pushdown.py
@@ -2455,7 +2455,7 @@ def test_snowflake_coalesce_constant_date_string_filter_pushdown(
             # Pushdown happens in the planner. Check the timer message instead.
             check_logger_msg(
                 stream,
-                f'FROM "TEST_DB"."PUBLIC"."{table_name.upper()}" WHERE COALESCE("L_COMMITDATE", TIMESTAMP \'2023-06-20 00:00:00.000000000\') >= TIMESTAMP \'2023-01-20 00:00:00.000000000\'',
+                'WHERE  ( ( COALESCE(\\"L_COMMITDATE\\",',
             )
 
 
