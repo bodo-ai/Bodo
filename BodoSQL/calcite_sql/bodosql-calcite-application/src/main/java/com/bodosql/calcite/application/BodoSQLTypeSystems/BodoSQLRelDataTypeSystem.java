135a8cf7572b262b034fdd2cf30aa7486cd27644
Nick Riasanovsky <nick@bodo.ai>
2023-09-26 16:35:56 -0400

    [BSE-1362] Fix type system to be a proper subclass (#6340)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/BodoSQLTypeSystems/BodoSQLRelDataTypeSystem.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/BodoSQLTypeSystems/BodoSQLRelDataTypeSystem.java
@@ -3,19 +3,12 @@ package com.bodosql.calcite.application.BodoSQLTypeSystems;
 import javax.annotation.Nullable;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
-import org.apache.calcite.rel.type.RelDataTypeSystem;
+import org.apache.calcite.rel.type.RelDataTypeSystemImpl;
 import org.apache.calcite.sql.type.BodoTZInfo;
 import org.apache.calcite.sql.type.SqlTypeName;
 
-/**
- * Class for the RelDataTypeSystem used by BodoSQL. This recycles the default type system but
- * specifies that chars should be converted to varchars.
- */
-public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
-  // A copy of the default type system. This will be used whenever
-  // we don't need custom behavior.
-  private RelDataTypeSystem defaultTypeSystem = RelDataTypeSystem.DEFAULT;
-
+/** Class for the RelDataTypeSystem used by BodoSQL. */
+public class BodoSQLRelDataTypeSystem extends RelDataTypeSystemImpl {
   private final BodoTZInfo defaultTZInfo;
 
   // This is found in SqlTypeName.
@@ -71,8 +64,9 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
     switch (typeName) {
       case DECIMAL:
         return getMaxNumericScale();
+      default:
+        return super.getMaxScale(typeName);
     }
-    return defaultTypeSystem.getMaxScale(typeName);
   }
 
   @Override
@@ -110,14 +104,15 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
       case TIME:
       case TIMESTAMP:
         return MAX_DATETIME_PRECISION;
+      default:
+        return super.getDefaultPrecision(typeName);
     }
-    return defaultTypeSystem.getDefaultPrecision(typeName);
   }
 
   @Override
   public int getMaxPrecision(SqlTypeName typeName) {
     // TODO(HA anothr PR): port other defaults.
-    // These're needed as getCastSpec call getMaxPrecision
+    // These are needed as getCastSpec call getMaxPrecision
     switch (typeName) {
       case TINYINT:
       case SMALLINT:
@@ -130,8 +125,9 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
       case VARBINARY:
       case BINARY:
         return 8388608;
+      default:
+        return super.getMaxPrecision(typeName);
     }
-    return defaultTypeSystem.getMaxPrecision(typeName);
   }
 
   @Override
@@ -145,26 +141,6 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
   }
 
   @Override
-  public String getLiteral(SqlTypeName typeName, boolean isPrefix) {
-    return defaultTypeSystem.getLiteral(typeName, isPrefix);
-  }
-
-  @Override
-  public boolean isCaseSensitive(SqlTypeName typeName) {
-    return defaultTypeSystem.isCaseSensitive(typeName);
-  }
-
-  @Override
-  public boolean isAutoincrement(SqlTypeName typeName) {
-    return defaultTypeSystem.isAutoincrement(typeName);
-  }
-
-  @Override
-  public int getNumTypeRadix(SqlTypeName typeName) {
-    return defaultTypeSystem.getNumTypeRadix(typeName);
-  }
-
-  @Override
   public RelDataType deriveSumType(RelDataTypeFactory typeFactory, RelDataType argumentType) {
     switch (argumentType.getSqlTypeName()) {
       case TINYINT:
@@ -175,8 +151,9 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
         return typeFactory.createTypeWithNullability(
             typeFactory.createSqlType(SqlTypeName.BIGINT), argumentType.isNullable());
       default:
-        // By default match the calcite defaults. Other types may need to be updated in the future.
-        return defaultTypeSystem.deriveSumType(typeFactory, argumentType);
+        // Match the calcite defaults if we haven't explicitly supported it.
+        // Other types may need to be updated in the future.
+        return super.deriveSumType(typeFactory, argumentType);
     }
   }
 
@@ -187,27 +164,6 @@ public class BodoSQLRelDataTypeSystem implements RelDataTypeSystem {
   // for this to upcast to double.
   public RelDataType deriveAvgAggType(RelDataTypeFactory typeFactory, RelDataType argumentType) {
     return typeFactory.createSqlType(SqlTypeName.DOUBLE);
-  }
-
-  @Override
-  public RelDataType deriveCovarType(
-      RelDataTypeFactory typeFactory, RelDataType arg0Type, RelDataType arg1Type) {
-    return defaultTypeSystem.deriveCovarType(typeFactory, arg0Type, arg1Type);
-  }
-
-  @Override
-  public RelDataType deriveFractionalRankType(RelDataTypeFactory typeFactory) {
-    return defaultTypeSystem.deriveFractionalRankType(typeFactory);
-  }
-
-  @Override
-  public RelDataType deriveRankType(RelDataTypeFactory typeFactory) {
-    return defaultTypeSystem.deriveRankType(typeFactory);
-  }
-
-  @Override
-  public boolean isSchemaCaseSensitive() {
-    return defaultTypeSystem.isSchemaCaseSensitive();
   }
 
   public BodoTZInfo getDefaultTZInfo() {
