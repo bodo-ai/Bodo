43aa28c6e1836b13b29cdc978e1e594561883fd5
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2023-09-27 00:29:42 -0400

    [BSE-1232] Simplify literals inside of ||, CONCAT, LOWER and UPPER, as well as comparisons with LEAST and GREATEST (#6328)
    Allows the simplifier to do constant folding on strings inside of a call (or chain of calls) to concatenation operations. This also results in trees of `||` calls being flattened into a single call to `CONCAT`. For example:
    
    `'%' || ' ' || T.A || '%' || '-' || 'ing'` becomes `CONCAT('% ', T.A, '%-ing')`
    
    Child PR #6329 
    
    Allows the simplifier to do constant folding on strings inside of a call to `LOWER` or `UPPER`. For example: `UPPER('Alphabet Soup)` becomes `'ALPHABET SOUP'`.
    
    Child PR #6330: 
    
    Add support for the following types of simplification patterns:
    `LEAST(A, B, C) <= D` -> `A <= D OR B <= D OR C <= D`
    `GREATEST(A, B, C) <= D` -> `A <= D AND B <= D AND C <= D`

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/rex/BodoRexSimplify.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/rex/BodoRexSimplify.kt
@@ -1,7 +1,11 @@
 package org.apache.calcite.rex
 
+import com.bodosql.calcite.application.operatorTables.StringOperatorTable
 import org.apache.calcite.plan.RelOptPredicateList
+import org.apache.calcite.sql.SqlBinaryOperator
 import org.apache.calcite.sql.SqlKind
+import org.apache.calcite.sql.SqlOperator
+import org.apache.calcite.sql.`fun`.SqlStdOperatorTable
 import org.apache.calcite.sql.type.BasicSqlType
 import org.apache.calcite.sql.type.SqlTypeFamily
 import org.apache.calcite.sql.type.SqlTypeName
@@ -273,6 +277,172 @@ class BodoRexSimplify(
     }
 
     /**
+     * @param e The RexNode being checked
+     * @return Whether e is a call to a concatenation operation without a separator
+     */
+    private fun isConcat(e: RexNode): Boolean {
+        return e is RexCall && (e.operator.name == "||" || e.operator.name == SqlStdOperatorTable.CONCAT.name)
+    }
+
+    /**
+     * Recursively probes through the arguments of calls to concatenation operations
+     * to append their arguments to a list, thus "flattening" the tree of concat calls.
+     *
+     * @param e The RexNode having its arguments extracted
+     * @param concatArgs The list where the leaves of the concatenation tree are added to
+     */
+    private fun collectConcatArgs(e: RexNode, concatArgs: MutableList<RexNode>) {
+        if (isConcat(e)) {
+            (e as RexCall).operands.forEach { collectConcatArgs(it, concatArgs) }
+        } else {
+            concatArgs.add(simplify(e))
+        }
+    }
+
+    /**
+     * Performs two kinds of simplifications on calls to || or CONCAT:
+     * 1. Flattens a tree of such calls into one call of concat
+     *    For example: '%' || ' ' || T.A || '%' || 'ing' -> CONCAT('%', ' ', T.A, '%', 'ing')
+     * 2. Combines any adjacent terms that are string literals
+     *    For example: CONCAT('%', ' ', T.A, '%', 'ing') -> CONCAT('% ', T.A, '%ing')
+     *
+     * @param e The call to a concatenation operation that is being simplified.
+     * @return A simplified version of the concatenation operation with adjacent
+     * string constants combined.
+     */
+    private fun simplifyConcat(e: RexCall): RexNode {
+        val concatArgs: MutableList<RexNode> = mutableListOf()
+        e.operands.forEach { collectConcatArgs(it, concatArgs) }
+        val compressedArgs: MutableList<RexNode> = mutableListOf()
+        concatArgs.forEach {
+            if (it is RexLiteral) {
+                if (compressedArgs.isNotEmpty() && compressedArgs[compressedArgs.size - 1] is RexLiteral) {
+                    val lhs = (compressedArgs.removeAt(compressedArgs.size - 1) as RexLiteral).getValueAs(String::class.java)
+                    val rhs = (it as RexLiteral).getValueAs(String::class.java)
+                    compressedArgs.add(rexBuilder.makeLiteral(lhs + rhs))
+                } else {
+                    compressedArgs.add(it)
+                }
+            } else {
+                compressedArgs.add(it)
+            }
+        }
+        // If the entire sequence was combined, there is no need for a CONCAT call
+        if (compressedArgs.size == 1) {
+            return compressedArgs[0]
+        }
+        // If no optimizations were performed, return the original call
+        if (e.operands == compressedArgs) {
+            return e
+        }
+        // Upgrade the operator from || to CONCAT if the number of operands changed
+        val operator = if (compressedArgs.size == e.operands.size) { e.operator } else { StringOperatorTable.CONCAT }
+        return rexBuilder.makeCall(operator, compressedArgs)
+    }
+
+    /**
+     * @param e The RexNode being checked
+     * @return Whether e is a call to a string capitalization operator
+     */
+    private fun isStringCapitalizationOp(e: RexNode): Boolean {
+        return e is RexCall && (e.operator.name == SqlStdOperatorTable.LOWER.name || e.operator.name == SqlStdOperatorTable.UPPER.name)
+    }
+
+    /**
+     * @param e The call to a string capitalization operation.
+     * @return A simplified version of the operation where string literals are
+     * folded.
+     */
+    private fun simplifyStringCapitalizationOp(e: RexCall): RexNode {
+        val operand = simplify(e.operands[0])
+        if (operand is RexLiteral) {
+            val asStr = operand.getValueAs(String::class.java)!!
+            val capitalizedStr = if (e.operator.name == SqlStdOperatorTable.UPPER.name) { asStr.uppercase(Locale.ROOT) } else { asStr.lowercase(Locale.ROOT) }
+            return rexBuilder.makeLiteral(capitalizedStr)
+        }
+        return e
+    }
+
+    /**
+     * @param e The RexNode being checked
+     * @param i The operand position being checked
+     * @return Whether e is a call to an ordering comparison where the ith argument
+     * is a call to LEAST or GREATEST.
+     */
+    private fun isCompareLeastGreatest(e: RexNode, i: Int): Boolean {
+        return e is RexCall && (e.kind.belongsTo(listOf(SqlKind.LESS_THAN, SqlKind.LESS_THAN_OR_EQUAL, SqlKind.GREATER_THAN, SqlKind.GREATER_THAN_OR_EQUAL))) && (e.operands[i].kind.belongsTo(listOf(SqlKind.LEAST, SqlKind.GREATEST)))
+    }
+
+    /**
+     * Returns a simplification in the formats below:
+     * LEAST(A, B, C) <= D  --> A <= D OR B <= D OR C <= D
+     * LEAST(A, B, C) >= D  --> A >= D AND B >= D AND C >= D
+     * GREATEST(A, B, C) <= D  --> A <= D AND B <= D AND C <= D
+     * GREATEST(A, B, C) >= D  --> A >= D OR B >= D OR C >= D
+     * (+ the same for > and <)
+     *
+     * @param leastGreatestArgs the arguments to the LEAST or GREATEST call
+     * @param rhs the value that the call to LEAST or GREATEST is being compared to
+     * @param comparison the type of comparison being done
+     * @param isConjunction whether the final answer can be represented by a conjunction
+     * (a sequence of AND calls) instead of a disjunction (a sequence of OR calls)
+     * @return The simplified operation in the format described above
+     */
+    private fun simplifyCompareLeastGreatest(leastGreatestArgs: List<RexNode>, rhs: RexNode, comparison: SqlOperator, isConjunction: Boolean): RexNode {
+        val distributedComparison = leastGreatestArgs.map { rexBuilder.makeCall(comparison, listOf(it, rhs)) }
+        if (isConjunction) { return RexUtil.composeConjunction(rexBuilder, distributedComparison) }
+        return RexUtil.composeDisjunction(rexBuilder, distributedComparison)
+    }
+
+    /**
+     * @param e The call to <, <=, > or >= where the first operand is a call to LEAST or GREATEST
+     * @return An equivalent expression where the call to LEAST or GREATEST is eliminated by
+     * distributing the comparison across the arguments, for example:
+     *
+     * LEAST(A, B, C) <= D  --> A <= D OR B <= D OR C <= D
+     */
+    private fun simplifyCompareLeastGreatest(e: RexCall): RexNode {
+        val lhs = e.operands[0] as RexCall
+        val leastGreatestArgs = lhs.operands
+        val rhs = e.operands[1]
+        // The operation will become a conjunction if it is a </<= with GREATEST or a >/>= with LEAST
+        val isConjunction = (e.kind.belongsTo(listOf(SqlKind.LESS_THAN, SqlKind.LESS_THAN_OR_EQUAL))) == (lhs.kind == SqlKind.GREATEST)
+        return simplifyCompareLeastGreatest(leastGreatestArgs, rhs, e.operator, isConjunction)
+    }
+
+    /**
+     * @param e The call to <, <=, > or >= where the second operand is a call to LEAST or GREATEST
+     * @return An equivalent expression where the call to LEAST or GREATEST is eliminated by
+     * distributing the comparison across the arguments, for example:
+     *
+     * D >= LEAST(A, B, C)  --> A <= D AND B <= D AND C <= D
+     */
+    private fun simplifyCompareLeastGreatestRhs(e: RexCall): RexNode {
+        // Switch the argument order
+        val lhs = e.operands[1] as RexCall
+        val leastGreatestArgs = lhs.operands
+        val rhs = e.operands[0]
+        // Flip the comparison
+        val comparison = (e.operator as SqlBinaryOperator).reverse()!!
+        // The operation will become a conjunction if it is a </<= with GREATEST or a >/>= with LEAST
+        val isConjunction = (comparison.kind.belongsTo(listOf(SqlKind.LESS_THAN, SqlKind.LESS_THAN_OR_EQUAL))) == (lhs.kind == SqlKind.GREATEST)
+        return simplifyCompareLeastGreatest(leastGreatestArgs, rhs, comparison, isConjunction)
+    }
+
+    /**
+     * @param e The call to <, <=, > or >=
+     * @return The same call but with the argument order and comparison switched.
+     *
+     * For example: A <= B  -->  B >= A
+     */
+    private fun reverseComparison(e: RexCall): RexNode {
+        val firstArg = e.operands[0]
+        val secondArg = e.operands[1]
+        val comparison = (e.operator as SqlBinaryOperator).reverse()!!
+        return rexBuilder.makeCall(comparison, listOf(secondArg, firstArg))
+    }
+
+    /**
      * Implementation of simplifyUnknownAs where we simplify custom Bodo functions
      * and then dispatch to the regular RexSimplifier.
      */
@@ -282,7 +452,13 @@ class BodoRexSimplify(
             SqlKind.PLUS -> simplifyBodoPlus(e as RexCall)
             SqlKind.MINUS -> simplifyBodoMinus(e as RexCall)
             SqlKind.TIMES -> simplifyBodoTimes(e as RexCall)
-            else -> e
+            else -> when {
+                isConcat(e) -> simplifyConcat(e as RexCall)
+                isStringCapitalizationOp(e) -> simplifyStringCapitalizationOp(e as RexCall)
+                isCompareLeastGreatest(e, 0) -> simplifyCompareLeastGreatest(e as RexCall)
+                isCompareLeastGreatest(e, 1) -> simplifyCompareLeastGreatest(reverseComparison(e as RexCall) as RexCall)
+                else -> e
+            }
         }
         return super.simplify(simplifiedNode, unknownAs)
     }
