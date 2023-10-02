b45ca9e5e5b71b30a2dbe62af9ce55de9218de02
Aneesh Durg <aneeshdurg17@gmail.com>
2023-10-02 09:18:36 -0500

    Introduce before_each/after_each to C++ testing framework (#6375)
    * Introduce before_each/after_each to C++ testing framework
    
    * [run CI]

--- bodo/tests/test_example.cpp
+++ bodo/tests/test_example.cpp
@@ -3,5 +3,13 @@
 #include "./test.hpp"
 
 bodo::tests::suite tests([] {
+    bodo::tests::before_each([] {
+        // Example before_each - this is optional and can be omitted entirely
+    });
+
+    bodo::tests::after_each([] {
+        // Example after_each - this is optional and can be omitted entirely
+    });
+
     bodo::tests::test("test_example", [] { bodo::tests::check(true); });
 });
