9a1bb3a405a358c330131c02bb1639de172dec6f
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2023-10-05 09:20:06 -0500

    BSE-1331: Make the STLBufferPoolAllocator track size (#6388)
    * Make the STLBufferPoolAllocator track size
    
    * [Run CI]
    
    * Track bytes instead of n_elements
    
    * [Run CI]
    
    * Fix deallocate size tracking
    
    * [Run CI]
    
    * [Run CI]
    
    * Fix !=
    
    * [Run CI]
    
    * [Run CI]
    
    * BSE-1331: Cap shuffle hashtable size (#6349)
    
    * Cap shuffle hash size
    
    * Cap max shuffle hashtable size
    
    * Use actual memory sizes
    
    * [Run CI]
    
    * Reset hashes and hashtable based on allocated size
    
    * [Run CI]
    
    * [Run CI]
    
    * Re-init shuffle_hash_table after reset
    
    * [Run CI]
    
    * Correctly re-init shuffle_hash_table
    
    * [Run CI]
    
    * Update comment
    
    Co-authored-by: Sahil Gupta <sahil@bodo.ai>
    
    * Always resize(0) for hashes
    
    Co-authored-by: Sahil Gupta <sahil@bodo.ai>
    
    * [Run CI]
    
    ---------
    
    Co-authored-by: Sahil Gupta <sahil@bodo.ai>
    
    ---------
    
    Co-authored-by: Sahil Gupta <sahil@bodo.ai>

--- bodo/libs/_memory.h
+++ bodo/libs/_memory.h
@@ -1025,7 +1025,11 @@ class STLBufferPoolAllocator {
         using other = STLBufferPoolAllocator<U>;
     };
 
-    STLBufferPoolAllocator(IBufferPool* pool) noexcept : pool_(pool) {}
+    STLBufferPoolAllocator(IBufferPool* pool, size_t size) noexcept
+        : pool_(pool), size_(size) {}
+
+    STLBufferPoolAllocator(IBufferPool* pool) noexcept
+        : STLBufferPoolAllocator(pool, 0) {}
 
     STLBufferPoolAllocator() noexcept
         : STLBufferPoolAllocator(BufferPool::DefaultPtr()) {}
@@ -1034,20 +1038,22 @@ class STLBufferPoolAllocator {
 
     template <class U>
     STLBufferPoolAllocator(const STLBufferPoolAllocator<U>& other) noexcept
-        : STLBufferPoolAllocator(other.pool()) {}
+        : STLBufferPoolAllocator(other.pool(), other.size()) {}
 
     template <class U>
     STLBufferPoolAllocator(STLBufferPoolAllocator<U>&& other)
-        : STLBufferPoolAllocator(other.pool()) {}
+        : STLBufferPoolAllocator(other.pool(), other.size()) {}
 
     template <class U>
     STLBufferPoolAllocator& operator=(const STLBufferPoolAllocator<U>& other) {
         this->pool_ = other.pool();
+        this->size_ = other.size();
     }
 
     template <class U>
     STLBufferPoolAllocator& operator=(STLBufferPoolAllocator<U>&& other) {
         this->pool_ = other.pool();
+        this->size_ = other.size();
     }
 
     T* address(T& r) const noexcept { return std::addressof(r); }
@@ -1056,25 +1062,29 @@ class STLBufferPoolAllocator {
 
     template <class U>
     inline bool operator==(const STLBufferPoolAllocator<U>& rhs) {
-        return this->pool_ == rhs.pool();
+        return this->pool_ == rhs.pool() && this->size_ == rhs.size();
     }
 
     template <class U>
     inline bool operator!=(const STLBufferPoolAllocator<U>& rhs) {
-        return this->pool_ != rhs.pool();
+        return this->pool_ != rhs.pool() || this->size_ != rhs.size();
     }
 
     T* allocate(size_t n) {
         uint8_t* data;
-        ::arrow::Status s = this->pool_->Allocate(n * sizeof(T), &data);
+        size_t bytes = n * sizeof(T);
+        ::arrow::Status s = this->pool_->Allocate(bytes, &data);
         if (!s.ok()) {
             throw std::bad_alloc();
         }
+        this->size_ += bytes;
         return reinterpret_cast<T*>(data);
     }
 
     void deallocate(T* p, size_t n) {
-        this->pool_->Free(reinterpret_cast<uint8_t*>(p), n * sizeof(T));
+        size_t bytes = n * sizeof(T);
+        this->pool_->Free(reinterpret_cast<uint8_t*>(p), bytes);
+        this->size_ -= bytes;
     }
 
     size_t size_max() const noexcept { return size_t(-1) / sizeof(T); }
@@ -1090,9 +1100,12 @@ class STLBufferPoolAllocator {
     }
 
     IBufferPool* pool() const noexcept { return this->pool_; }
+    size_t size() const noexcept { return this->size_; }
 
    private:
     IBufferPool* const pool_;
+    // Size of all allocations made through this allocator in bytes.
+    size_t size_;
 };
 
 template <typename T, class Allocator = STLBufferPoolAllocator<T>>
