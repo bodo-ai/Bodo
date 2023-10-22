784d974db0e824625720f507474b46402cd5466e
Ehsan Totoni <ehsan.tn@gmail.com>
2023-10-22 14:53:32 -0400

    [BSE-1400] Apply Numba patch to remove extra IR copying from Numba (#6573)
    * [BSE-1400] Apply Numba patch to remove extra IR copying from Numba [run ci]
    
    * remove breakpoint [run ci]

--- bodo/numba_compat.py
+++ bodo/numba_compat.py
@@ -6280,3 +6280,162 @@ if _check_numba_change:  # pragma: no cover
 numba.core.ir_utils.find_topo_order = find_topo_order
 
 #### END PATCH TO ADD AN ITERATIVE IMPLEMENTATION OF find_topo_order ####
+
+
+# Bodo change: apply Numba patch to avoid extra IR copy in inliner:
+# https://github.com/numba/numba/pull/8990
+# TODO(ehsan): Remove after upgrade to Numba 0.59
+def inline_ir(self, caller_ir, block, i, callee_ir, callee_freevars, arg_typs=None):
+    """Inlines the callee_ir in the caller_ir at statement index i of block
+    `block`, callee_freevars are the free variables for the callee_ir. If
+    the callee_ir is derived from a function `func` then this is
+    `func.__code__.co_freevars`. If `arg_typs` is given and the InlineWorker
+    instance was initialized with a typemap and calltypes then they will be
+    appropriately updated based on the arg_typs.
+    """
+    from numba.core.inline_closurecall import (
+        _add_definitions,
+        _debug_dump,
+        _get_all_scopes,
+        _get_callee_args,
+        _replace_args_with,
+        _replace_returns,
+        add_offset_to_labels,
+        next_label,
+        replace_vars,
+    )
+
+    # Always copy the callee IR, it gets mutated
+    def copy_ir(the_ir):
+        kernel_copy = the_ir.copy()
+        kernel_copy.blocks = {}
+        for block_label, block in the_ir.blocks.items():
+            new_block = copy.deepcopy(the_ir.blocks[block_label])
+            # Bodo change: remove extra copy:
+            # https://github.com/numba/numba/pull/8990
+            # new_block.body = []
+            # for stmt in the_ir.blocks[block_label].body:
+            #     scopy = copy.deepcopy(stmt)
+            #     new_block.body.append(scopy)
+            kernel_copy.blocks[block_label] = new_block
+        return kernel_copy
+
+    callee_ir = copy_ir(callee_ir)
+
+    # check that the contents of the callee IR is something that can be
+    # inlined if a validator is present
+    if self.validator is not None:
+        self.validator(callee_ir)
+
+    # save an unmutated copy of the callee_ir to return
+    callee_ir_original = copy_ir(callee_ir)
+    scope = block.scope
+    instr = block.body[i]
+    call_expr = instr.value
+    callee_blocks = callee_ir.blocks
+
+    # 1. relabel callee_ir by adding an offset
+    max_label = max(
+        ir_utils._the_max_label.next(),
+        max(caller_ir.blocks.keys()),
+    )
+    callee_blocks = add_offset_to_labels(callee_blocks, max_label + 1)
+    callee_blocks = simplify_CFG(callee_blocks)
+    callee_ir.blocks = callee_blocks
+    min_label = min(callee_blocks.keys())
+    max_label = max(callee_blocks.keys())
+    #    reset globals in ir_utils before we use it
+    ir_utils._the_max_label.update(max_label)
+    self.debug_print("After relabel")
+    _debug_dump(callee_ir)
+
+    # 2. rename all local variables in callee_ir with new locals created in
+    # caller_ir
+    callee_scopes = _get_all_scopes(callee_blocks)
+    self.debug_print("callee_scopes = ", callee_scopes)
+    #    one function should only have one local scope
+    assert len(callee_scopes) == 1
+    callee_scope = callee_scopes[0]
+    var_dict = {}
+    for var in tuple(callee_scope.localvars._con.values()):
+        if not (var.name in callee_freevars):
+            inlined_name = _created_inlined_var_name(
+                callee_ir.func_id.unique_name, var.name
+            )
+            # Update the caller scope with the new names
+            new_var = scope.redefine(inlined_name, loc=var.loc)
+            # Also update the callee scope with the new names. Should the
+            # type and call maps need updating (which requires SSA form) the
+            # transformation to SSA is valid as the IR object is internally
+            # consistent.
+            callee_scope.redefine(inlined_name, loc=var.loc)
+            var_dict[var.name] = new_var
+    self.debug_print("var_dict = ", var_dict)
+    replace_vars(callee_blocks, var_dict)
+    self.debug_print("After local var rename")
+    _debug_dump(callee_ir)
+
+    # 3. replace formal parameters with actual arguments
+    callee_func = callee_ir.func_id.func
+    args = _get_callee_args(call_expr, callee_func, block.body[i].loc, caller_ir)
+
+    # 4. Update typemap
+    if self._permit_update_type_and_call_maps:
+        if arg_typs is None:
+            raise TypeError("arg_typs should have a value not None")
+        self.update_type_and_call_maps(callee_ir, arg_typs)
+        # update_type_and_call_maps replaces blocks
+        callee_blocks = callee_ir.blocks
+
+    self.debug_print("After arguments rename: ")
+    _debug_dump(callee_ir)
+
+    _replace_args_with(callee_blocks, args)
+    # 5. split caller blocks into two
+    new_blocks = []
+    new_block = ir.Block(scope, block.loc)
+    new_block.body = block.body[i + 1 :]
+    new_label = next_label()
+    caller_ir.blocks[new_label] = new_block
+    new_blocks.append((new_label, new_block))
+    block.body = block.body[:i]
+    block.body.append(ir.Jump(min_label, instr.loc))
+
+    # 6. replace Return with assignment to LHS
+    topo_order = find_topo_order(callee_blocks)
+    _replace_returns(callee_blocks, instr.target, new_label)
+
+    # remove the old definition of instr.target too
+    if (
+        instr.target.name in caller_ir._definitions
+        and call_expr in caller_ir._definitions[instr.target.name]
+    ):
+        # NOTE: target can have multiple definitions due to control flow
+        caller_ir._definitions[instr.target.name].remove(call_expr)
+
+    # 7. insert all new blocks, and add back definitions
+    for label in topo_order:
+        # block scope must point to parent's
+        block = callee_blocks[label]
+        block.scope = scope
+        _add_definitions(caller_ir, block)
+        caller_ir.blocks[label] = block
+        new_blocks.append((label, block))
+    self.debug_print("After merge in")
+    _debug_dump(caller_ir)
+
+    return callee_ir_original, callee_blocks, var_dict, new_blocks
+
+
+if _check_numba_change:  # pragma: no cover
+    lines = inspect.getsource(numba.core.inline_closurecall.InlineWorker.inline_ir)
+    if (
+        hashlib.sha256(lines.encode()).hexdigest()
+        != "2e121cdc0227aba01adc49675e14e1edec867dbaa8bb5e11107fc1aee184c228"
+    ):
+        warnings.warn(
+            "numba.core.inline_closurecall.InlineWorker.inline_ir has changed"
+        )
+
+
+numba.core.inline_closurecall.InlineWorker.inline_ir = inline_ir
