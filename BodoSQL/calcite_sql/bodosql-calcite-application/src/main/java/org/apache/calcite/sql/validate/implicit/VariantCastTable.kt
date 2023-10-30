994462132c2878540e8da7c5b97a27e34228ba32
Nick Riasanovsky <nick@bodo.ai>
2023-10-30 09:00:59 -0400

    Add variant casting for the remaining string function + fix bugs in decode/encode functions (#6629)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/validate/implicit/VariantCastTable.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/validate/implicit/VariantCastTable.kt
@@ -2,8 +2,10 @@ package org.apache.calcite.sql.validate.implicit
 
 import com.bodosql.calcite.application.operatorTables.StringOperatorTable
 import com.bodosql.calcite.application.operatorTables.ThreeOperatorStringTable
+import com.bodosql.calcite.sql.func.SqlBodoOperatorTable
 import org.apache.calcite.rel.type.RelDataType
 import org.apache.calcite.rel.type.RelDataTypeFactory
+import org.apache.calcite.sql.`fun`.SqlLibraryOperators
 import org.apache.calcite.sql.`fun`.SqlStdOperatorTable
 import org.apache.calcite.sql.type.SqlTypeName
 
@@ -22,6 +24,14 @@ internal class VariantCastTable {
                 inType: RelDataType, factory: RelDataTypeFactory, _: Int ->
             factory.createTypeWithNullability(
                 factory.createSqlType(SqlTypeName.VARCHAR),
+                inType.isNullable,
+            )
+        }
+
+        private val anyArgIntegerCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, _: Int ->
+            factory.createTypeWithNullability(
+                factory.createSqlType(SqlTypeName.INTEGER),
                 inType.isNullable,
             )
         }
@@ -45,6 +55,158 @@ internal class VariantCastTable {
                 factory.createSqlType(typeName),
                 inType.isNullable,
             )
+        }
+
+        /**
+         * Generate the new types for the given argument for any function that
+         * has some arguments which need to be cast to string and other
+         * arguments that need to be cast to the same numeric type (integer, bigInt, etc).
+         *
+         * @param inType: The original input type. This is returned if the argument number
+         * is beyond the argument limit and is used to determine nullability.
+         * @param factory: The factory used to create the output data type.
+         * @param idx: The argument of the input to replace. This is used to determine which type
+         * should be output.
+         * @param argLimit: The maximum number of expected arguments for a function's signature.
+         * If any index is passed in that suggests more arguments than accounted for then we do not cast
+         * the variant type at all to avoid incorrect undefined behavior.
+         * @param stringIndices A set of argument numbers that map to string types. All other valid arguments
+         * map to the numeric type.
+         * @param numericTypeName The typename for the numeric type bing cast to. All numeric arguments must cast
+         * to the same type for this function
+         *
+         * @return The output data type that should be produced for the given variant argument.
+         */
+        private fun varcharNumericHelper(inType: RelDataType, factory: RelDataTypeFactory, idx: Int, argLimit: Int, stringIndices: Set<Int>, numericTypeName: SqlTypeName): RelDataType {
+            if (idx >= argLimit) {
+                return inType
+            }
+            val typeName = if (stringIndices.contains(idx)) {
+                SqlTypeName.VARCHAR
+            } else {
+                numericTypeName
+            }
+            return factory.createTypeWithNullability(
+                factory.createSqlType(typeName),
+                inType.isNullable,
+            )
+        }
+
+        /**
+         * Casting for 2 argument functions where the first argument is varchar and the last
+         * is tinyInt.
+         */
+        private val varcharTinyintCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 2, setOf(0), SqlTypeName.TINYINT)
+        }
+
+        /**
+         * Casting for 2 argument functions where the first argument is varchar and the last
+         * is integer.
+         */
+        private val varcharIntegerCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 2, setOf(0), SqlTypeName.INTEGER)
+        }
+
+        /**
+         * Casting for 3 argument functions where the first 2 arguments are varchar and the last
+         * is integer.
+         */
+        private val varcharVarcharIntegerCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 3, setOf(0, 1), SqlTypeName.INTEGER)
+        }
+
+        /**
+         * Casting for 3 argument functions where the first arguments is varchar and the last
+         * two are BIGINT.
+         */
+        private val varcharBigintBigintCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 3, setOf(0), SqlTypeName.BIGINT)
+        }
+
+        /**
+         * Function that casts variant to string if they are found in the allowed indices
+         * and just returns variant for all other arguments.
+         */
+        private val onlyStringCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int, stringIndices: Set<Int> ->
+            if (stringIndices.contains(idx)) {
+                factory.createTypeWithNullability(
+                    factory.createSqlType(SqlTypeName.VARCHAR),
+                    inType.isNullable,
+                )
+            } else {
+                inType
+            }
+        }
+
+        /**
+         * Cast for functions that change arg0 to string but other variant
+         * casting is not supported (either because it should never be supported
+         * or we don't support it yet).
+         */
+        private val arg0VarcharCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            onlyStringCast(inType, factory, idx, setOf(0))
+        }
+
+        /**
+         * Cast for insert, for which we currently only support casting the string
+         * arguments.
+         *
+         * Insert casts to of a function signature
+         * (VARCHAR, INTEGER, INTEGER, VARCHAR), but Snowflake's variant casting
+         * for the integers does VARIANT:DOUBLE:INTEGER, so we don't support this yet
+         * and only enable the VARCHAR
+         */
+        private val insertCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            onlyStringCast(inType, factory, idx, setOf(0, 3))
+        }
+
+        /**
+         * Cast for (VARCHAR, VARCHAR, BIGINT, VARCHAR).
+         */
+        private val varcharVarcharBigIntVarcharCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 4, setOf(0, 1, 3), SqlTypeName.BIGINT)
+        }
+
+        /**
+         * Cast for REGEXP_SUBSTR.
+         *
+         * REGEXP_SUBSTR casts to a function signature
+         * (VARCHAR, VARCHAR, BIGINT, BIGINT, VARCHAR, BIGINT)
+         */
+        private val regexpSubstrCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 6, setOf(0, 1, 4), SqlTypeName.BIGINT)
+        }
+
+        /**
+         * Cast for REGEXP_REPLACE.
+         *
+         * REGEXP_REPLACE casts to a function signature
+         * (VARCHAR, VARCHAR, VARCHAR, BIGINT, BIGINT, VARCHAR)
+         */
+        private val regexpReplaceCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 6, setOf(0, 1, 2, 5), SqlTypeName.BIGINT)
+        }
+
+        /**
+         * Cast for REGEXP_INSTR.
+         *
+         * REGEXP_INSTR casts to a function signature
+         * (VARCHAR, VARCHAR, BIGINT, BIGINT, BIGINT, VARCHAR, BIGINT)
+         */
+        private val regexpInstrCast = {
+                inType: RelDataType, factory: RelDataTypeFactory, idx: Int ->
+            varcharNumericHelper(inType, factory, idx, 7, setOf(0, 1, 5), SqlTypeName.BIGINT)
         }
 
         /**
@@ -84,6 +246,51 @@ internal class VariantCastTable {
             SqlStdOperatorTable.MINUS to anyArgDoubleCast,
             SqlStdOperatorTable.MULTIPLY to anyArgDoubleCast,
             SqlStdOperatorTable.DIVIDE to anyArgDoubleCast,
+            StringOperatorTable.CHAR to anyArgIntegerCast,
+            StringOperatorTable.CHR to anyArgIntegerCast,
+            StringOperatorTable.SPACE to anyArgIntegerCast,
+            StringOperatorTable.SPLIT to anyArgVarcharCast,
+            StringOperatorTable.STRTOK to varcharVarcharIntegerCast,
+            StringOperatorTable.SPLIT_PART to varcharVarcharIntegerCast,
+            SqlStdOperatorTable.LIKE to anyArgVarcharCast,
+            SqlStdOperatorTable.NOT_LIKE to anyArgVarcharCast,
+            SqlLibraryOperators.ILIKE to anyArgVarcharCast,
+            SqlLibraryOperators.NOT_ILIKE to anyArgVarcharCast,
+            SqlLibraryOperators.RLIKE to anyArgVarcharCast,
+            SqlLibraryOperators.NOT_RLIKE to anyArgVarcharCast,
+            SqlBodoOperatorTable.ANY_LIKE to arg0VarcharCast,
+            SqlBodoOperatorTable.ANY_ILIKE to arg0VarcharCast,
+            SqlBodoOperatorTable.ALL_LIKE to arg0VarcharCast,
+            SqlBodoOperatorTable.ALL_ILIKE to arg0VarcharCast,
+            StringOperatorTable.SHA2 to arg0VarcharCast,
+            StringOperatorTable.SHA2_HEX to arg0VarcharCast,
+            StringOperatorTable.HEX_DECODE_STRING to arg0VarcharCast,
+            StringOperatorTable.HEX_DECODE_BINARY to arg0VarcharCast,
+            StringOperatorTable.TRY_HEX_DECODE_STRING to arg0VarcharCast,
+            StringOperatorTable.TRY_HEX_DECODE_BINARY to arg0VarcharCast,
+            StringOperatorTable.BASE64_ENCODE to arg0VarcharCast,
+            StringOperatorTable.BASE64_DECODE_STRING to arg0VarcharCast,
+            StringOperatorTable.BASE64_DECODE_BINARY to arg0VarcharCast,
+            StringOperatorTable.TRY_BASE64_DECODE_STRING to arg0VarcharCast,
+            StringOperatorTable.TRY_BASE64_DECODE_BINARY to arg0VarcharCast,
+            StringOperatorTable.HEX_ENCODE to varcharTinyintCast,
+            StringOperatorTable.LEFT to varcharIntegerCast,
+            StringOperatorTable.RIGHT to varcharIntegerCast,
+            StringOperatorTable.CONTAINS to anyArgVarcharCast,
+            StringOperatorTable.JAROWINKLER_SIMILARITY to anyArgVarcharCast,
+            SqlLibraryOperators.TRANSLATE3 to anyArgVarcharCast,
+            StringOperatorTable.CHARINDEX to varcharVarcharIntegerCast,
+            StringOperatorTable.EDITDISTANCE to varcharVarcharIntegerCast,
+            StringOperatorTable.POSITION to varcharVarcharIntegerCast,
+            SqlStdOperatorTable.REPLACE to anyArgVarcharCast,
+            SqlStdOperatorTable.SUBSTRING to varcharBigintBigintCast,
+            StringOperatorTable.SUBSTR to varcharBigintBigintCast,
+            StringOperatorTable.INSERT to insertCast,
+            StringOperatorTable.REGEXP_LIKE to anyArgVarcharCast,
+            StringOperatorTable.REGEXP_COUNT to varcharVarcharBigIntVarcharCast,
+            StringOperatorTable.REGEXP_SUBSTR to regexpSubstrCast,
+            StringOperatorTable.REGEXP_INSTR to regexpInstrCast,
+            StringOperatorTable.REGEXP_REPLACE to regexpReplaceCast,
         ).mapKeys { it.key.name }
     }
 }
