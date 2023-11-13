e5f87b09fd6dc43a45d89f8fd77c1b91cd568de5
Srinivas Lade <srinivas@bodo.ai>
2023-11-13 16:43:52 -0500

    [BSE-1964] Use Sccache to Cache C++ Compilation on S3 (#6712)

--- buildscripts/setup_conda.sh
+++ buildscripts/setup_conda.sh
@@ -4,18 +4,15 @@ set -exo pipefail
 unamestr=`uname`
 
 # Install Mambaforge
-if [ "$RUNTIME" != "yes" ];
-then
-  if [[ "$unamestr" == 'Linux' ]]; then
-    wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O mambaforge.sh
-  elif [[ "$unamestr" == 'Darwin' ]]; then
-    wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-x86_64.sh -O mambaforge.sh
-  else
-    echo Error
-  fi
-  chmod +x mambaforge.sh
-  ./mambaforge.sh -b
+if [[ "$unamestr" == 'Linux' ]]; then
+  wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O mambaforge.sh
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-x86_64.sh -O mambaforge.sh
+else
+  echo Error
 fi
+chmod +x mambaforge.sh
+./mambaforge.sh -b
 export PATH=$HOME/mambaforge/bin:$PATH
 
 
@@ -29,23 +26,18 @@ set -x
 conda config --set remote_max_retries 5
 conda config --set remote_backoff_factor 60
 
-if [ "$RUNTIME" != "yes" ];
-then
-  # Conda / Mamba will attempt to upgrade packages listed in the
-  # "aggressive_update_packages" at every CLI call.
-  # To upgrade these packages, it will also upgrade packages that depend on
-  # them. OpenSSL is usually one of the defaults, and many packages Bodo
-  # uses depends on OpenSSL, thus breaking our dependency locking.
-  # Therefore, we need to remove all entries from this setting
-  # TODO: Replace with a conda config operation
-  echo "aggressive_update_packages: []" >> ~/.condarc
-  mamba install -y -c conda-forge conda-lock
-fi
-
+# Conda / Mamba will attempt to upgrade packages listed in the
+# "aggressive_update_packages" at every CLI call.
+# To upgrade these packages, it will also upgrade packages that depend on
+# them. OpenSSL is usually one of the defaults, and many packages Bodo
+# uses depends on OpenSSL, thus breaking our dependency locking.
+# Therefore, we need to remove all entries from this setting
+# TODO: Replace with a conda config operation
+echo "aggressive_update_packages: []" >> ~/.condarc
+mamba install -y -c conda-forge conda-lock
 conda-lock install --mamba -n $CONDA_ENV buildscripts/envs/conda-lock.yml
 
 # This docker image is LARGE, so we remove unnecessary files
 # after the environment is created.
 # TODO: Check how long this takes at PR CI runtime
 conda clean -a -y
-mamba install -y -c conda-forge -n $CONDA_ENV sccache 
