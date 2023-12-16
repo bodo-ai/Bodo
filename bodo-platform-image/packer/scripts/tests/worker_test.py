028dbc1b54523a7b446d3301a156ab63437c8158
Hadia Ahmed <hadia@bodo.ai>
2023-12-16 12:11:40 -0800

    [BSE-2292] Restrict Pandas version to < 2.1 and add restriction to platform image (#6971)

--- bodo-platform-image/packer/scripts/tests/worker_test.py
+++ bodo-platform-image/packer/scripts/tests/worker_test.py
@@ -201,6 +201,7 @@ def test_connectors(connector, pip_packages, conda_packages):
         ("ipyparallel", "8.6.1"),
         ("ipywidgets", "8.1.1"),
         ("bodo-azurefs-sas-token-provider", "1.0"),
+        ("pandas", "2.0.3"),
     ],
 )
 def test_versions(package, expected_version, conda_packages):
