4a373c813412002d3ead8c6233c9fbbb6ff58e4a
Nick Riasanovsky <nick@bodo.ai>
2023-12-20 18:25:36 -0500

    [BSE-2275] Add Describe Function Integration + Add signature parsing (#6980)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/SnowflakeUserDefinedFunction.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/SnowflakeUserDefinedFunction.java
@@ -1,18 +1,26 @@
 package org.apache.calcite.sql;
 
 import com.bodosql.calcite.catalog.SnowflakeCatalog;
+import com.bodosql.calcite.schema.SnowflakeUDFFunctionParameter;
+import com.bodosql.calcite.table.BodoSQLColumn;
+import com.bodosql.calcite.table.BodoSQLColumnImpl;
 import com.google.common.collect.ImmutableList;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.FunctionParameter;
 import org.apache.calcite.schema.ScalarFunction;
+import org.apache.calcite.sql.type.BodoTZInfo;
 import org.apache.calcite.sql.type.SqlOperandCountRanges;
 import org.apache.calcite.sql.type.SqlOperandTypeChecker;
 import org.apache.calcite.sql.type.SqlReturnTypeInference;
 import org.apache.calcite.sql.validate.SqlUserDefinedFunction;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import static com.bodosql.calcite.catalog.SnowflakeCatalog.snowflakeTypeNameToTypeInfo;
 
 /**
  * Function definition used for all UDFs defined inside Snowflake. These functions
@@ -22,16 +30,17 @@ public class SnowflakeUserDefinedFunction implements ScalarFunction {
     // -- Fields --
 
     // Full function path, including the name.
-    private final ImmutableList<String> functionPath;
-    // Hold a reference to the catalog to enable inlining the function.
-    private final SnowflakeCatalog catalog;
+    public final ImmutableList<String> functionPath;
 
-    // The Snowflake function signature.
-    // TODO: Replace this with a parsed function.
-    private final String signature;
+    private final SnowflakeCatalog.SnowflakeTypeInfo returnTypeInfo;
+
+    private final List<FunctionParameter> parameters;
+
+    // The function body
+    private @Nullable String body;
 
     // Is this function a table function. Eventually this should be removed
-    // when we add actual UserDefinedTableFunction support, but this 
+    // when we add actual UserDefinedTableFunction support, but this
     private final boolean isTableFunction;
     // Hold information to determine functions we cannot support or may produce
     // performance concerns.
@@ -45,17 +54,19 @@ public class SnowflakeUserDefinedFunction implements ScalarFunction {
      * Creates a new Function for a call to a Snowflake User Defined Function.
      *
      * @param functionPath         The full path to this function including the function name.
-     * @param catalog              A link to the Snowflake catalog for finding implementation information
-     * @param signature            The argument/return value signature from Snowflake
+     * @param args                 A string containing the args output from describe function.
+     * @param returns              A string containing the returns output from describe function
+     * @param body                 A string containing the body output from describe function or NULL if the function is not a SQL UDF.
      * @param minNumArgs           The minimum number of required arguments. This is used for error handling while we have partial support.
      * @param maxNumArgs           The maximum number of required arguments. This is used for error handling while we have partial support.
      * @param isTableFunction      Is this function actually a table function.
      * @param errorInfo            Other snowflake fields used for generating errors.
      */
-    protected SnowflakeUserDefinedFunction(ImmutableList<String> functionPath, SnowflakeCatalog catalog, String signature, int minNumArgs, int maxNumArgs, boolean isTableFunction, SnowflakeUserDefinedFunctionErrorInfo errorInfo) {
+    protected SnowflakeUserDefinedFunction(ImmutableList<String> functionPath, String args, String returns, @Nullable String body, int minNumArgs, int maxNumArgs, boolean isTableFunction, SnowflakeUserDefinedFunctionErrorInfo errorInfo) {
         this.functionPath = functionPath;
-        this.catalog = catalog;
-        this.signature = signature;
+        this.parameters = parseParameters(args);
+        this.returnTypeInfo = snowflakeTypeNameToTypeInfo(returns);
+        this.body = body;
         // TODO: Remove arg counts when we support default arguments.
         this.minNumArgs = minNumArgs;
         this.maxNumArgs = maxNumArgs;
@@ -64,9 +75,9 @@ public class SnowflakeUserDefinedFunction implements ScalarFunction {
     }
 
     // -- static creators
-    public static SnowflakeUserDefinedFunction create(ImmutableList<String> functionPath, SnowflakeCatalog catalog, String signature, int minNumArgs, int maxNumArgs, boolean isTableFunction, boolean isSecure, boolean isExternal, String language, boolean isMemoizable) {
+    public static SnowflakeUserDefinedFunction create(ImmutableList<String> functionPath, String args, String returns, @Nullable String body, int minNumArgs, int maxNumArgs, boolean isTableFunction, boolean isSecure, boolean isExternal, String language, boolean isMemoizable) {
         SnowflakeUserDefinedFunctionErrorInfo errorInfo = new SnowflakeUserDefinedFunctionErrorInfo(isSecure, isExternal, language, isMemoizable);
-        return new SnowflakeUserDefinedFunction(functionPath, catalog, signature, minNumArgs, maxNumArgs, isTableFunction, errorInfo);
+        return new SnowflakeUserDefinedFunction(functionPath, args, returns, body, minNumArgs, maxNumArgs, isTableFunction, errorInfo);
     }
 
     /**
@@ -77,14 +88,15 @@ public class SnowflakeUserDefinedFunction implements ScalarFunction {
      */
     @Override
     public RelDataType getReturnType(RelDataTypeFactory typeFactory) {
-        throw new RuntimeException(
-                String.format(
-                        Locale.ROOT,
-                        "Unable to resolve function: %s.%s.%s. BodoSQL does not have support for Snowflake"
-                                + " UDFs yet",
-                        functionPath.get(0),
-                        functionPath.get(1),
-                        functionPath.get(2)));
+        // Assume nullable for now.
+        final boolean nullable = true;
+        final BodoTZInfo tzInfo = BodoTZInfo.getDefaultTZInfo(typeFactory.getTypeSystem());
+        // Make a dummy BodoSQLColumn for now.
+        BodoSQLColumn.BodoSQLColumnDataType type = returnTypeInfo.columnDataType;
+        BodoSQLColumn.BodoSQLColumnDataType elemType = returnTypeInfo.elemType;
+        int precision = returnTypeInfo.precision;
+        BodoSQLColumn column = new BodoSQLColumnImpl("", "", type, elemType, nullable, tzInfo, precision);
+        return column.convertToSqlType(typeFactory, nullable, tzInfo, precision);
     }
 
     /**
@@ -94,14 +106,31 @@ public class SnowflakeUserDefinedFunction implements ScalarFunction {
      */
     @Override
     public List<FunctionParameter> getParameters() {
-        throw new RuntimeException(
-                String.format(
-                        Locale.ROOT,
-                        "Unable to resolve function: %s.%s.%s. BodoSQL does not have support for Snowflake"
-                                + " UDFs yet",
-                        functionPath.get(0),
-                        functionPath.get(1),
-                        functionPath.get(2)));
+       return parameters;
+    }
+
+
+    /**
+     * Parse the args returned by described function.
+     * @param args The arguments that are of the form:
+     *             (name1 arg1, name2, arg2, ... nameN argN)
+     */
+    private List<FunctionParameter> parseParameters(String args) {
+        List<FunctionParameter> parameters = new ArrayList<>();
+        // Remove the starting ( and trailing )
+        String trimmedArgs = args.substring(1, args.length() - 1).trim();
+        if (!trimmedArgs.isEmpty()) {
+            String[] inputs = trimmedArgs.split(",");
+            for (int i = 0; i < inputs.length; i++) {
+                String input = inputs[i];
+                String[] typeParts = input.split(" ");
+                String argName = typeParts[0];
+                String type = typeParts[1];
+                SnowflakeCatalog.SnowflakeTypeInfo typeInfo = snowflakeTypeNameToTypeInfo(type);
+                parameters.add(new SnowflakeUDFFunctionParameter(argName, i, typeInfo));
+            }
+        }
+        return parameters;
     }
 
     // -- Helper Classes --
