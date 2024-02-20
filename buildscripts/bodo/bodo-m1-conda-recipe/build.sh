c4febc60f2b9635a1238144363c8511863a57134
Srinivas Lade <srinivas@bodo.ai>
2024-02-20 12:37:36 -0500

    Enable SCCache on Azure Nightly (#7264)

--- buildscripts/bodo/bodo-m1-conda-recipe/build.sh
+++ buildscripts/bodo/bodo-m1-conda-recipe/build.sh
@@ -2,6 +2,12 @@
 # CC="mpicxx -cxx=$GXX -std=c++11" GXX="mpicxx -cxx=$GXX -std=c++11" \
 # OPENCV_DIR="${PREFIX}" DAALROOT="${PREFIX}"
 
+# Enable SCCache
+export SCCACHE_BUCKET=engine-codebuild-cache
+export SCCACHE_REGION=us-east-2
+export SCCACHE_S3_USE_SSL=true
+export SCCACHE_S3_SERVER_SIDE_ENCRYPTION=true
+
 # Conda-Build deletes .git, so we need to determine the version beforehand
 # and then pass it to setuptools_scm in the build script (setup.py).
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#using-setuptools-scm
@@ -18,3 +24,5 @@ $PYTHON -m pip install \
     --config-settings=logging.level="DEBUG" \
     --config-settings=cmake.args="-DCMAKE_C_COMPILER=$CC;-DCMAKE_CXX_COMPILER=$CXX;-DCMAKE_INSTALL_PREFIX=$PREFIX;-DCMAKE_INSTALL_LIBDIR=lib;-DCMAKE_FIND_ROOT_PATH='$PREFIX;$CONDA_PREFIX_OLD/x86_64-conda-linux-gnu/sysroot';-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY" \
     . 
+
+sccache --show-stats
