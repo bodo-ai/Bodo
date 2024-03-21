cbf8e092b465d2ae2060f8722eb9caaa7f545c43
Keaton Drebes <38367344+keatoooon@users.noreply.github.com>
2024-03-21 16:27:06 -0400

    [BSE-2959] fix obfuscation part 2 (#7449)
    There's still work to be done to make this fully correct, but this should at least cover the match statements as they currently exist in the codebase. The rest I'll have to add tests for in order to test, so I'm merging this separately to unblock everyone else.
    
    EDIT: After doing more testing/investigating, I realized that I vastly overestimated how many changes were necicary. Therefore, I've merged the secondary fix PR into this one, since it actually shrinks the number of overall changes across the two PRs.

--- obfuscation/test_obfuscation.py
+++ obfuscation/test_obfuscation.py
@@ -4,13 +4,7 @@ import shutil
 import subprocess
 import sys
 
-list_test_files = []
 
-complete_dir = os.path.join("./", "examples")
-content_dir = os.listdir(complete_dir)
-for e_file in content_dir:
-    if e_file.startswith("test"):
-        list_test_files.append(e_file)
 
 # Now treating the files one by one.
 
@@ -30,37 +24,66 @@ def get_variable_expression(func_text, var_name):
     exec(func_text, {}, loc_vars)
     return loc_vars[var_name]
 
+if __name__ == "__main__":
+    list_test_files = []
 
-for e_file in list_test_files:
-    # Copying the script obfuscating it
-    e_test_file = os.path.join(complete_dir, e_file)
-    TmpFile = "/tmp/test.py"
-    shutil.copy(e_test_file, TmpFile)
-    list_command_args = [sys.executable, "./obfuscate.py", "file", TmpFile]
-    returncode = subprocess.run(list_command_args).returncode
-    if returncode != 0:
-        print("obfuscate.py code failed, returncode=", returncode)
-        exit(0)
-    # Reading the python programs
-    func_text1 = get_func_text(TmpFile)
-    func_text2 = get_func_text(e_test_file)
-    if func_text1 == func_text2:
-        print("obfuscation failed to create a different program")
-        exit(0)
-    # Compiling the function
-    f1 = get_variable_expression(func_text1, "f")
-    f2 = get_variable_expression(func_text2, "f")
-    # Loading the data
-    l_split = e_file.split("_")
-    prefix = l_split[0]
-    input_file = os.path.join(complete_dir, "input" + prefix[4:])
-    data_text = get_func_text(input_file)
-    kwargs = get_variable_expression(data_text, "data")
-    # Running them
-    val1 = f1(**kwargs)
-    val2 = f2(**kwargs)
-    if val1 != val2:
-        print("obfuscation failed to work correctly")
-        exit(0)
+    complete_dir = os.path.join("./", "examples")
+    content_dir = os.listdir(complete_dir)
+    for e_file in content_dir:
+        if e_file.startswith("test"):
+            list_test_files.append(e_file)
 
-print("Successful termination of the obfuscation tests")
+
+    for e_file in list_test_files:
+        print("testing file: ", e_file)
+        # Copying the script obfuscating it
+        e_test_file = os.path.join(complete_dir, e_file)
+        TmpFile = "/tmp/test.py"
+        shutil.copy(e_test_file, TmpFile)
+        list_command_args = [sys.executable, "./obfuscate.py", "file", TmpFile]
+        returncode = subprocess.run(list_command_args).returncode
+        if returncode != 0:
+            print("obfuscate.py code failed, returncode=", returncode)
+            exit(0)
+
+        # Reading the python programs
+        func_text1 = get_func_text(TmpFile)
+        func_text2 = get_func_text(e_test_file)
+        print(f"Obfuscated code:\n{func_text1}")
+        if func_text1 == func_text2:
+            print("obfuscation failed to create a different program")
+            exit(0)
+        # Compiling the function
+        f1 = get_variable_expression(func_text1, "f")
+        f2 = get_variable_expression(func_text2, "f")
+        # Loading the data
+        l_split = e_file.split("_")
+        prefix = l_split[0]
+        input_file = os.path.join(complete_dir, "input" + prefix[4:])
+        data_text = get_func_text(input_file)
+        kwargs_list = get_variable_expression(data_text, "data")
+        assert isinstance(kwargs_list, list), "data is not a list of dictionaries"
+        for kwargs in kwargs_list:
+            assert isinstance(kwargs, dict), "data is not a list of dictionaries"
+            # Running them
+            try:
+                val2 = f2(**kwargs)
+            except Exception as e:
+                print(f"Original file failed with exception {e}. Did you make a syntax error? Original Code:\n{func_text2}")
+                exit(0)
+
+            try:
+                val1 = f1(**kwargs)
+            except Exception as e:
+                print(f"Obfuscated file failed with exception {e}.\nGeneratedCode:{func_text1}")
+                exit(0)
+
+            if val1 != val2:
+                print(f"obfuscation failed to create an equivalent program for inputs: {kwargs}. Expected={val1}, Found={val2}.\nGeneratedCode:{returncode}")
+                exit(0)
+
+
+
+    print("Successful termination of the obfuscation tests")
+
+
