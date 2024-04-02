4830531a17bc39bcafcfd32ed8b2192a0b2c93c1
Aneesh Durg <aneeshdurg17@gmail.com>
2024-04-02 13:30:03 -0500

    [BSE-3037] Output report from QueryProfileCollector::Finalize (#7507)
    * Define C++ object for query profiling
    
    * Define python bindings
    
    * Add C++ tests
    
    * Add python test
    
    * Add comments and ensure that operator id creation is well defined
    
    * Add tracing level
    
    * [RUN CI]
    
    * include vector
    
    * [RUN CI]
    
    * include chrono
    
    * [RUN CI]
    
    * signed -> unsigned in test
    
    * [RUN CI]
    
    * shared_ptr -> unique_ptr
    
    * Remove pymod file
    
    * [RUN CI]
    
    * typo fix
    
    * [RUN CI]
    
    * typo fix
    
    * [RUN CI]
    
    * Generate output report
    
    * [RUN CI]
    
    * Disable output generation when tracing is disabled
    
    * Set default trace level to 0
    
    * [RUN CI]
    
    * Changes requested from review
    
    * [RUN CI]
    
    * Add duration to pipeline output
    
    * [RUN CI]
    
    * Fix test
    
    * [RUN CI]
    
    * [RUN CI]
    
    * [RUN CI]

--- bodo/libs/_query_profile_collector.cpp
+++ bodo/libs/_query_profile_collector.cpp
@@ -1,6 +1,10 @@
 // Copyright (C) 2024 Bodo Inc. All rights reserved.
 #include "./_query_profile_collector.h"
+#include <mpi.h>
+#include <boost/json/src.hpp>
 #include <chrono>
+#include <unordered_set>
+#include "../io/_io.h"
 #include "_bodo_common.h"
 
 void QueryProfileCollector::Init() {
@@ -53,7 +57,118 @@ void QueryProfileCollector::RegisterOperatorStageMetrics(
     operator_stage_metrics[op_stage] = metrics;
 }
 
-void QueryProfileCollector::Finalize() {}
+template <MetricTypes::TypeEnum metric_type>
+static boost::json::object metric_to_json_helper(Metric<metric_type>& metric) {
+    boost::json::object info;
+    info["start"] = metric.get();
+    return info;
+}
+
+static std::optional<boost::json::object> metric_to_json(MetricBase& metric) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (metric.is_global && rank != 0) {
+        return std::nullopt;
+    }
+    boost::json::object metric_json;
+    metric_json["name"] = metric.name;
+    metric_json["type"] = metric.type;
+
+    boost::json::object info;
+    switch (metric.type) {
+        case MetricTypes::TIMER:
+            info = metric_to_json_helper<MetricTypes::TIMER>(
+                static_cast<TimerMetric&>(metric));
+            break;
+        case MetricTypes::STAT:
+            info = metric_to_json_helper<MetricTypes::STAT>(
+                static_cast<StatMetric&>(metric));
+            break;
+        case MetricTypes::BLOB:
+            info = metric_to_json_helper<MetricTypes::BLOB>(
+                static_cast<BlobMetric&>(metric));
+    }
+    return metric_json;
+}
+
+void QueryProfileCollector::Finalize() {
+    if (tracing_level == 0) {
+        return;
+    }
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    boost::json::object profile;
+    profile["rank"] = rank;
+    profile["trace_level"] = tracing_level;
+
+    boost::json::object pipelines;
+    for (const auto& [pipeline_id, timestamps] :
+         pipeline_start_end_timestamps) {
+        boost::json::object pipeline;
+        pipeline["start"] = timestamps.first;
+        pipeline["end"] = timestamps.second;
+        pipeline["duration"] = timestamps.second - timestamps.first;
+        pipeline["num_iterations"] = pipeline_num_iterations[pipeline_id];
+        pipelines[std::to_string(pipeline_id)] = pipeline;
+    }
+    profile["pipelines"] = pipelines;
+
+    std::unordered_set<operator_stage_t> seen_operator_stages;
+    for (const auto& [op_stage, _] : operator_stage_time) {
+        seen_operator_stages.insert(op_stage);
+    }
+    for (const auto& [op_stage, _] : operator_stage_metrics) {
+        seen_operator_stages.insert(op_stage);
+    }
+    for (const auto& [op_stage, _] : operator_stage_input_row_counts) {
+        seen_operator_stages.insert(op_stage);
+    }
+    for (const auto& [op_stage, _] : operator_stage_output_row_counts) {
+        seen_operator_stages.insert(op_stage);
+    }
+    for (const auto& [op_stage, _] : operator_stage_metrics) {
+        seen_operator_stages.insert(op_stage);
+    }
+
+    boost::json::object operator_stages;
+    for (const auto& op_stage : seen_operator_stages) {
+        boost::json::object operator_stage;
+        operator_stage["time"] = operator_stage_time[op_stage];
+        if (operator_stage_input_row_counts.count(op_stage) > 0) {
+            operator_stage["input_row_count"] =
+                operator_stage_input_row_counts[op_stage];
+        }
+        if (operator_stage_output_row_counts.count(op_stage) > 0) {
+            operator_stage["output_row_count"] =
+                operator_stage_output_row_counts[op_stage];
+        }
+
+        if (operator_stage_metrics.count(op_stage) > 0) {
+            boost::json::array metrics;
+            for (auto& metric : operator_stage_metrics[op_stage]) {
+                auto metric_json = metric_to_json(metric);
+                if (metric_json.has_value()) {
+                    metrics.push_back(metric_json.value());
+                }
+            }
+            operator_stage["metrics"] = metrics;
+        }
+        operator_stages[std::to_string(op_stage)] = operator_stage;
+    }
+    profile["operator_stages"] = operator_stages;
+
+    auto s = boost::json::serialize(profile);
+
+    char filename[100];
+    snprintf(filename, 100, "query_profile_%d.json", rank);
+
+    // Note that this write is not parallel because every rank should write to
+    // it's own location.
+    file_write(filename, (void*)s.c_str(), s.size());
+}
 
 // Python Interface
 
