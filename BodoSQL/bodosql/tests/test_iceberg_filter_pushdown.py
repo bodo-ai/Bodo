624af8ce416599ce904601700057908b7abfdf25
Keaton Drebes <38367344+keatoooon@users.noreply.github.com>
2024-04-18 13:55:47 -0400

    [BSE-3125] Have a minimal reproducer for the issue (#7596)
    Minimal reproducer for this issue: https://bodo.atlassian.net/browse/BSE-3125. Committing to the repository to make sure it is not lost.

--- BodoSQL/bodosql/tests/test_iceberg_filter_pushdown.py
+++ BodoSQL/bodosql/tests/test_iceberg_filter_pushdown.py
@@ -3,6 +3,7 @@
 Basic E2E tests for each type of filter pushdown on Iceberg tables.
 """
 import io
+from datetime import date
 
 import numpy as np
 import pandas as pd
@@ -82,3 +83,70 @@ def test_filter_pushdown_in(iceberg_database):
                 stream,
                 f"bic.FilterExpr('==', [bic.ColumnRef('INT_COL_FILE_FILTER'), bic.Scalar(f{i})])",
             )
+
+
+@pytest.mark.skip("https://bodo.atlassian.net/browse/BSE-3125")
+@pytest.mark.slow
+def test_filter_pushdown_in_large_amount_of_date_scalars(iceberg_database):
+    """
+    Test for an issue related to python's bytecode.
+    Specifically, if the number of arguments supplied to bodo's make_op function is greater than ~30,
+    the calling convention will be changed to use an array instead of individual arguments.
+    """
+    table_name = "filter_pushdown_date_table_0"
+
+    input_df = pd.DataFrame(
+        {
+            "A": pd.Series(
+                [
+                    date(2018, 11, 12),
+                    date(2019, 11, 12),
+                    date(2018, 12, 12),
+                    date(2017, 11, 16),
+                    None,
+                    date(2017, 11, 30),
+                    date(2016, 2, 3),
+                    date(2019, 11, 12),
+                    date(2018, 12, 20),
+                    date(2017, 12, 12),
+                ]
+                * 5
+            ),
+        }
+    )
+
+    @run_rank0
+    def setup():
+        create_iceberg_table(
+            input_df,
+            [
+                ("A", "date", True),
+            ],
+            table_name,
+        )
+
+    setup()
+    db_schema, warehouse_loc = iceberg_database()
+    # TODO: Fix the FileSystemCatalog so that it can take in a full connection string
+    # and not just a hardcoded path.
+    catalog = bodosql.FileSystemCatalog(".")
+    bc1 = bodosql.BodoSQLContext(catalog=catalog)
+
+    filter_str = ", ".join(
+        [repr(str(date(2018, 8 + i // 20, (i % 20) + 1))) for i in range(1, 100)]
+    )
+    query = f'SELECT * FROM "{db_schema}"."{table_name}" WHERE A in ({filter_str})'
+
+    def impl(bc, q):
+        return bc.sql(q)
+
+    output_df = input_df[input_df.A == date(2018, 11, 12)]
+
+    check_func(
+        impl,
+        (bc1, query),
+        check_names=True,
+        check_dtype=False,
+        reset_index=True,
+        py_output=output_df,
+    )
