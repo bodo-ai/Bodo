27edabb600918803753baf6aa3cddebfc3c66617
Keaton Drebes <38367344+keatoooon@users.noreply.github.com>
2024-04-19 11:21:37 -0400

    [BSE-3032] Replaces python lists with java arrays in filter pushdown (#7558)
    This PR does two things:
    
        Insures that Iceberg filters are always simplified. This is needed to make sure that we properly reduce an OR of several conditions into a search arg
        Makes sure that we can handle the array generated by the resulting search arg in our iceberg filter code.

--- BodoSQL/bodosql/tests/test_iceberg_filter_pushdown.py
+++ BodoSQL/bodosql/tests/test_iceberg_filter_pushdown.py
@@ -3,6 +3,7 @@
 Basic E2E tests for each type of filter pushdown on Iceberg tables.
 """
 import io
+import string
 from datetime import date
 
 import numpy as np
@@ -57,7 +58,7 @@ def test_filter_pushdown_in(iceberg_database):
     catalog = bodosql.FileSystemCatalog(".")
     bc1 = bodosql.BodoSQLContext(catalog=catalog)
 
-    query = f'SELECT * FROM "{db_schema}"."{table_name}" WHERE INT_COL_FILE_FILTER in (1,3,5,7,9)'
+    query = f'SELECT * FROM "{db_schema}"."{table_name}" WHERE INT_COL_FILE_FILTER in (1,3,5)'
 
     def impl(bc, q):
         return bc.sql(q)
@@ -77,12 +78,110 @@ def test_filter_pushdown_in(iceberg_database):
     logger = create_string_io_logger(stream)
     with set_logging_stream(logger, 2):
         bodo.jit(impl)(bc1, query)
-        # IN statement should be expanded to an OR of multiple equality statements
-        for i in range(5):
-            check_logger_msg(
-                stream,
-                f"bic.FilterExpr('==', [bic.ColumnRef('INT_COL_FILE_FILTER'), bic.Scalar(f{i})])",
-            )
+        check_logger_msg(
+            stream,
+            "ds.field('{INT_COL_FILE_FILTER}').isin(f0))",
+        )
+
+
+@pytest.mark.slow
+def test_filter_pushdown_in_int_as_array(iceberg_database):
+    """
+    Test for handling a simple IN filter with numerous int values in the IN clause.
+    """
+    table_name = "filter_pushdown_int_table_4"
+    input_df = pd.DataFrame(
+        {
+            "ID": np.arange(10),
+            "INT_COL_FILE_FILTER": [0 for i in range(5)] + [1 for i in range(5)],
+        }
+    )
+
+    @run_rank0
+    def setup():
+        create_iceberg_table(
+            input_df,
+            [
+                ("ID", "int", False),
+                ("INT_COL_FILE_FILTER", "int", False),
+            ],
+            table_name,
+            par_spec=[PartitionField("INT_COL_FILE_FILTER", "identity", -1)],
+        )
+
+    setup()
+    db_schema, warehouse_loc = iceberg_database()
+    # TODO: Fix the FileSystemCatalog so that it can take in a full connection string
+    # and not just a hardcoded path.
+    catalog = bodosql.FileSystemCatalog(".")
+    bc1 = bodosql.BodoSQLContext(catalog=catalog)
+
+    filter_str = ", ".join([str(i) for i in range(1, 100, 2)])
+    query = f'SELECT * FROM "{db_schema}"."{table_name}" WHERE INT_COL_FILE_FILTER in ({filter_str})'
+
+    def impl(bc, q):
+        return bc.sql(q)
+
+    output_df = input_df[input_df.INT_COL_FILE_FILTER == 1]
+
+    check_func(
+        impl,
+        (bc1, query),
+        check_names=True,
+        check_dtype=False,
+        reset_index=True,
+        py_output=output_df,
+    )
+
+
+@pytest.mark.slow
+def test_filter_pushdown_in_str_as_array(iceberg_database):
+    """
+    Test for handling a simple IN filter with numerous str values in the IN clause.
+    """
+    table_name = "filter_pushdown_str_table_4"
+    input_df = pd.DataFrame(
+        {
+            "ID": np.arange(10),
+            "STR_COL_FILE_FILTER": ["a" for i in range(5)] + ["z" for i in range(5)],
+        }
+    )
+
+    @run_rank0
+    def setup():
+        create_iceberg_table(
+            input_df,
+            [
+                ("ID", "int", False),
+                ("STR_COL_FILE_FILTER", "string", False),
+            ],
+            table_name,
+            par_spec=[PartitionField("STR_COL_FILE_FILTER", "identity", -1)],
+        )
+
+    setup()
+    db_schema, warehouse_loc = iceberg_database()
+    # TODO: Fix the FileSystemCatalog so that it can take in a full connection string
+    # and not just a hardcoded path.
+    catalog = bodosql.FileSystemCatalog(".")
+    bc1 = bodosql.BodoSQLContext(catalog=catalog)
+
+    filter_str = ", ".join([repr(c) for c in string.ascii_lowercase[:-1]])
+    query = f'SELECT * FROM "{db_schema}"."{table_name}" WHERE STR_COL_FILE_FILTER in ({filter_str})'
+
+    def impl(bc, q):
+        return bc.sql(q)
+
+    output_df = input_df[input_df.STR_COL_FILE_FILTER == "a"]
+
+    check_func(
+        impl,
+        (bc1, query),
+        check_names=True,
+        check_dtype=False,
+        reset_index=True,
+        py_output=output_df,
+    )
 
 
 @pytest.mark.skip("https://bodo.atlassian.net/browse/BSE-3125")
