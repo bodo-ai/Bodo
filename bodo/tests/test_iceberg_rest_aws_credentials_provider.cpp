680450d425f8f9e078b1e1741aeb1adb453e5daf
Sahil Gupta <sahil@bodo.ai>
2024-04-26 11:24:35 -0400

    Temporarily disable Iceberg Rest `test_credentials_refresh` test (#7661)
    (As title)

--- bodo/tests/test_iceberg_rest_aws_credentials_provider.cpp
+++ bodo/tests/test_iceberg_rest_aws_credentials_provider.cpp
@@ -1,17 +1,20 @@
 #include "../io/_s3_reader.h"
 #include "./test.hpp"
 
-bodo::tests::suite icberg_rest_aws_credentials_provider_tests([] {
+bodo::tests::suite iceberg_rest_aws_credentials_provider_tests([] {
     bodo::tests::test("test_credentials_refresh", [] {
-        std::string bearer_token = getenv("TABULAR_BEARER_TOKEN");
-        bodo::tests::check(bearer_token != "",
-                           "TABULAR_BEARER_TOKEN environment variable not set");
+        return;
+        // std::string bearer_token = getenv("TABULAR_BEARER_TOKEN");
+        // bodo::tests::check(bearer_token != "",
+        //                    "TABULAR_BEARER_TOKEN environment variable not
+        //                    set");
 
-        IcebergRestAwsCredentialsProvider provider(
-            "https://api.tabular.io/ws", bearer_token,
-            "Bodo-Test-Iceberg-Warehouse", "examples", "nyc_taxi_locations", 0);
-        auto creds1 = provider.GetAWSCredentials();
-        auto creds2 = provider.GetAWSCredentials();
-        bodo::tests::check(creds1 != creds2);
+        // IcebergRestAwsCredentialsProvider provider(
+        //     "https://api.tabular.io/ws", bearer_token,
+        //     "Bodo-Test-Iceberg-Warehouse", "examples", "nyc_taxi_locations",
+        //     0);
+        // auto creds1 = provider.GetAWSCredentials();
+        // auto creds2 = provider.GetAWSCredentials();
+        // bodo::tests::check(creds1 != creds2);
     });
 });
