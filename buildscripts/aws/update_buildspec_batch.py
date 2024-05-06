1c15881c68c2389a77f83e30a9db0fbf67ea2929
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2024-05-05 22:38:38 -0400

    Update `zstd` requirement to `<=1.5.5` (#7690)
    (As title)
    
    ---------
    
    Co-authored-by: Srinivas Lade <srinulade1@gmail.com>

--- buildscripts/aws/update_buildspec_batch.py
+++ buildscripts/aws/update_buildspec_batch.py
@@ -136,7 +136,7 @@ def get_compute_type(env_var_dict, image_path, buildspec):
 # Function to generate the batch portion for the CI build
 def generate_CI_buildspec(num_groups):
     images = {
-        "linux": "427443013497.dkr.ecr.us-east-2.amazonaws.com/bodo-codebuild:6.8"
+        "linux": "427443013497.dkr.ecr.us-east-2.amazonaws.com/bodo-codebuild:6.9"
     }
     pytest_starting_marker = "not slow and not weekly"
     pytest_options = [
