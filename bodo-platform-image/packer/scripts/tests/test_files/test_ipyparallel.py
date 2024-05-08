c1feb4dfe0ba4191e85681b76e7006de8854f49c
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2024-05-08 11:56:28 -0500

    BSE-3296: Add test for Bodo IPyParallelKernel and MPIEngineSetLauncher (#7712)
    * Move IPyParallelKernel to package level import
    
    * Add launcher
    
    * Add test
    
    * [Run CI]

--- bodo-platform-image/packer/scripts/tests/test_files/test_ipyparallel.py
+++ bodo-platform-image/packer/scripts/tests/test_files/test_ipyparallel.py
@@ -1,4 +1,5 @@
 import ipyparallel as ipp
+import bodo_platform_ipyparallel_kernel as bpik
 from mpi4py import MPI
 
 
@@ -10,6 +11,20 @@ def mpi_example():
 def test_ipyparallel():
     # request an MPI cluster with 4 engines
     with ipp.Cluster(engines="mpi", n=4) as rc:
+        # get a broadcast_view on the cluster which is best
+        # suited for MPI style computation
+        view = rc.broadcast_view()
+        # run the mpi_example function on all engines in parallel
+        r = view.apply_sync(mpi_example)
+        # Retrieve and print the result from the engines
+        print("\n".join(r))
+        # at this point, the cluster processes have been shutdown
+
+
+def test_bodo_platform_ipyparallel():
+    assert hasattr(bpik, "IPyParallelKernel")
+    # request an MPI cluster with 4 engines
+    with ipp.Cluster(engines=bpik.BodoPlatformMPIEngineSetLauncher, n=4) as rc:
         # get a broadcast_view on the cluster which is best
         # suited for MPI style computation
         view = rc.broadcast_view()
