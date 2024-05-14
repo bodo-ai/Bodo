bb903ef953b139374fcb268484a97af22d703725
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2024-05-14 15:54:36 -0500

    Update single_node_install.sh  (#7738)
    Update single_node_install.sh [run ci]

--- buildscripts/build-bodo-from-source-platform/single_node_install.sh
+++ buildscripts/build-bodo-from-source-platform/single_node_install.sh
@@ -6,10 +6,11 @@ read GITHUB_TOKEN
 echo -n "Enter a branch name: "
 read BRANCH_NAME
 git clone https://$GITHUB_TOKEN@github.com/Bodo-inc/Bodo.git
+cd ~/Bodo
 git checkout $BRANCH_NAME
 sudo /opt/conda/bin/conda update conda --force --yes
 sudo /opt/conda/bin/mamba install conda-lock -c conda-forge -n base --yes
-cd Bodo/buildscripts/envs
+cd buildscripts/envs
 conda-lock install --dev --mamba -n DEV conda-lock.yml
 conda run -n DEV conda remove mpi mpich --force --yes
 cd ~/Bodo
