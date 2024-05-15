151f8d9032527c6995af5387e3ddd700c4e7cbe4
Srinivas Lade <srinivas@bodo.ai>
2024-05-15 14:53:42 -0400

    [BSE-3242] Iceberg View Inlining (#7715)

---
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/catalog/IcebergRESTCatalog.kt
@@ -0,0 +1,277 @@
+package com.bodosql.calcite.catalog
+
+import com.bodosql.calcite.adapter.bodo.StreamingOptions
+import com.bodosql.calcite.application.BodoCodeGenVisitor
+import com.bodosql.calcite.application.RelationalAlgebraGenerator
+import com.bodosql.calcite.application.write.IcebergWriteTarget
+import com.bodosql.calcite.application.write.WriteTarget
+import com.bodosql.calcite.ir.Expr
+import com.bodosql.calcite.ir.Variable
+import com.bodosql.calcite.sql.ddl.SnowflakeCreateTableMetadata
+import com.bodosql.calcite.table.CatalogTable
+import com.bodosql.calcite.table.IcebergCatalogTable
+import com.google.common.collect.ImmutableList
+import org.apache.calcite.sql.ddl.SqlCreateTable
+import org.apache.hadoop.conf.Configuration
+import org.apache.iceberg.CatalogProperties
+import org.apache.iceberg.rest.RESTCatalog
+
+open class IcebergRESTCatalog(
+    uri: String,
+    warehouse: String,
+    token: String? = null,
+    credential: String? = null,
+    val defaultSchema: String? = null,
+) : IcebergCatalog<RESTCatalog>(
+        createRestCatalog(uri, warehouse, token, credential),
+    ) {
+    /**
+     * Returns a set of all table names with the given schema name.
+     *
+     * @param schemaPath The list of schemas to traverse before finding the table.
+     * @return Set of table names.
+     */
+    override fun getTableNames(schemaPath: ImmutableList<String>): MutableSet<String> {
+        val ns = schemaPathToNamespace(schemaPath)
+        val tableNames = getIcebergConnection().listTables(ns).map { it.name() }.toMutableSet()
+        // Views are initially treated as variables, so include their names here
+        val viewNames = getIcebergConnection().listViews(ns).map { it.name() }
+        return tableNames.union(viewNames).toMutableSet()
+    }
+
+    /**
+     * Returns a table with the given name and found in the given schema.
+     *
+     * @param schemaPath The list of schemas to traverse before finding the table.
+     * @param tableName Name of the table.
+     * @return The table object.
+     */
+    override fun getTable(
+        schemaPath: ImmutableList<String>,
+        tableName: String,
+    ): CatalogTable {
+        val columns = getIcebergTableColumns(schemaPath, tableName)
+        return IcebergCatalogTable(tableName, schemaPath, columns, this)
+    }
+
+    /**
+     * Get the available subSchema names for the given path.
+     *
+     * @param schemaPath The parent schema path to check.
+     * @return Set of available schema names.
+     */
+    override fun getSchemaNames(schemaPath: ImmutableList<String>): MutableSet<String> {
+        val ns = schemaPathToNamespace(schemaPath)
+        return getIcebergConnection().listNamespaces(ns).map { it.level(it.length() - 1) }.toMutableSet()
+    }
+
+    /**
+     * Return the list of implicit/default schemas for the given catalog, in the order that they
+     * should be prioritized during table resolution. The provided depth gives the "level" at which to
+     * provide the default. Each entry in the list is a schema name at that level, not the path to reach
+     * that level.
+     *
+     * @param depth The depth at which to find the default.
+     * @return List of default Schema for this catalog.
+     */
+    override fun getDefaultSchema(depth: Int): List<String> {
+        if (depth == 0 && defaultSchema != null) {
+            return listOf(defaultSchema)
+        }
+        return listOf()
+    }
+
+    /**
+     * Return the number of levels at which a default schema may be found.
+     * Tabular catalogs don't have subSchemas, so this method always returns 1.
+     * @return The number of levels a default schema can be found.
+     */
+    override fun numDefaultSchemaLevels(): Int {
+        return 1
+    }
+
+    /**
+     * Generates the code necessary to produce an append write expression from the given catalog.
+     *
+     * @param varName Name of the variable to write.
+     * @param tableName The path of schema used to reach the table from the root that includes the
+     * table.
+     * @return The generated code to produce the append write.
+     */
+    override fun generateAppendWriteCode(
+        visitor: BodoCodeGenVisitor,
+        varName: Variable,
+        tableName: ImmutableList<String>,
+    ): Expr {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Generates the code necessary to produce a write expression from the given catalog.
+     *
+     * @param varName Name of the variable to write.
+     * @param tableName The path of schema used to reach the table from the root that includes the
+     * table.
+     * @param ifExists Behavior to perform if the table already exists
+     * @param createTableType Type of table to create if it doesn't exist
+     * @return The generated code to produce a write.
+     */
+    override fun generateWriteCode(
+        visitor: BodoCodeGenVisitor,
+        varName: Variable,
+        tableName: ImmutableList<String>,
+        ifExists: WriteTarget.IfExistsBehavior,
+        createTableType: SqlCreateTable.CreateTableType,
+        meta: SnowflakeCreateTableMetadata,
+    ): Expr {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Generates the code necessary to produce a read expression from the given catalog.
+     *
+     * @param useStreaming Should we generate code to read the table as streaming (currently only
+     * supported for snowflake tables)
+     * @param tableName The path of schema used to reach the table from the root that includes the
+     * table.
+     * @param useStreaming Should we generate code to read the table as streaming (currently only
+     * supported for snowflake tables)
+     * @param streamingOptions The options to use if streaming is enabled.
+     * @return The generated code to produce a read.
+     */
+    override fun generateReadCode(
+        tableName: ImmutableList<String>,
+        useStreaming: Boolean,
+        streamingOptions: StreamingOptions,
+    ): Expr {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Generates the code necessary to submit the remote query to the catalog DB.
+     *
+     * @param query Query to submit.
+     * @return The generated code.
+     */
+    override fun generateRemoteQuery(query: String): Expr {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Return the db location to which this Catalog refers.
+     *
+     * @return The source DB location.
+     */
+    override fun getDBType(): String {
+        return "ICEBERG"
+    }
+
+    /**
+     * Returns if a schema with the given depth is allowed to contain tables.
+     * Since Tabular catalogs don't have subSchemas, this method always returns true.
+     *
+     * @param depth The number of parent schemas that would need to be visited to reach the root.
+     * @return True
+     */
+    override fun schemaDepthMayContainTables(depth: Int): Boolean {
+        return true
+    }
+
+    /**
+     * Returns if a schema with the given depth is allowed to contain subSchemas.
+     * Tabular catalogs do not support subSchemas, so this method always returns false for non-zero values.
+     *
+     * @param depth The number of parent schemas that would need to be visited to reach the root.
+     * @return false.
+     */
+    override fun schemaDepthMayContainSubSchemas(depth: Int): Boolean {
+        return depth == 0
+    }
+
+    /**
+     * Generate a Python connection string used to read from or write to a Catalog in Bodo's SQL
+     * Python code.
+     *
+     * TODO(jsternberg): This method is needed for the XXXToPandasConverter nodes, but exposing
+     * this is a bad idea and this class likely needs to be refactored in a way that the connection
+     * information can be passed around more easily.
+     *
+     * @param schemaPath The schema component to define the connection not including the table name.
+     * @return The connection string
+     */
+    override fun generatePythonConnStr(schemaPath: ImmutableList<String>): String {
+        val props = getIcebergConnection().properties()
+        val warehouse = props[CatalogProperties.WAREHOUSE_LOCATION]!!
+        val uri = props[CatalogProperties.URI]!!.replace(Regex("https?://"), "")
+        val token =
+            if (RelationalAlgebraGenerator.hideCredentials) {
+                "********"
+            } else {
+                props["token"]
+            }
+        return "REST://%s?token=%s&warehouse=%s".format(uri, token, warehouse)
+    }
+
+    /**
+     * Return the desired WriteTarget for a create table operation.
+     * This catalog only supports Iceberg tables, so the WriteTarget will be an IcebergWriteTarget.
+     *
+     * @param schema The schemaPath to the table.
+     * @param tableName The name of the type that will be created.
+     * @param createTableType The createTable type. This is unused by the file system catalog.
+     * @param ifExistsBehavior The createTable behavior for if there is already a table defined.
+     * @param columnNamesGlobal Global Variable holding the output column names.
+     * @return The selected WriteTarget.
+     */
+    override fun getCreateTableWriteTarget(
+        schema: ImmutableList<String>,
+        tableName: String,
+        createTableType: SqlCreateTable.CreateTableType,
+        ifExistsBehavior: WriteTarget.IfExistsBehavior,
+        columnNamesGlobal: Variable,
+    ): WriteTarget {
+        return IcebergWriteTarget(
+            tableName,
+            schema,
+            ifExistsBehavior,
+            columnNamesGlobal,
+            generatePythonConnStr(schema),
+        )
+    }
+
+    companion object {
+        /**
+         * Create a RESTCatalog object from the given connection string.
+         * @param connStr The connection string to the REST catalog.
+         * @return The RESTCatalog object.
+         */
+        @JvmStatic
+        private fun createRestCatalog(
+            uri: String,
+            warehouse: String,
+            token: String?,
+            credential: String?,
+        ): RESTCatalog {
+            val params: MutableMap<String, String> = HashMap()
+
+            // Catalog URI (without parameters)
+            params[CatalogProperties.URI] = uri
+            params[CatalogProperties.WAREHOUSE_LOCATION] = warehouse
+            if (token != null) {
+                params["token"] = token
+            }
+            if (credential != null) {
+                params["credential"] = credential
+            }
+
+            val conf = Configuration()
+            for ((key, value) in params) {
+                conf[key] = value
+            }
+            val restCatalog = RESTCatalog()
+            restCatalog.setConf(conf)
+            restCatalog.initialize("RESTCatalog", params)
+            return restCatalog
+        }
+    }
+}
---
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/table/CatalogTable.kt
@@ -0,0 +1,417 @@
+package com.bodosql.calcite.table
+
+import com.bodosql.calcite.adapter.bodo.StreamingOptions
+import com.bodosql.calcite.adapter.bodo.calciteLogicalProject
+import com.bodosql.calcite.application.BodoCodeGenVisitor
+import com.bodosql.calcite.application.PythonLoggers
+import com.bodosql.calcite.application.utils.CheckTablePermissions
+import com.bodosql.calcite.application.write.WriteTarget
+import com.bodosql.calcite.catalog.BodoSQLCatalog
+import com.bodosql.calcite.ddl.DDLExecutor
+import com.bodosql.calcite.ir.Expr
+import com.bodosql.calcite.ir.Variable
+import com.bodosql.calcite.schema.ExpandViewInput
+import com.bodosql.calcite.schema.InlineViewMetadata
+import com.bodosql.calcite.table.ColumnDataTypeInfo.Companion.fromSqlType
+import com.google.common.collect.ImmutableList
+import org.apache.calcite.plan.RelOptTable
+import org.apache.calcite.rel.RelNode
+import org.apache.calcite.rel.type.RelDataTypeField
+import org.apache.calcite.schema.Table
+import org.apache.calcite.schema.TranslatableTable
+import org.apache.calcite.util.BodoStatic
+import java.util.Locale
+
+/**
+ *
+ *
+ * <h1>Stores a table with its corresponding columns</h1>
+ *
+ * @author bodo
+ */
+open class CatalogTable(
+    name: String,
+    schemaPath: ImmutableList<String>,
+    columns: List<BodoSQLColumn>,
+    // The catalog that holds this table's origin.
+    private val catalog: BodoSQLCatalog,
+) : BodoSqlTable(name, schemaPath, columns), TranslatableTable {
+    /*
+     * See the design described on Confluence:
+     * https://bodo.atlassian.net/wiki/spaces/BodoSQL/pages/1130299393/Java+Table+and+Schema+Typing#Table
+     */
+
+    /**
+     * Return the fully qualified name. This should be of the form
+     * "DATABASE_NAME"."SCHEMA_NAME"."TABLE_NAME"
+     *
+     * @return
+     */
+    fun getQualifiedName(): String {
+        val quotedPath = ImmutableList.Builder<String>()
+        for (elem in getFullPath()) {
+            quotedPath.add(String.format(Locale.ROOT, "\"%s\"", elem))
+        }
+
+        return quotedPath.build().joinToString(".")
+    }
+
+    /** Interface to get the catalog for creating RelNodes. */
+    open fun getCatalog(): BodoSQLCatalog {
+        return catalog
+    }
+
+    /**
+     * Can BodoSQL write to this table. By default, this is true but in the future this may be extended
+     * to look at the permissions given in the catalog.
+     *
+     * @return Can BodoSQL write to this table.
+     */
+    override fun isWriteable(): Boolean {
+        // TODO: Update with the ability to check permissions from the schema/catalog
+        return true
+    }
+
+    /**
+     * Generate the code needed to write the given variable to storage. This table type generates code
+     * common to all tables in the catalog.
+     *
+     * @param varName Name of the variable to write.
+     * @return The generated code to write the table.
+     */
+    override fun generateWriteCode(
+        visitor: BodoCodeGenVisitor,
+        varName: Variable,
+    ): Expr {
+        return catalog.generateAppendWriteCode(visitor, varName, getFullPath())
+    }
+
+    /**
+     * Generate the code needed to write the given variable to storage.
+     *
+     * @param varName Name of the variable to write.
+     * @param extraArgs Extra arguments to pass to the Python API. They are assume to be escaped by
+     *     the calling function and are of the form "key1=value1, ..., keyN=valueN".
+     * @return The generated code to write the table.
+     */
+    override fun generateWriteCode(
+        visitor: BodoCodeGenVisitor,
+        varName: Variable,
+        extraArgs: String,
+    ): Variable {
+        throw UnsupportedOperationException("Catalog APIs do not support additional arguments")
+    }
+
+    /**
+     * Return the location from which the table is generated. The return value is always entirely
+     * capitalized.
+     *
+     * @return The source DB location.
+     */
+    override fun getDBType(): String {
+        return catalog.getDBType().toUpperCase()
+    }
+
+    /**
+     * Generate the code needed to read the table. This table type generates code common to all tables
+     * in the catalog.
+     *
+     * @param useStreaming Should we generate code to read the table as streaming (currently only
+     *     supported for snowflake tables)
+     * @param streamingOptions Streaming-related options including batch size
+     * @return The generated code to read the table.
+     */
+    override fun generateReadCode(
+        useStreaming: Boolean,
+        streamingOptions: StreamingOptions,
+    ): Expr {
+        return catalog.generateReadCode(getFullPath(), useStreaming, streamingOptions)
+    }
+
+    /**
+     * Generate the code needed to read the table. This function is called by specialized IO
+     * implementations that require passing 1 or more additional arguments.
+     *
+     * @param extraArgs: Extra arguments to pass to the Python API. They are assume to be escaped by
+     *     the calling function and are of the form "key1=value1, ..., keyN=valueN".
+     * @return The generated code to read the table.
+     */
+    override fun generateReadCode(extraArgs: String): Expr {
+        throw UnsupportedOperationException("Catalog APIs do not support additional arguments")
+    }
+
+    override fun generateReadCastCode(varName: Variable): Expr {
+        // Snowflake catalog uses _bodo_read_date_as_dt64=True to convert date columns to datetime64
+        // without astype() calls in the IR which cause issues for limit pushdown.
+        // see BE-4238
+        return varName
+    }
+
+    /**
+     * Generates the code necessary to submit the remote query to the catalog DB. This is not
+     * supported for local tables.
+     *
+     * @param query Query to submit.
+     * @return The generated code.
+     */
+    override fun generateRemoteQuery(query: String): Expr {
+        return catalog.generateRemoteQuery(query)
+    }
+
+    override fun extend(extensionFields: List<RelDataTypeField>): Table {
+        val name = this.name
+        val extendedColumns = mutableListOf<BodoSQLColumn>()
+        extendedColumns.addAll(this.columns)
+        for (i in 0..extensionFields.size) {
+            val curField = extensionFields[i]
+            val fieldName = curField.name
+            val colType = curField.type
+            val newColType = fromSqlType(colType)
+            val newCol = BodoSQLColumnImpl(fieldName, newColType)
+            extendedColumns.add(newCol)
+        }
+        return CatalogTable(name, parentFullPath, extendedColumns, this.catalog)
+    }
+
+    /**
+     * Returns if calling `generateReadCode()` for a table will result in an IO operation in the Bodo
+     * generated code.
+     *
+     * @return Does the table require IO?
+     */
+    override fun readRequiresIO(): Boolean {
+        return true
+    }
+
+    override fun toRel(
+        toRelContext: RelOptTable.ToRelContext,
+        relOptTable: RelOptTable,
+    ): RelNode? {
+        throw UnsupportedOperationException(
+            "toRel() must be implemented by specific catalog table implementations",
+        )
+    }
+
+    /**
+     * Generate a Python connection string to read from the given catalog at the provided path.
+     *
+     * @param schemaPath The path to the table not including the table name.
+     * @return A string that can be passed to Python to read from the table.
+     */
+    fun generatePythonConnStr(schemaPath: ImmutableList<String>): String {
+        return catalog.generatePythonConnStr(schemaPath)
+    }
+
+    /**
+     * Get the insert into write target for a particular table. Most tables must maintain the same
+     * table type as already exists for the table, so this will generally be implemented by
+     * subclasses.
+     *
+     * <p>TODO: Remove as an API when the CatalogTable becomes an abstract class
+     *
+     * @param columnNamesGlobal The global variable containing the column names. This should be
+     *     possible to remove in the future since we append to a table.
+     * @return The WriteTarget for the table.
+     */
+    override fun getInsertIntoWriteTarget(columnNamesGlobal: Variable): WriteTarget {
+        throw UnsupportedOperationException("Insert into is not supported for this table")
+    }
+
+    /** Get the DDL Executor for the table. This is used to execute DDL commands on the table. */
+    open fun getDDLExecutor(): DDLExecutor {
+        throw UnsupportedOperationException("DDL operations are not supported for this table")
+    }
+
+    /**
+     * Load the view metadata information from the catalog. If the table is not a view or no
+     * information can be found this should return NULL. This should be used to implement
+     * isAccessibleView(), canSafelyInlineView(), and getViewDefinitionString().
+     *
+     *
+     * This is currently only support for Snowflake catalogs.
+     *
+     * @return The InlineViewMetadata loaded from the catalog or null if no information is available.
+     */
+    private fun tryGetViewMetadata(): InlineViewMetadata? {
+        return catalog.tryGetViewMetadata(getFullPath())
+    }
+
+    /**
+     * Is this table definitely a view (meaning we can access its definition). If this returns False
+     * we may have a view if we lack the permissions necessary to know it is a view.
+     *
+     * @return True if this is a view for which we can load metadata information.
+     */
+    fun isAccessibleView(): Boolean {
+        return tryGetViewMetadata() != null
+    }
+
+    /**
+     * Is this table actually a materialized view.
+     *
+     * @return True if this table is definitely a materialized view.
+     */
+    fun isMaterializedView(): Boolean {
+        if (isAccessibleView()) {
+            val metadata = tryGetViewMetadata()
+            return metadata!!.isMaterialized
+        }
+        return false
+    }
+
+    /**
+     * Is this table actually a secure view.
+     *
+     * @return True if this table is definitely a secure view.
+     */
+    fun isSecureView(): Boolean {
+        if (isAccessibleView()) {
+            val metadata = tryGetViewMetadata()
+            return metadata!!.unsafeToInline
+        }
+        return false
+    }
+
+    /**
+     * Is this a view that can be safely inlined.
+     *
+     * @return Returns true if table is a view and the metadata indicates inlining is legal. If this
+     * table is not a view this return false.
+     */
+    fun canSafelyInlineView(): Boolean {
+        return isAccessibleView() && !(isSecureView() || isMaterializedView())
+    }
+
+    /**
+     * Get the SQL query definition used to define this table if it is a view.
+     *
+     * @return The string definition that was used to create the view. Returns null if the table is
+     * not a view.
+     */
+    fun getViewDefinitionString(): String? {
+        return if (isAccessibleView()) {
+            tryGetViewMetadata()!!.viewDefinition
+        } else {
+            null
+        }
+    }
+
+    // Cache used for inlining views. We cannot use the Memoizer here because the
+    // ToRelContext doesn't have .equals() properly defined. Here we know that
+    // all uses of the same CatalogTable are safe to cache.
+    private val inlineViewCache: MutableMap<ExpandViewInput, RelNode?> = HashMap()
+
+    /**
+     * Inline a view. If this inlining is not possible return Null.
+     *
+     * @param toRelContext The context used for expanding the view.
+     * @param input The inputs used to call toRelContext.expandView(). This is grouped into one object
+     * for caching purposes.
+     * @return The RelNode after expanding the view or NULL.
+     */
+    private fun inlineViewImpl(
+        toRelContext: RelOptTable.ToRelContext,
+        input: ExpandViewInput,
+    ): RelNode? {
+        try {
+            val root =
+                toRelContext.expandView(
+                    input.outputType,
+                    input.viewDefinition,
+                    input.defaultPath,
+                    input.viewPath,
+                )
+            val rel = root.calciteLogicalProject()
+            // Verify that we can read before inlining.
+            if (CheckTablePermissions.canRead(rel)) {
+                return rel
+            } else {
+                throw BodoStatic.BODO_SQL_RESOURCE.noReadPermissionExpandingView(getQualifiedName()).ex()
+            }
+        } catch (e: Exception) {
+            // Log the failure
+            val message =
+                String.format(
+                    Locale.ROOT,
+                    """
+                    Unable to expand view %s with definition:
+                    %s. Error encountered when compiling view:
+                    %s
+                    """.trimIndent(),
+                    getQualifiedName(),
+                    input.viewDefinition,
+                    e.message,
+                )
+            PythonLoggers.VERBOSE_LEVEL_ONE_LOGGER.warning(message)
+        } catch (e: Error) {
+            // Log the failure
+            val message =
+                String.format(
+                    Locale.ROOT,
+                    """
+                    Unable to expand view %s with definition:
+                    %s. Error encountered when compiling view:
+                    %s
+                    """.trimIndent(),
+                    getQualifiedName(),
+                    input.viewDefinition,
+                    e.message,
+                )
+            PythonLoggers.VERBOSE_LEVEL_ONE_LOGGER.warning(message)
+        }
+        return null
+    }
+
+    /**
+     * Try to inline a view. If the view cannot be inlined then return the baseRelNode instead.
+     *
+     * @param toRelContext The context used to expand a view.
+     * @param viewDefinition The view definition.
+     * @param baseRelNode The RelNode generated if inlining this view fails.
+     * @return Either the new tree generated from inlining a view or the baseRelNode.
+     */
+    fun tryInlineView(
+        toRelContext: RelOptTable.ToRelContext,
+        viewDefinition: String,
+        baseRelNode: RelNode,
+    ): RelNode {
+        val input =
+            ExpandViewInput(
+                baseRelNode.rowType,
+                viewDefinition,
+                parentFullPath,
+                fullPath,
+            )
+        // Check the cache. We can only use the cache if the clusters
+        // are the same.
+        val result: RelNode?
+        if (inlineViewCache.containsKey(input) && inlineViewCache[input]?.cluster == toRelContext.cluster) {
+            result = inlineViewCache[input]
+        } else {
+            result = inlineViewImpl(toRelContext, input)
+            // Store in the cache
+            inlineViewCache[input] = result
+        }
+        return if (result != null) {
+            // Log that we inlined the view.
+            val levelOneMessage =
+                String.format(
+                    Locale.ROOT,
+                    "Successfully inlined view %s",
+                    getQualifiedName(),
+                )
+            PythonLoggers.VERBOSE_LEVEL_ONE_LOGGER.info(levelOneMessage)
+            val levelTwoMessage =
+                String.format(
+                    Locale.ROOT,
+                    "Replaced view %s with definition %s",
+                    getQualifiedName(),
+                    input.viewDefinition,
+                )
+            PythonLoggers.VERBOSE_LEVEL_TWO_LOGGER.info(levelTwoMessage)
+            result
+        } else {
+            baseRelNode
+        }
+    }
+}
