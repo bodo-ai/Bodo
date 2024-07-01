2a7e541868de40080e03618e9dc97e33d91eb22d
Nick Riasanovsky <nick@bodo.ai>
2024-07-01 14:24:34 -0400

    [BSE-3532] Remove condition cost inside join (#7940)

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/logicalRules/JoinConditionToFilterRule.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/application/logicalRules/JoinConditionToFilterRule.java
@@ -1,11 +1,18 @@
 package com.bodosql.calcite.application.logicalRules;
 
+import com.bodosql.calcite.application.utils.BodoJoinConditionUtil;
 import com.bodosql.calcite.application.utils.BodoSQLStyleImmutable;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.calcite.plan.RelOptRuleCall;
+import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.plan.RelRule;
 import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.core.JoinRelType;
 import org.apache.calcite.rel.rules.TransformationRule;
+import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.rex.RexUtil;
 import org.apache.calcite.tools.RelBuilder;
 import org.immutables.value.Value;
 
@@ -21,16 +28,31 @@ public class JoinConditionToFilterRule extends RelRule<JoinConditionToFilterRule
 
   @Override
   public void onMatch(RelOptRuleCall call) {
-    Join join = call.rel(0);
-
     RelBuilder builder = call.builder();
+    RexBuilder rexBuilder = builder.getRexBuilder();
+    Join join = call.rel(0);
+    RexNode existingCondition = join.getCondition();
+    List<RexNode> candidatesFilters = RelOptUtil.conjunctions(existingCondition);
+    List<RexNode> keptFilters = new ArrayList();
+    List<RexNode> movedFilters = new ArrayList();
+    int totalColumns = join.getRowType().getFieldCount();
+    int leftColumns = join.getLeft().getRowType().getFieldCount();
+
+    for (RexNode filter : candidatesFilters) {
+      if (!BodoJoinConditionUtil.isValidNode(filter)
+          && !BodoJoinConditionUtil.isPushableFunction(filter, leftColumns, totalColumns)) {
+        movedFilters.add(filter);
+      } else {
+        keptFilters.add(filter);
+      }
+    }
 
     // Set the join condition to True and
     // copy over all other attributes.
     Join newJoin =
         join.copy(
             join.getTraitSet(),
-            builder.literal(true),
+            RexUtil.composeConjunction(rexBuilder, keptFilters),
             join.getLeft(),
             join.getRight(),
             join.getJoinType(),
@@ -39,7 +61,7 @@ public class JoinConditionToFilterRule extends RelRule<JoinConditionToFilterRule
     builder.push(newJoin);
 
     // Create a filter using the join's condition
-    builder.filter(join.getCondition());
+    builder.filter(RexUtil.composeConjunction(rexBuilder, movedFilters));
 
     call.transformTo(builder.build());
   }
@@ -48,20 +70,23 @@ public class JoinConditionToFilterRule extends RelRule<JoinConditionToFilterRule
   @Value.Immutable
   public interface Config extends RelRule.Config {
     // Descriptions of classes to match. The rule matches any node, b,
-    // so long as its type is Join, and it contains an OR in the
-    // condition.
+    // so long as its type is inner Join, the join condition is not always
+    // true, and the join condition is not a valid join condition for
+    // generating code/cannot be converted to a valid join by pushing
+    // condition components into the input(s).
     //
-    // Only matches join conditions that are inner joins. This is because
-    // the pattern of pulling the join condition into a filter only works
-    // with inner joins since there's no way for filter to add a column
-    // with null values for the right side of the join after the join
-    // has been performed.
+    // Note: This doesn't work for OUTER joins because of nulls.
+    //
     JoinConditionToFilterRule.Config DEFAULT =
         ImmutableJoinConditionToFilterRule.Config.of()
             .withOperandSupplier(
                 b ->
                     b.operand(Join.class)
-                        .predicate(join -> join.getJoinType() == JoinRelType.INNER)
+                        .predicate(
+                            join ->
+                                join.getJoinType() == JoinRelType.INNER
+                                    && !join.getCondition().isAlwaysTrue()
+                                    && !BodoJoinConditionUtil.isValidNode(join.getCondition()))
                         .anyInputs())
             .as(JoinConditionToFilterRule.Config.class);
 
