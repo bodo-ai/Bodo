20d937bf94f1fd8682975cb8e0a4a41bc1fcd386
Sahil Gupta <sahil@bodo.ai>
2024-07-22 14:36:37 -0400

    [BSE-3529] Construct Dataset Scanner in C++ instead of Python (#8031)
    (As title)
    This is required for the upcoming PR that integrates our ThreadPool into Arrow. The integration point is during the Scanner creation. To require minimal changes, we do this in C++ instead.

--- bodo/libs/_stream_join.cpp
+++ bodo/libs/_stream_join.cpp
@@ -4460,7 +4460,7 @@ PyObject* get_runtime_join_filter_unique_values_py_entrypt(
         HashJoinState* join_state = (HashJoinState*)join_state_;
         std::unique_ptr<bodo::DataType>& col_type =
             join_state->build_table_schema->column_types[key_idx];
-        assert(static_cast<uint64_t>(key_idx) < join_state->n_keys);
+        assert(key_idx < static_cast<int64_t>(join_state->n_keys));
         assert(join_state->build_input_finalized);
         assert(join_state->unique_values[key_idx].has_value());
         std::unordered_set<int64_t>& unique_vals_set =
