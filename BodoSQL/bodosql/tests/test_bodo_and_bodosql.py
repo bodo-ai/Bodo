3ba2f0bf52e95674d9bddcfaccc4c94f6bcae313
Nick Riasanovsky <nick@bodo.ai>
2024-07-30 14:32:31 -0400

    Adds an API to allow reusing the same RelationAlgebraGenerator (#8118)

--- BodoSQL/bodosql/tests/test_bodo_and_bodosql.py
+++ BodoSQL/bodosql/tests/test_bodo_and_bodosql.py
@@ -6,7 +6,7 @@ JIT functions.
 import pandas as pd
 
 import bodosql
-from bodo.tests.utils import check_func
+from bodo.tests.utils import check_func, pytest_mark_one_rank
 
 
 def test_count_head(datapath, memory_leak_check):
@@ -29,3 +29,19 @@ def test_count_head(datapath, memory_leak_check):
     count = read_df.B.count()
     expected_output = pd.DataFrame({"CNT": count}, index=pd.Index([0]))
     check_func(impl, (filename,), py_output=expected_output, is_out_distributed=False)
+
+
+@pytest_mark_one_rank
+def test_planner_reset(memory_leak_check):
+    """Tests that we have an API exposed that allows resetting the planner,
+    allowing parsing multiple queries without crashing."""
+    generator = bodosql.BodoSQLContext()._create_generator(False)
+    queries = ["SELECT 1", "SELECT COUNT(*) FROM TABLE1", "Select MAX(A) FROM TABLE1"]
+    final_query = "DESCRIBE TABLE MYTABLE"
+    for query in queries:
+        generator.parseQuery(query)
+        generator.resetPlanner()
+    generator.parseQuery(final_query)
+    # Verify that the internal state is based on the final query
+    # (e.g. its been reset). All other queries are not DDL.
+    assert generator.isDDLProcessedQuery()
