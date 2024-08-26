fee2cb539c1743bd8c5fe10cf8f6d42075696123
Ehsan Totoni <ehsan.tn@gmail.com>
2024-08-26 16:00:16 -0400

    [BSE-3828] Create native type inference infrastructure in Cython/C++ (#8227)
    * Initial Python/Cython/C++ structure
    
    * unwrap args and pass to C++
    
    * clean up arguments
    
    * fix refcount
    
    * add Signature and NoneType classes
    
    * box C++ types
    
    * wrap numba.types in singleton class
    
    * call typer registry
    
    * fix compilation errors
    
    * pass function path properly
    
    * fix string error
    
    * use char* to improve performance
    
    * make NoneType singleton
    
    * create int64 singleton
    
    * pass vector by reference
    
    * make signature static
    
    * cache CType in Numba type for reuse
    
    * avoid vector alloc overheads
    
    * add comments
    
    * add comments
    
    * add comments
    
    * fix segfault
    
    * [run ci]
    
    * [run ci]
    
    * avoid exception
    
    * avoid assertion in Cython code
    
    * make typer map const
    
    * [run ci]

---
+++ bodo/transforms/typeinfer.py
@@ -0,0 +1,38 @@
+import numba
+from numba.core.typing.templates import (
+    AbstractTemplate,
+    Registry,
+)
+from numba.extending import models, register_model
+from numba.types import Function
+
+
+class BodoFunction(Function):
+    """
+    Type class for builtin functions implemented by Bodo.
+    """
+
+    def get_impl_key(self, sig):
+        return self.templates[0].key
+
+
+register_model(BodoFunction)(models.OpaqueModel)
+
+
+class BodoRegistry(Registry):
+    """Registry of functions typed by Bodo's native typer to plug into Numba"""
+
+    def __init__(self):
+        from bodo.libs.memory_budget import register_operator
+
+        super().__init__()
+
+        class BodoTemplate(AbstractTemplate):
+            key = register_operator
+            path = b"bodo.libs.memory_budget.register_operator"
+
+        self.globals.append((register_operator, BodoFunction(BodoTemplate)))
+
+
+bodo_registry = BodoRegistry()
+numba.core.registry.cpu_target.typing_context.install_registry(bodo_registry)
