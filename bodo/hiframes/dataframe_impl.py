557f53b701efa7f2c5e986ec434247eb1412ed8b
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2024-10-01 11:02:17 -0400

    [BSE-3973] Fix Azure Nightly "Div" error (#8350)
    Pandas pushed a minor release change which broke an aspect of Bodo referencing its internals, specifically regarding a "Div" module. Deleting that module from our codebase since it is only referenced in a "save-replace-restore" pattern via try-finally.

--- bodo/hiframes/dataframe_impl.py
+++ bodo/hiframes/dataframe_impl.py
@@ -6357,14 +6357,6 @@ def _parse_query_expr(
             " {0} ".format(self.op).join(parened)
         )
 
-    def Div__init__(self, lhs, rhs):
-        """
-        Replace Div.__init__ wit BinOp.__init__ to avoid checking for return type
-        since at this stage Bodo is just parsing and no evaluation information is available yet.
-        """
-        pandas.core.computation.ops.BinOp.__init__(self, "/", lhs, rhs)
-
-    saved_Div__init__ = pandas.core.computation.ops.Div.__init__  # type: ignore
     saved_rewrite_membership_op = (
         pandas.core.computation.expr.BaseExprVisitor._rewrite_membership_op  # type: ignore
     )
@@ -6382,7 +6374,6 @@ def _parse_query_expr(
         pandas.core.computation.ops.BinOp._disallow_scalar_only_bool_ops  # type: ignore
     )
     try:
-        pandas.core.computation.ops.Div.__init__ = Div__init__  # type: ignore
         pandas.core.computation.expr.BaseExprVisitor._rewrite_membership_op = (  # type: ignore
             _rewrite_membership_op
         )
@@ -6422,7 +6413,6 @@ def _parse_query_expr(
             #                undefined local variable using @
             raise BodoError(f"df.query(): undefined variable, {e}")
     finally:
-        pandas.core.computation.ops.Div.__init__ = saved_Div__init__  # type: ignore
         pandas.core.computation.expr.BaseExprVisitor._rewrite_membership_op = (  # type: ignore
             saved_rewrite_membership_op
         )
