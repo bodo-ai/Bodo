cedf05c0999b0c5009b9007f8cedc6197863d8e3
Nick Riasanovsky <nick@bodo.ai>
2024-10-06 12:28:18 -0400

    Remove the generated lib from BodoSQL (#8363)

--- BodoSQL/bodosql/__init__.py
+++ BodoSQL/bodosql/__init__.py
@@ -33,20 +33,6 @@ import bodosql.libs.sql_operators
 import bodosql.libs.ntile_helper
 import bodosql.libs.iceberg_merge_into
 
-# Import the library, throwing an error if it does not exist
-import os
-
-# TODO: put the generated library path in a global variable somewhere that is commonly accessible
-GENERATED_LIB_FILE_PATH = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "libs", "generated_lib.py"
-)
-if os.path.isfile(GENERATED_LIB_FILE_PATH):
-    import bodosql.libs.generated_lib
-else:
-    raise Exception(
-        "Error durring module import, did not find the generated library in the expected location: bodosql/libs/generated_lib.py"
-    )
-
 from bodosql.context import BodoSQLContext
 
 # ------------------------------ Version Import ------------------------------
