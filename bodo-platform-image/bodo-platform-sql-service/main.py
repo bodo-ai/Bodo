e17ffa381745d17066ad1510da99f38464b971b9
piotrszajer <85481504+piotrszajer@users.noreply.github.com>
2024-10-09 17:13:13 +0200

    BP-4862 Merge DDL service into SQL service (#8331)

--- bodo-platform-image/bodo-platform-sql-service/main.py
+++ bodo-platform-image/bodo-platform-sql-service/main.py
@@ -1,10 +1,13 @@
+import datetime
 import os
+import re
 import time
 from pathlib import Path
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
 from mpi4py import MPI
 from pydantic import BaseModel
 
@@ -19,8 +22,19 @@ from bodo_platform_utils.query import (
     get_query_metrics_file,
     check_cache_plan_location,
 )
+from bodo_platform_utils.query import write_query_metadata, consume_query_result
+from bodo_platform_utils.type_convertor import parse_output_types
 from bodo_platform_utils.utils import read_sql_query_param, read_sql_query
 
+from bodosql.context import update_schema, _PlannerType
+from bodosql.py4j_gateway import get_gateway
+from bodosql.utils import error_to_string
+from bodo.libs.distributed_api import bcast_scalar
+import numba
+import numpy as np
+import pandas as pd
+
+SQL_QUERY_TIMEOUT_SECONDS = os.environ.get('SQL_QUERY_TIMEOUT_SECONDS', 3600)
 # Set ENV variable for AzureFS authentication to look for credentials
 # Otherwise it will use anonymous access by default, only for public buckets
 # Must be set before calling `fsspec.open`
@@ -33,6 +47,14 @@ app = FastAPI()
 # Initialize the MPI communicator
 comm = MPI.COMM_WORLD
 rank = bodo.get_rank()
+
+CATALOG_CACHE = {}
+PLANNER_CACHE = {}
+QUERY_DB = {}
+
+
+class NotDDLQuery(Exception):
+    pass
 
 
 class HealthCheck(BaseModel):
@@ -47,13 +69,13 @@ class SQLQueryModel(BaseModel):
     database: Optional[str] = None
     schema: Optional[str] = None
 
-    iceberg_volume: str = None
-    iceberg_rest_url: str = None
+    iceberg_volume: Optional[str] = None
+    iceberg_rest_url: Optional[str] = None
 
-    result_dir: str
+    result_dir: Optional[str] = None
 
-    sql_query_text: Optional[str]
-    sql_query_filename: Optional[str]
+    sql_query_text: Optional[str] = None
+    sql_query_filename: Optional[str] = None
     sql_query_param_filename: Optional[str] = None
 
     verbose_filename: Optional[str] = None
@@ -68,16 +90,203 @@ def health_endpoint():
 
 @app.post("/api/v1/sql")
 def sql_endpoint(data: SQLQueryModel):
-    logger.info(f"rank {rank} received query model: {data}")
-    req = comm.Ibarrier()
-    req.Wait()
+    QUERY_DB[data.job_uuid] = {"status": "RUNNING", "start": datetime.datetime.now()}
+    try:
+        return DDLExecutor(data).execute()
+    except NotDDLQuery:
+        logger.info(f"rank {rank} received query model: {data}")
+        req = comm.Ibarrier()
+        req.Wait()
 
-    comm.bcast(data, root=rank)
-    run_sql_query(data)
-    return {}
+        comm.bcast(data, root=rank)
+        handle_non_ddl_query(data)
+        return {}
+    finally:
+        del QUERY_DB[data.job_uuid]
 
 
-def run_sql_query(data: SQLQueryModel):
+@app.get("/api/v1/sql")
+def get_running_queries():
+    return [{"job_uuid": uuid, "status": data.get("status"), "start": data.get('start')} for uuid, data in QUERY_DB.items()]
+
+
+class DDLExecutor:
+    def __init__(self, data: SQLQueryModel):
+        self.data = data
+        bsql_catalog = self.get_catalog_data()
+        logger.info(f"Job: {data.job_uuid} create context")
+        self.bc = bodosql.BodoSQLContext(catalog=bsql_catalog)
+        logger.info(f"Job: {data.job_uuid} context created")
+        logger.info(f"Job: {data.job_uuid} create generator")
+        self.plan_generator = self.get_plan_generator()
+        self.plan_generator.resetPlanner()
+        logger.info(f"Job: {data.job_uuid} planner reset")
+        logger.info(f"Job: {data.job_uuid} execute query")
+
+    def execute(self):
+        try:
+            result = self.execute_if_ddl_query()
+        except NotDDLQuery:
+            raise
+        except Exception as e:
+            logger.error(e)
+            raise HTTPException(status_code=500, detail=f"{error_to_string(e)}")
+        return self.parse_result(result)
+
+    def parse_result(self, result):
+        if result is not None:
+            total_len = len(result)
+            try:
+                schema = parse_output_types(result)
+            except Exception:
+                logger.error('Clould not parse schema')
+                schema = {"error": "Could not parse schema"}
+            if self.data.result_dir:
+                # Write schema to metadata.json
+                metadata_file = os.path.join(self.data.result_dir, "metadata.json")
+                write_query_metadata(schema, total_len, metadata_file)
+
+                # Write output to parquet file
+                result_pq = os.path.join(self.data.result_dir, "output.pq")
+                consume_query_result(result, result_pq, False)
+
+            return {
+                "result": result.to_dict(orient="records"),
+                "metadata": {"num_rows": total_len, "schema": schema},
+            }
+        return None
+
+    def execute_if_ddl_query(self):
+        sql_query_text = self.get_query()
+        match = re.match("^select ([0-9]+)$", sql_query_text, re.IGNORECASE)
+
+        if match:
+            sql_query_text = match.groups()[0]
+            logger.info(f"Job: {self.data.job_uuid} query is select {sql_query_text}")
+            return self.execute_select_const(sql_query_text)
+
+        if self.is_ddl_query(sql_query_text):
+            logger.info(f"Job: {self.data.job_uuid} query is ddl")
+            ddl_result = self.plan_generator.executeDDL(sql_query_text)
+            # Convert the output to a DataFrame.
+            column_names = [x for x in ddl_result.getColumnNames()]
+            data = [pd.array(column, dtype=object) for column in ddl_result.getColumnValues()]
+            df_dict = {column_names[i]: data[i] for i in range(len(column_names))}
+            return pd.DataFrame(df_dict)
+        raise NotDDLQuery
+
+    def get_query(self):
+        sql_query_text = self.data.sql_query_text
+        if self.data.sql_query_filename:
+            sql_query_text = read_sql_query(self.data.sql_query_filename)
+        return sql_query_text
+
+    def is_ddl_query(self, query):
+        try:
+            logger.info(f"Job: {self.data.job_uuid} parse query")
+            self.plan_generator.parseQuery(query)
+
+            logger.info(f"Job: {self.data.job_uuid} get write type")
+            write_type = self.plan_generator.getWriteType(query)
+            logger.info(f"Job: {self.data.job_uuid} write type is {write_type}")
+
+            logger.info(f"Job: {self.data.job_uuid} update schema")
+            update_schema(
+                self.bc.schema,
+                self.bc.names,
+                self.bc.df_types,
+                self.bc.estimated_row_counts,
+                self.bc.estimated_ndvs,
+                self.bc.orig_bodo_types,
+                False,
+                write_type,
+            )
+        except Exception as e:
+            err = {
+                "type": "string_type",
+                "loc": ["body", "query"],
+                "msg": f"Unable to parse SQL Query. Error message:\n{e}",
+                "input": query,
+            }
+
+            raise RequestValidationError([err])
+
+        logger.info(f"Job: {self.data.job_uuid} process ddl")
+        is_ddl = self.plan_generator.isDDLProcessedQuery()
+        is_ddl = bcast_scalar(is_ddl)
+        logger.info(f"Job: {self.data.job_uuid} ddl: {is_ddl}")
+        return is_ddl
+
+    def execute_select_const(self, constant):
+        return pd.DataFrame({
+            'EXP': np.int64(constant)
+        }, index=[0])
+
+    @staticmethod
+    def get_relation_algebra_generator():
+        gateway = get_gateway()
+        gateway.jvm.System.currentTimeMillis()
+        return gateway.jvm.com.bodosql.calcite.application.RelationalAlgebraGenerator
+
+    def get_plan_generator(self):
+        if self.data.catalog in PLANNER_CACHE:
+            logger.info(
+                f"Job: {self.data.job_uuid} plan generator catalog: {self.data.catalog} is in cache"
+            )
+            return PLANNER_CACHE[self.data.catalog]
+
+        logger.info(
+            f"Job: {self.data.job_uuid} plan generator catalog: {self.data.catalog} not in cache"
+        )
+        if bodo.bodosql_use_streaming_plan:
+            planner_type = _PlannerType.Streaming.value
+        else:
+            planner_type = _PlannerType.Volcano.value
+
+        logger.info(
+            f"Job: {self.data.job_uuid} create new plan generator catalog {self.data.catalog} type {planner_type}"
+        )
+
+        relation_algebra_generator = self.get_relation_algebra_generator()
+        plan_generator = relation_algebra_generator(
+            self.bc.catalog.get_java_object(),
+            self.bc.schema,
+            planner_type,
+            0,
+            0,
+            bodo.bodosql_streaming_batch_size,
+            False,
+            bodo.enable_snowflake_iceberg,
+            bodo.enable_timestamp_tz,
+            bodo.enable_runtime_join_filters,
+            bodo.enable_streaming_sort,
+            bodo.enable_streaming_sort_limit_offset,
+            bodo.bodo_sql_style,
+            bodo.bodosql_full_caching,
+        )
+
+        logger.info(
+            f"Job: {self.data.job_uuid} plan generator catalog: {self.data.catalog} type: {planner_type} created"
+        )
+        PLANNER_CACHE[self.data.catalog] = plan_generator
+        return plan_generator
+
+    def get_catalog_data(self):
+        if not self.data.catalog:
+            return bodosql.FileSystemCatalog(".")
+
+        # Catalog can be cached by name since names are guaranteed to be unique
+        if self.data.catalog not in CATALOG_CACHE:
+            logger.info(f"Catalog {self.data.catalog} NOT in cache")
+            bsql_catalog = create_catalog(self.data)
+            CATALOG_CACHE[self.data.catalog] = bsql_catalog
+        else:
+            logger.info(f"Catalog {self.data.catalog} in cache")
+
+        return CATALOG_CACHE.get(self.data.catalog)
+
+
+def handle_non_ddl_query(data: SQLQueryModel):
     setup_bodo_logger(data.verbose_filename)
     is_root_rank = rank == 0
 
@@ -175,7 +384,7 @@ def main():
     if is_primary() and rank in bodo.get_nodes_first_ranks():
         logger.info(f"rank {rank} waiting for query from endpoint ...")
         comm.allreduce(rank, op=MPI.SUM)
-        uvicorn.run(app, port=5000)
+        uvicorn.run(app, port=5000, timeout_keep_alive=SQL_QUERY_TIMEOUT_SECONDS)
     else:
         bcast_rank = comm.allreduce(0, op=MPI.SUM)
 
@@ -187,7 +396,7 @@ def main():
                 time.sleep(0.05)
 
             query = comm.bcast(None, root=bcast_rank)
-            run_sql_query(query)
+            handle_non_ddl_query(query)
 
 
 if __name__ == "__main__":
