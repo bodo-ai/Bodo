a9014bea748e386e1f49c332f5f5fcce38c2cbe7
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2024-10-10 11:58:16 -0500

    Fix wheel conda channel (#8377)
    Changes pip wheels to not use bodo.ai channel for pyarrow packages, tested with 2024.10 https://github.com/Bodo-inc/Bodo/actions/runs/11259355286

--- buildscripts/bodo/pip/manylinux/setup_env.sh
+++ buildscripts/bodo/pip/manylinux/setup_env.sh
@@ -11,9 +11,15 @@ PYTHON_VERSION=$(echo "$PYTHON_VERSION" | cut -d. -f1,2)
 eval "$(micromamba shell hook --shell bash)"
 # Replace python version in main.yml
 sed  "s/- python=.*/- python=${PYTHON_VERSION}/g" "${ENVS_PATH}/main.yml" > "${ENVS_PATH}/main-mod-version.yml"
+# Replace bodo.ai::pyarrow with conda-forge::pyarrow
+sed  "s_bodo.ai/linux-64::__g" "$ENVS_PATH/main-mod-version.yml" > "${ENVS_PATH}/main-mod-version-2.yml"
+# Remove bodo.ai channel to use conda-forge pyarrow in main
+sed  "/  - bodo.ai/d" "$ENVS_PATH/main-mod-version-2.yml" > "${ENVS_PATH}/main-mod-version-3.yml"
+# Remove bodo.ai channel to use conda-forge pyarrow in dev
+sed  "/  - bodo.ai/d" "$ENVS_PATH/dev.yml" > "${ENVS_PATH}/dev-mod-version.yml"
 # Create the lock file
 pip install conda-lock
-conda-lock -f "${ENVS_PATH}"/main-mod-version.yml -f "${ENVS_PATH}"/dev.yml -c conda-forge
+conda-lock -f "${ENVS_PATH}"/main-mod-version-3.yml -f "${ENVS_PATH}"/dev-mod-version.yml -c conda-forge
 # Create the environment
 micromamba install --category main --category dev -f conda-lock.yml -y --force-reinstall
 
