bb2406534a6ce4fdf7ab8a2ee3ba5ebd1d05c90a
Sahil Gupta <sahil@bodo.ai>
2024-10-10 07:25:41 -0400

    [BSE-3990] Add a simple `ASSERT` macro for checking assertions in release builds (#8372)
    (As title)
    
    For now, I'm only using it in `_stream_groupby.cpp`, but we can add it in other places in the future.

--- bodo/libs/_utils.h
+++ bodo/libs/_utils.h
@@ -33,3 +33,19 @@ std::optional<std::chrono::steady_clock::time_point> start_now(bool get);
 
 /// @brief Get the amount of physically installed memory in bytes
 uint64_t get_physically_installed_memory();
+
+// Helper macro to check for conditions. Unlike the builtin 'assert', this is
+// executed even in release builds, so it should be used judiciously. It
+// includes the line number and file name (basename) in the error message.
+// NOTE: The calling file must include <fmt/format.h>.
+#define ASSERT_WITH_ERR_MSG(x, ERR_MSG_PREFIX)                            \
+    do {                                                                  \
+        if (!(x)) {                                                       \
+            std::string err_msg =                                         \
+                fmt::format("{}Failed assertion '{}' on line {} in {}.",  \
+                            ERR_MSG_PREFIX, #x, __LINE__, __FILE_NAME__); \
+            throw std::runtime_error(err_msg);                            \
+        }                                                                 \
+    } while (0)
+
+#define ASSERT(x) ASSERT_WITH_ERR_MSG(x, "")
