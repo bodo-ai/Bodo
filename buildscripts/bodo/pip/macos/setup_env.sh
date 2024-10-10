85982c18ae1916dc8096d36c9645a10127c1bf36
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2024-10-10 11:58:16 -0500

    Fix wheel conda channel (#8377)
    Changes pip wheels to not use bodo.ai channel for pyarrow packages, tested with 2024.10 https://github.com/Bodo-inc/Bodo/actions/runs/11259355286

--- buildscripts/bodo/pip/macos/setup_env.sh
+++ buildscripts/bodo/pip/macos/setup_env.sh
@@ -10,6 +10,11 @@ micromamba remove -n build-env --all || true-
 PYTHON_VERSION=$(echo "$PYTHON_VERSION" | cut -d. -f1,2)
 # Replace python version in main.yml
 sed -i '' "s/^  - python=.*/  - python=${PYTHON_VERSION}/" "$ENVS_PATH/main.yml"
+# Remove bodo.ai channel to use conda-forge pyarrow main
+sed -i '' "/  - bodo.ai/d" "$ENVS_PATH/main.yml"
+# Remove bodo.ai channel to use conda-forge pyarrow dev
+sed -i '' "/  - bodo.ai/d" "$ENVS_PATH/dev.yml"
+
 pip install conda-lock
 # Create the lock file
 conda-lock -f "$ENVS_PATH"/main.yml -f "$ENVS_PATH"/dev.yml
