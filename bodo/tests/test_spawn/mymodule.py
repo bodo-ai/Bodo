83f61a3a6507439bdc8566a014c799664198e240
Aneesh Durg <aneeshdurg17@gmail.com>
2024-10-23 09:16:14 -0400

    [BSE-4045] Fix recursive compilation in spawn mode (#8390)
    * prototype
    
    * Fix
    
    * Fix
    
    * fix module test
    
    * Add test of recursive compilation across modules
    
    * Remove prototype tests
    
    * [run CI]
    
    * fix test/improve name
    
    * [run CI]
    
    * Fix typo
    
    * Fix unpickled object using the wrong compiler
    
    * [run CI]
    
    * debug
    
    * [run CI]
    
    * Don't execute spawn test with mpiexec
    
    * [run CI]
    
    * Fix test
    
    * [run CI]
    
    * Only allow NP=0 for spawn tests
    
    * Comments
    
    * [run CI]
    
    * Use BODO_TEST_SPAWN_MODe instead of NP=0
    
    * Make SubmitDispatcher construct the real dispatcher
    
    * Directly execute module importing tests
    
    * [run CI]
    
    * Remove special handling for spawn tests/mpiexec
    
    * [run CI]
    
    * Undo comment change
    
    * [run CI]
    
    * Remove outdated comment
    
    * [run CI]
    
    * Relax assertion to allow for mangled lines from mpi prepend rank
    
    * [run CI]
    
    * skip test
    
    * [run CI]
    
    * Relax check on output
    
    * [run CI]
    
    * Skip flaky test
    
    * [run CI]

--- bodo/tests/test_spawn/mymodule.py
+++ bodo/tests/test_spawn/mymodule.py
@@ -1,3 +1,5 @@
+from bodo.submit.spawner import submit_jit
+
 # TODO(aneesh): this print stmt helps us track how many times mymodule was
 # imported. In the future, when we can pass environment variables across the
 # spawner/executor, we should have this module write to a file controlled by an
@@ -10,3 +12,8 @@ print("imported mymodule")
 def f():
     # TODO(aneesh): replace this print with returning an actual value
     print("called mymodule.f")
+
+
+@submit_jit
+def jit_fn():
+    print("called mymodule.jit_fn")
