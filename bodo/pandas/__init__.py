0c371a1024bccdb432d0a3bfa11a8efa9885d9cf
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2024-11-08 10:53:24 -0600

    BSE-4111: Support Lazy Datastructures as function args in spawn mode (#8493)
    * Add tests
    
    * Add spawn=True
    
    * Refactor ArgMetadata into enum
    
    * Support lazy data structures as args to spawn funcs
    
    * Add more tests
    
    * Expand testing to kwargs
    
    * Get updated arguments from workers
    
    * Comment
    
    * Add todo
    
    * Add BodoLazyWrapper ABC
    
    * Refactor DistributedReturnMetadata into LazyMetadata
    
    * Move get_lazy_manager(single)_class
    
    * Add from_lazy_metadata and update_from_lazy_metadata
    
    * Clean up tests
    
    * Add missing index_data
    
    * Bugfix
    
    * [Run CI]
    
    * Review comments
    
    * [Run CI]

--- bodo/pandas/__init__.py
+++ bodo/pandas/__init__.py
@@ -3,24 +3,8 @@ from bodo.pandas.series import BodoSeries
 from bodo.pandas.arrow.array import LazyArrowExtensionArray
 from bodo.pandas.managers import LazyBlockManager, LazySingleBlockManager
 from bodo.pandas.array_manager import LazyArrayManager, LazySingleArrayManager
-from pandas._config.config import _get_option
-import typing as pt
+from bodo.pandas.lazy_wrapper import BodoLazyWrapper
+from bodo.pandas.lazy_metadata import LazyMetadata
+from bodo.pandas.utils import get_lazy_manager_class, get_lazy_single_manager_class
 
 
-def get_lazy_manager_class() -> pt.Type[LazyArrayManager | LazyBlockManager]:
-    """Get the lazy manager class based on the pandas option mode.data_manager, suitable for DataFrame."""
-    data_manager = _get_option("mode.data_manager", silent=True)
-    if data_manager == "block":
-        return LazyBlockManager
-    elif data_manager == "array":
-        return LazyArrayManager
-    raise Exception(f"Got unexpected value of pandas option mode.manager: {data_manager}")
-
-def get_lazy_single_manager_class() -> pt.Type[LazySingleArrayManager | LazySingleBlockManager]:
-    """Get the lazy manager class based on the pandas option mode.data_manager, suitable for Series."""
-    data_manager = _get_option("mode.data_manager", silent=True)
-    if data_manager == "block":
-        return LazySingleBlockManager
-    elif data_manager == "array":
-        return LazySingleArrayManager
-    raise Exception(f"Got unexpected value of pandas option mode.manager: {data_manager}")
--- bodo/tests/test_spawn/test_input_output.py
+++ bodo/tests/test_spawn/test_input_output.py
@@ -6,9 +6,16 @@ import pytest
 from pandas.core.indexes.range import RangeIndex
 
 import bodo
+from bodo.pandas.frame import BodoDataFrame
+from bodo.pandas.series import BodoSeries
 from bodo.tests.dataframe_common import df_value  # noqa
 from bodo.tests.series_common import series_val  # noqa
-from bodo.tests.utils import _get_dist_arg, check_func, pytest_spawn_mode
+from bodo.tests.utils import (
+    _get_dist_arg,
+    _test_equal_guard,
+    check_func,
+    pytest_spawn_mode,
+)
 
 pytestmark = pytest_spawn_mode
 
@@ -112,3 +119,185 @@ def test_scalar_tuple_return():
         (pd.DataFrame({"A": [1, 5, 8]}),),
         only_spawn=True,
     )
+
+
+def test_bodo_dataframe_arg_doesnt_collect():
+    """
+    Test that passing a BodoDataFrame as an argument to a function doesn't collect the data
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(df):
+        return df
+
+    df = pd.DataFrame({"A": [1, 2, 3] * 100})
+    bodo_df = impl(df)
+    assert isinstance(bodo_df, BodoDataFrame)
+    new_bodo_df = impl(bodo_df)
+    # Test that using it as an arg doesn't collect the result
+    assert bodo_df._mgr._md_result_id is not None
+    _test_equal_guard(bodo_df, df, check_pandas_types=False)
+    _test_equal_guard(new_bodo_df, df, check_pandas_types=False)
+
+
+def test_bodo_dataframe_arg_updates():
+    """
+    Test that passing a BodoDataFrame as an argument to a function updates the data returned when collected
+    """
+
+    @bodo.jit(spawn=True)
+    def _get_bodo_df(df):
+        return df
+
+    @bodo.jit(spawn=True)
+    def impl(df, kwarg=None):
+        df["A"] += 3
+        if kwarg is not None:
+            kwarg["A"] += 3
+        return 1
+
+    df = pd.DataFrame({"A": [1, 2, 3] * 100})
+    arg_bodo_df = _get_bodo_df(df)
+    kwarg_bodo_df = _get_bodo_df(df)
+    assert isinstance(arg_bodo_df, BodoDataFrame)
+    assert isinstance(kwarg_bodo_df, BodoDataFrame)
+    impl(arg_bodo_df, kwarg=kwarg_bodo_df)
+
+    _test_equal_guard(arg_bodo_df.head(), df.head() + 3, check_pandas_types=False)
+    _test_equal_guard(kwarg_bodo_df.head(), df.head() + 3, check_pandas_types=False)
+    # Check that the BodoDataFrameas are still lazy
+    assert arg_bodo_df._lazy
+    assert kwarg_bodo_df._lazy
+
+    _test_equal_guard(arg_bodo_df, df + 3, check_pandas_types=False)
+    _test_equal_guard(kwarg_bodo_df, df + 3, check_pandas_types=False)
+
+
+def test_bodo_dataframe_arg_collected():
+    """
+    Test that passing a BodoDataFrame that's been collected as an argument to a function works
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(df):
+        return df
+
+    df = pd.DataFrame({"A": [1, 2, 3] * 100})
+    bodo_df = impl(df)
+    assert isinstance(bodo_df, BodoDataFrame)
+    # Force collection
+    bodo_df["A"].sum()
+    new_bodo_df = impl(bodo_df)
+    assert isinstance(new_bodo_df, BodoDataFrame)
+    _test_equal_guard(new_bodo_df, df, check_pandas_types=False)
+
+
+def test_bodo_dataframe_arg_modified_on_spawner():
+    """
+    Test that passing a BodoDataFrame that's been modified works as expected
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(df):
+        return df
+
+    df = pd.DataFrame({"A": [1, 2, 3] * 100})
+    bodo_df = impl(df)
+    assert isinstance(bodo_df, BodoDataFrame)
+    bodo_df.loc[0, "A"] = 0
+    new_bodo_df = impl(bodo_df)
+    assert isinstance(new_bodo_df, BodoDataFrame)
+    _test_equal_guard(new_bodo_df, bodo_df, check_pandas_types=False)
+
+
+def test_bodo_series_arg_doesnt_collect():
+    """
+    Test that passing a BodoSeries as an argument to a function doesn't collect the data
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(series):
+        return series
+
+    series = pd.Series([1, 2, 3] * 100)
+    bodo_series = impl(series)
+    assert isinstance(bodo_series, BodoSeries)
+    new_bodo_series = impl(bodo_series)
+    # Test that using it as an arg doesn't collect the result
+    assert bodo_series._mgr._md_result_id is not None
+    _test_equal_guard(bodo_series, series, check_pandas_types=False)
+    _test_equal_guard(new_bodo_series, series, check_pandas_types=False)
+
+
+def test_bodo_series_arg_updates():
+    """
+    Test that passing a BodoSeries as an argument to a function updates the data returned when collected
+    """
+
+    @bodo.jit(spawn=True)
+    def _get_bodo_series(series):
+        return series
+
+    @bodo.jit(spawn=True)
+    def impl(series, kwarg=None):
+        series += 3
+        if kwarg is not None:
+            kwarg += 3
+        return 1
+
+    series = pd.Series([1, 2, 3] * 100)
+    arg_bodo_series = _get_bodo_series(series)
+    kwarg_bodo_series = _get_bodo_series(series)
+    assert isinstance(arg_bodo_series, BodoSeries)
+    assert isinstance(kwarg_bodo_series, BodoSeries)
+    impl(arg_bodo_series, kwarg=kwarg_bodo_series)
+
+    _test_equal_guard(
+        arg_bodo_series.head(), series.head() + 3, check_pandas_types=False
+    )
+    _test_equal_guard(
+        kwarg_bodo_series.head(), series.head() + 3, check_pandas_types=False
+    )
+    # Check that the BodoSeries are still lazy
+    assert arg_bodo_series._lazy
+    assert kwarg_bodo_series._lazy
+
+    _test_equal_guard(arg_bodo_series, series + 3, check_pandas_types=False)
+    _test_equal_guard(kwarg_bodo_series, series + 3, check_pandas_types=False)
+
+
+def test_bodo_series_arg_collected():
+    """
+    Test that passing a BodoSeries that's been collected as an argument to a function works
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(series):
+        return series
+
+    series = pd.Series([1, 2, 3] * 100)
+    bodo_series = impl(series)
+    assert isinstance(bodo_series, BodoSeries)
+    # Force collection
+    bodo_series.sum()
+    new_bodo_series = impl(bodo_series)
+    assert isinstance(new_bodo_series, BodoSeries)
+    _test_equal_guard(new_bodo_series, series, check_pandas_types=False)
+
+
+def test_bodo_series_arg_modified_on_spawner():
+    """
+    Test that passing a BodoSeries that's been modified works as expected
+    """
+
+    @bodo.jit(spawn=True)
+    def impl(series):
+        return series
+
+    series = pd.Series([1, 2, 3] * 100)
+    bodo_series = impl(series)
+    assert isinstance(bodo_series, BodoSeries)
+    bodo_series[0] = 0
+    new_bodo_series = impl(bodo_series)
+    assert isinstance(new_bodo_series, BodoSeries)
+    _test_equal_guard(new_bodo_series, bodo_series, check_pandas_types=False)
