c29f01defd76e84aec37b1be9281d11b94c9a60e
Srinivas Lade <srinulade1@gmail.com>
2024-12-03 14:30:27 -0500

    Removed files

--- buildscripts/aws/merge_with_develop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -exo pipefail
-
-# File for merging with develop. Doesn't work if the merge has to be done manually.
-
-# Set info to avoid errors
-git config --global user.email "nick@bodo.ai"
-git config --global user.name "Nick Riasanovsky"
-git pull --no-edit origin develop
