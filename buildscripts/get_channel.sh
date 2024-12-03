c906836aa6df0ee1014e2d853f58004549af0b47
Ehsan Totoni <ehsan.tn@gmail.com>
2024-12-03 11:05:51 -0500

    [BSE-4267] remove license checks for open source release (#8671)
    * remove license check
    
    * update env variables
    
    * update channels
    
    * remove gen license script
    
    * update license in meta.yaml
    
    * review feedback
    
    * remove platform license dependencies

--- buildscripts/get_channel.sh
+++ buildscripts/get_channel.sh
@@ -3,16 +3,7 @@ set -exo pipefail
 
 PLATFORM_DEV_RELEASE=${1:-false}
 IS_RELEASE=`git tag --points-at HEAD`
-if [[ $CHECK_LICENSE_EXPIRED  == 1 ]]; then
-    # if it's not a release, publish to the dev channel
-    if [[ -z $IS_RELEASE ]]; then
-        artifactory_channel="bodo.ai-dev"
-    else
-        artifactory_channel="bodo.ai"
-    fi
-elif [[ $CHECK_LICENSE_PLATFORM == 1 ]]; then
-        artifactory_channel="bodo.ai-platform-dev"
-elif [[ $CHECK_LICENSE_PLATFORM == 1 ]]; then
+if [[ $IS_BODO_PLATFORM == 1 ]]; then
     # if release or platform-dev-release, then to bodo.ai-platform, else to bodo.ai-platform-dev
     if [[ ! -z "$IS_RELEASE" ]] || [[ "$PLATFORM_DEV_RELEASE" == "true" ]]; then
         artifactory_channel="bodo.ai-platform"
@@ -20,6 +11,6 @@ elif [[ $CHECK_LICENSE_PLATFORM == 1 ]]; then
         artifactory_channel="bodo.ai-platform-dev"
     fi
 else
-    artifactory_channel="bodo-binary"
+    artifactory_channel="bodo.ai"
 fi
 echo $artifactory_channel
