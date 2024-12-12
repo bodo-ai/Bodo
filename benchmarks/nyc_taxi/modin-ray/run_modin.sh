f09e83ee94d5e775a49029f10e9d92cf2d30ebe8
scott-routledge2 <scott@bodo.ai>
2024-12-12 17:35:04 -0500

    [BSE-4341] Update benchmarking and finalize scripts (#31)
    * refactored nyc_taxi benchmark related files -> nyc_taxi directory to match the structure of the tpch benchmark and set us up for adding other benchmarks.
    * Added script to run bodo example on platform using SDK, currently supports "WORKSPACE" type although could be moved to github once we fix that API
    * Renamed files with typo in name
    * Other cleanup to have dask, bodo example write to parquet file on one of the workers.

---
+++ benchmarks/nyc_taxi/modin-ray/run_modin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# start cluster
+ray up modin-cluster.yaml -y
+
+# scale cluster up to 256 vCPUs
+ray submit modin-cluster.yaml scale_cluster.py 256
+
+# run full benchmark 
+# WARNING! This might take up to 3 hours.
+ray submit modin-cluster.yaml nyc_taxi_precipitation.py 
+ray submit modin-cluster.yaml nyc_taxi_precipitation.py 
+ray submit modin-cluster.yaml nyc_taxi_precipitation.py 
+
+# finally, tear down the cluster
+ray down modin-cluster.yaml -y
+
