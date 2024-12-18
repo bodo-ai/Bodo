9156537671d085d15b5861d594d50f22a50f04fd
= <=>
2024-12-18 14:10:18 -0500

    check output with pandas

--- BodoSQL/bodosql/tests/test_quickstart_docs.py
+++ BodoSQL/bodosql/tests/test_quickstart_docs.py
@@ -3,16 +3,17 @@ import pandas as pd
 import pytest
 
 import bodosql
-from bodo.tests.utils import temp_env_override
+from bodo.tests.utils import pytest_spawn_mode, temp_env_override
 from bodo.utils.testing import ensure_clean2
 
-pytestmark = pytest.mark.test_docs
+pytestmark = pytest_spawn_mode + [pytest.mark.test_docs]
 
 
 def test_quickstart_local_sql():
     """Test example equivalent to docs/quick_start/quickstart_local_sql.md"""
+    print("running test")
     NUM_GROUPS = 30
-    NUM_ROWS = 20_000_000
+    NUM_ROWS = 2_000
 
     df = pd.DataFrame({"A": np.arange(NUM_ROWS) % NUM_GROUPS, "B": np.arange(NUM_ROWS)})
 
@@ -20,9 +21,12 @@ def test_quickstart_local_sql():
     with ensure_clean2(output_df_path):
         df.to_parquet(output_df_path)
 
-        with temp_env_override({"BODO_SPAWN_MODE": "1"}):
+        with temp_env_override("BODO_SPAWN_MODE", "1"):
             bc = bodosql.BodoSQLContext(
                 {"TABLE1": bodosql.TablePath(output_df_path, "parquet")}
             )
 
-            bc.sql("SELECT SUM(A) as SUM_OF_COLUMN_A FROM TABLE1 WHERE B > 4")
+            out_df = bc.sql("SELECT SUM(A) as SUM_OF_COLUMN_A FROM TABLE1 WHERE B > 4")
+
+            answer_df = pd.DataFrame({"SUM_OF_COLUMN_A": [28890]})
+            pd.testing.assert_frame_equal(answer_df, out_df, check_dtype=False)
--- bodo/tests/test_quickstart_docs.py
+++ bodo/tests/test_quickstart_docs.py
@@ -3,9 +3,10 @@ import pandas as pd
 import pytest
 
 import bodo
+from bodo.tests.utils import pytest_spawn_mode
 from bodo.utils.testing import ensure_clean2
 
-pytestmark = pytest.mark.test_docs
+pytestmark = pytest_spawn_mode + [pytest.mark.test_docs]
 
 
 def test_quickstart_local_python():
@@ -14,7 +15,7 @@ def test_quickstart_local_python():
     """
     # Generate sample data
     NUM_GROUPS = 30
-    NUM_ROWS = 20_000_000
+    NUM_ROWS = 2_000
 
     df = pd.DataFrame({"A": np.arange(NUM_ROWS) % NUM_GROUPS, "B": np.arange(NUM_ROWS)})
 
@@ -24,7 +25,6 @@ def test_quickstart_local_python():
     with ensure_clean2(input_df_path):
         df.to_parquet(input_df_path)
 
-        @bodo.jit(cache=True, spawn=True)
         def computation():
             df = pd.read_parquet(input_df_path)
             df2 = pd.DataFrame(
@@ -33,24 +33,31 @@ def test_quickstart_local_python():
             df2.to_parquet(output_df_path)
 
         with ensure_clean2(output_df_path):
+            bodo.jit(cache=True, spawn=True)(computation)()
+            bodo_out = pd.read_parquet(output_df_path)
+
+        with ensure_clean2(output_df_path):
             computation()
+            pandas_out = pd.read_parquet(output_df_path)
+
+        pd.testing.assert_frame_equal(bodo_out, pandas_out)
 
 
 @pytest.mark.iceberg
 def test_quickstart_local_iceberg():
     """Test that the example in docs/quick_start/quickstart_local_iceberg.md"""
     NUM_GROUPS = 30
-    NUM_ROWS = 20_000_000
+    NUM_ROWS = 2_000
 
     db_name = "MY_DATABASE"
+    input_df = pd.DataFrame(
+        {"A": np.arange(NUM_ROWS) % NUM_GROUPS, "B": np.arange(NUM_ROWS)}
+    )
 
     with ensure_clean2(db_name):
 
         @bodo.jit(spawn=True)
-        def example_write_iceberg_table():
-            df = pd.DataFrame(
-                {"A": np.arange(NUM_ROWS) % NUM_GROUPS, "B": np.arange(NUM_ROWS)}
-            )
+        def example_write_iceberg_table(df):
             df.to_sql(
                 name="MY_TABLE",
                 con=f"iceberg://{db_name}",
@@ -58,14 +65,14 @@ def test_quickstart_local_iceberg():
                 if_exists="replace",
             )
 
-        example_write_iceberg_table()
+        example_write_iceberg_table(input_df)
 
         @bodo.jit(spawn=True)
         def example_read_iceberg():
             df = pd.read_sql_table(
                 table_name="MY_TABLE", con=f"iceberg://{db_name}", schema="MY_SCHEMA"
             )
-            print(df)
             return df
 
-        example_read_iceberg()
+        out_df = example_read_iceberg()
+        pd.testing.assert_frame_equal(out_df, input_df)
