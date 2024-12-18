74864ee413a45315a5c8245504817b03f661625b
Ehsan Totoni <ehsan.tn@gmail.com>
2024-12-18 17:46:10 -0500

    Avoid logging in __del__ functions [run ci]

--- bodo/pandas/managers.py
+++ bodo/pandas/managers.py
@@ -244,10 +244,6 @@ class LazyBlockManager(BlockManager, LazyMetadataMixin[BlockManager]):
         Delete the result from the workers if it hasn't been collected yet.
         """
         if (r_id := self._md_result_id) is not None:
-            debug_msg(
-                self.logger,
-                f"[LazyBlockManager] Asking workers to delete result '{r_id}'",
-            )
             assert self._del_func is not None
             self._del_func(r_id)
             self._del_func = None
@@ -437,10 +433,6 @@ class LazySingleBlockManager(SingleBlockManager, LazyMetadataMixin[SingleBlockMa
         Delete the result from the workers if it hasn't been collected yet.
         """
         if (r_id := self._md_result_id) is not None:
-            debug_msg(
-                self.logger,
-                f"[LazySingleBlockManager] Asking workers to delete result '{r_id}'",
-            )
             assert self._del_func is not None
             self._del_func(r_id)
             self._del_func = None
