b7af43710763411b70f7124e65b5bf4dab8aab3d
= <=>
2025-01-02 14:02:58 -0500

    run subset of the tests

--- e2e-tests/search_grids/test_search_grids.py
+++ e2e-tests/search_grids/test_search_grids.py
@@ -9,6 +9,7 @@ import bodo
 BUCKET_NAME = "s3://bodotest-customer-data/search_grid"
 
 
+# @pytest.mark.skip
 def test_search(tmp_path):
     # Get latest AWS creds for any pandas S3 operation.
     # The env vars should be populated by the assume_iam_role
