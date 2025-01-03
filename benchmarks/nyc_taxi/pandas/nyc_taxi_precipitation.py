e87087a8caa9256e0084e3a375b41d25936ec4f2
Srinivas Lade <srinulade1@gmail.com>
2025-01-03 15:50:39 -0500

    [run ci] better default

--- benchmarks/nyc_taxi/pandas/nyc_taxi_precipitation.py
+++ benchmarks/nyc_taxi/pandas/nyc_taxi_precipitation.py
@@ -96,7 +96,7 @@ def get_monthly_travels_weather(weather_dataset, hvfhv_dataset):
     end = time.time()
     print("Writing time:", (end - start_write))
     print("Total E2E time:", (end - start_read))
-    return monthly_trips_weather
+    return
 
 
 if __name__ == "__main__":
