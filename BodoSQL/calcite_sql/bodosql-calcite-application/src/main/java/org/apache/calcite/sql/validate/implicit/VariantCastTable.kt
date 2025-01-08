f660666a53c5a22b08edaed5bc0de8c2a59f46a4
Nicholas <njriasanovsky@berkeley.edu>
2025-01-08 15:25:21 -0500

    Minor code refactoring

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/validate/implicit/VariantCastTable.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/sql/validate/implicit/VariantCastTable.kt
@@ -120,13 +120,13 @@ internal class VariantCastTable {
         }
 
         /**
-         * @param prec The precision of the decimal type.
+         * @param precision The precision of the decimal type.
          * @return An integer type that can represent it.
          */
-        private fun chooseIntegerType(prec: Int): SqlTypeName =
-            if (prec < 3) { SqlTypeName.TINYINT } else {
-                if (prec < 5) { SqlTypeName.SMALLINT } else {
-                    if (prec < 10) { SqlTypeName.INTEGER } else { SqlTypeName.BIGINT }
+        private fun chooseIntegerType(precision: Int): SqlTypeName =
+            if (precision < 3) { SqlTypeName.TINYINT } else {
+                if (precision < 5) { SqlTypeName.SMALLINT } else {
+                    if (precision < 10) { SqlTypeName.INTEGER } else { SqlTypeName.BIGINT }
                 }
             }
 
@@ -134,10 +134,10 @@ internal class VariantCastTable {
          * @param precisions: the precision that each argument should be casted to.
          *
          * @return A function that returns the type to implicitly cast each variant
-         * arguments to, where each argument is casted to a decimal type with the
+         * arguments to, where each argument is cast to a decimal type with the
          * specified precision.
          *
-         * Note: for the time being it is casted to an integer type based on
+         * Note: for the time being it is cast to an integer type based on
          * the precision. In future, we will switch this to use the correct
          * decimal type.
          */
