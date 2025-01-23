68a50843d71e3fea08c604acc02e509d4bdc2367
= <=>
2025-01-23 10:41:01 -0500

    fix link

--- benchmarks/nyc_taxi/modin_ray/nyc_taxi_precipitation.py
+++ benchmarks/nyc_taxi/modin_ray/nyc_taxi_precipitation.py
@@ -98,5 +98,5 @@ if __name__ == "__main__":
     print("RAY CPU COUNT: ", cpu_count)
 
     weather_dataset = "s3://bodo-example-data/nyc-taxi/central_park_weather.csv"
-    hvfhv_dataset = "s3://bodo-example-data/nyc-taxi/fhvhv/"
+    hvfhv_dataset = "s3://bodo-example-data/nyc-taxi/fhvhv_tripdata/"
     get_monthly_travels_weather(weather_dataset, hvfhv_dataset)
