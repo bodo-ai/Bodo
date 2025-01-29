39489f098f50158fa8186a13e7a080bff65726d3
Ehsan Totoni <ehsan.tn@gmail.com>
2025-01-29 12:21:24 -0500

    fix relative path

--- bodo/io/fs_io.py
+++ bodo/io/fs_io.py
@@ -653,7 +653,9 @@ def directory_of_files_common_filter(fname):
     return not (
         fname.endswith(".crc")  # Checksums
         or fname.endswith("_$folder$")  # HDFS directories in S3
-        or fname.startswith(".")  # Hidden files starting with .
+        or (
+            fname.startswith(".") and not fname.startswith("./")
+        )  # Hidden files starting with .
         or fname.startswith("_")
         and fname != "_delta_log"  # Hidden files starting with _ skip deltalake
     )
