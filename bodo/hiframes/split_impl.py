6907d0e78efefd893af33c5ba9a8bb0f9dea5e93
Ehsan Totoni <ehsan.tn@gmail.com>
2025-01-31 17:14:24 -0500

    Upgrade to Numba 0.61  (#98)

--- bodo/hiframes/split_impl.py
+++ bodo/hiframes/split_impl.py
@@ -220,7 +220,7 @@ def box_str_arr_split_view(typ, val, c):
 
     # create array of objects with num_items shape
     mod_name = context.insert_const_string(builder.module, "numpy")
-    np_class_obj = c.pyapi.import_module_noblock(mod_name)
+    np_class_obj = c.pyapi.import_module(mod_name)
     dtype = c.pyapi.object_getattr_string(np_class_obj, "object_")
     l_num_items = builder.sext(sp_view.num_items, c.pyapi.longlong)
     num_items_obj = c.pyapi.long_from_longlong(l_num_items)
