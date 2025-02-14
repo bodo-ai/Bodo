4abcfc0fd250fb82574597657cd40a2f96b2fb23
Ehsan Totoni <ehsan.tn@gmail.com>
2025-02-14 11:11:19 -0500

    Support decimal multiply on Windows

--- bodo/libs/_decimal_ext.cpp
+++ bodo/libs/_decimal_ext.cpp
@@ -854,16 +854,22 @@ arrow::Decimal128 multiply_decimal_scalars_util(
 /**
  * @brief Python entry point to multiply two decimal scalars.
  */
-arrow::Decimal128 multiply_decimal_scalars_py_entry(
-    arrow::Decimal128 v1, int64_t p1, int64_t s1, arrow::Decimal128 v2,
-    int64_t p2, int64_t s2, int64_t out_precision, int64_t out_scale,
-    bool* overflow) noexcept {
+void multiply_decimal_scalars_py_entry(uint64_t v1_low, int64_t v1_high,
+                                       int64_t p1, int64_t s1, uint64_t v2_low,
+                                       int64_t v2_high, int64_t p2, int64_t s2,
+                                       int64_t out_precision, int64_t out_scale,
+                                       uint64_t* out_low_ptr,
+                                       uint64_t* out_high_ptr,
+                                       bool* overflow) noexcept {
     try {
-        return multiply_decimal_scalars_util(
+        arrow::Decimal128 v1(v1_high, v1_low);
+        arrow::Decimal128 v2(v2_high, v2_low);
+        arrow::Decimal128 res = multiply_decimal_scalars_util(
             v1, p1, s1, v2, p2, s2, out_precision, out_scale, overflow);
+        *out_low_ptr = res.low_bits();
+        *out_high_ptr = res.high_bits();
     } catch (const std::exception& e) {
         PyErr_SetString(PyExc_RuntimeError, e.what());
-        return arrow::Decimal128(0);
     }
 }
 
--- bodo/libs/decimal_arr_ext.py
+++ bodo/libs/decimal_arr_ext.py
@@ -1941,14 +1941,18 @@ def _multiply_decimal_scalars(typingctx, d1_t, d2_t, precision_t, scale_t):
         fnty = lir.FunctionType(
             lir.IntType(128),
             [
-                lir.IntType(128),
-                lir.IntType(64),
-                lir.IntType(64),
-                lir.IntType(128),
                 lir.IntType(64),
                 lir.IntType(64),
                 lir.IntType(64),
                 lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64),
+                lir.IntType(64).as_pointer(),  # out_low_ptr
+                lir.IntType(64).as_pointer(),  # out_high_ptr
                 lir.IntType(1).as_pointer(),
             ],
         )
@@ -1960,23 +1964,32 @@ def _multiply_decimal_scalars(typingctx, d1_t, d2_t, precision_t, scale_t):
             builder.module, fnty, name="multiply_decimal_scalars"
         )
         overflow_pointer = cgutils.alloca_once(builder, lir.IntType(1))
-        ret = builder.call(
+        d1_low, d1_high = _ll_get_int128_low_high(builder, d1)
+        d2_low, d2_high = _ll_get_int128_low_high(builder, d2)
+        out_low_ptr = cgutils.alloca_once(builder, lir.IntType(64))
+        out_high_ptr = cgutils.alloca_once(builder, lir.IntType(64))
+        builder.call(
             fn,
             [
-                d1,
+                d1_low,
+                d1_high,
                 d1_precision_const,
                 d1_scale_const,
-                d2,
+                d2_low,
+                d2_high,
                 d2_precision_const,
                 d2_scale_const,
                 output_precision,
                 output_scale,
+                out_low_ptr,
+                out_high_ptr,
                 overflow_pointer,
             ],
         )
         bodo.utils.utils.inlined_check_and_propagate_cpp_exception(context, builder)
+        res = _ll_int128_from_low_high(builder, out_low_ptr, out_high_ptr)
         overflow = builder.load(overflow_pointer)
-        return context.make_tuple(builder, signature.return_type, [ret, overflow])
+        return context.make_tuple(builder, signature.return_type, [res, overflow])
 
     output_decimal_type = Decimal128Type(output_precision, output_scale)
     ret_type = types.Tuple([output_decimal_type, types.bool_])
