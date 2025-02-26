619ba6a2f7f8d924609d759a9e5a9253d559f51a
Scott Routledge <scott@bodo.ai>
2025-02-26 16:31:48 -0500

    add env vars

--- buildscripts/bodo/conda-recipe/bld.bat
+++ buildscripts/bodo/conda-recipe/bld.bat
@@ -1,6 +1,11 @@
 echo "building bodo for Windows OS!!"
 
+set SETUPTOOLS_SCM_PRETEND_VERSION="%PKG_VERSION%"
+
+set CONDA_PREFIX="%PREFIX%"
+set CMAKE_GENERATOR="Visual Studio 17 2022"
+
 :: Build using pip and CMake
 "%PYTHON%" -m pip install --no-deps --no-build-isolation -vv ^
     --config-settings=build.verbose=true ^
-    --config-settings=logging.level="DEBUG" ^ .
+    --config-settings=logging.level="DEBUG" .
