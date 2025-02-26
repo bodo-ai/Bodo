29a04c16bf13e456373792d1c99ba441fc639f69
Scott Routledge <scott@bodo.ai>
2025-02-26 17:54:01 -0500

    test version

--- buildscripts/bodo/conda-recipe/bld.bat
+++ buildscripts/bodo/conda-recipe/bld.bat
@@ -1,11 +1,16 @@
 echo "building bodo for Windows OS!!"
 
-set SETUPTOOLS_SCM_PRETEND_VERSION="%PKG_VERSION%"
+echo %VSINSTALLDIR%
 
-set CONDA_PREFIX="%PREFIX%"
-set CMAKE_GENERATOR="Visual Studio 17 2022"
+set "SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%"
+
+set "CONDA_PREFIX=%PREFIX%"
+set "CMAKE_GEN=Visual Studio 17 2022"
 
 :: Build using pip and CMake
 "%PYTHON%" -m pip install --no-deps --no-build-isolation -vv ^
     --config-settings=build.verbose=true ^
     --config-settings=logging.level="DEBUG" .
+
+
+@REM conda build . -c conda-forge --no-verify --no-anaconda-upload --croot C:\Users\owner\dev\conda-bld --build-id-pat bb --python 3.12
