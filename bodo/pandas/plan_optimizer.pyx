c8c15dc2dd468877fd045b1a3b886fa8a15294e1
Ehsan Totoni <ehsan.tn@gmail.com>
2025-03-20 11:10:09 -0400

    add join wrapper

--- bodo/pandas/plan_optimizer.pyx
+++ bodo/pandas/plan_optimizer.pyx
@@ -1,5 +1,33 @@
 # distutils: language = c++
 # cython: c_string_type=unicode, c_string_encoding=utf8
 
+from libcpp.memory cimport unique_ptr, make_unique
+
+
+cdef extern from "duckdb/common/enums/join_type.hpp" namespace "duckdb" nogil:
+    cdef enum class CJoinType" duckdb::JoinType":
+        INVALID "duckdb::JoinType::INVALID"
+        LEFT "duckdb::JoinType::LEFT"
+        RIGHT "duckdb::JoinType::RIGHT"
+        INNER "duckdb::JoinType::INNER"
+        OUTER "duckdb::JoinType::OUTER"
+        SEMI "duckdb::JoinType::SEMI"
+        ANTI "duckdb::JoinType::ANTI"
+        MARK "duckdb::JoinType::MARK"
+        SINGLE "duckdb::JoinType::SINGLE"
+        RIGHT_SEMI "duckdb::JoinType::RIGHT_SEMI"
+        RIGHT_ANTI "duckdb::JoinType::RIGHT_ANTI"
+
+
+cdef extern from "duckdb/planner/operator/logical_comparison_join.hpp" namespace "duckdb" nogil:
+    cdef cppclass CLogicalComparisonJoin" duckdb::LogicalComparisonJoin":
+        CLogicalComparisonJoin(CJoinType join_type)
+
+
 cdef class LogicalComparisonJoin:
-    pass
+    cdef:
+        unique_ptr[CLogicalComparisonJoin] c_logical_comparison_join
+
+    def __cinit__(self):
+       # self.c_logical_comparison_join = make_unique[CLogicalComparisonJoin]()
+       pass
