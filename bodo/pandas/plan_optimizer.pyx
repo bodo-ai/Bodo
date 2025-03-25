bdf4a25a013ff59fc24b9d24c08797899653788a
Ehsan Totoni <ehsan.tn@gmail.com>
2025-03-25 10:02:36 -0400

    Add comments, str and test

--- bodo/pandas/plan_optimizer.pyx
+++ bodo/pandas/plan_optimizer.pyx
@@ -1,3 +1,5 @@
+"""Provides wrappers around DuckDB's nodes and optimizer for use in Python.
+"""
 # distutils: language = c++
 # cython: c_string_type=unicode, c_string_encoding=utf8
 
@@ -22,11 +24,47 @@ cdef extern from "duckdb/common/enums/join_type.hpp" namespace "duckdb" nogil:
 cdef extern from "duckdb/planner/operator/logical_comparison_join.hpp" namespace "duckdb" nogil:
     cdef cppclass CLogicalComparisonJoin" duckdb::LogicalComparisonJoin":
         CLogicalComparisonJoin(CJoinType join_type)
+        CJoinType join_type
+
+
+def join_type_to_string(CJoinType join_type):
+    """
+    Convert a CJoinType enum to a string representation for printing purposes.
+    """
+    if join_type == CJoinType.INVALID:
+        return "INVALID"
+    elif join_type == CJoinType.LEFT:
+        return "LEFT"
+    elif join_type == CJoinType.RIGHT:
+        return "RIGHT"
+    elif join_type == CJoinType.INNER:
+        return "INNER"
+    elif join_type == CJoinType.OUTER:
+        return "OUTER"
+    elif join_type == CJoinType.SEMI:
+        return "SEMI"
+    elif join_type == CJoinType.ANTI:
+        return "ANTI"
+    elif join_type == CJoinType.MARK:
+        return "MARK"
+    elif join_type == CJoinType.SINGLE:
+        return "SINGLE"
+    elif join_type == CJoinType.RIGHT_SEMI:
+        return "RIGHT_SEMI"
+    elif join_type == CJoinType.RIGHT_ANTI:
+        return "RIGHT_ANTI"
+    else:
+        raise ValueError("Unknown Join Type")
 
 
 cdef class LogicalComparisonJoin:
+    """Wrapper around DuckDB's LogicalComparisonJoin to provide access in Python.
+    """
     cdef:
         unique_ptr[CLogicalComparisonJoin] c_logical_comparison_join
 
     def __cinit__(self, CJoinType join_type):
        self.c_logical_comparison_join = make_unique[CLogicalComparisonJoin](join_type)
+
+    def __str__(self):
+        return f"LogicalComparisonJoin({join_type_to_string(self.c_logical_comparison_join.get().join_type)})"
