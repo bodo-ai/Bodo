09f7b87b5733658acd35030b1f6bffc0906f46af
Ehsan Totoni <ehsan.tn@gmail.com>
2025-03-26 12:10:08 -0400

    add Parquet node test

--- bodo/tests/test_df_lib/test_plan.py
+++ bodo/tests/test_df_lib/test_plan.py
@@ -11,3 +11,10 @@ def test_join_node():
         bodo.pandas.plan_optimizer.CJoinType.INNER
     )
     assert str(A) == "LogicalComparisonJoin(INNER)"
+
+
+def test_parquet_node():
+    """Make Sure Cython wrapper around the Parquet node works. Just tests node creation."""
+    A = bodo.pandas.plan_optimizer.LogicalGetParquetRead(b"example.parquet")
+    assert str(A) == "LogicalGetParquetRead(example.parquet)"
+    assert A.path == "example.parquet"
