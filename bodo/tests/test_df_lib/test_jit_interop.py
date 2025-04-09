a5716dd8c61b5abd54b50a3513584dc6c28ac952
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-04-09 15:56:26 -0700

    Add tests

---
+++ bodo/tests/test_df_lib/test_jit_interop.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+import bodo
+import bodo.pandas as bd
+from bodo.tests.utils import _test_equal, pytest_mark_spawn_mode
+
+
+def test_bodo_df_to_jit():
+    df = pd.DataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": [4, 5, 6],
+            "c": ["a", "b", "c"],
+        }
+    )
+    bdf = bd.from_pandas(df)
+
+    @bodo.jit()
+    def f(df):
+        return df
+
+    _test_equal(f(bdf), df)
+
+
+@pytest_mark_spawn_mode
+def test_bodo_df_to_jit_spawn():
+    df = pd.DataFrame(
+        {
+            "a": [1, 2, 3],
+            "b": [4, 5, 6],
+            "c": ["a", "b", "c"],
+        }
+    )
+    bdf = bd.from_pandas(df)
+
+    @bodo.jit(spawn=True)
+    def f(df):
+        return df
+
+    _test_equal(f(bdf), df)
