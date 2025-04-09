85ab0bdee50ad1db7d7522bd99db625cbaa39a35
Scott Routledge <scott@bodo.ai>
2025-04-09 10:21:59 -0400

    add test

--- bodo/tests/test_df_lib/test_read_parquet.py
+++ bodo/tests/test_df_lib/test_read_parquet.py
@@ -15,3 +15,19 @@ def test_read_parquet(datapath):
         bodo_out,
         py_out,
     )
+
+
+def test_read_parquet_len_shape(datapath):
+    """Test length after read parquet is correct"""
+    path = datapath("example.parquet")
+
+    bodo_out = bd.read_parquet(path)
+    py_out = pd.read_parquet(path)
+
+    len(bodo_out) == py_out
+
+    # create a new lazy DF
+    bodo_out2 = bd.read_parquet(path)
+
+    # test shape
+    bodo_out2.shape == py_out.shape
