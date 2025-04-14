bec2781c1ae8c6f036e28b5be1aa2110e052bffb
Srinivas Lade <srinulade1@gmail.com>
2025-04-14 12:01:01 -0400

    mini test5

--- bodo/pandas/vendor/duckdb/test.cpp
+++ bodo/pandas/vendor/duckdb/test.cpp
@@ -3,17 +3,13 @@
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/main/database.hpp"
 
-duckdb::ClientContext &get_duckdb_context() {
+int main() {
     std::cerr << "get_duckdb_context" << '\n';
     duckdb::DuckDB db(nullptr);
     std::cerr << "get_duckdb_context context" << '\n';
     duckdb::ClientContext context(db.instance);
     std::cerr << "get_duckdb_context done" << '\n';
-    return context;
-}
 
-int main() {
-	duckdb::ClientContext &context = get_duckdb_context();
 	std::cerr << std::boolalpha << "ClientContext initialized " << context.interrupted << '\n';
 	return 0;
 }
