74c374f921bac7baab8f9b161c0f90603528556e
Srinivas Lade <srinulade1@gmail.com>
2025-04-14 11:40:44 -0400

    mini test3

--- bodo/pandas/vendor/duckdb/test.cpp
+++ bodo/pandas/vendor/duckdb/test.cpp
@@ -3,17 +3,17 @@
 #include "duckdb/main/client_context.hpp"
 #include "duckdb/main/database.hpp"
 
-// duckdb::ClientContext &get_duckdb_context() {
-//     std::cerr << "get_duckdb_context" << '\n';
-//     static duckdb::DuckDB db(nullptr);
-//     std::cerr << "get_duckdb_context context" << '\n';
-//     static duckdb::ClientContext context(db.instance);
-//     std::cerr << "get_duckdb_context done" << '\n';
-//     return context;
-// }
+duckdb::ClientContext &get_duckdb_context() {
+    std::cerr << "get_duckdb_context" << '\n';
+    static duckdb::DuckDB db(nullptr);
+    std::cerr << "get_duckdb_context context" << '\n';
+    static duckdb::ClientContext context(db.instance);
+    std::cerr << "get_duckdb_context done" << '\n';
+    return context;
+}
 
 int main() {
-	// duckdb::ClientContext &context = get_duckdb_context();
-	std::cerr << std::boolalpha << "ClientContext initialized " << false << '\n';
+	duckdb::ClientContext &context = get_duckdb_context();
+	std::cerr << std::boolalpha << "ClientContext initialized " << context.interrupted << '\n';
 	return 0;
 }
