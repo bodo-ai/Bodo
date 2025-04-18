b7c7704ca21b25386a5479cc43acd501cec8ff4a
Scott Routledge <scott@bodo.ai>
2025-04-18 15:21:31 -0400

    fix mac build

--- bodo/pandas/physical/expression.h
+++ bodo/pandas/physical/expression.h
@@ -268,9 +268,8 @@ class PhysicalComparisonExpression : public PhysicalExpression {
             std::swap(left_as_scalar, right_as_scalar);
         }
         // Create output boolean array same size as input.
-        std::shared_ptr<array_info> result =
-            std::move(alloc_nullable_array_no_nulls(input_batch->nrows(),
-                                                    Bodo_CTypes::_BOOL));
+        std::shared_ptr<array_info> result = alloc_nullable_array_no_nulls(
+            input_batch->nrows(), Bodo_CTypes::_BOOL);
         // Get uint8_t raw data pointer for the boolean output array.
         uint8_t *result_data1 =
             result->data1<bodo_array_type::NULLABLE_INT_BOOL, uint8_t>();
