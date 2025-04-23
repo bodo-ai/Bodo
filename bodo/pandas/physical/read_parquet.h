dcef73d833724ebb753f2e7f030fd3ecd1103543
Todd A. Anderson <toddanderson@bodo.ai>
2025-04-23 13:33:40 -0700

    Code review changes.

--- bodo/pandas/physical/read_parquet.h
+++ bodo/pandas/physical/read_parquet.h
@@ -5,19 +5,10 @@
 #include <arrow/python/pyarrow.h>
 #include <memory>
 #include <utility>
+#include "../io/arrow_compat.h"
 #include "../io/parquet_reader.h"
 #include "_duckdb_util.h"
 #include "operator.h"
-
-// When generating the PyObject filter expression for passing to the internal
-// read parquet function, we can either generate Python objects directly or
-// generate C++ arrow::compute::Expression and then as a last step convert that
-// to Python.  The latter approach is cleaner and preferred but the convert to
-// Python function is segfaulting now.  Once that is no longer segfaulting then
-// permanently remove the Python version of the code below.
-#define GEN_PYTHON
-
-#ifndef GEN_PYTHON
 
 std::function<arrow::compute::Expression(arrow::compute::Expression,
                                          arrow::compute::Expression)>
@@ -40,67 +31,14 @@ expressionTypeToArrowCompute(const duckdb::ExpressionType &expr_type) {
     }
 }
 
-#else
-
-std::string expressionTypeToArrowCompute(
-    const duckdb::ExpressionType &expr_type) {
-    switch (expr_type) {
-        case duckdb::ExpressionType::COMPARE_EQUAL:
-            return "equal";
-        case duckdb::ExpressionType::COMPARE_NOTEQUAL:
-            return "not_equal";
-        case duckdb::ExpressionType::COMPARE_GREATERTHAN:
-            return "greater";
-        case duckdb::ExpressionType::COMPARE_LESSTHAN:
-            return "less";
-        case duckdb::ExpressionType::COMPARE_GREATERTHANOREQUALTO:
-            return "greater_equal";
-        case duckdb::ExpressionType::COMPARE_LESSTHANOREQUALTO:
-            return "less_equal";
-        default:
-            throw std::runtime_error("Unhandled comparison expression type.");
-    }
-}
-
-PyObject *valueToPyObject(const duckdb::Value &value) {
-    duckdb::LogicalTypeId type = value.type().id();
-    switch (type) {
-        case duckdb::LogicalTypeId::TINYINT:
-            return PyLong_FromLong(value.GetValue<int8_t>());
-        case duckdb::LogicalTypeId::SMALLINT:
-            return PyLong_FromLong(value.GetValue<int16_t>());
-        case duckdb::LogicalTypeId::INTEGER:
-            return PyLong_FromLong(value.GetValue<int32_t>());
-        case duckdb::LogicalTypeId::BIGINT:
-            return PyLong_FromLong(value.GetValue<int64_t>());
-        case duckdb::LogicalTypeId::FLOAT:
-            return PyFloat_FromDouble(value.GetValue<float>());
-        case duckdb::LogicalTypeId::DOUBLE:
-            return PyFloat_FromDouble(value.GetValue<double>());
-        default:
-            throw std::runtime_error("valueToPyObject unhandled type" +
-                                     std::to_string(static_cast<int>(type)));
-    }
-}
-
-#endif
-
 PyObject *tableFilterSetToArrowCompute(duckdb::TableFilterSet &filters,
                                        PyObject *schema_fields) {
     PyObject *ret = Py_None;
     if (filters.filters.size() == 0) {
         return ret;
     }
-#ifdef GEN_PYTHON
+    arrow::py::import_pyarrow_wrappers();
     std::vector<PyObject *> to_be_freed;
-
-    // Import pyarrow.compute
-    PyObject *pyarrow_compute = PyImport_ImportModule("pyarrow.compute");
-    if (!pyarrow_compute) {
-        throw std::runtime_error("Failed to import pyarrow.compute");
-    }
-    to_be_freed.push_back(pyarrow_compute);
-#endif
 
     if (filters.filters.size() > 1) {
         throw std::runtime_error(
@@ -116,12 +54,12 @@ PyObject *tableFilterSetToArrowCompute(duckdb::TableFilterSet &filters,
                         std::move(tf.second));
                 PyObject *py_selected_field =
                     PyList_GetItem(schema_fields, tf.first);
+                to_be_freed.push_back(py_selected_field);
                 if (!PyUnicode_Check(py_selected_field)) {
                     throw std::runtime_error(
                         "tableFilterSetToPhysicalExpression selected field is "
                         "not unicode object.");
                 }
-#ifndef GEN_PYTHON
                 auto column_ref = arrow::compute::field_ref(
                     PyUnicode_AsUTF8(py_selected_field));
                 auto scalar_val = std::visit(
@@ -132,55 +70,7 @@ PyObject *tableFilterSetToArrowCompute(duckdb::TableFilterSet &filters,
                 auto expr = expressionTypeToArrowCompute(
                     constantFilter->comparison_type)(column_ref, scalar_val);
 
-                ret = pyarrow_wrap_expression(expr);
-#else
-                PyObject *field_func =
-                    PyObject_GetAttrString(pyarrow_compute, "field");
-                PyObject *scalar_func =
-                    PyObject_GetAttrString(pyarrow_compute, "scalar");
-                PyObject *comp_func = PyObject_GetAttrString(
-                    pyarrow_compute, expressionTypeToArrowCompute(
-                                         constantFilter->comparison_type)
-                                         .c_str());
-                if (!field_func || !scalar_func || !comp_func) {
-                    throw std::runtime_error(
-                        "tableFilterSetToPhysicalExpression failed to create "
-                        "field, scalar, or comp.");
-                }
-                to_be_freed.push_back(field_func);
-                to_be_freed.push_back(scalar_func);
-                to_be_freed.push_back(comp_func);
-
-                // Create "column_name" field object
-                PyObject *field_obj = PyObject_CallFunctionObjArgs(
-                    field_func, PyList_GetItem(schema_fields, tf.first), NULL);
-                if (!field_obj) {
-                    throw std::runtime_error(
-                        "tableFilterSetToPhysicalExpression failed to create "
-                        "field_obj.");
-                }
-                PyObject *scalar_obj = PyObject_CallFunctionObjArgs(
-                    scalar_func, valueToPyObject(constantFilter->constant),
-                    NULL);
-                if (!scalar_obj) {
-                    throw std::runtime_error(
-                        "tableFilterSetToPhysicalExpression failed to create "
-                        "scalar_obj.");
-                }
-                to_be_freed.push_back(field_obj);
-                to_be_freed.push_back(scalar_obj);
-
-                // Create final arrow.compute expression
-                PyObject *expr_obj = PyObject_CallFunctionObjArgs(
-                    comp_func, field_obj, scalar_obj, NULL);
-
-                if (!expr_obj) {
-                    throw std::runtime_error(
-                        "tableFilterSetToPhysicalExpression failed to create "
-                        "expression.");
-                }
-                ret = expr_obj;
-#endif
+                ret = arrow::py::wrap_expression(expr);
             } break;
             default:
                 throw std::runtime_error(
@@ -190,12 +80,10 @@ PyObject *tableFilterSetToArrowCompute(duckdb::TableFilterSet &filters,
         }
     }
 
-#ifdef GEN_PYTHON
     // Clean up Python objects
     for (auto &pyo : to_be_freed) {
         Py_DECREF(pyo);
     }
-#endif
 
     return ret;
 }
