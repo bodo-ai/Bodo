f13c62273813333532160d7753edf2ea5fdf1c27
Ehsan Totoni <ehsan.tn@gmail.com>
2025-04-29 10:50:26 -0400

    move func to cpp file

--- bodo/pandas/physical/operator.h
+++ bodo/pandas/physical/operator.h
@@ -79,7 +79,4 @@ class PhysicalSourceSink : public PhysicalOperator {
  *
  * @return int batch size to be used in streaming operators
  */
-int get_streaming_batch_size() {
-    char* env_str = std::getenv("BODO_STREAMING_BATCH_SIZE");
-    return (env_str != nullptr) ? std::stoi(env_str) : 4096;
-}
+int get_streaming_batch_size();
