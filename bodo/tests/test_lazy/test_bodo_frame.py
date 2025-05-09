a9ef5dd407fed1bdb643a3a3f2237b1f59956545
Ehsan Totoni <ehsan.tn@gmail.com>
2025-05-09 09:43:07 -0400

    avoid head

--- bodo/tests/test_lazy/test_bodo_frame.py
+++ bodo/tests/test_lazy/test_bodo_frame.py
@@ -291,10 +291,7 @@ def test_bodo_data_frame_pandas_manager(pandas_managers):
     assert df.shape == (40, 2)
     assert df.dtypes.equals(pd.Series(["Int64", "string[python]"], index=["A0", "B5"]))
 
-    # dtypes may be different since dataframe library computes the output
-    _test_equal(
-        base_df.head(5), df.head(5), check_dtype=False, check_pandas_types=False
-    )
+    _test_equal(base_df, df, check_dtype=False, check_pandas_types=False)
 
     agg_df = df.groupby("A0").size()
     _test_equal(
