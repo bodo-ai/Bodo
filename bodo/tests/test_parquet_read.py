a1b0ec430077881cb5082f1658e80e8371f5115b
Ehsan Totoni <ehsan.tn@gmail.com>
2025-05-10 21:50:12 -0400

    enable test

--- bodo/tests/test_parquet_read.py
+++ bodo/tests/test_parquet_read.py
@@ -573,10 +573,6 @@ def test_pq_multi_idx(memory_leak_check):
 
 # ---------------------------- Test Read Partitions ---------------------------- #
 @pytest.mark.slow
-@pytest.mark.skipif(
-    bodo.dataframe_library_enabled,
-    reason="[BSE-4769] Fix duckdb internal error while creating expression.",
-)
 def test_read_partitions(memory_leak_check):
     """test reading and filtering partitioned parquet data"""
 
