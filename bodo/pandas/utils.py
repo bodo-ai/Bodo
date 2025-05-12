85d35da30840fc936cf81905f31c5f38b5bcac80
Scott Routledge <scott@bodo.ai>
2025-05-12 11:19:20 -0400

    fix assert

--- bodo/pandas/utils.py
+++ bodo/pandas/utils.py
@@ -643,11 +643,7 @@ def wrap_plan(plan, res_id=None, nrows=None):
 
 def get_proj_expr_single(proj: LazyPlan):
     """Get the single expression from a LogicalProjection node."""
-    assert (
-        isinstance(proj, LazyPlan)
-        and proj.plan_class == "LogicalProjection"
-        and len(proj.args[1]) == 1
-    ), "get_proj_expr_single: LogicalProjection with a single expr expected"
+    assert is_single_projection(proj)
     return proj.args[1][0]
 
 
