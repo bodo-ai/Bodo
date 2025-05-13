582c54d32bb6bcb24eafb48db21bdf3287a5e4d2
Ehsan Totoni <ehsan.tn@gmail.com>
2025-05-13 09:38:01 -0400

    join node template

---
+++ bodo/pandas/physical/join.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "operator.h"
+
+/**
+ * @brief Physical node for join.
+ *
+ */
+class PhysicalJoin : public PhysicalSourceSink, public PhysicalSink {
+   public:
+    explicit PhysicalJoin() {}
+
+    virtual ~PhysicalJoin() = default;
+
+    void Finalize() override {}
+
+    /**
+     * @brief process input tables to build side of join (populate the hash
+     * table)
+     *
+     * @return OperatorResult
+     */
+    OperatorResult ConsumeBatch(
+        std::shared_ptr<table_info> input_batch) override {
+        return OperatorResult::NEED_MORE_INPUT;
+    }
+
+    /**
+     * @brief Run join probe on the input batch
+     *
+     * @param input_batch input batch to probe
+     * @return output batch of probe and return flag
+     */
+    std::pair<std::shared_ptr<table_info>, OperatorResult> ProcessBatch(
+        std::shared_ptr<table_info> input_batch) override {
+        return {out_table, OperatorResult::NEED_MORE_INPUT};
+    }
+};
