f997ddeb086065da680b91dcbce6956ee8992696
Todd A. Anderson <toddanderson@bodo.ai>
2025-05-20 14:52:31 -0700

    Add new file.

---
+++ bodo/pandas/physical/aggregate.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+#include "../libs/_array_utils.h"
+#include "expression.h"
+#include "operator.h"
+
+/**
+ * @brief Physical node for count_star().
+ *
+ */
+class PhysicalCountStar : public PhysicalSource, public PhysicalSink {
+   public:
+    explicit PhysicalCountStar() : local_count(0), global_count(0) {}
+
+    virtual ~PhysicalCountStar() = default;
+
+    void Finalize() override {
+        int result = MPI_Allreduce(&local_count, &global_count, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, MPI_COMM_WORLD);
+        if (result != MPI_SUCCESS) {
+            throw std::runtime_error("PhysicalCountStar::Finalize MPI_Allreduce failed.");
+        }
+    }
+
+    OperatorResult ConsumeBatch(
+        std::shared_ptr<table_info> input_batch,
+        OperatorResult prev_op_result) override {
+        local_count += input_batch->nrows();
+        return prev_op_result == OperatorResult::FINISHED ? OperatorResult::FINISHED : OperatorResult::NEED_MORE_INPUT;
+    }
+
+    std::shared_ptr<table_info> GetResult() override {
+        throw std::runtime_error("GetResult called on a PhysicalCountStar node.");
+    }
+
+    const std::shared_ptr<bodo::Schema> getOutputSchema() override {
+        std::vector<std::unique_ptr<bodo::DataType>> types;
+        types.emplace_back(std::make_unique<bodo::DataType>(bodo_array_type::arr_type_enum::NULLABLE_INT_BOOL, Bodo_CTypes::CTypeEnum::UINT64));
+        std::vector<std::string> names = {std::string("count_star()")};
+        return std::make_shared<bodo::Schema>(std::move(types), names);
+    }
+
+    std::pair<std::shared_ptr<table_info>, OperatorResult> ProduceBatch()
+        override {
+        std::shared_ptr<arrow::Array> array =
+            CreateOneElementArrowArray(global_count);
+
+        std::shared_ptr<array_info> result =
+            arrow_array_to_bodo(array, bodo::BufferPool::DefaultPtr());
+        std::vector<std::shared_ptr<array_info>> cvec = {result};
+        std::shared_ptr<table_info> next_batch = std::make_shared<table_info>(cvec);
+        return {next_batch, OperatorResult::FINISHED};
+    }
+
+   private:
+    uint64_t local_count, global_count;
+};
