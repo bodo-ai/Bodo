18cace0d16bf78d0a18654d2970072129042d172
Scott Routledge <scott@bodo.ai>
2025-05-21 16:33:19 -0400

    expand check

--- bodo/libs/_array.cpp
+++ bodo/libs/_array.cpp
@@ -414,6 +414,13 @@ void info_to_numpy_array(array_info* info, uint64_t* n_items, char** data,
 void info_to_null_array(array_info* info, uint64_t* n_items) {
     // TODO[BSE-433]: Replace with proper null array requirements once
     // they are integrated into C++.
+    if (info->arr_type != bodo_array_type::NULLABLE_INT_BOOL &&
+        info->arr_type != bodo_array_type::STRING) {
+        PyErr_SetString(PyExc_RuntimeError,
+                        "_array.cpp:: info_to_null_array: "
+                        "info_to_null_array requires nullable input");
+        return;
+    }
     *n_items = info->length;
 }
 
