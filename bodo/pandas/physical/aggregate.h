485f0855b64c3456318f69875c9ea3e551be9fb4
Todd A. Anderson <toddanderson@bodo.ai>
2025-05-22 08:11:19 -0700

    Switch to simple empty metadata for aggregate.

--- bodo/pandas/physical/aggregate.h
+++ bodo/pandas/physical/aggregate.h
@@ -21,44 +21,9 @@ class PhysicalCountStar : public PhysicalSource, public PhysicalSink {
             Bodo_CTypes::CTypeEnum::UINT64));
         std::vector<std::string> names = {std::string("count_star()")};
         out_schema = std::make_shared<bodo::Schema>(std::move(types), names);
-
-        // Import pandas and pyarrow
-        PyObject *pandas = PyImport_ImportModule("pandas");
-        PyObject *pyarrow = PyImport_ImportModule("pyarrow");
-
-        // Create an empty DataFrame with a uint64 column
-        PyObject *dict = PyDict_New();
-        PyObject *series_class = PyObject_GetAttrString(pandas, "Series");
-        PyObject *empty_series = PyObject_CallFunction(
-            series_class, "(O)", PyUnicode_FromString("uint64"));
-        PyDict_SetItemString(dict, "count_star()", empty_series);
-
-        PyObject *df_class = PyObject_GetAttrString(pandas, "DataFrame");
-        PyObject *df = PyObject_CallFunctionObjArgs(df_class, dict, NULL);
-
-        // Convert to PyArrow Table
-        PyObject *table_class = PyObject_GetAttrString(pyarrow, "Table");
-        PyObject *from_pandas =
-            PyObject_GetAttrString(table_class, "from_pandas");
-        PyObject *table = PyObject_CallFunctionObjArgs(from_pandas, df, NULL);
-
-        PyObject *pyarrow_schema = PyObject_GetAttrString(table, "schema");
-        std::shared_ptr<arrow::Schema> arrow_schema =
-            unwrap_schema(pyarrow_schema);
-
         out_schema->metadata =
-            std::make_shared<TableMetadata>(arrow_schema->metadata()->keys(),
-                                            arrow_schema->metadata()->values());
-        Py_DECREF(pandas);
-        Py_DECREF(pyarrow);
-        Py_DECREF(dict);
-        Py_DECREF(series_class);
-        Py_DECREF(empty_series);
-        Py_DECREF(df_class);
-        Py_DECREF(df);
-        Py_DECREF(table_class);
-        Py_DECREF(from_pandas);
-        Py_DECREF(pyarrow_schema);
+            std::make_shared<TableMetadata>(std::vector<std::string>({}),
+                                            std::vector<std::string>({}));
     }
 
     virtual ~PhysicalCountStar() = default;
