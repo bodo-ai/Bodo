9cae34ee6b26d253ddd6743425278405b2efe947
scott-routledge2 <scott@bodo.ai>
2025-05-22 11:19:21 -0400

    [BSE-4827] Read all-null columns (#507)
    Allow reading all-null columns in Dataframe library read_parquet function.

--- bodo/libs/_array.cpp
+++ bodo/libs/_array.cpp
@@ -414,7 +414,9 @@ void info_to_numpy_array(array_info* info, uint64_t* n_items, char** data,
 void info_to_null_array(array_info* info, uint64_t* n_items) {
     // TODO[BSE-433]: Replace with proper null array requirements once
     // they are integrated into C++.
-    if (info->arr_type != bodo_array_type::NULLABLE_INT_BOOL) {
+    // Arrow NA type is converted to arr_type STRING.
+    if (info->arr_type != bodo_array_type::NULLABLE_INT_BOOL &&
+        info->arr_type != bodo_array_type::STRING) {
         PyErr_SetString(PyExc_RuntimeError,
                         "_array.cpp:: info_to_null_array: "
                         "info_to_null_array requires nullable input");
