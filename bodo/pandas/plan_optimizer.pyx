44998885b0a1fdbd1b15c42351ff935705a1d57a
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-05-21 19:55:43 -0500

    Fix type

--- bodo/pandas/plan_optimizer.pyx
+++ bodo/pandas/plan_optimizer.pyx
@@ -589,13 +589,13 @@ cdef class LogicalGetIcebergRead(LogicalOperator):
     """
     Wrapper around DuckDB's LogicalGet for reading Iceberg datasets.
     """
-    cdef readonly c_string table_identifier
+    cdef readonly str table_identifier
 
     def __cinit__(self, object out_schema, str table_identifier, object catalog, object iceberg_filter):
         self.out_schema = out_schema
+        self.table_identifier = table_identifier
         cdef unique_ptr[CLogicalGet] c_logical_get = make_iceberg_get_node(out_schema, table_identifier.encode(), catalog, iceberg_filter)
         self.c_logical_operator = unique_ptr[CLogicalOperator](<CLogicalGet*> c_logical_get.release())
-        self.table_identifier = table_identifier
 
     def __str__(self):
         return f"LogicalGetIcebergRead({self.table_identifier})"
