04eac5368e548fdd4827d94833a73c43d340e1b0
Scott Routledge <scott@bodo.ai>
2025-05-27 11:26:59 -0400

    fill in issue #

--- bodo/tests/test_parquet_read.py
+++ bodo/tests/test_parquet_read.py
@@ -1741,7 +1741,7 @@ def test_read_pq_trailing_sep(datapath, memory_leak_check):
 
 @pytest.mark.skipif(
     bodo.test_dataframe_library_enabled,
-    reason="[BSE-XXX] Fix glob support.",
+    reason="[BSE-4848] Fix glob support.",
 )
 def test_read_parquet_glob(datapath, memory_leak_check):
     def test_impl(filename):
