eb25b108501ad75afbf59301256e0ca63504db89
Todd A. Anderson <toddanderson@bodo.ai>
2025-05-30 10:04:52 -0700

    Fix pandas api typo.

--- bodo/pandas/series.py
+++ bodo/pandas/series.py
@@ -227,7 +227,7 @@ class BodoSeries(pd.Series, BodoLazyWrapper):
             (
                 isinstance(other, BodoSeries)
                 and isinstance(other.dtype, pd.ArrowDtype)
-                and pd.api.types.is_numeric_type(other.dtype)
+                and pd.api.types.is_numeric_dtype(other.dtype)
             )
             or isinstance(other, numbers.Number)
         ):
