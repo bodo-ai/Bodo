dd711d69e8d9a1bf9c8fa8f3872b83b4abad15f2
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-06-13 14:21:44 -0500

    Fix save directory

--- examples/AI/pleias_ocr_correction.py
+++ examples/AI/pleias_ocr_correction.py
@@ -1,5 +1,7 @@
 import bodo
 import bodo.pandas as pd
+from pandas import ArrowDtype
+import pyarrow as pa
 import os
 from transformers import AutoTokenizer
 
@@ -8,24 +10,37 @@ model_name = "LLMDH/pleias_350m_ocr"
 model_len = 2048
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
-prompts = pd.read_parquet("hf://datasets/LLMDH/English-PD-bad-OCR/*.parquet")
+prompts = pd.read_parquet(
+    "hf://datasets/LLMDH/English-PD-bad-OCR/english_pd_105.parquet"
+).head(10)
+
 
 def tokenize(row):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    text_start = [2,65522]
+    text_start = [2, 65522]
     text_end = [65523]
     ocr_correction_start = [65528]
     extra_tokens = len(text_start) + len(text_end) + len(ocr_correction_start)
     row_encoded = tokenizer.encode(row.text)
     num_row_tokens = (model_len // 2) - extra_tokens
-    split_row_encoded = [row_encoded[i:i+num_row_tokens] for i in range(0, len(row_encoded), num_row_tokens)]
-    split_row_encoded = [text_start + split_row_encoded_chunk[1:] + text_end + ocr_correction_start for split_row_encoded_chunk in split_row_encoded]
+    split_row_encoded = [
+        row_encoded[i : i + num_row_tokens]
+        for i in range(0, len(row_encoded), num_row_tokens)
+    ]
+    split_row_encoded = [
+        text_start + split_row_encoded_chunk[1:] + text_end + ocr_correction_start
+        for split_row_encoded_chunk in split_row_encoded
+    ]
     return split_row_encoded
+
 
 def ocr_correction(prompts):
     from vllm import LLM, SamplingParams, TokensPrompt
+
     gpu_ranks = bodo.libs.distributed_api.get_gpu_ranks()
-    assert bodo.get_rank() in gpu_ranks, "Only use 1 bodo worker per GPU in the cluster, set workers with BODO_NUM_WORKERS environment variable"
+    assert bodo.get_rank() in gpu_ranks, (
+        "Only use 1 bodo worker per GPU in the cluster, set workers with BODO_NUM_WORKERS environment variable"
+    )
     llm = LLM(
         model=model_name,
         dtype="auto",
@@ -40,9 +55,8 @@ def ocr_correction(prompts):
     )
     batches = [[]]
     prompt_idx = 0
-    batch_idx_to_prompt_idx = []
+    batch_to_prompt_idx = [[]]
     while prompt_idx < len(prompts):
-
         split_encoded_prompts = prompts["split_encoded_prompts"][prompt_idx]
 
         split_prompt_idx = 0
@@ -51,15 +65,23 @@ def ocr_correction(prompts):
             gen_prompt = TokensPrompt(prompt_token_ids=encoded_prompt)
             if len(batches[-1]) < batch_size:
                 batches[-1].append(gen_prompt)
+                batch_to_prompt_idx[-1].append(prompt_idx)
             else:
                 batches.append([gen_prompt])
-            split_prompt_idx  += 1
+                batch_to_prompt_idx.append([prompt_idx])
+            split_prompt_idx += 1
         prompt_idx += 1
-    
-    for batch, i in enumerate(batches):
+
+    text_results = pd.Series([""] * len(prompts), dtype=ArrowDtype(pa.large_string()))
+    for i, batch in enumerate(batches):
         batch_result = llm.generate(prompts=batch, sampling_params=sampling_params)
-        print(f"Finished batch {i} of {len(batches)}")
-    return None
+        for j, result in enumerate(batch_result):
+            text = "".join([output.text for output in result.outputs])
+            text_results[batch_to_prompt_idx[i][j]] += text
+        print(f"Finished batch {i + 1} of {len(batches)}")
+    return text_results
+
 
 prompts["split_encoded_prompts"] = prompts.apply(tokenize, axis=1)
 prompts["corrected_text"] = prompts.map_partitions(ocr_correction)
+prompts.to_parquet("corrected_text.parquet")
