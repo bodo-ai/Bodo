b4ea8216cfbbf3f3fac4e07c2e74ef94b8ea13fd
Ehsan Totoni <ehsan.tn@gmail.com>
2025-07-01 19:55:41 -0400

    remove df lib test

--- bodo/tests/test_series_part1.py
+++ bodo/tests/test_series_part1.py
@@ -2859,7 +2859,6 @@ def test_series_map_args(memory_leak_check):
 
 
 @pytest.mark.slow
-@pytest.mark.df_lib
 def test_series_map_na_action_kws(memory_leak_check):
     """Test Series.map with na_action and kw arguments"""
 
