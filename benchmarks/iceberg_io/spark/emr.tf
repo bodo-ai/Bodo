6a999498c9aaaccc52e3ad9b9bfb63e57a929d0b
Chris Oh <chrisoh@Chriss-MacBook-Pro.local>
2025-07-09 10:46:34 -0400

    Includes policies

--- benchmarks/iceberg_io/spark/emr.tf
+++ benchmarks/iceberg_io/spark/emr.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "random_id" "bucket_id" {
-  byte_length = 8
-}
-
 resource "aws_s3_bucket" "emr_bucket" {
   bucket        = "spark-benchmark-python-script-bucket-${random_id.bucket_id.hex}"
   force_destroy = true
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
 }
 
 resource "aws_s3_object" "python_script" {
@@ -22,13 +22,160 @@ resource "aws_s3_object" "bootstrap_script" {
   key     = "scripts/bootstrap.sh"
   content = <<EOF
 #!/bin/bash
-set -ex
-ICEBERG_JAR_URL="https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.5_2.12/1.6.1/iceberg-spark-runtime-3.5_2.12-1.6.1.jar"
-ICEBERG_JAR_DEST="/usr/lib/spark/jars/iceberg-spark-runtime-3.5_2.12-1.6.1.jar"
-if [ ! -f "$ICEBERG_JAR_DEST" ]; then
-  sudo curl -L "$ICEBERG_JAR_URL" -o "$ICEBERG_JAR_DEST"
-fi
+sudo pip install -U pandas numpy==1.26.4 pyarrow
 EOF
+}
+
+resource "aws_iam_role" "emr_service_role" {
+  name = "EMR_Service_Role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticmapreduce.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "emr_ec2_instance_role" {
+  name = "EMR_EC2_CustomRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "emr_service_role_policy" {
+  role       = aws_iam_role.emr_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEMRServicePolicy_v2"
+}
+
+resource "aws_iam_role_policy" "emr_pass_instance_role_policy" {
+  name   = "EMR_Pass_Instance_Role_Policy"
+  role   = aws_iam_role.emr_service_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = "iam:PassRole",
+      Resource = aws_iam_role.emr_ec2_instance_role.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "emr_instance_profile_policy" {
+  name = "EMR_S3_Access_Policy"
+  role = aws_iam_role.emr_ec2_instance_role.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "S3Access",
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}/*",
+        "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "emr_profile" {
+  name = "EMR_EC2_Instance_Profile"
+  role = aws_iam_role.emr_ec2_instance_role.name
+}
+
+resource "aws_vpc" "emr_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    for-use-with-amazon-emr-managed-policies = "true"
+  }
+}
+
+resource "aws_subnet" "emr_subnet" {
+  vpc_id            = aws_vpc.emr_vpc.id
+  cidr_block        = "10.0.0.0/16"
+  availability_zone = "us-east-2b"
+  tags = {
+    for-use-with-amazon-emr-managed-policies = "true"
+  }
+}
+
+resource "aws_security_group" "allow_access" {
+  name                   = "allow_access"
+  description            = "Allow inbound traffic"
+  vpc_id                 = aws_vpc.emr_vpc.id
+  revoke_rules_on_delete = true
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.emr_vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [aws_subnet.emr_subnet]
+
+  lifecycle {
+    ignore_changes = [
+      ingress,
+      egress,
+    ]
+  }
+
+  tags = {
+    for-use-with-amazon-emr-managed-policies = "true"
+  }
+}
+
+resource "aws_internet_gateway" "emr_igw" {
+  vpc_id = aws_vpc.emr_vpc.id
+}
+
+resource "aws_route_table" "emr_route_table" {
+  vpc_id = aws_vpc.emr_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.emr_igw.id
+  }
+}
+
+resource "aws_route_table_association" "emr_route_table_association" {
+  subnet_id      = aws_subnet.emr_subnet.id
+  route_table_id = aws_route_table.emr_route_table.id
 }
 
 resource "aws_emr_cluster" "emr_cluster" {
@@ -58,12 +205,13 @@ resource "aws_emr_cluster" "emr_cluster" {
     }
   }
 
-  log_uri = "s3://${aws_s3_bucket.emr_bucket.id}/logs/"
-
   bootstrap_action {
-    name = "Install Iceberg Runtime"
+    name = "Install Python Dependencies"
     path = "s3://${aws_s3_bucket.emr_bucket.id}/scripts/bootstrap.sh"
+    args = []
   }
+
+  log_uri = "s3://${aws_s3_bucket.emr_bucket.id}/logs/"
 
   step {
     name              = "Run Python Script"
@@ -83,155 +231,12 @@ resource "aws_emr_cluster" "emr_cluster" {
   tags = {
     for-use-with-amazon-emr-managed-policies = "true"
   }
-}
 
-resource "aws_iam_role" "emr_service_role" {
-  name = "EMR_Service_Role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "elasticmapreduce.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
+  depends_on = [
+    aws_iam_role.emr_service_role,
+    aws_iam_role_policy.emr_pass_instance_role_policy,
+    aws_iam_role.emr_ec2_instance_role
   ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "emr_service_role_policy" {
-  role       = aws_iam_role.emr_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEMRServicePolicy_v2"
-}
-
-resource "aws_iam_role_policy" "emr_pass_instance_role_policy" {
-  name   = "EMR_Pass_Instance_Role_Policy"
-  role   = aws_iam_role.emr_service_role.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PassRoleForEC2",
-      "Effect": "Allow",
-      "Action": "iam:PassRole",
-      "Resource": "${aws_iam_role.emr_instance_role.arn}"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role" "emr_instance_role" {
-  name = "EMR_EC2_Instance_Role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "emr_instance_profile_policy" {
-  name = "EMR_S3_Access_Policy"
-  role = aws_iam_role.emr_instance_role.name
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "S3Access",
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}/*",
-        "arn:aws:s3:::${aws_s3_bucket.emr_bucket.id}"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "emr_profile" {
-  name = "EMR_EC2_Instance_Profile"
-  role = aws_iam_role.emr_instance_role.name
-}
-
-resource "aws_vpc" "emr_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    for-use-with-amazon-emr-managed-policies = "true"
-  }
-}
-
-resource "aws_subnet" "emr_subnet" {
-  vpc_id            = aws_vpc.emr_vpc.id
-  cidr_block        = "10.0.0.0/16"
-  availability_zone = "us-east-2b"
-  tags = {
-    for-use-with-amazon-emr-managed-policies = "true"
-  }
-}
-
-resource "aws_security_group" "allow_access" {
-  name        = "allow_access"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.emr_vpc.id
-  revoke_rules_on_delete = true
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [aws_vpc.emr_vpc.cidr_block]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  depends_on = [aws_subnet.emr_subnet]
-
-  lifecycle {
-    ignore_changes = [ingress, egress]
-  }
-
-  tags = {
-    for-use-with-amazon-emr-managed-policies = "true"
-  }
-}
-
-resource "aws_internet_gateway" "emr_igw" {
-  vpc_id = aws_vpc.emr_vpc.id
-}
-
-resource "aws_route_table" "emr_route_table" {
-  vpc_id = aws_vpc.emr_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.emr_igw.id
-  }
-}
-
-resource "aws_route_table_association" "emr_route_table_association" {
-  subnet_id      = aws_subnet.emr_subnet.id
-  route_table_id = aws_route_table.emr_route_table.id
 }
 
 data "aws_region" "current" {}
