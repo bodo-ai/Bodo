7d0af373d45f6eb8cd94bf81d6449436ca3c6a45
Todd A. Anderson <toddanderson@bodo.ai>
2025-07-21 13:39:07 -0700

    TPCH hang testing.

--- bodo/tests/test_df_lib/test_tpch.py
+++ bodo/tests/test_df_lib/test_tpch.py
@@ -36,11 +36,11 @@ def run_tpch_query_test(query_func):
         assert np.isclose(pd_result, bd_result)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q01():
     run_tpch_query_test(tpch.tpch_q01)
 
 
-@pytest.mark.skip("hanging?")
 def test_tpch_q02():
     run_tpch_query_test(tpch.tpch_q02)
 
@@ -55,6 +55,7 @@ def test_tpch_q04():
     run_tpch_query_test(tpch.tpch_q04)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q05():
     run_tpch_query_test(tpch.tpch_q05)
 
@@ -63,65 +64,81 @@ def test_tpch_q06():
     run_tpch_query_test(tpch.tpch_q06)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q07():
     run_tpch_query_test(tpch.tpch_q07)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q08():
     run_tpch_query_test(tpch.tpch_q08)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q09():
     run_tpch_query_test(tpch.tpch_q09)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q10():
     run_tpch_query_test(tpch.tpch_q10)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q11():
     run_tpch_query_test(tpch.tpch_q11)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q12():
     run_tpch_query_test(tpch.tpch_q12)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q13():
     run_tpch_query_test(tpch.tpch_q13)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q14():
     run_tpch_query_test(tpch.tpch_q14)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q15():
     run_tpch_query_test(tpch.tpch_q15)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q16():
     run_tpch_query_test(tpch.tpch_q16)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q17():
     run_tpch_query_test(tpch.tpch_q17)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q18():
     run_tpch_query_test(tpch.tpch_q18)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q19():
     run_tpch_query_test(tpch.tpch_q19)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q20():
     run_tpch_query_test(tpch.tpch_q20)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q21():
     run_tpch_query_test(tpch.tpch_q21)
 
 
+@pytest.mark.skip("hanging?")
 def test_tpch_q22():
     run_tpch_query_test(tpch.tpch_q22)
