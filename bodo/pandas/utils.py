5883338c9a2496f894db87fdbbd78b9591061ead
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-07-24 16:09:01 -0500

    Use perf counter instead of monotonic [Run CI]

--- bodo/pandas/utils.py
+++ bodo/pandas/utils.py
@@ -499,7 +499,7 @@ def run_func_on_table(cpp_table, result_type, in_args):
         )
     )
 
-    cpp_to_py_start = time.monotonic_ns()
+    cpp_to_py_start = time.perf_counter_ns()
     if is_series:
         # NOTE: Assuming Series operations ignore Indexes.
         # delete_input=False since cpp_table is needed for output below
@@ -511,9 +511,9 @@ def run_func_on_table(cpp_table, result_type, in_args):
         )
     else:
         input = cpp_table_to_df(cpp_table, use_arrow_dtypes=use_arrow_dtypes)
-    cpp_to_py = time.monotonic_ns() - cpp_to_py_start
+    cpp_to_py = time.perf_counter_ns() - cpp_to_py_start
 
-    udf_time_start = time.monotonic_ns()
+    udf_time_start = time.perf_counter_ns()
     if isinstance(func, str) and is_attr:
         func_path_str = func
         func = input
@@ -529,7 +529,7 @@ def run_func_on_table(cpp_table, result_type, in_args):
         out = func(input, *args, **kwargs)
     else:
         out = func(input, *args, **kwargs)
-    udf_time = time.monotonic_ns() - udf_time_start
+    udf_time = time.perf_counter_ns() - udf_time_start
 
     # astype can fail in some cases when input is empty
     if len(out):
@@ -542,14 +542,14 @@ def run_func_on_table(cpp_table, result_type, in_args):
     if out.name is None:
         out.name = "OUT"
 
-    py_to_cpp_start = time.monotonic_ns()
+    py_to_cpp_start = time.perf_counter_ns()
     if is_series:
         out_ptr = plan_optimizer.arrow_array_to_cpp_table(
             out.array._pa_array.combine_chunks(), str(out.name), cpp_table
         )
     else:
         out_ptr = df_to_cpp_table(pd.DataFrame({out.name: out}))
-    py_to_cpp = time.monotonic_ns() - py_to_cpp_start
+    py_to_cpp = time.perf_counter_ns() - py_to_cpp_start
     return out_ptr, cpp_to_py, udf_time, py_to_cpp
 
 
