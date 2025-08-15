3d067654dcc4037f3d5d4d8d2cb7d715fa57baea
Isaac Warren <42949629+IsaacWarren@users.noreply.github.com>
2025-08-15 10:46:43 -0500

    BSE-5058: Fix llm_generate and embed in Jupyter (#758)

--- bodo/pandas/series.py
+++ bodo/pandas/series.py
@@ -8,6 +8,7 @@ import numbers
 import typing as pt
 import warnings
 from collections.abc import Callable, Hashable
+from concurrent.futures import ThreadPoolExecutor
 
 import numpy
 import pandas as pd
@@ -1564,7 +1565,25 @@ class BodoSeriesAiMethods:
                 tasks = [per_row(row, client, generation_kwargs) for row in series]
                 return await asyncio.gather(*tasks, return_exceptions=True)
 
-            return pd.Series(asyncio.run(all_tasks(series, client, generation_kwargs)))
+            # Check if there is a running event loop to determine if we need to run in a thread pool
+            # or if we can run the async function directly.
+            run_in_threadpool = False
+            try:
+                asyncio.get_running_loop()
+                run_in_threadpool = True
+            except RuntimeError:
+                pass
+            if run_in_threadpool:
+                with ThreadPoolExecutor(1) as pool:
+                    return pool.submit(
+                        lambda: pd.Series(
+                            asyncio.run(all_tasks(series, client, generation_kwargs))
+                        )
+                    ).result()
+            else:
+                return pd.Series(
+                    asyncio.run(all_tasks(series, client, generation_kwargs))
+                )
 
         return self._series.map_partitions(
             map_func, api_key, base_url, generation_kwargs=generation_kwargs
@@ -1699,7 +1718,25 @@ class BodoSeriesAiMethods:
                 tasks = [per_row(row, client, embedding_kwargs) for row in series]
                 return await asyncio.gather(*tasks, return_exceptions=True)
 
-            return pd.Series(asyncio.run(all_tasks(series, client, embedding_kwargs)))
+            # Check if there is a running event loop to determine if we need to run in a thread pool
+            # or if we can run the async function directly.
+            run_in_threadpool = False
+            try:
+                asyncio.get_running_loop()
+                run_in_threadpool = True
+            except RuntimeError:
+                pass
+            if run_in_threadpool:
+                with ThreadPoolExecutor(1) as pool:
+                    return pool.submit(
+                        lambda: pd.Series(
+                            asyncio.run(all_tasks(series, client, embedding_kwargs))
+                        )
+                    ).result()
+            else:
+                return pd.Series(
+                    asyncio.run(all_tasks(series, client, embedding_kwargs))
+                )
 
         return self._series.map_partitions(
             map_func, api_key, base_url, embedding_kwargs=embedding_kwargs
