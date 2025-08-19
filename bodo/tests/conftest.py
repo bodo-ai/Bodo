4b9769223448c463d6240de564d2e87251bc5977
Ehsan Totoni <ehsan.tn@gmail.com>
2025-08-18 20:52:33 -0400

    split puffin tests

--- bodo/tests/conftest.py
+++ bodo/tests/conftest.py
@@ -791,15 +791,17 @@ def pytest_collect_file(parent, file_path: Path):
     """
     try:
         from bodo.ext import test_cpp
+        from bodo.libs.puffin_file import test_cpp as puffin_test_cpp
     except ImportError:
         test_cpp = None
+        puffin_test_cpp = None
 
     if (
         file_path.suffix == ".cpp"
         and file_path.name.startswith("test_")
         and file_path.name != "test_framework.cpp"
     ):
-        if test_cpp is None:
+        if test_cpp is None or puffin_test_cpp is None:
             import warnings
 
             warnings.warn(
@@ -810,6 +812,10 @@ def pytest_collect_file(parent, file_path: Path):
         tests = [
             test
             for test in test_cpp.tests
+            if file_path.name == os.path.basename(test.filename)
+        ] + [
+            test
+            for test in puffin_test_cpp.tests
             if file_path.name == os.path.basename(test.filename)
         ]
         return CppTestFile.from_parent(
