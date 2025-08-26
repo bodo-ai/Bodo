a76cbbedaaba680d2f7fcf4dfb80c53c17bb9ffd
Ehsan Totoni <ehsan.tn@gmail.com>
2025-08-25 21:47:52 -0400

    avoid bcast compilation

--- bodo/pandas/plan_optimizer.pyx
+++ bodo/pandas/plan_optimizer.pyx
@@ -790,10 +790,12 @@ cdef class LogicalGetParquetRead(LogicalOperator):
     cdef readonly int nrows
 
     def __cinit__(self, object out_schema, object parquet_path, object storage_options):
+        from bodo.ext import hdist
+
         self.out_schema = out_schema
         self.path = parquet_path
         self.storage_options = storage_options
-        self.nrows = bodo.libs.distributed_api.bcast_scalar(self._get_nrows() if bodo.get_rank() == 0 else 0)
+        self.nrows = hdist.bcast_int64_py_wrapper(self._get_nrows() if bodo.get_rank() == 0 else 0)
         cdef unique_ptr[CLogicalGet] c_logical_get = make_parquet_get_node(parquet_path, out_schema, storage_options, self.getCardinality())
         self.c_logical_operator = unique_ptr[CLogicalOperator](<CLogicalGet*> c_logical_get.release())
 
