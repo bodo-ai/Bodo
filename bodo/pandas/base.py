ba3be563ba8748794fbe3766f757faf73c256fde
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-09-10 11:12:35 -0400

    Use iloc [run ci]

--- bodo/pandas/base.py
+++ bodo/pandas/base.py
@@ -81,7 +81,7 @@ def from_pandas(df):
 
     # TODO [BSE-4788]: Refactor with convert_to_arrow_dtypes util
     for col in df.select_dtypes(include=["object"]).columns:
-        if len(df[col]) > 0 and isinstance(df[col][0], BodoScalar):
+        if len(df[col]) > 0 and isinstance(df[col].iloc[0], BodoScalar):
             df[col] = df[col].apply(lambda x: x.get_value() if x is not None else None)
     pa_schema = pa.Schema.from_pandas(df.iloc[:sample_size])
     empty_df = arrow_to_empty_df(pa_schema)
