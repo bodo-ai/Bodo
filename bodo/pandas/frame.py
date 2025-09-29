2f155ceef4713b0cd384f54ee20da434806eb33a
Todd A. Anderson <drtodd13@comcast.net>
2025-09-29 13:13:13 -0700

    Add support for subset and keep to drop_duplicates. (#851)
    Co-authored-by: Todd A. Anderson <toddanderson@bodo.ai>

--- bodo/pandas/frame.py
+++ bodo/pandas/frame.py
@@ -1510,7 +1510,7 @@ class BodoDataFrame(pd.DataFrame, BodoLazyWrapper):
             ),
         )
 
-    @check_args_fallback(supported="none")
+    @check_args_fallback(supported=["subset", "keep"])
     def drop_duplicates(
         self,
         subset: Hashable | Sequence[Hashable] | None = None,
@@ -1520,6 +1520,17 @@ class BodoDataFrame(pd.DataFrame, BodoLazyWrapper):
         ignore_index: bool = False,
     ) -> BodoDataFrame | None:
         from bodo.pandas.base import _empty_like
+
+        if subset is not None:
+            subset_group = self.groupby(subset, as_index=False, sort=False)
+            if keep == "first":
+                return subset_group.first()
+            elif keep == "last":
+                return subset_group.last()
+            else:
+                raise BodoLibNotImplementedException(
+                    "DataFrame.drop_duplicates() keep argument: only 'first' and 'last' are supported"
+                )
 
         zero_size_self = _empty_like(self)
         exprs = make_col_ref_exprs(list(range(len(zero_size_self.columns))), self._plan)
