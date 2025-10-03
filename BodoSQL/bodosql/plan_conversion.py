cb264bb5a0563a609f1687c9ba3e2a09cc9040c1
Ehsan Totoni <ehsan.tn@gmail.com>
2025-10-03 17:08:54 -0400

    Support addition

--- BodoSQL/bodosql/plan_conversion.py
+++ BodoSQL/bodosql/plan_conversion.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 import pyarrow as pa
 
 import bodo
-from bodo.pandas.plan import LogicalProjection, arrow_to_empty_df, make_col_ref_exprs
-from bodosql.imported_java_classes import JavaEntryPoint
+from bodo.pandas.plan import (
+    ArithOpExpression,
+    LogicalProjection,
+    arrow_to_empty_df,
+    make_col_ref_exprs,
+)
+from bodosql.imported_java_classes import JavaEntryPoint, gateway
 
 
 def java_plan_to_python_plan(ctx, java_plan):
@@ -59,4 +64,33 @@ def java_expr_to_python_expr(java_expr, input_plan):
         col_index = java_expr.getIndex()
         return make_col_ref_exprs([col_index], input_plan)[0]
 
+    if java_class_name == "RexCall":
+        return java_call_to_python_call(java_expr, input_plan)
+
     raise NotImplementedError(f"Expression {java_class_name} not supported yet")
+
+
+def java_call_to_python_call(java_call, input_plan):
+    """Convert a BodoSQL Java call expression to a DataFrame library expression
+    (bodo.pandas.plan.Expression).
+    """
+    operator = java_call.getOperator()
+    operator_class_name = operator.getClass().getSimpleName()
+
+    if (
+        operator_class_name == "SqlMonotonicBinaryOperator"
+        and len(java_call.getOperands()) == 2
+    ):
+        operands = java_call.getOperands()
+        left = java_expr_to_python_expr(operands[0], input_plan)
+        right = java_expr_to_python_expr(operands[1], input_plan)
+        kind = operator.getKind()
+        SqlKind = gateway.jvm.org.apache.calcite.sql.SqlKind
+
+        if kind.equals(SqlKind.PLUS):
+            # TODO: support all BodoSQL data types in backend (including date/time)
+            # TODO: upcast output to avoid overflow?
+            expr = ArithOpExpression(left.empty_data, left, right, "__add__")
+            return expr
+
+    raise NotImplementedError(f"Call operator {operator_class_name} not supported yet")
