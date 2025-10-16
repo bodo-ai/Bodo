a6c37da78cd559bc00f85d4537f5cd6e49692595
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2025-10-16 11:05:51 -0500

    Use bodo distributed sampler

--- examples/ML/pandas_ddp_example.py
+++ examples/ML/pandas_ddp_example.py
@@ -1,13 +1,13 @@
 import bodo.pandas as pd
+from bodo.mpi4py import MPI
 import bodo.ai
-import bodo.spawn.spawner
 import torch
 import torch.distributed as dist
 import torch.distributed.checkpoint
 import tqdm
 from torch import nn
 from torch.optim import Adam
-from torch.utils.data import DataLoader, DistributedSampler
+from torch.utils.data import DataLoader
 from transformers import BertModel, BertTokenizer
 
 LR = 1e-6
@@ -29,23 +29,52 @@ class PandasDataset(torch.utils.data.Dataset):
         batch_texts = row["label"]
         batch_texts = torch.tensor(batch_texts, device=self.device)
         batch_y = row["tokenized"]
-        batch_y = {k: torch.tensor(v, device=self.device) for k, v in batch_y.items()}
-        return batch_y, batch_texts
+        input_ids = torch.tensor(batch_y["input_ids"], device=self.device)
+        attention_mask = torch.tensor(batch_y["attention_mask"], device=self.device)
+        return input_ids, attention_mask, batch_texts
 
-    #def __getitems__(self, idxs):
-    #    rows = self.df.iloc[idxs]
-    #    batch_texts = rows["label"].to_numpy()
-    #    batch_texts = torch.tensor(batch_texts, device=self.device)
-    #    batch_y = rows["tokenized"].to_list()
-    #    batch_y = {k: torch.tensor([d[k] for d in batch_y], device=self.device) for k in batch_y[0]}
-    #    return batch_y, batch_texts
+class BodoDistributedSampler(torch.utils.data.Sampler):
+    def __init__(self, dataset: PandasDataset, worker_ranks: list[int], shuffle=True):
+        assert isinstance(dataset, PandasDataset), (
+            "BodoDistributedSampler only works with PandasDataset"
+        )
+        self.dataset = dataset
+        self.worker_ranks = worker_ranks
+        self.shuffle = shuffle
+        world_group = MPI.COMM_WORLD.Get_group()
+        self.worker_group = world_group.Incl(worker_ranks)
+        world_group.Free()
+        self.worker_subcomm = MPI.COMM_WORLD.Create(self.worker_group)
+        self.seed = 0
 
-    #def __getitems__(self, idxs):
-    #    rows = self.df.iloc[idxs]
-    #    tensor = torch.tensor(rows.to_numpy(dtype=self.dtype), device=self.device)
-    #    return [tensor.select(0, i) for i in range(tensor.shape[0])]
+    def __del__(self):
+        if hasattr(self, "worker_group") and self.worker_group != MPI.GROUP_NULL:
+            self.worker_group.Free()
+        if hasattr(self, "worker_subcomm") and self.worker_subcomm != MPI.COMM_NULL:
+            self.worker_subcomm.Free()
 
+    def __iter__(self):
+        # Create a list of all indices
+        indices = list(range(len(self.dataset)))
 
+        # Shuffle the indices if required
+        if self.shuffle:
+            shuffled_df = bodo.random_shuffle(
+                self.dataset.df, seed=self.seed, dests=self.worker_ranks
+            )
+            self.dataset = PandasDataset(shuffled_df, device=self.dataset.device)
+            g = torch.Generator()
+            g.manual_seed(self.seed)
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+            self.seed += 1  # Change seed for next epoch
+
+        # Ensure all ranks have the same number of samples
+        max_sample_len = self.worker_subcomm.allreduce(len(indices), op=MPI.MAX)
+        indices += indices[: (max_sample_len - len(indices))]
+        return iter(indices)
+
+    def __len__(self):
+        return len(self.dataset)
 
 class BertClassifier(nn.Module):
 
@@ -114,7 +143,7 @@ def ddp_validation(model, val_loader, loss_fn):
             range(len(val_loader)), colour="green"
         )
     with torch.no_grad():
-        for val_input, val_label in val_loader:
+        for input_id, mask, train_label in train_loader:
             val_label = val_label.to(device)
             mask = val_input['attention_mask'].to(device)
             input_id = val_input['input_ids'].squeeze(1).to(device)
@@ -155,11 +184,7 @@ def ddp_train_one_epoch(model, train_loader, loss_fn, optimizer, epoch):
             range(len(train_loader)), colour="blue"
         )
 
-    for train_input, train_label in train_loader:
-        train_label = train_label.to(device)
-        mask = train_input['attention_mask'].to(device)
-        input_id = train_input['input_ids'].squeeze(1).to(device)
-        print('input_id shape:', input_id.shape)
+    for input_id, mask, train_label in train_loader:
 
         output = model(input_id, mask)
 
@@ -191,20 +216,50 @@ def ddp_train_one_epoch(model, train_loader, loss_fn, optimizer, epoch):
 
 def train_main(train_df, val_df, test_df):
     model = bodo.ai.prepare_model(BertClassifier())
+    if model:
+        device = next(model.parameters()).device
+    else:
+        device = None
+    gpu_ranks = bodo.get_gpu_ranks()
+
+    # Rebalance data if using accelerators onto 
+    # acclerator ranks
+    accelerators_used = len(gpu_ranks) != 0
+    if accelerators_used:
+        train_df = bodo.rebalance(train_df, dests=gpu_ranks)
+        val_df = bodo.rebalance(val_df, dests=gpu_ranks)
+        test_df = bodo.rebalance(test_df, dests=gpu_ranks)
+
+
+    train_dataset = PandasDataset(train_df, device)
+    val_dataset = PandasDataset(val_df, device)
+    test_dataset = PandasDataset(test_df, device)
+    train_sampler = BodoDistributedSampler(
+        train_dataset,
+        worker_ranks=gpu_ranks
+        if accelerators_used
+        else list(range(MPI.COMM_WORLD.Get_size())),
+    )
+    val_sampler = BodoDistributedSampler(
+        val_dataset,
+        worker_ranks=gpu_ranks
+        if accelerators_used
+        else list(range(MPI.COMM_WORLD.Get_size())),
+    )
+    test_sampler = BodoDistributedSampler(
+        test_dataset,
+        worker_ranks=gpu_ranks
+        if accelerators_used
+        else list(range(MPI.COMM_WORLD.Get_size())),
+    )
+    print('created samplers')
     if model == None:
         return
-    rank = dist.get_rank()
+    pytorch_rank = dist.get_rank()
 
-    train_dataset = PandasDataset(train_df)
-    val_dataset = PandasDataset(val_df)
-    test_dataset = PandasDataset(test_df)
-    #train_sampler = DistributedSampler(train_dataset)
-    #val_sampler = DistributedSampler(val_dataset)
-    #test_sampler = DistributedSampler(test_dataset)
-
-    train_loader = DataLoader(train_dataset, batch_size=2,)# sampler=train_sampler)
-    val_loader = DataLoader(val_dataset, batch_size=2, )#sampler=val_sampler)
-    test_loader = DataLoader(test_dataset, batch_size=2,)# sampler=test_sampler)
+    train_loader = DataLoader(train_dataset, batch_size=2, sampler=train_sampler)
+    val_loader = DataLoader(val_dataset, batch_size=2, sampler=val_sampler)
+    test_loader = DataLoader(test_dataset, batch_size=2, sampler=test_sampler)
 
 
     loss_fn = nn.CrossEntropyLoss()
@@ -212,11 +267,11 @@ def train_main(train_df, val_df, test_df):
 
     for epoch in range(EPOCHS):
 
-        if rank == 0:
+        if pytorch_rank == 0:
             print(f"Train Epoch: \t{epoch}")
         ddp_train_one_epoch(model, train_loader, loss_fn, optimizer, epoch)
 
-        if rank == 0:
+        if pytorch_rank == 0:
             print("Validation: ")
         ddp_validation(model, val_loader, loss_fn)
 
@@ -228,7 +283,7 @@ def train_main(train_df, val_df, test_df):
             checkpoint_id=CHECKPOINT_DIR
         )
     
-    if rank == 0:
+    if pytorch_rank == 0:
         print("Test: ")
     ddp_validation(model, test_loader, loss_fn)
 
