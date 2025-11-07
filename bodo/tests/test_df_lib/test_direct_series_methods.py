0938461122577409c7408a0ce8dca309fa3437b5
Todd A. Anderson <toddanderson@bodo.ai>
2025-11-07 15:06:35 -0800

    Reset index always necessary for larger number of workers.

--- bodo/tests/test_df_lib/test_direct_series_methods.py
+++ bodo/tests/test_df_lib/test_direct_series_methods.py
@@ -34,7 +34,7 @@ def test_series_isin(index_val, use_index1, use_index2):
         check_pandas_types=False,
         sort_output=True,
         # We don't track RangeIndex in output
-        reset_index=isinstance(S2.index, pd.RangeIndex),
+        reset_index=True,
     )
 
 
