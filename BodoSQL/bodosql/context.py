d123a67cd053af1a672b3c57d2ad07b6f12f4801
Ehsan Totoni <ehsan.tn@gmail.com>
2025-11-10 10:54:16 -0500

    avoid pa schema issues

--- BodoSQL/bodosql/context.py
+++ BodoSQL/bodosql/context.py
@@ -1126,16 +1126,16 @@ def _get_sql_types(df_type, from_jit):
     try:
         # Dropping Indexes since BodoSQL doesn't support them yet and can lead to issues
         # in JIT path.
-        pa_schema = (
-            df_type._plan.pa_schema
+        df_type = (
+            df_type._plan.empty_data
             if isinstance(df_type, bodo.pandas.BodoDataFrame)
-            else pa.Schema.from_pandas(df_type.reset_index(drop=True))
+            else df_type
         )
         return [
             get_sql_column_type(
                 f.type, f.name, _is_nullable_dtype(df_type.dtypes.iloc[i])
             )
-            for i, f in enumerate(pa_schema)
+            for i, f in enumerate(pa.Schema.from_pandas(df_type.reset_index(drop=True)))
         ]
     except Exception:
         # Fallback to JIT version if Arrow version failed
