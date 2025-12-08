ad50f039c5da0428bf388304013e9c31aed78eb9
Scott Routledge <scott@bodo.ai>
2025-12-08 13:55:22 -0500

    avoid hot start

--- benchmarks/tpch/pds-benchmark/queries/common_utils.py
+++ benchmarks/tpch/pds-benchmark/queries/common_utils.py
@@ -111,7 +111,7 @@ def run_query_generic(
 ) -> None:
     """Execute a query."""
     # execute the query once to ensure we are getting a hot run
-    query()
+    # query()
     for _ in range(settings.run.iterations):
         with CodeTimer(
             name=f"Run {library_name} query {query_number}", unit="s"
