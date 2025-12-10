a723d70073c7e43374e61aa9c2a04cfef627a489
Scott Routledge <scott@bodo.ai>
2025-12-10 10:41:10 -0500

    sysroot

--- buildscripts/setup_osx_sdk.sh
+++ buildscripts/setup_osx_sdk.sh
@@ -3,7 +3,6 @@
 set -exo pipefail
 
 export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-11.0}
-export CONDA_BUILD_SYSROOT="/opt/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
-echo "Downloading ${MACOSX_DEPLOYMENT_TARGET} sdk"
-curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz
-sudo tar -xf MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk.tar.xz -C /opt
+export CONDA_BUILD_SYSROOT=$(xcrun --show-sdk-path)
+
+echo "Using sysroot: $CONDA_BUILD_SYSROOT"
