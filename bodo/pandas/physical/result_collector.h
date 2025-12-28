75f9ab5bee4b8b9c527dc17fa91dc5483f0a58c6
Scott Routledge <scott@bodo.ai>
2025-12-28 17:02:27 -0500

    print result collector

--- bodo/pandas/physical/result_collector.h
+++ bodo/pandas/physical/result_collector.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include "../libs/_table_builder.h"
 
 #include "physical/operator.h"
@@ -38,6 +39,8 @@ class PhysicalResultCollector : public PhysicalSink {
 
     OperatorResult ConsumeBatch(std::shared_ptr<table_info> input_batch,
                                 OperatorResult prev_op_result) override {
+        std::cout << "PhysicalResultCollector::ConsumeBatch called"
+                  << std::endl;
         buffer->UnifyTablesAndAppend(input_batch, dict_builders);
 
         return prev_op_result == OperatorResult::FINISHED
