8027afd7fe2ee3eac986b9d97e74f9417c992275
Ehsan Totoni <ehsan.tn@gmail.com>
2026-01-15 16:45:50 -0500

    fix float corner case

--- BodoSQL/bodosql/tests/test_conversions/test_snowflake_conversion_fns.py
+++ BodoSQL/bodosql/tests/test_conversions/test_snowflake_conversion_fns.py
@@ -724,6 +724,9 @@ def to_double_equiv(arr):
     # Workaround Int NA being passed as NaN
     if arr.dtype == pd.Int64Dtype():
         out_arr = out_arr.map(lambda x: None if x == "nan" else x)
+    # Workaround float64 NA ambiguity
+    if arr.dtype == np.float64:
+        out_arr = arr.map(lambda x: str(x))
     # Make sure NaNs are preserved correctly in Arrow conversion
     out_arr = pa.compute.cast(
         out_arr.astype(pd.ArrowDtype(pa.large_string())).array._pa_array,
