20bffd05dc3eeaa75cc55921bfd3e882687f8b60
Scott Routledge <scott@bodo.ai>
2026-01-23 16:36:48 -0500

    refactor and try test on linux ci [run ci]

--- bodo/libs/distributed_impl.py
+++ bodo/libs/distributed_impl.py
@@ -81,7 +81,7 @@ c_gatherv = types.ExternalFunction(
 
 # sendbuff, sendcounts, displs, recvbuf, recv_count, dtype
 c_scatterv = types.ExternalFunction(
-    "",
+    "c_scatterv",
     types.void(
         types.voidptr,
         types.voidptr,
