0b2e491aae3e1e756293c95e850cbc1059225d80
Ehsan Totoni <ehsan.tn@gmail.com>
2026-02-02 13:46:01 -0500

    fix test_projection_expression_mod

--- bodo/tests/test_df_lib/test_end_to_end.py
+++ bodo/tests/test_df_lib/test_end_to_end.py
@@ -1806,9 +1806,7 @@ def test_projection_expression_mod(datapath):
         bodo_df1 = bd.read_parquet(datapath("dataframe_library/df1.parquet"))
         bodo_df2 = bodo_df1[(bodo_df1.A % 5) > 2]
 
-        py_df1 = pd.read_parquet(
-            datapath("dataframe_library/df1.parquet"), dtype_backend="pyarrow"
-        )
+        py_df1 = pd.read_parquet(datapath("dataframe_library/df1.parquet"))
         py_df2 = py_df1[(py_df1.A % 5) > 2]
 
     _test_equal(
