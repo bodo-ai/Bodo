be78c2e4524261ee925aa02f9d48adac3e7cf2a8
Scott Routledge <scott@bodo.ai>
2026-02-11 22:23:29 +0000

    Set use async to false for now

--- bodo/pandas/physical/operator.cpp
+++ bodo/pandas/physical/operator.cpp
@@ -35,7 +35,7 @@ int64_t get_parquet_chunk_size() {
                                 : 256e6;  // Default to 256 MiB
 }
 
-extern const bool G_USE_ASYNC = true;
+extern const bool G_USE_ASYNC = false;
 
 #ifdef USE_CUDF
 
