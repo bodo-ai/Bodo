699d390becb7031ae978a1e948d0192f9968a13c
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2026-02-12 13:29:21 -0600

    Remove cancel

--- bodo/libs/gpu_utils.cpp
+++ bodo/libs/gpu_utils.cpp
@@ -474,9 +474,9 @@ bool GpuShuffleManager::all_complete() {
             // If global completion is reached, we can cancel any inflight
             // shuffle coordination since we know all data has been sent
             if (this->shuffle_coordination.req != MPI_REQUEST_NULL) {
-                CHECK_MPI(
-                    MPI_Cancel(&this->shuffle_coordination.req),
-                    "GpuShuffleManager::all_complete: MPI_Cancel failed:");
+                // CHECK_MPI(
+                //     MPI_Cancel(&this->shuffle_coordination.req),
+                //     "GpuShuffleManager::all_complete: MPI_Cancel failed:");
             }
             this->shuffle_coordination.req = MPI_REQUEST_NULL;
             this->global_completion_req = MPI_REQUEST_NULL;
