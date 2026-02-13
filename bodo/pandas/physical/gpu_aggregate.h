95085f9da3f5ab8ea5482a0c9c99616cb96279de
Todd A. Anderson <toddanderson@bodo.ai>
2026-02-13 11:18:13 -0800

    Single GPU, single batch, first attempt.

--- bodo/pandas/physical/gpu_aggregate.h
+++ bodo/pandas/physical/gpu_aggregate.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <Python.h>
 #include <object.h>
+#include <cudf/column/column_factories.hpp>
 #include <memory>
 #include <optional>
 #include <stdexcept>
@@ -9,8 +11,8 @@
 #include "../io/arrow_reader.h"
 #include "../libs/_array_utils.h"
 #include "../libs/_query_profile_collector.h"
-#include "../libs/_table_builder_utils.h"
 #include "../libs/_utils.h"
+#include "../libs/groupby/_groupby_common.h"
 #include "../libs/streaming/cuda_groupby.h"
 #include "duckdb/planner/expression/bound_aggregate_expression.hpp"
 #include "duckdb/planner/expression/bound_columnref_expression.hpp"
@@ -30,7 +32,27 @@ inline bool gpu_capable(duckdb::LogicalAggregate& logical_aggregate) {
     std::cout << "gpu_capable for LogicalAggregate "
               << !logical_aggregate.groups.empty() << std::endl;
     // Temporarily don't support count_start and quantile on GPU.
-    return !logical_aggregate.groups.empty();
+    if (!logical_aggregate.groups.empty()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < logical_aggregate.expressions.size(); i++) {
+        const auto& expr = logical_aggregate.expressions[i];
+
+        if (expr->type != duckdb::ExpressionType::BOUND_AGGREGATE) {
+            throw std::runtime_error(
+                "Aggregate expression is not a bound aggregate: " +
+                expr->ToString());
+        }
+        auto& agg_expr = expr->Cast<duckdb::BoundAggregateExpression>();
+
+        // Check if the aggregate function is supported
+        bool is_udf = agg_expr.function.name.starts_with("udf");
+        if (is_udf) {
+            return false;
+        }
+    }
+    return true;
 }
 
 inline bool gpu_capable(duckdb::LogicalDistinct& logical_distinct) {
@@ -49,19 +71,12 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
         std::map<std::pair<duckdb::idx_t, duckdb::idx_t>, size_t> col_ref_map =
             getColRefMap(op.children[0]->GetColumnBindings());
 
-        std::shared_ptr<bodo::Schema> in_table_schema_reordered;
-        std::vector<bool> cols_to_keep_vec;
-        this->initKeysAndSchema(col_ref_map, op.groups, in_table_schema,
-                                in_table_schema_reordered, cols_to_keep_vec);
+        std::vector<uint64_t> keys;
+        std::vector<std::pair<uint64_t, int32_t>> column_agg_funcs;
 
-        std::vector<int32_t> ftypes;
-        // Create input data column indices
-        std::vector<int32_t> f_in_cols;
-        std::vector<int32_t> f_in_offsets = {0};
+        this->initKeysAndSchema(col_ref_map, op.groups, in_table_schema, keys);
+
         std::optional<bool> dropna = std::nullopt;
-
-        std::vector<stream_udf_t*> udf_cfuncs;
-        std::vector<int> udf_idxs;
 
         for (size_t i = 0; i < op.expressions.size(); i++) {
             const auto& expr = op.expressions[i];
@@ -74,66 +89,55 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
             auto& agg_expr = expr->Cast<duckdb::BoundAggregateExpression>();
 
             // Check if the aggregate function is supported
-            bool is_udf = agg_expr.function.name.starts_with("udf");
             if (function_to_ftype.find(agg_expr.function.name) ==
-                    function_to_ftype.end() &&
-                !is_udf) {
+                function_to_ftype.end()) {
                 throw std::runtime_error("Unsupported aggregate function: " +
                                          agg_expr.function.name);
             }
 
-            if (!is_udf && agg_expr.children.size() != 1 &&
-                agg_expr.function.name != "size") {
+            if (agg_expr.children.size() != 1) {
                 throw std::runtime_error(
                     "Aggregate expression for builtin funcs must have exactly "
                     "one child expression" +
                     expr->ToString());
             }
 
-            size_t col_idx = 0;
-            for (auto& child_expr : agg_expr.children) {
-                if (child_expr->type !=
-                    duckdb::ExpressionType::BOUND_COLUMN_REF) {
-                    throw std::runtime_error(
-                        "Aggregate expression must have only col ref "
-                        "expression children" +
-                        expr->ToString());
-                }
-                auto& colref =
-                    child_expr->Cast<duckdb::BoundColumnRefExpression>();
-                col_idx = col_ref_map.at(
-                    {colref.binding.table_index, colref.binding.column_index});
-
-                size_t reorder_col_idx =
-                    std::find(this->input_col_inds.begin(),
-                              this->input_col_inds.end(), col_idx) -
-                    this->input_col_inds.begin();
-                f_in_cols.push_back(reorder_col_idx);
+            auto& child_expr = agg_expr.children[0];
+            if (child_expr->type != duckdb::ExpressionType::BOUND_COLUMN_REF) {
+                throw std::runtime_error(
+                    "Aggregate expression must have only col ref "
+                    "expression children" +
+                    expr->ToString());
             }
-            f_in_offsets.push_back(f_in_cols.size());
-
+            auto& colref = child_expr->Cast<duckdb::BoundColumnRefExpression>();
             // Extract bind_info
             BodoAggFunctionData& bind_info =
                 agg_expr.bind_info->Cast<BodoAggFunctionData>();
-
             std::unique_ptr<bodo::DataType> out_arr_type;
-            if (is_udf) {
-                throw std::runtime_error(
-                    "GPU aggregate expressions don't support UDFs");
-            } else {
-                ftypes.push_back(function_to_ftype.at(agg_expr.function.name));
-                std::tuple<bodo_array_type::arr_type_enum,
-                           Bodo_CTypes::CTypeEnum>
-                    output_dtype = get_groupby_output_dtype(
-                        ftypes.back(),
-                        in_table_schema->column_types[col_idx]->array_type,
-                        in_table_schema->column_types[col_idx]->c_type);
-                std::string timezone =
-                    in_table_schema->column_types[col_idx]->timezone;
-                out_arr_type = std::make_unique<bodo::DataType>(
-                    std::get<0>(output_dtype), std::get<1>(output_dtype), -1,
-                    -1, timezone);
-            }
+
+            uint64_t col_idx = col_ref_map.at(
+                {colref.binding.table_index, colref.binding.column_index});
+            std::cout << "agg expr " << i << " " << col_idx << std::endl;
+
+#if 0
+            size_t reorder_col_idx =
+                std::find(this->input_col_inds.begin(),
+                          this->input_col_inds.end(), col_idx) -
+                this->input_col_inds.begin();
+            f_in_cols.push_back(reorder_col_idx);
+#endif
+
+            auto ftype = function_to_ftype.at(agg_expr.function.name);
+            column_agg_funcs.push_back({col_idx, ftype});
+            std::tuple<bodo_array_type::arr_type_enum, Bodo_CTypes::CTypeEnum>
+                output_dtype = get_groupby_output_dtype(
+                    ftype, in_table_schema->column_types[col_idx]->array_type,
+                    in_table_schema->column_types[col_idx]->c_type);
+            std::string timezone =
+                in_table_schema->column_types[col_idx]->timezone;
+            out_arr_type = std::make_unique<bodo::DataType>(
+                std::get<0>(output_dtype), std::get<1>(output_dtype), -1, -1,
+                timezone);
 
             this->output_schema->append_column(std::move(out_arr_type));
             this->output_schema->column_names.push_back(agg_expr.function.name);
@@ -152,22 +156,26 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
             }
         }
 
-        auto udf_table = alloc_table(this->output_schema->Project(udf_idxs));
-
         // TODO: propagate dropna value when agg columns are pruned out.
         if (!dropna.has_value()) {
             dropna = true;
         }
-        this->groupby_state = std::make_unique<CudaGroupbyState>(
+        this->groupby_state =
+            std::make_unique<CudaGroupbyState>(keys, column_agg_funcs);
+
+        arrow_output_schema = this->output_schema->ToArrowSchema();
+#if 0
             std::make_unique<bodo::Schema>(*in_table_schema_reordered), ftypes,
             std::vector<int32_t>(), f_in_offsets, f_in_cols, this->keys.size(),
             std::vector<bool>(), std::vector<bool>(), cols_to_keep_vec, nullptr,
             get_streaming_batch_size(), true, -1, getOpId(), -1, false,
             std::nullopt,
             /*use_sql_rules*/ false, /* pandas_drop_na_*/ dropna.value());
+#endif
         this->metrics.init_time += end_timer(start_init);
     }
 
+#if 0
     explicit PhysicalGPUAggregate(std::shared_ptr<bodo::Schema> in_table_schema,
                                   duckdb::LogicalDistinct& op) {
         time_pt start_init = start_timer();
@@ -198,6 +206,7 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
             /*use_sql_rules*/ false, /* pandas_drop_na_*/ false);
         this->metrics.init_time += end_timer(start_init);
     }
+#endif
 
     virtual ~PhysicalGPUAggregate() = default;
 
@@ -227,36 +236,26 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
         GPU_DATA input_batch, OperatorResult prev_op_result,
         std::shared_ptr<StreamAndEvent> se) override {
         time_pt start_consume = start_timer();
-        bool request_input = true;
-#if 0
         bool local_is_last = prev_op_result == OperatorResult::FINISHED;
-        std::shared_ptr<table_info> input_batch_reordered =
-            ProjectTable(input_batch, this->input_col_inds);
-        bool global_is_last = groupby_state->build_consume_batch(
-            input_batch_reordered, local_is_last,
-            true, &request_input);
+        groupby_state->build_consume_batch(input_batch.table, local_is_last,
+                                           se->stream);
 
-        se->event.record(se->stream);
-        if (global_is_last) {
-            return OperatorResult::FINISHED;
-        }
-#endif
         this->metrics.consume_time += end_timer(start_consume);
-        return request_input ? OperatorResult::NEED_MORE_INPUT
-                             : OperatorResult::HAVE_MORE_OUTPUT;
+        return local_is_last ? OperatorResult::FINISHED
+                             : OperatorResult::NEED_MORE_INPUT;
     }
 
     std::pair<GPU_DATA, OperatorResult> ProduceBatchGPU(
         std::shared_ptr<StreamAndEvent> se) override {
         time_pt start_produce = start_timer();
         bool out_is_last = false;
-        std::shared_ptr<table_info> next_batch;
-        next_batch = groupby_state->produce_output_batch(&out_is_last, true);
+        std::unique_ptr<cudf::table> next_batch;
+        next_batch = std::move(groupby_state->produce_output_batch(
+            out_is_last /* output */, true));
         this->metrics.produce_time += end_timer(start_produce);
-        next_batch->column_names = this->output_schema->column_names;
-        se->event.record(se->stream);
-        return {next_batch, out_is_last ? OperatorResult::FINISHED
-                                        : OperatorResult::HAVE_MORE_OUTPUT};
+        GPU_DATA out_table_info(std::move(next_batch), arrow_output_schema, se);
+        return {out_table_info, out_is_last ? OperatorResult::FINISHED
+                                            : OperatorResult::HAVE_MORE_OUTPUT};
     }
 
     /**
@@ -292,8 +291,7 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
             col_ref_map,
         const duckdb::vector<duckdb::unique_ptr<duckdb::Expression>>& groups,
         const std::shared_ptr<bodo::Schema>& in_table_schema,
-        std::shared_ptr<bodo::Schema>& in_table_schema_reordered,
-        std::vector<bool>& cols_to_keep_vec) {
+        std::vector<uint64_t>& keys) {
         for (const auto& expr : groups) {
             if (expr->type != duckdb::ExpressionType::BOUND_COLUMN_REF) {
                 throw std::runtime_error(
@@ -301,26 +299,26 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
                     expr->ToString());
             }
             auto& colref = expr->Cast<duckdb::BoundColumnRefExpression>();
-            this->keys.push_back(col_ref_map.at(
+            keys.push_back(col_ref_map.at(
                 {colref.binding.table_index, colref.binding.column_index}));
+            std::cout << "initKeysandSchema " << colref.binding.table_index
+                      << " " << colref.binding.column_index << " "
+                      << col_ref_map.at({colref.binding.table_index,
+                                         colref.binding.column_index})
+                      << std::endl;
         }
 
         uint64_t ncols = in_table_schema->ncols();
-        initInputColumnMapping(this->input_col_inds, this->keys, ncols);
-
-        in_table_schema_reordered =
-            in_table_schema->Project(this->input_col_inds);
-
-        cols_to_keep_vec = std::vector<bool>(ncols, true);
+        initInputColumnMapping(this->input_col_inds, keys, ncols);
 
         // Add keys to output schema
         this->output_schema = std::make_shared<bodo::Schema>();
-        for (size_t i = 0; i < this->keys.size(); i++) {
+        for (size_t i = 0; i < keys.size(); i++) {
             this->output_schema->append_column(
-                in_table_schema_reordered->column_types[i]->copy());
-            if (in_table_schema_reordered->column_names.size() > 0) {
+                in_table_schema->column_types[keys[i]]->copy());
+            if (in_table_schema->column_names.size() > 0) {
                 this->output_schema->column_names.push_back(
-                    in_table_schema_reordered->column_names[i]);
+                    in_table_schema->column_names[keys[i]]);
             } else {
                 this->output_schema->column_names.push_back("key_" +
                                                             std::to_string(i));
@@ -328,11 +326,13 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
         }
         this->output_schema->metadata = std::make_shared<bodo::TableMetadata>(
             std::vector<std::string>({}), std::vector<std::string>({}));
+        std::cout << "initKeyandschema done " << output_schema->ToString()
+                  << std::endl;
     }
 
     std::shared_ptr<CudaGroupbyState> groupby_state;
     std::shared_ptr<bodo::Schema> output_schema;
-    std::vector<uint64_t> keys;
+    std::shared_ptr<arrow::Schema> arrow_output_schema;
     PhysicalGPUAggregateMetrics metrics;
     // Mapping of input table column indices to move keys to the front.
     std::vector<int64_t> input_col_inds;
@@ -340,16 +340,6 @@ class PhysicalGPUAggregate : public PhysicalGPUSource, public PhysicalGPUSink {
     // Map from function name to Bodo_FTypes
     static const std::map<std::string, int32_t> function_to_ftype;
 };
-
-// Definition of the static member
-const std::map<std::string, int32_t> PhysicalGPUAggregate::function_to_ftype = {
-    {"count", Bodo_FTypes::count}, {"max", Bodo_FTypes::max},
-    {"mean", Bodo_FTypes::mean},   {"median", Bodo_FTypes::median},
-    {"min", Bodo_FTypes::min},     {"nunique", Bodo_FTypes::nunique},
-    {"size", Bodo_FTypes::size},   {"skew", Bodo_FTypes::skew},
-    {"std", Bodo_FTypes::std},     {"sum", Bodo_FTypes::sum},
-    {"var", Bodo_FTypes::var},     {"first", Bodo_FTypes::first},
-    {"last", Bodo_FTypes::last}};
 
 /**
  * @brief Physical node for count_star().
@@ -386,7 +376,6 @@ class PhysicalGPUCountStar : public PhysicalGPUSource, public PhysicalGPUSink {
         GPU_DATA input_batch, OperatorResult prev_op_result,
         std::shared_ptr<StreamAndEvent> se) override {
         local_count += input_batch.table->num_rows();
-        se->event.record(se->stream);
         return prev_op_result == OperatorResult::FINISHED
                    ? OperatorResult::FINISHED
                    : OperatorResult::NEED_MORE_INPUT;
@@ -408,10 +397,9 @@ class PhysicalGPUCountStar : public PhysicalGPUSource, public PhysicalGPUSink {
         std::vector<std::unique_ptr<cudf::column>> cols;
         cols.push_back(std::move(col));
         auto scalar_table = std::make_unique<cudf::table>(std::move(cols));
-        se->event.record(se->stream);
         std::pair<GPU_DATA, OperatorResult> ret = std::make_pair(
             GPU_DATA(std::move(scalar_table), out_schema->ToArrowSchema(), se),
-            OperatorResult::Finished);
+            OperatorResult::FINISHED);
         return ret;
     }
 
