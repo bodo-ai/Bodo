e0b55f94cca02cae7921f547bdce6c3f1358925b
Todd A. Anderson <toddanderson@bodo.ai>
2026-02-26 16:59:00 -0800

    Add two more GPU tests.

--- bodo/tests/test_df_lib/test_gpu/test_gpu_end_to_end.py
+++ bodo/tests/test_df_lib/test_gpu/test_gpu_end_to_end.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 
 import pandas as pd
+import pytest
 
 import bodo.pandas as bd
 from bodo.tests.utils import _test_equal
@@ -29,6 +30,75 @@ def test_gpu_join(datapath):
         orders_pd = pd.read_parquet(orders_path)
         out_path_pd = os.path.join(tmp, "out_pd.pq")
         merge_impl(cust_pd, orders_pd, out_path_pd)
+
+        result_bodo = pd.read_parquet(out_path_bodo)
+        result_pd = pd.read_parquet(out_path_pd)
+        _test_equal(result_bodo, result_pd, sort_output=True, reset_index=True)
+
+
+def test_project_filter1(datapath):
+    """Test end-to-end projection and filter workflow on GPU."""
+    df1_path = datapath("dataframe_library/df1.parquet")
+
+    def merge_impl(df1_df, out_path):
+        proj_df = df1_df[["B", "D"]]
+        filt_df = proj_df[proj_df.D > 30]
+        filt_df.to_parquet(out_path)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        # TODO: Check that entire pipeline is being executed on GPU.
+        df1_bodo = bd.read_parquet(df1_path)
+        out_path_bodo = os.path.join(tmp, "out_bodo.pq")
+        merge_impl(df1_bodo, out_path_bodo)
+
+        df1_pd = pd.read_parquet(df1_path)
+        out_path_pd = os.path.join(tmp, "out_pd.pq")
+        merge_impl(df1_pd, out_path_pd)
+
+        result_bodo = pd.read_parquet(out_path_bodo)
+        result_pd = pd.read_parquet(out_path_pd)
+        _test_equal(result_bodo, result_pd, sort_output=True, reset_index=True)
+
+
+@pytest.mark.parametrize(
+    "func",
+    [
+        "sum",
+        "mean",
+        "count",
+        "max",
+        "min",
+        "nunique",
+        "size",
+        "var",
+        "std",
+        "skew",
+        "distinct",
+    ],
+)
+def test_groupby_agg(datapath, func):
+    """Test end-to-end projection and filter workflow on GPU."""
+    df1_path = datapath("dataframe_library/df1.parquet")
+
+    def merge_impl(df1_df, out_path):
+        if func == "distinct":
+            df1_agg = df1_df.drop_duplicates()
+        else:
+            df1_agg = getattr(
+                df1_df.groupby("D", as_index=False, sort=False)["A"], func
+            )()
+
+        df1_agg.to_parquet(out_path)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        # TODO: Check that entire pipeline is being executed on GPU.
+        df1_bodo = bd.read_parquet(df1_path)
+        out_path_bodo = os.path.join(tmp, "out_bodo.pq")
+        merge_impl(df1_bodo, out_path_bodo)
+
+        df1_pd = pd.read_parquet(df1_path)
+        out_path_pd = os.path.join(tmp, "out_pd.pq")
+        merge_impl(df1_pd, out_path_pd)
 
         result_bodo = pd.read_parquet(out_path_bodo)
         result_pd = pd.read_parquet(out_path_pd)
