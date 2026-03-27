8a71ad3c5b7186804cc607331cf7bf938efaa89a
Scott Routledge <scott@bodo.ai>
2026-03-27 19:42:44 +0000

    add ping pong test to confirm zero copy

---
+++ bodo/tests/test_streaming/test_ping_pong.cpp
@@ -0,0 +1,72 @@
+#include "../libs/gpu_utils.h"
+#include "../test.hpp"
+#include "cuda_runtime_api.h"
+
+#include <cudf/column/column_factories.hpp>
+#include <cudf/filling.hpp>
+#include <cudf/scalar/scalar.hpp>
+#include <cudf/table/table.hpp>
+#include <cudf/types.hpp>
+#include <iostream>
+#include <numeric>
+#include <rmm/cuda_device.hpp>
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/device_buffer.hpp>
+#include <vector>
+
+#define N (64)  // ~64MB
+
+static bodo::tests::suite tests([] {
+    bodo::tests::test("test_mpi_cuda_ping_pong", [] {
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        rmm::cuda_device_id device_id = get_gpu_id();
+        if (device_id.value() >= 0) {
+            cudaSetDevice(device_id.value());
+        }
+        cudaFree(nullptr);
+
+        // Host buffer
+        std::vector<int> h_buf(N);
+
+        // Device buffer
+        int* d_buf;
+        cudaMalloc(&d_buf, N * sizeof(int));
+
+        if (rank == 0) {
+            // Fill on CPU
+            for (int i = 0; i < N; i++) {
+                h_buf[i] = i % 100;
+            }
+
+            // Copy to GPU
+            cudaMemcpy(d_buf, h_buf.data(), N * sizeof(int),
+                       cudaMemcpyHostToDevice);
+            cudaDeviceSynchronize();
+
+            std::cout << "Rank 0 sending GPU buffer..." << std::endl;
+
+            MPI_Send(d_buf, N, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        }
+
+        if (rank == 1) {
+            std::cout << "Rank 1 receiving GPU buffer..." << std::endl;
+
+            cudaDeviceSynchronize();
+            MPI_Recv(d_buf, N, MPI_INT, 0, 0, MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+
+            // Copy back to host
+            cudaMemcpy(h_buf.data(), d_buf, N * sizeof(int),
+                       cudaMemcpyDeviceToHost);
+
+            // CPU checksum
+            long long sum = std::accumulate(h_buf.begin(), h_buf.end(), 0LL);
+
+            std::cout << "Checksum: " << sum << std::endl;
+        }
+
+        cudaFree(d_buf);
+    });
+});
