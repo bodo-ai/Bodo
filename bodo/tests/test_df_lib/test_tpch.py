f350ddef083bb861ad09c9e6ac17cf972d8db06b
Ehsan Totoni <ehsan.tn@gmail.com>
2026-03-27 13:59:28 -0400

    [GPU] enable TPC-H Q3/Q4 tests on GPU (#1093)

--- bodo/tests/test_df_lib/test_tpch.py
+++ bodo/tests/test_df_lib/test_tpch.py
@@ -99,10 +99,12 @@ def test_tpch_q02():
     run_tpch_query_test(tpch.tpch_q02, ctes_created=1)
 
 
+@pytest.mark.gpu(allow_fallback=True)
 def test_tpch_q03():
     run_tpch_query_test(tpch.tpch_q03)
 
 
+@pytest.mark.gpu(allow_fallback=True)
 def test_tpch_q04():
     run_tpch_query_test(tpch.tpch_q04)
 
