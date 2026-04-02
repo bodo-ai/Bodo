bd7857b6c1130693694118170e4914c00934f4bd
Scott Routledge <scott@bodo.ai>
2026-04-02 10:13:12 -0400

    platform scripts

--- buildscripts/build-bodo-from-source-platform/build_cuda_mpich.sh
+++ buildscripts/build-bodo-from-source-platform/build_cuda_mpich.sh
@@ -7,4 +7,5 @@ tar -xzf mpich-5.0.0.tar.gz
 cd mpich-5.0.0
 ./configure --prefix=/opt/mpich --disable-doc --disable-dependency-tracking --disable-cxx --disable-fortran --disable-f08 --enable-mpi-abi --with-wrapper-dl-type=none --disable-static --with-hwloc=${CONDA_PREFIX} --with-yaksa=embedded --with-pm=hydra:gforker --with-device=ch4:ucx --with-ucx=${CONDA_PREFIX} --with-cuda=${CONDA_PREFIX}
 make -j20
+sudo chmod 777 /opt
 make install
