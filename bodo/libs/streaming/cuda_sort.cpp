90c217ad9aaa5ae4def1635d4c52ef3d16627b7f
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2026-04-03 14:00:06 -0500

    Push back pivots even if = n_samples

--- bodo/libs/streaming/cuda_sort.cpp
+++ bodo/libs/streaming/cuda_sort.cpp
@@ -182,15 +182,11 @@ void CudaSortState::ExecutePsrs(rmm::cuda_stream_view stream) {
             samples_gpu.table->view(), column_order, null_precedence, stream);
 
         // Select P-1 pivots
-        cudf::size_type n_total_samples = sorted_samples->num_rows();
         std::vector<cudf::size_type> pivot_indices;
 
         for (int i = 1; i < n_ranks; ++i) {
             cudf::size_type idx = (i * n_ranks) + (n_ranks / 2) - 1;
-
-            if (idx < n_total_samples) {
-                pivot_indices.push_back(idx);
-            }
+            pivot_indices.push_back(idx);
         }
 
         if (!pivot_indices.empty()) {
