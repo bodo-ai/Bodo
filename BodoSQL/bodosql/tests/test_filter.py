bbf33b6d14a9ed9727f3249c6347e72654da731f
Ehsan Totoni <ehsan.tn@gmail.com>
2026-05-12 11:32:10 -0400

    add markers

--- BodoSQL/bodosql/tests/test_filter.py
+++ BodoSQL/bodosql/tests/test_filter.py
@@ -94,6 +94,8 @@ def test_filter_null_binary(bodosql_binary_types, memory_leak_check):
     )
 
 
+@pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_null_datetime(bodosql_datetime_types, memory_leak_check):
     """test is null on Timestamp columns"""
     query1 = "select A,C from table1 where B is NULL"
@@ -106,6 +108,8 @@ def test_filter_null_datetime(bodosql_datetime_types, memory_leak_check):
     )
 
 
+@pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_null_interval(bodosql_interval_types, memory_leak_check):
     """test is null on Interval columns"""
     query1 = "select A,C from table1 where B is NULL"
@@ -127,6 +131,8 @@ def test_filter_null_interval(bodosql_interval_types, memory_leak_check):
     )
 
 
+@pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_1(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,B from table1 where B = FALSE",
@@ -138,6 +144,7 @@ def test_filter_boolean_1(bodosql_boolean_types, memory_leak_check):
 
 
 @pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_2(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,C from table1 where NOT B = TRUE",
@@ -149,6 +156,7 @@ def test_filter_boolean_2(bodosql_boolean_types, memory_leak_check):
 
 
 @pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_3(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,C from table1 where A",
@@ -160,6 +168,7 @@ def test_filter_boolean_3(bodosql_boolean_types, memory_leak_check):
 
 
 @pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_4(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,C from table1 where A = C",
@@ -171,6 +180,7 @@ def test_filter_boolean_4(bodosql_boolean_types, memory_leak_check):
 
 
 @pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_5(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select B,A from table1 where A <> C",
@@ -182,6 +192,7 @@ def test_filter_boolean_5(bodosql_boolean_types, memory_leak_check):
 
 
 @pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_6(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,C from table1 where NOT A and B = TRUE or C <> FALSE",
@@ -192,6 +203,8 @@ def test_filter_boolean_6(bodosql_boolean_types, memory_leak_check):
     )
 
 
+@pytest.mark.slow
+@pytest.mark.bodosql_cpp
 def test_filter_boolean_7(bodosql_boolean_types, memory_leak_check):
     check_query(
         "select A,C from table1 where A or B = FALSE and Not FALSE",
