1f2e1a2d64a0f6a827ce6ff24da9643c328596ec
Todd A. Anderson <toddanderson@bodo.ai>
2026-05-14 15:49:07 -0700

    Mark one more test as working with c++ backend.

--- BodoSQL/bodosql/tests/test_agg_groupby.py
+++ BodoSQL/bodosql/tests/test_agg_groupby.py
@@ -1112,7 +1112,7 @@ def test_boolor_booland_boolxor_agg(query, res, memory_leak_check):
 
 
 @pytest.mark.slow
-# @pytest.mark.bodosql_cpp   # aggregation OTHER not supported yet
+@pytest.mark.bodosql_cpp
 def test_booland_agg_having(memory_leak_check):
     """Test having with booland_agg aggregation in the condition"""
     query = (
