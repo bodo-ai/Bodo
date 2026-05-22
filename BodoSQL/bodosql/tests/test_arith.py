2858a0a56e67094134c85452d2165d7aacd2a23c
Ehsan Totoni <ehsan.tn@gmail.com>
2026-05-22 10:51:46 -0400

    Use DuckDB instead of Spark

--- BodoSQL/bodosql/tests/test_arith.py
+++ BodoSQL/bodosql/tests/test_arith.py
@@ -16,272 +16,326 @@ pytestmark = pytest_slow_unless_codegen
 
 
 @pytest.mark.slow
-def test_basic_arithmetic_select(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_basic_arithmetic_select(bodosql_numeric_types, memory_leak_check):
     """test select calls with an arithmetic expression"""
     check_query(
         "select A + 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 + A from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select A - 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 - A from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_column_arithmetic_select(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_column_arithmetic_select(bodosql_numeric_types, memory_leak_check):
     """test select calls with an arithmetic expression"""
     check_query(
         "select A + C from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select C - A from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select A * (1 - C) from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
 
 
-def test_sum_arith_select(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_sum_arith_select(bodosql_numeric_types, memory_leak_check):
     """test sum on a call with an arithmetic expression"""
     check_query(
         "select sum(A + 1) from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
         is_out_distributed=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_nested_arithmetic_select(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_nested_arithmetic_select(bodosql_numeric_types, memory_leak_check):
     """test select calls with an arithmetic expression"""
     check_query(
         "select A + 1 + 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 + A + 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 + 1 + A from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select A - 1 - 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 - A - 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 - 1 - A from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_multicolumn_arithmetic_select(
-    bodosql_numeric_types, spark_info, memory_leak_check
-):
+def test_multicolumn_arithmetic_select(bodosql_numeric_types, memory_leak_check):
     """test select calls with an arithmetic expression"""
     check_query(
         "select A + 1, B from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
     check_query(
         "select 1 + A, A - 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_select_col_arith(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_select_col_arith(bodosql_numeric_types, memory_leak_check):
     """test selecting a single column and performing arithmetic
     on that columns
     """
     check_query(
         "select A, A + 1 from table1",
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
+        use_duckdb=True,
     )
 
 
-def test_addition_constants(basic_df, spark_info, memory_leak_check):
+def test_addition_constants(basic_df, memory_leak_check):
     """
     Tests that addition works on integer constants
     """
     query = "Select A from table1 where B = .8 + 1 + 1 + -2 + 4 + .2"
-    check_query(query, basic_df, spark_info, check_dtype=False)
+    check_query(query, basic_df, None, check_dtype=False, use_duckdb=True)
 
 
-def test_addition_columns(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_addition_columns(bodosql_numeric_types, memory_leak_check):
     """
     Tests that addition works on columns
     """
     query = "Select A + B from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
-def test_subtraction_constants(basic_df, spark_info, memory_leak_check):
+def test_subtraction_constants(basic_df, memory_leak_check):
     """
     Tests that subtraction works on constants
     """
     query = "Select A from table1 where B = 10 - .001 - -2 - 1 - .2 - .7 - .3 -.8 - 2"
-    check_query(query, basic_df, spark_info, check_dtype=False)
+    check_query(query, basic_df, None, check_dtype=False, use_duckdb=True)
 
 
-def test_subtraction_columns(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_subtraction_columns(bodosql_numeric_types, memory_leak_check):
     """
     Tests that subtraction works on columns
     """
     query = "Select B - A from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
-def test_multiplication_constants(basic_df, spark_info, memory_leak_check):
+def test_multiplication_constants(basic_df, memory_leak_check):
     """
     Tests that multiplication works on constants
     """
     query = "Select A from table1 where B = -1.0 * -1.0 * 2 * 3"
-    check_query(query, basic_df, spark_info, check_dtype=False, check_names=False)
+    check_query(
+        query, basic_df, None, check_dtype=False, check_names=False, use_duckdb=True
+    )
 
 
-def test_multiply_columns(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_multiply_columns(bodosql_numeric_types, memory_leak_check):
     """
     Tests that multiplication works on columns
     """
     query = "Select A * B from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
-def test_division_constants(basic_df, spark_info, memory_leak_check):
+def test_division_constants(basic_df, memory_leak_check):
     """
     Tests that division works on constants
     """
     query = "Select A from table1 where A = 24 / 2 / 4 / -1.0 / -1.0"
-    check_query(query, basic_df, spark_info, check_dtype=False, check_names=False)
+    check_query(
+        query, basic_df, None, check_dtype=False, check_names=False, use_duckdb=True
+    )
 
 
-def test_division_columns(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_division_columns(bodosql_numeric_types, memory_leak_check):
     """
     Tests that division works on columns
     """
     query = "Select B / A from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_op_order(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_op_order(bodosql_numeric_types, memory_leak_check):
     """
     Tests that order of operations is correct
     """
     query = "Select A from table1 where B = 5 + 10 / 2 - 3 / 3 - 5"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_op_order_cols(bodosql_numeric_types, spark_info, memory_leak_check):
+def test_op_order_cols(bodosql_numeric_types, memory_leak_check):
     """
     Tests that order of operations is correct on columns
     """
     query = "Select A + C / B - A from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
 @pytest.mark.slow
-def test_arith_ops_between_tables(basic_df, spark_info, memory_leak_check):
+def test_arith_ops_between_tables(basic_df, memory_leak_check):
     """
     Tests that arith operations between tables work as intended
     """
     query = "Select table1.A + table2.C / table1.B - table2.A from table1, table2"
     newCtx = {"TABLE1": basic_df["TABLE1"], "TABLE2": basic_df["TABLE1"]}
-    check_query(query, newCtx, spark_info, check_dtype=False, check_names=False)
+    check_query(
+        query, newCtx, None, check_dtype=False, check_names=False, use_duckdb=True
+    )
 
 
 @pytest.mark.skip("BS[112]")
-def test_wraparound_unsigned_numerics(
-    bodosql_numeric_types, spark_info, memory_leak_check
-):
+def test_wraparound_unsigned_numerics(bodosql_numeric_types, memory_leak_check):
     """
     Tests that wraparound occurs when dealing with unsigned integer types
     """
     query = "Select A - B from table1"
     check_query(
-        query, bodosql_numeric_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_numeric_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
@@ -315,9 +369,7 @@ def test_wraparound_unsigned_numerics(
         ),
     ],
 )
-def test_add_sub_intervals(
-    bodosql_datetime_types, spark_info, memory_leak_check, interval
-):
+def test_add_sub_intervals(bodosql_datetime_types, memory_leak_check, interval):
     """
     Tests support for + and - with various intervals.
     """
@@ -325,19 +377,39 @@ def test_add_sub_intervals(
     negative_interval = interval[0] + "-" + interval[1:]
     query = f"Select A + interval {interval} from table1"
     check_query(
-        query, bodosql_datetime_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_datetime_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
     query = f"Select A + interval {negative_interval} from table1"
     check_query(
-        query, bodosql_datetime_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_datetime_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
     query = f"Select A - interval {interval} from table1"
     check_query(
-        query, bodosql_datetime_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_datetime_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
     query = f"Select A - interval {negative_interval} from table1"
     check_query(
-        query, bodosql_datetime_types, spark_info, check_dtype=False, check_names=False
+        query,
+        bodosql_datetime_types,
+        None,
+        check_dtype=False,
+        check_names=False,
+        use_duckdb=True,
     )
 
 
@@ -351,7 +423,7 @@ def test_add_sub_intervals(
         ),
     ],
 )
-def test_negation(query, bodosql_numeric_types, spark_info, memory_leak_check):
+def test_negation(query, bodosql_numeric_types, memory_leak_check):
     """Tests unary negation"""
     # Exact value depends on bitwidth so we have to pass the Spark schema
     pyspark_schemas = {}
@@ -361,10 +433,11 @@ def test_negation(query, bodosql_numeric_types, spark_info, memory_leak_check):
     check_query(
         query,
         bodosql_numeric_types,
-        spark_info,
+        None,
         check_dtype=False,
         check_names=False,
         pyspark_schemas=pyspark_schemas,
+        use_duckdb=True,
     )
 
 
