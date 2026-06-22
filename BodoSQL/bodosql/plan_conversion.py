2c5a21d02e0830c570d3355f08a1d9c721b4b544
Ehsan Totoni <ehsan.tn@gmail.com>
2026-06-22 12:11:54 -0400

    Support RADIANS

--- BodoSQL/bodosql/plan_conversion.py
+++ BodoSQL/bodosql/plan_conversion.py
@@ -1179,6 +1179,24 @@ def java_call_to_python_call(ctx, java_call, input_plan):
                 (),
             )
 
+        if func_name == "RADIANS" and len(op_exprs) == 1:
+            src = op_exprs[0]
+            # Return float32 for float32 input and float64 for float64 and decimal input
+            src_dtype = src.empty_data.dtypes.iloc[0]
+            out_dtype = pd.ArrowDtype(
+                pa.float32()
+                if src_dtype.pyarrow_dtype == pa.float32()
+                else pa.float64()
+            )
+            dummy_empty_data = pd.Series(dtype=out_dtype)
+            ceof_expr = ConstantExpression(dummy_empty_data, input_plan, np.pi / 180.0)
+            return ArithOpExpression(
+                dummy_empty_data,
+                src,
+                ceof_expr,
+                "__mul__",
+            )
+
         # If we didn't match a supported basic function, fall through to NotImplemented
         raise NotImplementedError(
             f"SqlBasicFunction {func_name} not supported yet: " + java_call.toString()
