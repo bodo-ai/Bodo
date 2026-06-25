6e6bc2ceb72ade901dcbffb288ec6fd70a9b824d
Todd A. Anderson <toddanderson@bodo.ai>
2026-06-25 16:40:20 -0700

    script fixes

--- benchmarks/tpch/bodo_sql/run_isolate.bash
+++ benchmarks/tpch/bodo_sql/run_isolate.bash
@@ -2,5 +2,5 @@
 
 for q in {1..22}; do
     echo Running $q
-    mpiexec -n 1 python bodosql-queries.py --folder s3://tpch-data-parquet/SF1000 --scale_factor 1000
+    mpiexec -n 1 python bodosql_queries.py --folder s3://tpch-data-parquet/SF1000 --queries $q --scale_factor 1000
 done
