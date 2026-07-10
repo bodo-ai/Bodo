fb7df10da47b60af23f845a0406b3ca107c9f17d
Todd A. Anderson <toddanderson@bodo.ai>
2026-07-10 15:40:33 -0700

    Use connection_string for location.

--- BodoSQL/bodosql/context.py
+++ BodoSQL/bodosql/context.py
@@ -732,7 +732,7 @@ class BodoSQLContext:
                 out = bodo.pandas.plan.execute_plan(
                     plan.internal_plan, optimize=False, use_sql_rules=True
                 )
-                location = os.path.join(*plan.table_create_node.getPath())
+                location = self.catalog.connection_string
                 out.to_iceberg(plan.table_create_node.getTableName(), location=location)
                 out = None
             else:
