50ff9219d4a14a287e5520accf0c06684248aa17
Daniel Anderson <69274205+dmsa-or@users.noreply.github.com>
2026-07-30 09:51:26 -0700

    Support SUBSTRING_INDEX and SPLIT_PART in BodoSQL C++ backend (#1329)
    Co-authored-by: Daniel Anderson <daniel@bodo.ai>
    Co-authored-by: scott-routledge2 <scott@bodo.ai>

--- BodoSQL/bodosql/plan_conversion.py
+++ BodoSQL/bodosql/plan_conversion.py
@@ -4501,6 +4501,50 @@ def java_call_to_python_call(ctx, java_call, input_plan):
                 new_index,
             )
 
+        elif func_name == "SUBSTRING_INDEX" and len(op_exprs) == 3:
+            src = op_exprs[0]
+            delim_expr = op_exprs[1]
+            count_expr = op_exprs[2]
+
+            ensure_type_of_expr(src, "src", (str, pa.binary()))
+            ensure_arg_is_const_expr_of_type(
+                delim_expr, "delim_expr", (str, pa.binary())
+            )
+            ensure_arg_is_const_expr_of_type(count_expr, "count_expr", int)
+
+            # Implementation involves fixed_size_list results, so easier
+            # to do it on the C++ side.
+            return ArrowScalarFuncExpression(
+                src.empty_data,
+                [src],
+                "substring_index",
+                (delim_expr.value, count_expr.value),
+            )
+
+        elif func_name == "SPLIT_PART" and len(op_exprs) == 3:
+            src = op_exprs[0]
+            delim_expr = op_exprs[1]
+            part_num_expr = op_exprs[2]
+
+            ensure_type_of_expr(src, "src", (str, pa.binary()))
+            ensure_arg_is_const_expr_of_type(
+                delim_expr, "delim_expr", (str, pa.binary())
+            )
+            ensure_arg_is_const_expr_of_type(part_num_expr, "part_num_expr", int)
+
+            # Snowflake treats a part number of 0 the same as 1.
+            # part_num is 1-based.
+            part_num = part_num_expr.value if part_num_expr.value != 0 else 1
+
+            # Implementation involves fixed_size_list results, so easier
+            # to do it on the C++ side.
+            return ArrowScalarFuncExpression(
+                src.empty_data,
+                [src],
+                "split_part",
+                (delim_expr.value, part_num),
+            )
+
         elif func_name == "REGEXP_REPLACE" and len(op_exprs) in (2, 3, 4, 5, 6):
             src = op_exprs[0]
             regexp = op_exprs[1]
