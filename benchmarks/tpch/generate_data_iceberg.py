25398b18b67b3c1df93defb0206b1a4a11424309
Scott Routledge <scott@bodo.ai>
2026-08-28 18:49:11 -0400

    [run ci]

--- benchmarks/tpch/generate_data_iceberg.py
+++ benchmarks/tpch/generate_data_iceberg.py
@@ -144,7 +144,7 @@ def create_iceberg_tables(parquet_path: str, iceberg_path: str, sf: int):
                 pa.field(
                     field.name,
                     field.type,
-                    nullable=True,
+                    nullable=False,
                     metadata=field.metadata,
                 )
                 for field in schema
