50e681511bd691d72cba5b1a4cbf44e9e38272e1
Scott Routledge <scott@bodo.ai>
2026-09-11 15:01:21 -0400

    fix sarg equivalency semantics [run ci]

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/rex/RexSimplify.java
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/org/apache/calcite/rex/RexSimplify.java
@@ -1716,8 +1716,8 @@ public class RexSimplify {
         }
 
         // Assuming UNKNOWN and FALSE are equivalent in the outer expression.
-        if (sargA.nullAs == TRUE
-                || sargB.nullAs == TRUE) {
+        if ((sargA.nullAs == TRUE || sargB.nullAs == TRUE)
+                && sargA != sargB) {
             return false;
         }
 
