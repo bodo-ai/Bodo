2b07005a300b4de8bc4703dc5150aeb2da46d134
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2026-09-17 15:10:30 -0500

    Review comments [run ci]

--- BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/core/JoinBase.kt
+++ BodoSQL/calcite_sql/bodosql-calcite-application/src/main/java/com/bodosql/calcite/rel/core/JoinBase.kt
@@ -25,7 +25,16 @@ abstract class JoinBase(
     right: RelNode,
     condition: RexNode,
     joinType: JoinRelType,
-) : Join(cluster, traitSet, hints, left, right, RexNormalizer.normalize(cluster.rexBuilder, condition), ImmutableSet.of(), joinType) {
+) : Join(
+        cluster,
+        traitSet,
+        hints,
+        left,
+        right,
+        RexNormalizer.normalizeCondition(cluster.rexBuilder, condition),
+        ImmutableSet.of(),
+        joinType,
+    ) {
     override fun computeSelfCost(
         planner: RelOptPlanner,
         mq: RelMetadataQuery,
