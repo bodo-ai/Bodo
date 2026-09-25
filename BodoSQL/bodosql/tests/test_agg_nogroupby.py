a12872fe6b5623f141239c1b4bbb663372351d3c
Todd A. Anderson <toddanderson@bodo.ai>
2026-09-25 15:09:17 -0700

    Improve debugging on test failure.

--- BodoSQL/bodosql/tests/test_agg_nogroupby.py
+++ BodoSQL/bodosql/tests/test_agg_nogroupby.py
@@ -1313,7 +1313,7 @@ def test_sum0_init_value(tpch_data):
     bc = bodosql.BodoSQLContext(tpch_data)
     plan_str = bc.generate_plan(query)
     assert "BodoPhysicalAggregate(group=[{}], CNT=[$SUM0($0)])" in plan_str, (
-        "Expected SUM0 in the plan."
+        f"Expected SUM0 in the plan. {plan_str}"
     )
 
     check_query(
