0f83287879d6c6dccbcf30a662bb7d29c83dd25d
Scott Routledge <scott@bodo.ai>
2026-09-25 10:40:53 -0500

    analyze plans and profile

---
+++ benchmarks/tpch/bodo_sql/run_bodo_analysis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+folder=$1
+scale_factor=$2
+
+# clear plans
+rm -rf plans_sf=$scale_factor
+rm -rf profiles
+
+mkdir -p profiles
+
+for query in {1..2}; do
+    echo "Running query $query"
+    BODO_TRACING_LEVEL=1 \
+    BODO_NUM_WORKERS=2 \
+    BODO_TRACING_OUTPUT_DIR="profiles/profile_q$query" \
+    mpiexec -n 1 python bodosql_queries.py \
+    --folder $folder \
+    --scale_factor $scale_factor \
+    --queries $query \
+    --log_timings profile_timings.csv \
+    --explain_first
+
+    # Aggregate each profile generated for this query.
+    for run_dir in profiles/profile_q"$query"/run_*; do
+        echo "Aggregating profile: $run_dir"
+        python -m bodo.utils.aggregate_query_profiles "$run_dir" > /dev/null
+    done
+done
