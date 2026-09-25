0e02d91bdfa152556638d1834168cd5f71c3cb8a
Scott Routledge <scott@bodo.ai>
2026-09-25 11:20:51 -0500

    use bodofs for profile dir

--- benchmarks/tpch/bodo_sql/run_bodo_analysis.sh
+++ benchmarks/tpch/bodo_sql/run_bodo_analysis.sh
@@ -5,14 +5,12 @@ scale_factor=$2
 
 # clear plans
 rm -rf plans_sf$scale_factor
-rm -rf profiles
-
-mkdir -p profiles
+profile_dir=/bodofs/Users/scott/profiles
 
 for query in {1..2}; do
     echo "Running query $query"
     BODO_TRACING_LEVEL=1 \
-    BODO_TRACING_OUTPUT_DIR="profiles/profile_q$query" \
+    BODO_TRACING_OUTPUT_DIR="$profile_dir/profile_q$query" \
     mpiexec -n 1 python bodosql_queries.py \
     --folder $folder \
     --scale_factor $scale_factor \
@@ -21,8 +19,8 @@ for query in {1..2}; do
     --explain_first
 
     # Aggregate each profile generated for this query.
-    for run_dir in profiles/profile_q"$query"/run_*; do
+    for run_dir in "$profile_dir"/profile_q"$query"/run_*; do
         echo "Aggregating profile: $run_dir"
-        python -m bodo.utils.aggregate_query_profiles "$run_dir" > /dev/null
+        mpiexec -n 1 python -m bodo.utils.aggregate_query_profiles "$run_dir" > /dev/null
     done
 done
